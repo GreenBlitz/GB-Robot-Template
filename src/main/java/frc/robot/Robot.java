c01d7bfe2c66ee725b30b7b3b7efbf85b8bdb0dc
YHerm <0549405422.yh@gmail.com>
2024-03-22 22:14:07 +0200

    yoav - impirt

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.constants.RobotConstants;
 import frc.utils.LoggerUtils;
 import frc.utils.RobotTypeUtils;
 import org.littletonrobotics.junction.LoggedRobot;

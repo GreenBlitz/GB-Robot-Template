8ff0ebd85ddc5253967f1912996ade18c3f72a5c
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-04-01 13:17:46 +0300

    yoav - workred

--- src/main/java/frc/robot/JoysticksBindings.java
+++ src/main/java/frc/robot/JoysticksBindings.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import com.pathplanner.lib.path.PathConstraints;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.constants.Ports;
@@ -9,63 +10,59 @@ import frc.utils.joysticks.SmartJoystick;
 
 public class JoysticksBindings {
 
-    private static final SmartJoystick MAIN_JOYSTICK = new SmartJoystick(Ports.JoystickDriverStationPorts.MAIN);
-    private static final SmartJoystick SECOND_JOYSTICK = new SmartJoystick(Ports.JoystickDriverStationPorts.SECOND);
-    private static final SmartJoystick THIRD_JOYSTICK = new SmartJoystick(Ports.JoystickDriverStationPorts.THIRD);
-    private static final SmartJoystick FOURTH_JOYSTICK = new SmartJoystick(Ports.JoystickDriverStationPorts.FOURTH);
+	private static final SmartJoystick MAIN_JOYSTICK = new SmartJoystick(Ports.JoystickDriverStationPorts.MAIN);
+	private static final SmartJoystick SECOND_JOYSTICK = new SmartJoystick(Ports.JoystickDriverStationPorts.SECOND);
+	private static final SmartJoystick THIRD_JOYSTICK = new SmartJoystick(Ports.JoystickDriverStationPorts.THIRD);
+	private static final SmartJoystick FOURTH_JOYSTICK = new SmartJoystick(Ports.JoystickDriverStationPorts.FOURTH);
 
-    public static void configureBindings() {
-        mainJoystickButtons();
-        secondJoystickButtons();
-        thirdJoystickButtons();
-        fourthJoystickButtons();
-    }
+	public static void configureBindings() {
+		mainJoystickButtons();
+		secondJoystickButtons();
+		thirdJoystickButtons();
+		fourthJoystickButtons();
+	}
 
-    private static void mainJoystickButtons() {
-        SmartJoystick usedJoystick = MAIN_JOYSTICK;
-        //bindings
+	private static void mainJoystickButtons() {
+		SmartJoystick usedJoystick = MAIN_JOYSTICK;
+		//bindings
 
-        //TODO - return to last pose estimator
-        usedJoystick.Y.onTrue(new InstantCommand(() -> RobotContainer.POSE_ESTIMATOR.setHeading(new Rotation2d())));
-        usedJoystick.Y.onTrue(new InstantCommand(() ->
-                RobotContainer.POSE_ESTIMATOR.resetPose(
-                        AlliancePose2d.fromBlueAlliancePose(5,5, new Rotation2d())
-                )));
+		//TODO - return to last pose estimator
+		usedJoystick.Y.onTrue(new InstantCommand(() -> RobotContainer.POSE_ESTIMATOR.setHeading(new Rotation2d())));
+		usedJoystick.Y.onTrue(new InstantCommand(() ->
+				RobotContainer.POSE_ESTIMATOR.resetPose(
+						AlliancePose2d.fromBlueAlliancePose(5, 5, new Rotation2d())
+				)));
 
-        usedJoystick.A.onTrue(new InstantCommand(RobotContainer.SWERVE::resetByEncoder));
+		usedJoystick.A.onTrue(new InstantCommand(RobotContainer.SWERVE::resetByEncoder));
 
-        usedJoystick.POV_DOWN.whileTrue(SwerveCommands.getRotateToAngleCommand(
-                Rotation2d.fromDegrees(0)));
-        usedJoystick.POV_LEFT.whileTrue(SwerveCommands.getRotateToAngleCommand(
-                Rotation2d.fromDegrees(90)));
-        usedJoystick.POV_RIGHT.whileTrue(SwerveCommands.getRotateToAngleCommand(
-                Rotation2d.fromDegrees(180)));
-        usedJoystick.POV_UP.whileTrue(SwerveCommands.getRotateToAngleCommand(
-                Rotation2d.fromDegrees(270)));
+		usedJoystick.POV_DOWN.whileTrue(SwerveCommands.getDriveToPoseCommand(
+				() -> AlliancePose2d.fromBlueAlliancePose(4, 4, new Rotation2d()),
+				new PathConstraints(3.3, 3.3, 4, 4)
+		));
 
-        RobotContainer.SWERVE.setDefaultCommand(
-                SwerveCommands.getOpenLoopFieldRelativeDriveCommand(
-                        () -> usedJoystick.getAxisValue(SmartJoystick.Axis.LEFT_Y),
-                        () -> usedJoystick.getAxisValue(SmartJoystick.Axis.LEFT_X),
-                        () -> usedJoystick.getAxisValue(SmartJoystick.Axis.RIGHT_X)
-                )
-        );
-    }
+		RobotContainer.SWERVE.setDefaultCommand(
+				SwerveCommands.getOpenLoopFieldRelativeDriveCommand(
+						() -> usedJoystick.getAxisValue(SmartJoystick.Axis.LEFT_Y),
+						() -> usedJoystick.getAxisValue(SmartJoystick.Axis.LEFT_X),
+						() -> usedJoystick.getAxisValue(SmartJoystick.Axis.RIGHT_X)
+				)
+		);
+	}
 
-    private static void secondJoystickButtons() {
-        SmartJoystick usedJoystick = SECOND_JOYSTICK;
-        //bindings
-    }
+	private static void secondJoystickButtons() {
+		SmartJoystick usedJoystick = SECOND_JOYSTICK;
+		//bindings
+	}
 
-    private static void thirdJoystickButtons() {
-        SmartJoystick usedJoystick = THIRD_JOYSTICK;
-        //bindings
-    }
+	private static void thirdJoystickButtons() {
+		SmartJoystick usedJoystick = THIRD_JOYSTICK;
+		//bindings
+	}
 
-    private static void fourthJoystickButtons() {
-        SmartJoystick usedJoystick = FOURTH_JOYSTICK;
-        //bindings
-    }
+	private static void fourthJoystickButtons() {
+		SmartJoystick usedJoystick = FOURTH_JOYSTICK;
+		//bindings
+	}
 
 
 }
--- src/main/java/frc/robot/subsystems/swerve/SwerveCommands.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveCommands.java
@@ -1,12 +1,22 @@
 package frc.robot.subsystems.swerve;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.*;
+import frc.robot.RobotContainer;
+import frc.utils.allianceutils.AlliancePose2d;
 import frc.utils.commands.InitExecuteCommand;
+import org.littletonrobotics.junction.Logger;
 
+import java.util.List;
+import java.util.Set;
 import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
 
 import static frc.robot.RobotContainer.SWERVE;
 
@@ -32,5 +42,49 @@ public class SwerveCommands {
         return new InstantCommand(SWERVE::resetRotationController)
                 .andThen(new RunCommand(() -> SWERVE.rotateToAngle(targetAngle)))
                         .until(() -> SWERVE.isAtAngle(targetAngle));
+    }
+
+    private static Command getPIDToPoseCommand(AlliancePose2d targetPose) {
+        return new InstantCommand(SWERVE::resetRotationController)
+                .andThen(new RunCommand(() -> SWERVE.pidToPose(targetPose.toMirroredAlliancePose()))
+                        .until(() -> SWERVE.isAtPosition(targetPose.toMirroredAlliancePose())));
+    }
+
+    private static Command getPathfindToPoseCommand(AlliancePose2d targetPose, PathConstraints pathConstraints) {
+        final Pose2d targetMirroredAlliancePose = targetPose.toMirroredAlliancePose();
+        final Pose2d currentBluePose = RobotContainer.POSE_ESTIMATOR.getCurrentPose().toBlueAlliancePose();
+        if (currentBluePose.getTranslation().getDistance(targetMirroredAlliancePose.getTranslation()) < 0.35)
+            return createOnTheFlyPathCommand(targetMirroredAlliancePose, pathConstraints);
+        return AutoBuilder.pathfindToPose(targetMirroredAlliancePose, pathConstraints);
+    }
+
+    public static Command getDriveToPoseCommand(Supplier<AlliancePose2d> targetPose, PathConstraints constraints) {
+        return new DeferredCommand(() -> getCurrentDriveToPoseCommand(targetPose.get(), constraints), Set.of(SWERVE));
+    }
+
+    private static Command getCurrentDriveToPoseCommand(AlliancePose2d targetPose, PathConstraints constraints) {
+        Logger.recordOutput("target roto pose", targetPose.toBlueAlliancePose());
+        return new SequentialCommandGroup(
+                new InstantCommand(() -> SWERVE.initializeDrive(true)),
+                getPathfindToPoseCommand(targetPose, constraints)
+                ,getPIDToPoseCommand(targetPose)
+        );
+    }
+
+    private static Command createOnTheFlyPathCommand(Pose2d targetPose, PathConstraints constraints) {
+        List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(
+                RobotContainer.POSE_ESTIMATOR.getCurrentPose().toAlliancePose(),
+                targetPose
+        );
+
+        PathPlannerPath path = new PathPlannerPath(
+                bezierPoints,
+                constraints,
+                new GoalEndState(0, targetPose.getRotation())
+        );
+
+        path.preventFlipping = true;
+
+        return AutoBuilder.followPath(path);
     }
 }

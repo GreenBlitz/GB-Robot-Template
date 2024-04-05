04b6487fee1dfcf8d5a152f174882d9b67adc27d
YHerm <0549405422.yh@gmail.com>
2024-04-05 17:50:35 +0300

    yoav - sysid  constants values

--- src/main/java/frc/utils/calibration/sysid/SysIdConstants.java
+++ src/main/java/frc/utils/calibration/sysid/SysIdConstants.java
@@ -2,7 +2,7 @@ package frc.utils.calibration.sysid;
 
 public class SysIdConstants {
 
-    protected static final double DEFAULT_VOLTAGE_STEP = 1;
+    protected static final double DEFAULT_VOLTAGE_STEP = 7;
     protected static final double DEFAULT_RAMP_RATE = 1;
     protected static final double DEFAULT_TIMEOUT = 10;
 

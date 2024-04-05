e96482b7b481be2631d948a5efef51e34c55a6d9
YHerm <0549405422.yh@gmail.com>
2024-04-05 23:34:41 +0300

    yoav - imports

--- src/main/java/frc/robot/subsystems/swerve/SwerveCommands.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveCommands.java
@@ -8,10 +8,8 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.*;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.RobotContainer;
 import frc.utils.allianceutils.AlliancePose2d;
-import frc.utils.calibration.sysid.SysIdObject;
 import frc.utils.commands.InitExecuteCommand;
 
 import java.util.List;

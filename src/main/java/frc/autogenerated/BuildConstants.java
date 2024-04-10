d161762a746f1d6bce6618c1241bdb92a617e989
YHerm <0549405422.yh@gmail.com>
2024-04-10 18:53:30 +0300

    yoav - git igonere

--- src/main/java/frc/autogenerated/BuildConstants.java
+++ src/main/java/frc/autogenerated/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "Off-Season-2024";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 97;
-    public static final String GIT_SHA = "289aa21f5b2774217b45e3246141b1b1e2409e26";
-    public static final String GIT_DATE = "2024-04-10 15:47:44 GMT";
+    public static final int GIT_REVISION = 103;
+    public static final String GIT_SHA = "d9b7001353d20e39d5c477ed4b401f348483db07";
+    public static final String GIT_DATE = "2024-04-10 15:52:30 GMT";
     public static final String GIT_BRANCH = "pre-pre-commit";
-    public static final String BUILD_DATE = "2024-04-10 15:49:37 GMT";
-    public static final long BUILD_UNIX_TIME = 1712764177217L;
-    public static final int DIRTY = 0;
+    public static final String BUILD_DATE = "2024-04-10 15:53:22 GMT";
+    public static final long BUILD_UNIX_TIME = 1712764402165L;
+    public static final int DIRTY = 1;
 
     private BuildConstants() {}
 }

1275c44fee0e16378d96e6ad0dd3ab2fd0f0fa72
YHerm <0549405422.yh@gmail.com>
2024-04-12 17:05:07 +0300

    yoav - deleted pose estim

--- src/main/java/frc/robot/subsystems/swerve/mk4iswerve/MK4ISwerve.java
+++ src/main/java/frc/robot/subsystems/swerve/mk4iswerve/MK4ISwerve.java
@@ -2,20 +2,14 @@ package frc.robot.subsystems.swerve.mk4iswerve;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.subsystems.swerve.gyro.gyropigeon2.GyroPigeon2;
-import frc.robot.subsystems.swerve.swerveinterface.ISwerve;
-import frc.robot.subsystems.swerve.swerveinterface.SwerveInputsAutoLogged;
-import java.util.Queue;
+import frc.robot.subsystems.swerve.swerveinterface.*;
 
 public class MK4ISwerve implements ISwerve {
 
     private final GyroPigeon2 pigeon2Gyro;
-    private Queue<Double> yawQueue, timestampQueue;
 
     public MK4ISwerve() {
         this.pigeon2Gyro = new GyroPigeon2();
-        //        yawQueue = TalonFXOdometryThread6328.getInstance().registerSignal(pigeon2Gyro.getGyro(),
-        // pigeon2Gyro.YAW_SIGNAL);
-        //        timestampQueue = TalonFXOdometryThread6328.getInstance().getTimestampQueue();
     }
 
     @Override
@@ -30,11 +24,5 @@ public class MK4ISwerve implements ISwerve {
         inputs.accelerationX = pigeon2Gyro.getX_ACCELERATION_SIGNAL().getValue();
         inputs.accelerationY = pigeon2Gyro.getY_ACCELERATION_SIGNAL().getValue();
         inputs.accelerationZ = pigeon2Gyro.getZ_ACCELERATION_SIGNAL().getValue();
-
-        //        inputs.odometryUpdatesYawDegrees = yawQueue.stream().mapToDouble(Double::doubleValue).toArray();
-        //        inputs.odometryUpdatesTimestamp = timestampQueue.stream().mapToDouble(Double::doubleValue).toArray();
-
-        //        yawQueue.clear();
-        //        timestampQueue.clear();
     }
 }

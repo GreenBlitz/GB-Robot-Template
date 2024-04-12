e9e7c778053efa96c6fc6dd36ab679d0e30daa7a
YHerm <0549405422.yh@gmail.com>
2024-04-12 17:33:04 +0300

    yoav - cleaned

--- src/main/java/frc/robot/poseestimation/PoseEstimatorConstants.java
+++ src/main/java/frc/robot/poseestimation/PoseEstimatorConstants.java
@@ -1,9 +1,10 @@
 package frc.robot.poseestimation;
 
+import frc.robot.poseestimation.PoseEstimatorFactory.PoseEstimatorType;
+
 public class PoseEstimatorConstants {
 
-    public static final PoseEstimatorFactory.PoseEstimatorType POSE_ESTIMATOR_TYPE =
-            PoseEstimatorFactory.PoseEstimatorType.WPILIB_ODOMETRY;
-
+    public static final PoseEstimatorType POSE_ESTIMATOR_TYPE = PoseEstimatorType.WPILIB_ODOMETRY;
     public static final double ODOMETRY_FREQUENCY_HERTZ = 250;
+
 }

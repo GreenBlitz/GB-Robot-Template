47d06aee68d8138ca1049a3e27fbf4c4d2ede7e5
YHerm <0549405422.yh@gmail.com>
2024-04-14 00:29:27 +0300

    yoav - find ks

---
+++ src/main/java/frc/utils/calibration/autocalibration/findks/FindKsConstants.java
@@ -0,0 +1,9 @@
+package frc.utils.calibration.autocalibration.findks;
+
+public class FindKsConstants {
+
+    protected static final double VOLTAGE_STEP = 0.01;
+
+    protected static final double TIME_BETWEEN_VOLTAGE_RAMPS_SECONDS = 0.1;
+
+}

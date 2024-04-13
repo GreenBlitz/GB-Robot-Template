e35b57ca298f07667797241e63affcdbd0e98f8b
YHerm <0549405422.yh@gmail.com>
2024-04-13 21:59:40 +0300

    yoav - fixed and improved logger utils

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,7 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.constants.RobotConstants;
-import frc.utils.LoggerUtils;
+import frc.utils.loggerutils.LoggerUtils;
 import org.littletonrobotics.junction.LoggedRobot;
 
 /**
--- src/main/java/frc/robot/constants/SimulationConstants.java
+++ src/main/java/frc/robot/constants/SimulationConstants.java
@@ -4,6 +4,4 @@ public class SimulationConstants {
 
     public static final double TIME_STEP = 0.02;
 
-    public static final String SIMULATION_LOG_PATH = System.getProperty("user.home") + "\\Desktop\\SimulationLogs";
-
 }

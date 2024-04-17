316559486cdcddf305172b558d7b3aba75358b7b
YHerm <0549405422.yh@gmail.com>
2024-04-17 20:04:17 +0300

    yoav - cleaned joystick

---
+++ src/main/java/frc/utils/joysticks/SmartJoystickConstants.java
@@ -0,0 +1,9 @@
+package frc.utils.joysticks;
+
+public class SmartJoystickConstants {
+
+    protected static final double DEADZONE = 0.07;
+
+    protected static final double JOYSTICK_AXIS_TO_SQUARE_FACTOR = 1.4;
+
+}

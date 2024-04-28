33f4681180f2976ff77482a5f8cfcf14bd92aa8a
YHerm <0549405422.yh@gmail.com>
2024-04-28 11:38:06 +0300

    yoav- extraact gyro sim

--- src/main/java/frc/robot/simulation/GyroSimulation.java
+++ src/main/java/frc/robot/simulation/GyroSimulation.java
@@ -4,18 +4,46 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public class GyroSimulation {
 
-    private double simulationRadians = 0;
+    private double yawRadians = 0;
+
+    private double rollRadians = 0;
+
+    private double pitchRadians = 0;
 
     public Rotation2d getGyroYaw() {
-        return Rotation2d.fromRadians(simulationRadians);
+        return Rotation2d.fromRadians(yawRadians);
     }
 
-    public void update(Rotation2d omegaPerSecond, double timeSeconds) {
-        simulationRadians += omegaPerSecond.getRadians() * timeSeconds;
+    public Rotation2d getGyroRoll() {
+        return Rotation2d.fromRadians(rollRadians);
     }
 
-    public void setHeading(Rotation2d heading) {
-        simulationRadians = heading.getRadians();
+    public Rotation2d getGyroPitch() {
+        return Rotation2d.fromRadians(pitchRadians);
+    }
+
+    public void updateYaw(Rotation2d omegaPerSecond, double timeSeconds) {
+        yawRadians += omegaPerSecond.getRadians() * timeSeconds;
+    }
+
+    public void updateRoll(Rotation2d omegaPerSecond, double timeSeconds) {
+        rollRadians += omegaPerSecond.getRadians() * timeSeconds;
+    }
+
+    public void updatePitch(Rotation2d omegaPerSecond, double timeSeconds) {
+        pitchRadians += omegaPerSecond.getRadians() * timeSeconds;
+    }
+
+    public void setYaw(Rotation2d yaw) {
+        yawRadians = yaw.getRadians();
+    }
+
+    public void setRoll(Rotation2d roll) {
+        rollRadians = roll.getRadians();
+    }
+
+    public void setPitch(Rotation2d pitch) {
+        pitchRadians = pitch.getRadians();
     }
 
 }

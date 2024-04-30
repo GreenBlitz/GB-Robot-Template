a928307451a0c236eb68fde9e40fdce055987f8f
itamaroryan <itamar.oryan@gmail.com>
2024-04-30 15:37:50 +0300

    yoav - cleaned

--- src/main/java/frc/utils/calibration/staticcharacterization/StaticCharacterizationConstants.java
+++ src/main/java/frc/utils/calibration/staticcharacterization/StaticCharacterizationConstants.java
@@ -4,7 +4,7 @@ class StaticCharacterizationConstants {
 
     protected static final double TIME_BETWEEN_COMMANDS = 5;
 
-    protected static final double RAMP_VOLTS_PER_SEC = 0.01;
+    protected static final double RAMP_VOLTS_PER_SEC = 0.01;//TODO 0.1
 
     protected static final double VELOCITY_DEADBAND = 0.000001;
 

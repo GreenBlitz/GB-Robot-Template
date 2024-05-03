2a3197232521bdb39f5cbcae04f2312d8455f562
YHerm <0549405422.yh@gmail.com>
2024-05-03 22:40:11 +0300

    yoav - space

--- src/main/java/frc/robot/subsystems/swerve/odometryThread/OdometryThreadConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/odometryThread/OdometryThreadConstants.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.swerve.odometryThread;
 class OdometryThreadConstants {
 
     protected static final int MAX_QUEUE_SIZE = 100;
+
     protected static final double DELAY_STARTING_TIME_SECONDS = 5;
 
 }

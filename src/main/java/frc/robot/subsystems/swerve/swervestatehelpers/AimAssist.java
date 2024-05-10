501a1693ef9dd287b9fff47527194b764e53ef45
YHerm <0549405422.yh@gmail.com>
2024-05-11 00:04:13 +0300

    yoav - started aim assist

---
+++ src/main/java/frc/robot/subsystems/swerve/swervestatehelpers/AimAssist.java
@@ -0,0 +1,23 @@
+//package frc.robot.subsystems.swerve.swervestatehelpers;
+//
+//import frc.utils.allianceutils.AllianceRotation2d;
+//import frc.utils.allianceutils.AllianceTranslation2d;
+//
+//import java.util.function.Supplier;
+//
+//public enum AimAssist {
+//
+//    NOTE(),
+//    SPEAKER(),
+//    AMP(),
+//    STAGE();
+//
+//    public final Supplier<AllianceRotation2d> targetAngleSupplier;
+//
+//    AimAssist(AllianceRotation2d targetAllianceRotation) {}
+//
+//    AimAssist(Supplier<AllianceTranslation2d> targetAllianceTranslation) {}
+//
+//    //Todo - pose to angle. Maybe in kinda math util or pose util
+//}
+//

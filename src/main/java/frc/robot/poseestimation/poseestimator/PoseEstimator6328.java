4bf764b58ae201e4b5364a7d815245ab6328cc3f
YHerm <0549405422.yh@gmail.com>
2024-05-10 09:59:35 +0300

    yoav- todos

--- src/main/java/frc/robot/poseestimation/poseestimator/PoseEstimator6328.java
+++ src/main/java/frc/robot/poseestimation/poseestimator/PoseEstimator6328.java
@@ -74,7 +74,7 @@ public class PoseEstimator6328 {
      */
     public void addOdometryObservation(OdometryObservation observation) {
         // Set the created values to starting values
-        if (isFirstOdometryUpdate) {
+        if (isFirstOdometryUpdate) {//todo - check about this
             lastWheelPositions = SWERVE.getSwerveWheelPositions(0);
             lastGyroAngle = SWERVE.getOdometryYawUpdates()[0];
             isFirstOdometryUpdate = false;

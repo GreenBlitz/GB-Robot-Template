150e2e18bed8620798b4c745e3cf08b5788e245b
YHerm <0549405422.yh@gmail.com>
2024-05-13 14:50:16 +0300

    yoav - clean

--- src/main/java/frc/robot/subsystems/swerve/swervestatehelpers/RotateAxis.java
+++ src/main/java/frc/robot/subsystems/swerve/swervestatehelpers/RotateAxis.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.swerve.swervestatehelpers;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.RobotContainer;
+import frc.robot.constants.MathConstants;
 import frc.robot.subsystems.swerve.modules.ModuleUtils;
 
 public enum RotateAxis {
@@ -28,30 +29,24 @@ public enum RotateAxis {
     }
 
     public static RotateAxis getLeftFarRotateAxis() {
-        Rotation2d currentAllianceAngle = RobotContainer.POSE_ESTIMATOR.getCurrentPose().getRotation2d().getAllianceAngle();
-        if (Math.abs(currentAllianceAngle.getDegrees()) <= 45) { // -45 <= deg <= 45
-            return FRONT_LEFT_MODULE;
-        }
-        if (Math.abs(currentAllianceAngle.getDegrees()) >= 135) {// -135 - x - 135
-            return BACK_RIGHT_MODULE;
-        }
-        if (currentAllianceAngle.getDegrees() > 0) {// 45 <= x <= 135
-            return FRONT_RIGHT_MODULE;
-        }
-        return BACK_LEFT_MODULE;// -45 >= x >= -135
+        return getFarRotateAxis(true);
     }
 
     public static RotateAxis getRightFarRotateAxis() {
+        return getFarRotateAxis(false);
+    }
+
+    private static RotateAxis getFarRotateAxis(boolean isLeft) {
         Rotation2d currentAllianceAngle = RobotContainer.POSE_ESTIMATOR.getCurrentPose().getRotation2d().getAllianceAngle();
-        if (Math.abs(currentAllianceAngle.getDegrees()) <= 45) { // -45 <= deg <= 45
-            return FRONT_RIGHT_MODULE;
+        if (Math.abs(currentAllianceAngle.getDegrees()) <= MathConstants.EIGHTH_CIRCLE.getDegrees()) { // -45 <= deg <= 45
+            return isLeft ? FRONT_LEFT_MODULE : FRONT_RIGHT_MODULE;
         }
-        if (Math.abs(currentAllianceAngle.getDegrees()) >= 135) {// -135 - x - 135
-            return BACK_LEFT_MODULE;
+        if (Math.abs(currentAllianceAngle.getDegrees()) >= MathConstants.EIGHTH_CIRCLE.getDegrees() * 3) { // -135 - x - 135
+            return isLeft ? BACK_RIGHT_MODULE : BACK_LEFT_MODULE;
         }
-        if (currentAllianceAngle.getDegrees() > 0) {// 45 <= x <= 135
-            return BACK_RIGHT_MODULE;
+        if (currentAllianceAngle.getDegrees() > 0) { // 45 <= x <= 135
+            return isLeft ? FRONT_RIGHT_MODULE : BACK_RIGHT_MODULE;
         }
-        return FRONT_LEFT_MODULE;// -45 >= x >= -135
+        return isLeft ? BACK_LEFT_MODULE : FRONT_LEFT_MODULE; // -45 >= x >= -135
     }
 }

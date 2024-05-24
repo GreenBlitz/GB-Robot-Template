ac65cc2127d895bcf493c064d33420556fe8fd70
YHerm <0549405422.yh@gmail.com>
2024-05-24 15:35:59 +0300

    yoav - todo

--- src/main/java/frc/robot/constants/Phoenix6Constants.java
+++ src/main/java/frc/robot/constants/Phoenix6Constants.java
@@ -2,7 +2,7 @@ package frc.robot.constants;
 
 public class Phoenix6Constants {
 
-    public static final String CANIVORE_NAME = "canivore";
+    public static final String CANIVORE_NAME = "canivore"; //todo - put real name
 
     public static final String CANBUS_NAME = "rio";
 

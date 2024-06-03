1c4aa8eddab635401802c99acefe9fb1ff030a6d
YoniKiriaty <YoniKiriaty@gmail.com>
2024-06-03 18:06:38 +0300

    Yoni - changed controller java

--- src/main/java/frc/utils/controllers/keyboard/KeyboardController.java
+++ src/main/java/frc/utils/controllers/keyboard/KeyboardController.java
@@ -1,5 +1,9 @@
 package frc.utils.controllers.keyboard;
 
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.utils.controllers.Controller;
 import org.littletonrobotics.junction.networktables.LoggedDashboardBoolean;
@@ -100,6 +104,36 @@ public class KeyboardController implements Controller{
         NUMPAD_7 = new Trigger(new LoggedDashboardBoolean("keyboard/numpad7", false)::get);
         NUMPAD_8 = new Trigger(new LoggedDashboardBoolean("keyboard/numpad8", false)::get);
         NUMPAD_9 = new Trigger(new LoggedDashboardBoolean("keyboard/numpad9", false)::get);
+    }
+
+    private void keyListener() {
+
+        try {
+            GlobalScreen.registerNativeHook();
+            GlobalScreen.addNativeKeyListener(new NativeKeyListener()
+            {
+
+                @Override
+                public void nativeKeyTyped(NativeKeyEvent nativeEvent)
+                {
+                }
+
+                @Override
+                public void nativeKeyReleased(NativeKeyEvent nativeEvent)
+                {
+                    String keyText=NativeKeyEvent.getKeyText(nativeEvent.getKeyCode());
+                    System.out.println("User typed: "+keyText);
+                }
+
+                @Override
+                public void nativeKeyPressed(NativeKeyEvent nativeEvent)
+                {
+
+                }
+            });
+        } catch (NativeHookException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public double getAxisValue(Controller.Axis axis) {
---
+++ src/main/java/frc/utils/controllers/keyboard/KeyboardListener.java
@@ -0,0 +1,21 @@
+package frc.utils.controllers.keyboard;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+class KeyboardListener implements KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}

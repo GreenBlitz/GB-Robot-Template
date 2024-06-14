01b2ff886b3b8f8c790f4e6e5988b37811bf2b6b
YHerm <0549405422.yh@gmail.com>
2024-06-14 15:07:04 +0300

    yoav - state des

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -8,7 +8,7 @@ public class RobotConstants {
 
     public static final RobotType ROBOT_TYPE = RobotTypeUtils.determineRobotType(RobotType.REAL);
 
-    public static final DriverStation.Alliance SIMULATION_ALLIANCE = DriverStation.Alliance.Blue;
+    public static final DriverStation.Alliance SIMULATION_ALLIANCE = DriverStation.Alliance.Red;
 
     public static final boolean ENABLE_BATTERY_LIMITER = true;
 
--- src/main/java/frc/robot/subsystems/swerve/swervestatehelpers/DriveMode.java
+++ src/main/java/frc/robot/subsystems/swerve/swervestatehelpers/DriveMode.java
@@ -1,27 +1,7 @@
 package frc.robot.subsystems.swerve.swervestatehelpers;
 
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import frc.robot.subsystems.swerve.Swerve;
-
 public enum DriveMode {
 
-    FIELD_RELATIVE(Swerve::fieldRelativeSpeedsToSelfRelativeSpeeds),//todo - math class
-    SELF_RELATIVE((speeds) -> speeds);
-
-
-    private final DriveFunction driveFunction;
-
-    DriveMode(DriveFunction driveFunction) {
-        this.driveFunction = driveFunction;
-    }
-
-    public ChassisSpeeds getDriveModeRelativeChassisSpeeds(ChassisSpeeds speeds) {
-        return driveFunction.getDriveModeRelativeChassisSpeeds(speeds);
-    }
-
-    private interface DriveFunction {
-
-        ChassisSpeeds getDriveModeRelativeChassisSpeeds(ChassisSpeeds chassisSpeeds);
-
-    }
+    FIELD_RELATIVE,
+    SELF_RELATIVE;
 }

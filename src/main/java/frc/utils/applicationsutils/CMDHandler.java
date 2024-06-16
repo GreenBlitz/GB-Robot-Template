3e1abdf11e3cf1e44ae3f56f2d939cdde0e643f5
YoniKiriaty <YoniKiriaty@gmail.com>
2024-06-16 17:06:37 +0300

    Yoni - fixed magic strings

--- src/main/java/frc/utils/applicationsutils/CMDHandler.java
+++ src/main/java/frc/utils/applicationsutils/CMDHandler.java
@@ -9,12 +9,16 @@ public class CMDHandler {
     public static final String PATH_TO_PYTHON_DIRECTORY = REPOSITORY_PATH + "/src/main/python";
     public static final String PATH_TO_JAVA_DIRECTORY = REPOSITORY_PATH + "/src/main/java";
 
+    private static final String APPLICATION = "cmd.exe";
+    private static final String CMD_DEFAULT_DIRECTORY = "/c";
+    private static final String ERROR_MESSAGE = "Unable to execute: ";
+
     public static void runCMDCommand(String command) {
         Runtime runtime = Runtime.getRuntime();
         try {
-            runtime.exec(new String[]{"cmd.exe", "/c", command});
+            runtime.exec(new String[]{APPLICATION, CMD_DEFAULT_DIRECTORY, command});
         } catch (IOException exception) {
-            System.out.println("Unable to execute: " + command);// can't be logged because on computer side
+            System.out.println(ERROR_MESSAGE + command);// can't be logged because on computer side
         }
     }
 }

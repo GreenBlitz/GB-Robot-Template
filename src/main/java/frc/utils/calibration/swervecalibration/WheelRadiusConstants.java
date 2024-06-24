71091615b9ee318ad07026f2bea52deebe7f0217
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-06-24 17:03:03 +0300

    yoav: remove all frc java coding

--- src/main/java/frc/utils/calibration/swervecalibration/WheelRadiusConstants.java
+++
@@ -1,9 +0,0 @@
-package frc.utils.calibration.swervecalibration;
-
-class WheelRadiusConstants {
-
-    protected static final int NUMBER_OF_MODULES = 4;
-
-    protected static final String LOG_PATH = "Calibration/RadiusCharacterization/Wheel Radius";
-
-}

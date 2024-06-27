736f15380f36771267b01cb5ac3ff59d7b6ac7a6
YHerm <0549405422.yh@gmail.com>
2024-06-27 23:57:05 +0300

    yoav: removed stop logger

--- src/main/java/frc/utils/calibration/sysid/SysIdCalibrator.java
+++ src/main/java/frc/utils/calibration/sysid/SysIdCalibrator.java
@@ -23,13 +23,15 @@ public class SysIdCalibrator {
 
     private final boolean isCTRE;
 
+    //todo - check if comment is right
+
     /**
      * IMPORTANT:
      *
      * @param voltageSetControl - note that this function needs to use kg in it so the mechanism won't move because of gravity.
      */
     public SysIdCalibrator(boolean isCTRE, GBSubsystem subsystem, Consumer<Double> voltageSetControl, double voltageStepVolts,
-                           double rampRateVoltsPerSecond) {
+            double rampRateVoltsPerSecond) {
         this(
                 isCTRE,
                 subsystem,
@@ -78,7 +80,7 @@ public class SysIdCalibrator {
      * @param voltageSetControl - note that this function needs to use kg in it so the mechanism won't move because of gravity.
      */
     public SysIdCalibrator(boolean isCTRE, GBSubsystem subsystem, Consumer<Double> voltageSetControl, double voltageStepVolts,
-                           double rampRateVoltsPerSecond, double timeoutSeconds) {
+            double rampRateVoltsPerSecond, double timeoutSeconds) {
         this.usedSubSystem = subsystem;
         this.isCTRE = isCTRE;
 
@@ -120,11 +122,7 @@ public class SysIdCalibrator {
     }
 
     private Command getCTRECommand(Command sysIdCommand) {
-        return new SequentialCommandGroup(
-                new InstantCommand(SignalLogger::start),
-                sysIdCommand,
-                new InstantCommand(SignalLogger::stop)
-        ).handleInterrupt(SignalLogger::stop);
+        return new SequentialCommandGroup(new InstantCommand(SignalLogger::start), sysIdCommand);
     }
 
 }

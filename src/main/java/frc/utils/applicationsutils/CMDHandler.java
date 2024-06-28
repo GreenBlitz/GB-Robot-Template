1a293bd8e87b8b8a9346332f7bb8774b0309562b
Yoni Kiriaty <YoniKiriaty@gmail.com>
2024-06-28 08:44:03 +0300

    Yoni - removed package

--- src/main/java/frc/utils/applicationsutils/CMDHandler.java
+++ src/main/java/frc/utils/applicationsutils/CMDHandler.java
@@ -49,7 +49,7 @@ public class CMDHandler {
 
     /**
      * @param pythonPath The path from the java package to the class.
-     *                   example: "keyboard/keyboard_to_nt_router"
+     *                   example: "keyboard_to_nt_router"
      */
     public static void runPythonClass(String pythonPath) {
         runCMDCommand(PATH_TO_PYTHON_DIRECTORY, "py " + pythonPath + ".py");
@@ -57,7 +57,7 @@ public class CMDHandler {
 
     /**
      * @param pythonPath The path from the java package to the class.
-     *                   example: "keyboard/keyboard_to_nt_router"
+     *                   example: "keyboard_to_nt_router"
      *
      * @param arguments The arguments given to the python file, each argument separated by a space.
      *                  Note that it is only possible to transfer strings into the python class.

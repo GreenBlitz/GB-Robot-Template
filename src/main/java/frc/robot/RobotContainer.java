100e07c280ccb170bfa00492c346c6bc7ee54476
YHerm <0549405422.yh@gmail.com>
2024-06-29 23:14:46 +0300

    yoav: comment

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,7 +21,6 @@ public class RobotContainer {
      * The container for the robot. Contains subsystems, OI devices, and commands.
      */
     public RobotContainer() {
-        // Configure the trigger bindings
         configureBindings();
     }
 

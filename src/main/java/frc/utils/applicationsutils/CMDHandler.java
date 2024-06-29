730b73559c653340bbd85b37f7ab892f708a32a1
YHerm <0549405422.yh@gmail.com>
2024-06-29 15:56:53 +0300

    yoav: clean

--- src/main/java/frc/utils/applicationsutils/CMDHandler.java
+++ src/main/java/frc/utils/applicationsutils/CMDHandler.java
@@ -6,36 +6,35 @@ import java.nio.file.Path;
 public class CMDHandler {
 
     public static final Path REPOSITORY_PATH = Path.of("").toAbsolutePath();
-    public static final Path PATH_TO_PYTHON_DIRECTORY = REPOSITORY_PATH.resolve( "src/main/python");
-    public static final Path PATH_TO_JAVA_DIRECTORY = REPOSITORY_PATH.resolve( "src/main/java");
+    public static final Path PATH_TO_PYTHON_DIRECTORY = REPOSITORY_PATH.resolve("src/main/python");
+    public static final Path PATH_TO_JAVA_DIRECTORY = REPOSITORY_PATH.resolve("src/main/java");
 
     private static final String APPLICATION = "cmd.exe";
     private static final String CMD_DIRECTORY = "/c";
     private static final String ERROR_MESSAGE = "Unable to execute: ";
 
-    public static void runCMDCommand(String command) {
-        Runtime runtime = Runtime.getRuntime();
-        try {
-            runtime.exec(new String[]{APPLICATION, CMD_DIRECTORY, command});
-        } catch (IOException exception) {
-            System.out.println(ERROR_MESSAGE + command);// can't be logged because on computer side
-        }
-    }
 
     public static void runCMDCommand(Path directory, String command) {
         runCMDCommand("cd " + directory + "&&" + command);
     }
 
-    /**
-     * @param javaPath The path from the java package to the class. example: "directory/to/my/Example".
-     */
+    public static void runCMDCommand(String command) {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            runtime.exec(new String[]{APPLICATION, CMD_DIRECTORY, command});
+        }
+        catch (IOException exception) {
+            System.out.println(ERROR_MESSAGE + command);// can't be logged because on computer side
+        }
+    }
+
+
     public static void runJavaClass(Path javaPath) {
         runJavaClass(javaPath, "");
     }
 
     /**
      * @param javaPath The path from the java package to the class. example: "directory/to/my/Example".
-     *
      * @param arguments The arguments given to the java file, each argument separated by a space.
      */
     public static void runJavaClass(Path javaPath, String arguments) {
@@ -44,9 +43,7 @@ public class CMDHandler {
         runCMDCommand(PATH_TO_JAVA_DIRECTORY.resolve(packageName), "java " + className + ".java " + arguments);
     }
 
-    /**
-     * @param classToRun The class to run. example: Example.class .
-     */
+
     public static void runJavaClass(Class classToRun) {
         runJavaClass(classToRun, "");
     }
@@ -62,9 +59,7 @@ public class CMDHandler {
         runJavaClass(pathOfClass, arguments);
     }
 
-    /**
-     * @param pythonPath The path from the java package to the class. example: "directory/example_class".
-     */
+
     public static void runPythonClass(Path pythonPath) {
         runPythonClass(pythonPath, "");
     }
@@ -76,4 +71,5 @@ public class CMDHandler {
     public static void runPythonClass(Path pythonPath, String arguments) {
         runCMDCommand(PATH_TO_PYTHON_DIRECTORY, "py " + pythonPath + ".py " + arguments);
     }
+
 }

697fbb50391232c187ae8b36bd05b2c8a122b8fc
YHerm <0549405422.yh@gmail.com>
2024-06-30 18:54:59 +0300

    yoav: added nt manager in oython

---
+++ src/main/python/network_table_manager.py
@@ -0,0 +1,35 @@
+# if this library is not installed, dont install ntcore but pyntcore
+import ntcore
+import sys
+import time
+
+TEAM_NUMBER = 4590  # GREENBLITZ 🐐🐐🐐🐐
+
+CONNECTION_TIMEOUT_SECONDS = 30
+LOOPS_COOLDOWN_SECONDS = 0.1
+
+
+def get_network_table(ip: str, client_name: str):
+    network_table_instance = ntcore.NetworkTableInstance.getDefault()
+
+    print("Setting up NetworkTables client for team {}".format(TEAM_NUMBER))
+    network_table_instance.startClient4(client_name)
+    network_table_instance.setServer(ip)
+    network_table_instance.startDSClient()
+
+    print("Waiting for connection to NetworkTables server...")
+    started_time = time.time()
+    while not network_table_instance.isConnected():
+        # terminate client and program if it takes to long to connect
+        if time.time() - started_time > CONNECTION_TIMEOUT_SECONDS:
+            print("Didn't connected to network tables. Terminating...")
+            close_client(network_table_instance)
+            sys.exit()
+        time.sleep(LOOPS_COOLDOWN_SECONDS)
+
+    print("Connected NetworkTables server")
+    return network_table_instance
+
+
+def close_client(network_table_instance: ntcore.NetworkTableInstance):
+    ntcore.NetworkTableInstance.destroy(network_table_instance)

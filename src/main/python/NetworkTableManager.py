4f8896ce9b82babcdf45cb25e9c55ed66a2db10a
YHerm <0549405422.yh@gmail.com>
2024-07-01 00:54:08 +0300

    yoav: clean

--- src/main/python/NetworkTableManager.py
+++ src/main/python/NetworkTableManager.py
@@ -14,10 +14,30 @@ import sys
 import time
 
 CONNECTION_TIMEOUT_SECONDS = 30
-LOOPS_COOLDOWN_SECONDS = 0.1
+CONNECTION_COOLDOWN_SECONDS = 0.1
 
 
-def get_connected_network_table_instance(ip: str, client_name: str):
+def terminate_client(network_table_instance: ntcore.NetworkTableInstance, client_name: str):
+    print("Terminating client named: " + client_name)
+    ntcore.NetworkTableInstance.destroy(network_table_instance)
+
+
+def passed_connection_timeout(starting_time):
+    return time.time() - starting_time > CONNECTION_TIMEOUT_SECONDS
+
+
+def connect_client_to_server(network_table_instance, client_name):
+    starting_time = time.time()
+    while not network_table_instance.isConnected():
+        # terminate client and program if it takes to long to connect
+        if passed_connection_timeout(starting_time):
+            print("Didn't connect to network tables. Terminating...")
+            terminate_client(network_table_instance, client_name)
+            sys.exit()
+        time.sleep(CONNECTION_COOLDOWN_SECONDS)
+
+
+def get_connected_client(ip: str, client_name: str):
     network_table_instance = ntcore.NetworkTableInstance.getDefault()
 
     print("Setting up NetworkTables client")
@@ -26,19 +46,7 @@ def get_connected_network_table_instance(ip: str, client_name: str):
     network_table_instance.startDSClient()
 
     print("Waiting for connection to NetworkTables server...")
-    started_time = time.time()
-    while not network_table_instance.isConnected():
-        # terminate client and program if it takes to long to connect
-        if time.time() - started_time > CONNECTION_TIMEOUT_SECONDS:
-            print("Didn't connect to network tables. Terminating...")
-            terminate_network_table_instance(network_table_instance, client_name)
-            sys.exit()
-        time.sleep(LOOPS_COOLDOWN_SECONDS)
+    connect_client_to_server(network_table_instance, client_name)
 
     print("Connected {} to NetworkTables server".format(client_name))
     return network_table_instance
-
-
-def terminate_network_table_instance(network_table_instance: ntcore.NetworkTableInstance, client_name: str):
-    print("Terminating client named: " + client_name)
-    ntcore.NetworkTableInstance.destroy(network_table_instance)

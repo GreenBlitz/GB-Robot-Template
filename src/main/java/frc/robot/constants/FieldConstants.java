0b8226f3b5882e7107ac97d23ada5b437c183e8b
GreenBlitz <greenblitz4590@gmail.com>
2024-07-03 17:35:30 +0300

    yoav: mirror utlis

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -4,10 +4,10 @@ import edu.wpi.first.wpilibj.DriverStation;
 
 public class FieldConstants {
 
-    public static final DriverStation.Alliance RELATIVE_FIELD_CONVETION_ALLIANCE = DriverStation.Alliance.Blue;
+    public static final DriverStation.Alliance RELATIVE_FIELD_CONVENTION_ALLIANCE = DriverStation.Alliance.Blue;
 
-    public static final double FIELD_LENGTH_METERS = 16.54175;
+    public static final double LENGTH_METERS = 16.54175;
 
-    public static final double FIELD_WIDTH_METERS = 8.0137;
+    public static final double WIDTH_METERS = 8.0137;
 
 }

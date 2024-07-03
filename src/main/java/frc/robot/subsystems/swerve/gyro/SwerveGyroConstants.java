c2aa163f1f63ea91fe68a75421129d322ad1daf8
GreenBlitz <greenblitz4590@gmail.com>
2024-07-03 19:43:11 +0300

    yoav: one line

--- src/main/java/frc/robot/subsystems/swerve/gyro/SwerveGyroConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/gyro/SwerveGyroConstants.java
@@ -9,10 +9,6 @@ public class SwerveGyroConstants {
     public static final String LOG_PATH = SwerveConstants.SWERVE_LOG_PATH + "Gyro/";
     public static final String ALERT_LOG_PATH = LogPathsConstants.ALERT_LOG_PATH + LOG_PATH;
 
-    public static final Rotation3d GYRO_MOUNT_POSITION_DEGREES = new Rotation3d(
-            180,
-            0,
-            0
-    );
+    public static final Rotation3d GYRO_MOUNT_POSITION_DEGREES = new Rotation3d(180, 0, 0);
 
 }

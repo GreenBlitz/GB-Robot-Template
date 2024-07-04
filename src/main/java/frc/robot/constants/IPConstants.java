3a5c5af5e11db25f528b17fbb8b99ad7956b7a34
YHerm <0549405422.yh@gmail.com>
2024-07-04 13:44:11 +0300

    yoav: ip's

---
+++ src/main/java/frc/robot/constants/IPConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.constants;
+
+public class IPConstants {
+
+    public static final String SIMULATION_IP = "127.0.0.1";
+    public static final String ROUTER_IP = "10.45.90.2";
+    public static final String USB_B_IP = "172.22.11.2";
+
+}

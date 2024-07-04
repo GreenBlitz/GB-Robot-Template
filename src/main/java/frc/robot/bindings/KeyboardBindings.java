73604a609d1b903c6415cac04a86cb5fff494593
Yoni Kiriaty <YoniKiriaty@gmail.com>
2024-07-04 06:13:56 +0300

    Yoni - changed bindings

--- src/main/java/frc/robot/bindings/KeyboardBindings.java
+++ src/main/java/frc/robot/bindings/KeyboardBindings.java
@@ -1,6 +1,5 @@
 package frc.robot.bindings;
 
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import frc.utils.controllers.keyboard.KeyboardController;
 
 public class KeyboardBindings {
@@ -9,11 +8,7 @@ public class KeyboardBindings {
 
     public static void configureBindings() {
         KeyboardController usedKeyboard = KEYBOARD_CONTROLLER;
-
-        usedKeyboard.A.onTrue(new PrintCommand("A PRESSED"));
-        usedKeyboard.A.onFalse(new PrintCommand("A RELEASED"));
-        usedKeyboard.B.onTrue(new PrintCommand("B PRESSED"));
-        usedKeyboard.B.onFalse(new PrintCommand("B RELEASED"));
+        // bindings
     }
 
 }

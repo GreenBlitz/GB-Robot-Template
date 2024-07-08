c29da75cf68eb5411aae4e748415ef434cf5279c
NoamRosenberg08 <noamrosenberg097@gmail.com>
2024-07-08 22:08:36 +0300

    noam - added basic turret subsystem

---
+++ src/main/java/frc/robot/turret/TurretSubsystem.java
@@ -0,0 +1,50 @@
+package frc.robot.turret;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.utils.GBSubsystem;
+
+public class TurretSubsystem extends GBSubsystem {
+
+    ITurret turret;
+    TurretState state;
+
+    public TurretSubsystem(ITurret turret){
+        this.turret = turret;
+        this.state = TurretState.HOLD_POSITION_RELATIVE_TO_ROBOT;
+    }
+
+
+    public void setState (TurretState targetState){
+        this.state = targetState;
+    }
+
+    public void handleRotateToPoint (Translation2d targetPoint, Translation2d robotPosition){
+        Translation2d normalizedRobotPosition = targetPoint.minus(robotPosition);
+        this.turret.setPosition(
+                new Rotation2d(
+                        targetPoint.minus(robotPosition).getX(),
+                        targetPoint.minus(robotPosition).getY()
+                )
+        );
+    }
+    public void handleHoldPositionRelativeToRobot (Rotation2d targetAngle){
+        this.turret.setPosition(targetAngle);
+    }
+
+
+
+
+
+
+    @Override
+    protected String getLogPath() {
+        return null;
+    }
+
+    @Override
+    protected void subsystemPeriodic() {
+
+    }
+
+}

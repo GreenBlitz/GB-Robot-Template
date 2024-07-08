48e6ced5257973d7f96e1ca9c0b6fdf47aab8dbf
NoamRosenberg08 <noamrosenberg097@gmail.com>
2024-07-08 22:16:52 +0300

    noam - added turret constants

---
+++ src/main/java/frc/robot/turret/TurretConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.turret;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class TurretConstants {
+    public static final Translation2d LOOKING_TARGET = new Translation2d(5,5);
+
+}

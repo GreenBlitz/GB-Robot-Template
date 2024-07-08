392944cb39c2ec4a5a7d4bbc175fe1870100dddc
NoamRosenberg08 <noamrosenberg097@gmail.com>
2024-07-08 22:01:12 +0300

    noam - added turret interface

---
+++ src/main/java/frc/robot/turret/ITurret.java
@@ -0,0 +1,9 @@
+package frc.robot.turret;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public interface ITurret {
+    void setVelocity (double velocity);
+    void setPosition (Rotation2d angle);
+    void stop();
+}

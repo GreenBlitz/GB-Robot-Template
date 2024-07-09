6eb0108742c19fd704dc09aa0caefaed5dcbae81
YHerm <0549405422.yh@gmail.com>
2024-07-09 18:59:10 +0300

    yoav: comments

--- src/main/java/frc/robot/RobotManager.java
+++ src/main/java/frc/robot/RobotManager.java
@@ -14,12 +14,6 @@ import frc.utils.cycletime.CycleTimeUtils;
 import frc.utils.logger.LoggerFactory;
 import org.littletonrobotics.junction.LoggedRobot;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class RobotManager extends LoggedRobot {
 
     private Command autonomousCommand;

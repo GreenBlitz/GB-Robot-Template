e6dedfd580135e1d781819f5b3d372167a3dd33f
YHerm <0549405422.yh@gmail.com>
2024-07-10 12:09:49 +0300

    yoav: spaces

--- src/main/java/frc/robot/subsystems/swerve/modules/ModuleConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/modules/ModuleConstants.java
@@ -16,6 +16,4 @@ public class ModuleConstants {
     public static final Rotation2d ANGLE_VELOCITY_DEADBAND = Rotation2d.fromDegrees(3);
     public static final double SPEED_TOLERANCE_METERS_PER_SECOND = 0.1;
 
-
-
 }

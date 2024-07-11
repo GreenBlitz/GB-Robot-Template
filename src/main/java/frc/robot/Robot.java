780a3a14b66a0918e0855f3716708b3a749494f4
YHerm <0549405422.yh@gmail.com>
2024-07-11 23:32:29 +0300

    yoav; robot funcs

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,8 +6,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,31 +15,23 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class Robot {
 
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
+
     public Robot() {
         configureBindings();
     }
 
-    /**
-     * Use this method to define your trigger->command mappings. Triggers can be created via the
-     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-     * predicate, or via the named factories in {@link
-     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-     * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-     * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-     * joysticks}.
-     */
+    private void initializeSubsystems() {
+
+    }
+
+    private void configureCommands() {
+
+    }
+
     private void configureBindings() {
         JoysticksBindings.configureBindings();
     }
 
-    /**
-     * Use this to pass the autonomous command to the main {@link RobotManager} class.
-     *
-     * @return the command to run in autonomous
-     */
     public Command getAutonomousCommand() {
         return new InstantCommand();
     }

9563c675a47739dda34c6fd8ec97d84e8e157563
NoamRosenberg08 <noamrosenberg097@gmail.com>
2024-07-12 18:19:32 +0300

    another format

--- src/main/java/frc/robot/constants/GlobalConstants.java
+++ src/main/java/frc/robot/constants/GlobalConstants.java
@@ -7,6 +7,7 @@ public class GlobalConstants {
     public static final DriverStation.Alliance SIMULATION_ALLIANCE = DriverStation.Alliance.Blue;
 
     public static final boolean ENABLE_BATTERY_LIMITER = true;
+
     public static final double DEFAULT_CAN_FREQUENCY = 50;
 
 }
--- src/main/java/frc/robot/turret/talonfx/TalonFXConstants.java
+++ src/main/java/frc/robot/turret/talonfx/TalonFXConstants.java
@@ -7,13 +7,11 @@ public class TalonFXConstants {
     public static final TalonFXConfiguration TURRET_MOTOR_CONFIGURATION = new TalonFXConfiguration();
 
     static {
-
         TURRET_MOTOR_CONFIGURATION.Slot0.kP = 1;
         TURRET_MOTOR_CONFIGURATION.Slot0.kI = 0;
         TURRET_MOTOR_CONFIGURATION.Slot0.kD = 0;
 
         TURRET_MOTOR_CONFIGURATION.Feedback.RotorToSensorRatio = 50;
-
     }
 
 }

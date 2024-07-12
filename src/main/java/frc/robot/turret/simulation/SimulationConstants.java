c1bd0219e584134e6c5804c72a1eeef5d918eb15
Noam Rosenberg <95913534+NoamRosenberg08@users.noreply.github.com>
2024-07-12 16:14:47 +0300

    Update src/main/java/frc/robot/turret/simulation/SimulationConstants.java
    Co-authored-by: YHerm <140002259+YHerm@users.noreply.github.com>

--- src/main/java/frc/robot/turret/simulation/SimulationConstants.java
+++ src/main/java/frc/robot/turret/simulation/SimulationConstants.java
@@ -4,7 +4,7 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import edu.wpi.first.math.controller.PIDController;
 
 public class SimulationConstants {
-    public static double GEARING = 50.0;
+    public static double GEAR_RATIO = 50.0;
     public static int AMOUNT_OF_MOTORS = 1;
     public static double JKG_METER_SQ = 0.00048;
     public static TalonFXConfiguration MOTOR_CONFIGURATION = new TalonFXConfiguration();

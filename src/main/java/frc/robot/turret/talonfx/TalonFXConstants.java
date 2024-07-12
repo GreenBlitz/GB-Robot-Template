d9b018beab501786b728f96376c7a0ebaad6b255
NoamRosenberg08 <noamrosenberg097@gmail.com>
2024-07-12 18:17:20 +0300

    noam - added basic configs for turret motor

--- src/main/java/frc/robot/turret/talonfx/TalonFXConstants.java
+++ src/main/java/frc/robot/turret/talonfx/TalonFXConstants.java
@@ -1,11 +1,19 @@
 package frc.robot.turret.talonfx;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
 public class TalonFXConstants {
 
-    protected static final double GEAR_RATIO = 1;
+    public static final TalonFXConfiguration TURRET_MOTOR_CONFIGURATION = new TalonFXConfiguration();
 
+    static {
+        TURRET_MOTOR_CONFIGURATION.Slot0.kP = 1;
+        TURRET_MOTOR_CONFIGURATION.Slot0.kI = 0;
+        TURRET_MOTOR_CONFIGURATION.Slot0.kD = 0;
 
+        TURRET_MOTOR_CONFIGURATION.Feedback.RotorToSensorRatio = 50;
 
+    }
 
 
 }

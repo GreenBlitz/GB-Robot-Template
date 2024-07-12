235662917dc4986a9574f77e076d78e7de86a903
NoamRosenberg08 <noamrosenberg097@gmail.com>
2024-07-12 17:52:29 +0300

    fixed bug

--- src/main/java/frc/robot/turret/simulation/SimulationTurret.java
+++ src/main/java/frc/robot/turret/simulation/SimulationTurret.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import frc.robot.simulation.SimpleMotorSimulation;
 import frc.robot.turret.ITurret;
 import frc.robot.turret.TurretInputsAutoLogged;
-import frc.utils.cycletimeutils.CycleTimeUtils;
 
 import static frc.robot.turret.simulation.SimulationConstants.GEARING;
 import static frc.robot.turret.simulation.SimulationConstants.JKG_METER_SQ;
@@ -22,9 +21,11 @@ public class SimulationTurret implements ITurret {
 
     public SimulationTurret() {
         motor = new SimpleMotorSimulation(
-                DCMotor.getKrakenX60(SimulationConstants.AMOUNT_OF_MOTORS),
-                GEARING,
-                JKG_METER_SQ
+                new DCMotorSim(
+                        DCMotor.getKrakenX60(SimulationConstants.AMOUNT_OF_MOTORS),
+                        GEARING,
+                        JKG_METER_SQ
+                )
         );
 
         positionDutyCycle = new PositionDutyCycle(0);

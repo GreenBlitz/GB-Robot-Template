14dc0b7c363f937d39edfe610471008a616a2d15
NoamRosenberg08 <noamrosenberg097@gmail.com>
2024-07-12 18:18:52 +0300

    noam - formated constants

--- src/main/java/frc/robot/turret/talonfx/TalonFXConstants.java
+++ src/main/java/frc/robot/turret/talonfx/TalonFXConstants.java
@@ -7,6 +7,7 @@ public class TalonFXConstants {
     public static final TalonFXConfiguration TURRET_MOTOR_CONFIGURATION = new TalonFXConfiguration();
 
     static {
+
         TURRET_MOTOR_CONFIGURATION.Slot0.kP = 1;
         TURRET_MOTOR_CONFIGURATION.Slot0.kI = 0;
         TURRET_MOTOR_CONFIGURATION.Slot0.kD = 0;
@@ -14,6 +15,5 @@ public class TalonFXConstants {
         TURRET_MOTOR_CONFIGURATION.Feedback.RotorToSensorRatio = 50;
 
     }
-
 
 }

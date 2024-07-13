6829cc0f668f4d2b8f9588f29c87bb74d9db7d1d
hillelv <hillel.vider@gmail.com>
2024-07-13 18:37:29 +0300

    hillel - four-ever after

---
+++ src/main/java/frc/utils/pathplannerutils/AutonomousChooser.java
@@ -0,0 +1,23 @@
+package frc.utils.pathplannerutils;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+import org.littletonrobotics.junction.Logger;
+
+public class AutonomousChooser {
+
+    private static final SendableChooser<Command> CHOOSER = AutoBuilder.buildAutoChooser();
+
+    public static void addSelectorToShuffleboard() {
+        ShuffleboardTab tab = Shuffleboard.getTab("auto");
+        tab.add("autonomous chooser", CHOOSER);
+    }
+
+    public static Command getChosenValue() {
+        Logger.recordMetadata("autonomous chosen", CHOOSER.getSelected().getName());
+        return CHOOSER.getSelected();
+    }
+}

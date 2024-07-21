cf89e1121726746fa92c3639032bc086d917fa84
hillelv <hillel.vider@gmail.com>
2024-07-21 17:36:13 +0300

    hillel - merge fix

--- src/main/java/frc/utils/auto/PathPlannerUtils.java
+++ src/main/java/frc/utils/auto/PathPlannerUtils.java
@@ -3,19 +3,23 @@ package frc.utils.auto;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathfindingCommand;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.pathfinding.Pathfinding;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.PathPlannerLogging;
+import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -61,6 +65,18 @@ public class PathPlannerUtils {
 
     public static void registerCommand(String commandName, Command command) {
         NamedCommands.registerCommand(commandName, command);
+    }
+
+    public static void setDynamicObstacles(List<Pair<Translation2d, Translation2d>> obstacles, Pose2d currentRobotPose) {
+        Pathfinding.setDynamicObstacles(obstacles, currentRobotPose.getTranslation());
+    }
+
+    public static void removeAllDynamicObstacles(Pose2d currentRobotPose) {
+        setDynamicObstacles(List.of(), currentRobotPose);
+    }
+
+    public static void setTargetRotationOverride(Supplier<Optional<Rotation2d>> overrider) {
+        PPHolonomicDriveController.setRotationTargetOverride(overrider);
     }
 
     public static Command createOnTheFlyPathCommand(Pose2d currentPose, Pose2d targetPose, PathConstraints constraints) {

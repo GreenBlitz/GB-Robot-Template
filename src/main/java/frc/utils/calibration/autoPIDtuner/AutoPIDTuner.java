1e224bea1d3357462578809ec13f74cc91d8d295
RJ <ranelj2008@gmail.com>
2024-07-21 17:26:24 +0300

    Ran-added constants but now without my computer turning off in the middle

--- src/main/java/frc/utils/calibration/autoPIDtuner/AutoPIDTuner.java
+++ src/main/java/frc/utils/calibration/autoPIDtuner/AutoPIDTuner.java
@@ -7,15 +7,20 @@ import edu.greenblitz.robotName.utils.PIDObject;
 public class AutoPIDTuner {
 
     public PIDObject tunePController(double ku) {
-        return new PIDObject(ku/2);
+        return new PIDObject(ku/AutoPIDTunerConstants.P_CONTROLLER.DIVIDE_KU_TO_GET_KP);
     }
 
     public PIDObject tunePIController(double ku, double pu) {
-        return new PIDObject(ku / 2.2, pu / 1.2, 0, 0, 0);
+        return new PIDObject(ku / AutoPIDTunerConstants.PI_CONTROLLER.DIVIDE_KU_TO_GET_KP,
+                pu / AutoPIDTunerConstants.PI_CONTROLLER.DIVIDE_PU_TO_GET_KI,
+                0, 0, 0);
     }
 
     public PIDObject tunePIDController(double ku, double pu) {
-        return new PIDObject(ku / 1.7, pu / 2, pu / 8, 0, 0);
+        return new PIDObject(ku / AutoPIDTunerConstants.PID_CONTROLLER.DIVIDE_KU_TO_GET_KP,
+                pu / AutoPIDTunerConstants.PID_CONTROLLER.DIVIDE_PU_TO_GET_KI,
+                pu / AutoPIDTunerConstants.PID_CONTROLLER.DIVIDE_PU_TO_GET_KD,
+                0, 0);
     }
 
 }
--- src/main/java/frc/utils/calibration/autoPIDtuner/AutoPIDTunerConstants.java
+++ src/main/java/frc/utils/calibration/autoPIDtuner/AutoPIDTunerConstants.java
@@ -7,7 +7,7 @@ public class AutoPIDTunerConstants {
     }
     public class PI_CONTROLLER{
         public static final double DIVIDE_KU_TO_GET_KP = 2.2;
-        public static final double DIVIDE_PU_TO_GET_KP = 1.2;
+        public static final double DIVIDE_PU_TO_GET_KI = 1.2;
     }
     public class PID_CONTROLLER{
         public static final double DIVIDE_KU_TO_GET_KP = 1.7;

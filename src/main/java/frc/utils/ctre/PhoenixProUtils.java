ef378c7eeec9e873a57f18b48dd7d8448eb74570
itamaroryan <itamar.oryan@gmail.com>
2024-07-24 18:02:34 +0300

    Aviv - Create a config applier

--- src/main/java/frc/utils/ctre/PhoenixProUtils.java
+++ src/main/java/frc/utils/ctre/PhoenixProUtils.java
@@ -1,11 +1,26 @@
 package frc.utils.ctre;
 
+import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import frc.utils.devicewrappers.TalonFXWrapper;
+
+import java.util.function.Supplier;
 
 public class PhoenixProUtils {
 
     public static <T> StatusSignal<T> getRefreshedSignal(boolean refresh, StatusSignal<T> signal) {
         return refresh ? signal.refresh() : signal;
+    }
+
+    public static boolean applyThing(Supplier<StatusCode> statusCodeSupplier, int tries){
+        for (int i = 0; i < tries; i++){
+            if(statusCodeSupplier.get().isOK()){
+                return true;
+            }
+        }
+        return false;
     }
 
 }

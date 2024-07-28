aa870efad40ee37caf3a788ee29c184ada418a9d
YHerm <0549405422.yh@gmail.com>
2024-07-29 00:24:12 +0300

    yoav: 1 space

--- src/main/java/frc/utils/ctre/PhoenixProUtils.java
+++ src/main/java/frc/utils/ctre/PhoenixProUtils.java
@@ -11,9 +11,9 @@ public class PhoenixProUtils {
         return refresh ? signal.refresh() : signal;
     }
 
-    public static boolean checkAndRetry(Supplier<StatusCode> statusCodeSupplier, int numberOfTries){
-        for (int i = 0; i <numberOfTries; i++){
-            if(statusCodeSupplier.get().isOK()){
+    public static boolean checkAndRetry(Supplier<StatusCode> statusCodeSupplier, int numberOfTries) {
+        for (int i = 0; i < numberOfTries; i++) {
+            if (statusCodeSupplier.get().isOK()) {
                 return true;
             }
         }

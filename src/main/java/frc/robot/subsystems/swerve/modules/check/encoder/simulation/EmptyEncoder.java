c882c9e152e3b1aae83b585add3ec1519455fb65
YHerm <0549405422.yh@gmail.com>
2024-08-01 16:33:10 +0300

    sim steer

---
+++ src/main/java/frc/robot/subsystems/swerve/modules/check/encoder/simulation/EmptyEncoder.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.swerve.modules.check.encoder.simulation;
+
+import frc.robot.subsystems.swerve.modules.check.encoder.IEncoder;
+import frc.robot.subsystems.swerve.modules.inputs.ModuleInputsContainer;
+
+public class EmptyEncoder implements IEncoder {
+
+    @Override
+    public void updateInputs(ModuleInputsContainer inputs) {
+
+    }
+
+}

28721e8f81284e27caf52ed83523b623583158b9
YHerm <0549405422.yh@gmail.com>
2024-08-02 14:31:49 +0300

    naming

--- src/main/java/frc/robot/subsystems/swerve/modules/steer/simulation/SimulationSteer.java
+++ src/main/java/frc/robot/subsystems/swerve/modules/steer/simulation/SimulationSteer.java
@@ -9,13 +9,13 @@ import frc.robot.subsystems.swerve.modules.steer.ISteer;
 
 public class SimulationSteer implements ISteer {
 
-    private final SimpleMotorSimulation steerMotor;
+    private final SimpleMotorSimulation motor;
 
     private final PositionVoltage positionRequest;
     private final VoltageOut voltageRequest;
 
     public SimulationSteer(SimulationSteerConstants constants){
-        this.steerMotor = constants.getMotor();
+        this.motor = constants.getMotor();
 
         this.positionRequest = new PositionVoltage(0).withEnableFOC(constants.getEnableFOC());
         this.voltageRequest = new VoltageOut(0).withEnableFOC(constants.getEnableFOC());
@@ -34,25 +34,25 @@ public class SimulationSteer implements ISteer {
 
     @Override
     public void stop() {
-        steerMotor.stop();
+        motor.stop();
     }
 
     @Override
     public void setVoltage(double voltage) {
-        steerMotor.setControl(voltageRequest.withOutput(voltage));
+        motor.setControl(voltageRequest.withOutput(voltage));
     }
 
     @Override
     public void setTargetAngle(Rotation2d angle) {
-        steerMotor.setControl(positionRequest.withPosition(angle.getRotations()));
+        motor.setControl(positionRequest.withPosition(angle.getRotations()));
     }
 
 
     @Override
     public void updateInputs(ModuleInputsContainer inputs) {
-        inputs.getSteerMotorInputs().angle = steerMotor.getPosition();
-        inputs.getSteerMotorInputs().velocity = steerMotor.getVelocity();
-        inputs.getSteerMotorInputs().voltage = steerMotor.getVoltage();
+        inputs.getSteerMotorInputs().angle = motor.getPosition();
+        inputs.getSteerMotorInputs().velocity = motor.getVelocity();
+        inputs.getSteerMotorInputs().voltage = motor.getVoltage();
         inputs.getSteerMotorInputs().angleOdometrySamples = new Rotation2d[]{inputs.getSteerMotorInputs().angle};
     }
 

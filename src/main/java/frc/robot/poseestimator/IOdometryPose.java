87e85daf1d32d9a97eeaab5a3a4f4b331656c093
GilStein1 <gils.server@gmail.com>
2024-08-04 21:04:11 +0300

    Gil - changes

--- src/main/java/frc/robot/poseestimator/IOdometryPose.java
+++ src/main/java/frc/robot/poseestimator/IOdometryPose.java
@@ -2,7 +2,7 @@ package frc.robot.poseestimator;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveWheelPositions;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
@@ -10,7 +10,7 @@ import edu.wpi.first.math.numbers.N3;
 public interface IOdometryPose {
     void updateOdometry(OdometryObservation odometryObservation);
 
-    void resetOdometry(Transform2d gyroAngle,SwerveDriveWheelPositions wheelPositions,Pose2d pose);
+    void resetOdometry(Rotation2d gyroAngle,SwerveDriveWheelPositions wheelPositions,Pose2d pose);
 
     Pose2d getOdometryPosition();
 
--- src/main/java/frc/robot/poseestimator/IVisionPose.java
+++ src/main/java/frc/robot/poseestimator/IVisionPose.java
@@ -1,15 +1,15 @@
 package frc.robot.poseestimator;
 
 import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 
-import java.awt.geom.Point2D;
 
 public interface IVisionPose {
     void updateVision(VisionObservation visionObservation);
 
-    Point2D getVisionPose();
+    Pose2d getVisionPose();
 
     void setStandardDeviations(Matrix<N3, N1> stdDevs);
 }

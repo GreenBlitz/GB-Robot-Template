71c7be3600d5351995a9bff5c9bba57bb88973ef
GilStein1 <gils.server@gmail.com>
2024-08-04 17:31:11 +0300

    Gil - removed unnecessary methods

--- src/main/java/frc/utils/JsonConverter.java
+++ src/main/java/frc/utils/JsonConverter.java
@@ -2,19 +2,9 @@ package frc.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import frc.robot.poseestimator.OdometryObservation;
-import frc.robot.poseestimator.VisionObservation;
 
 public class JsonConverter {
     private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    public static VisionObservation visionObservationFromJson(String content) {
-        return objectFromJson(content, VisionObservation.class);
-    }
-
-    public static OdometryObservation odometryObservationFromJson(String content) {
-        return objectFromJson(content, OdometryObservation.class);
-    }
 
     public static <T> T objectFromJson(String content, Class<T> classofObject) {
         try {

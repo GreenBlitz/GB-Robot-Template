10d5d1acfb97954fe3e09bcc5c6e94f030ccd1ea
hillelv <hillel.vider@gmail.com>
2024-08-05 22:55:58 +0300

    hillel - gym

--- src/main/java/frc/robot/subsystems/swerve/modules/ModuleConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/modules/ModuleConstants.java
@@ -27,7 +27,8 @@ public record ModuleConstants(
         );
     }
 
-    public static final String LOG_PATH_ADDITION = "Modules/";
+    static final String LOG_PATH_ADDITION = "Modules/";
+
     static final Rotation2d ANGLE_TOLERANCE = Rotation2d.fromDegrees(3);
     static final Rotation2d ANGLE_VELOCITY_DEADBAND = Rotation2d.fromDegrees(3);
     static final double SPEED_TOLERANCE_METERS_PER_SECOND = 0.1;

cba1255f3b37c739efc61962b77fd9409675d236
YHerm <0549405422.yh@gmail.com>
2024-08-05 16:02:21 +0300

    clean battery

--- src/main/python/BatteryMessage.py
+++ src/main/python/BatteryMessage.py
@@ -1,10 +1,8 @@
-# if this library is not installed, dont install ntcore but pyntcore
-import ntcore
 import sys
 import time
 import tkinter as tk
 from tkinter import PhotoImage
-import NetworkTableManager
+from NetworkTableManager import NetworkTableClient
 
 __IMAGE_PATH = "noam-battery-message.png"
 __WINDOW_NAME = "Battery Message"
@@ -18,19 +16,19 @@ __TIME_BETWEEN_MESSAGES_SECONDS = 120
 __SHOW_MESSAGE_CHECK_COOLDOWN_SECONDS = 0.1
 
 
-def __config_window(window: tk.Tk):
+def __config_window(window: tk.Tk) -> None:
     window.title(__WINDOW_NAME)
     window.attributes("-topmost", True)
     window.resizable(False, False)
     window.bind("<Unmap>", lambda event: __cancel_minimize(event, window))
 
 
-def __cancel_minimize(event, window: tk.Tk):
+def __cancel_minimize(event, window: tk.Tk) -> None:
     window.attributes("-topmost", True)
     window.state('normal')
 
 
-def __create_image_label(window: tk.Tk, image: PhotoImage):
+def __create_image_label(window: tk.Tk, image: PhotoImage) -> None:
     """Create a label widget to display the image on the given window."""
     label = tk.Label(window, image=image)
     label.pack()
@@ -40,13 +38,11 @@ def __load_image(image_path: str) -> PhotoImage:
     return PhotoImage(file=image_path)
 
 
-def __should_show_message(battery_table: ntcore.NetworkTable, last_time_showed: float) -> bool:
-    is_time_to_message = time.time() - last_time_showed > __TIME_BETWEEN_MESSAGES_SECONDS
-    is_battery_low = battery_table.getBoolean(__KEY_NAME, defaultValue=False)
-    return is_battery_low and is_time_to_message
+def is_time_to_message(last_time_showed: float) -> bool:
+    return time.time() - last_time_showed > __TIME_BETWEEN_MESSAGES_SECONDS
 
 
-def __show_message():
+def __show_message() -> None:
     """Set up and run the Tkinter event loop."""
     window = tk.Tk()
     __config_window(window)
@@ -55,21 +51,21 @@ def __show_message():
     window.mainloop()
 
 
-def __track_message_until_client_disconnect(battery_message_client: ntcore.NetworkTableInstance):
-    battery_table = battery_message_client.getTable(__TABLE_NAME)
+def __track_message_until_client_disconnect(battery_message_client: NetworkTableClient) -> None:
+    battery_table = battery_message_client.get_table(__TABLE_NAME)
 
     last_time_showed = 0
-    while battery_message_client.isConnected():
-        if __should_show_message(battery_table, last_time_showed):
+    while battery_message_client.is_connected():
+        if battery_table.getBoolean(__KEY_NAME, defaultValue=False) and is_time_to_message(last_time_showed):
             __show_message()
             last_time_showed = time.time()
         time.sleep(__SHOW_MESSAGE_CHECK_COOLDOWN_SECONDS)
 
 
-def __run_battery_message_client():
-    battery_message_client = NetworkTableManager.get_connected_client(__IP, __CLIENT_NAME)
+def __run_battery_message_client() -> None:
+    battery_message_client = NetworkTableClient(__IP, __CLIENT_NAME)
     __track_message_until_client_disconnect(battery_message_client)
-    NetworkTableManager.terminate_client(battery_message_client, __CLIENT_NAME)
+    battery_message_client.terminate()
 
 
 if __name__ == '__main__':

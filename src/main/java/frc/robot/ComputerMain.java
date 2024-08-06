60189822769f49d72c0eec363c1368c66653394e
YHerm <0549405422.yh@gmail.com>
2024-08-06 21:00:38 +0300

    computer main

--- src/main/java/frc/robot/ComputerMain.java
+++ src/main/java/frc/robot/ComputerMain.java
@@ -1,16 +1,21 @@
 package frc.robot;
 
+import frc.utils.CMDHandler;
+
+import java.nio.file.Path;
+
 /**
  *  Unless you know what you are doing, do not rename this file because it's being used elsewhere.
  */
 public class ComputerMain {
 
     public static void main(String[] args) {
-        startComputerPrograms();
+        startComputerPrograms(args);
     }
 
-    private static void startComputerPrograms() {
-        // start programs on computer...
+    private static void startComputerPrograms(String... args) {
+        String connectedIP = args[0];
+        CMDHandler.runPythonClass(Path.of("BatteryMessage"), connectedIP);
     }
 
 }

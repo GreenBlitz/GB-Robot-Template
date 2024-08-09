6766a3a686c045b01c7c35ec351158a34d8c2de2
YHerm <0549405422.yh@gmail.com>
2024-08-09 23:59:58 +0300

    alert naming

--- src/main/java/frc/robot/subsystems/swerve/modules/Module.java
+++ src/main/java/frc/robot/subsystems/swerve/modules/Module.java
@@ -91,13 +91,13 @@ public class Module {
 
 	public void reportAlerts() {
 		if (!moduleInputsContainer.getEncoderInputs().isConnected) {
-			Logger.recordOutput(LogPaths.ALERT_LOG_PATH + constants.logPath() + "encoder disconnect", Timer.getFPGATimestamp());
+			Logger.recordOutput(LogPaths.ALERT_LOG_PATH + constants.logPath() + "encoder disconnect at", Timer.getFPGATimestamp());
 		}
 		if (!moduleInputsContainer.getSteerMotorInputs().isConnected) {
-			Logger.recordOutput(LogPaths.ALERT_LOG_PATH + constants.logPath() + "steer motor disconnect", Timer.getFPGATimestamp());
+			Logger.recordOutput(LogPaths.ALERT_LOG_PATH + constants.logPath() + "steer disconnect at", Timer.getFPGATimestamp());
 		}
 		if (!moduleInputsContainer.getDriveMotorInputs().isConnected) {
-			Logger.recordOutput(LogPaths.ALERT_LOG_PATH + constants.logPath() + "drive motor disconnect", Timer.getFPGATimestamp());
+			Logger.recordOutput(LogPaths.ALERT_LOG_PATH + constants.logPath() + "drive disconnect at", Timer.getFPGATimestamp());
 		}
 	}
 

6ea7e6e4a6d94e7c333f4aab032fa3b98bd91c21
Yoni Kiriaty <YoniKiriaty@gmail.com>
2024-08-10 17:20:56 +0300

    Yoni - visualized sound

--- src/main/python/GuitarToNetworkTables.py
+++ src/main/python/GuitarToNetworkTables.py
@@ -1,24 +1,43 @@
+import matplotlib.pyplot as plt
 import numpy
-from pvrecorder import PvRecorder
 import time
+from pvrecorder import PvRecorder
 
-audio_resolution = 1;
+audio_resolution = 1
+
 
 def start():
-    recorder = PvRecorder(device_index=-1, frame_length= 32 * audio_resolution)
+    recorder = PvRecorder(device_index=-1, frame_length=32 * audio_resolution)
     try:
         recorder.start()
         start_time = time.time()
-        values = {}
+        values = []
         while True:
             frame = recorder.read()
-            values.__setitem__(time.time() - start_time,frame)
-            # Do something ...
+            values.extend(frame)
     except KeyboardInterrupt:
         recorder.stop()
     finally:
-        print(values)
+        plot_sound(values)
+        plot_frequencies(values)
         recorder.delete()
+
+
+def plot_frequencies(values: list):
+    frequencies = numpy.fft.fft(values)
+    plt.figure(figsize=(8, 6))
+    plt.plot(numpy.arange(0, 1, 1 / len(frequencies)), numpy.abs(frequencies), 'r')
+    plt.xlabel('Freq (Hz)')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+
+def plot_sound(values: list):
+    plt.figure(figsize=(8, 6))
+    plt.plot(numpy.arange(0, 1, 1 / len(values)), values, 'r')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    plt.show()
 
 
 if __name__ == "__main__":

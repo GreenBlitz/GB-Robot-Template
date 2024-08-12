2362446e3afc6f91159d32fef0acd7c73690c148
YHerm <0549405422.yh@gmail.com>
2024-08-12 18:05:14 +0300

    remove unsued stuff

--- src/main/java/frc/utils/battery/BatteryLimiter.java
+++ src/main/java/frc/utils/battery/BatteryLimiter.java
@@ -4,30 +4,24 @@ import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
-import frc.robot.constants.GlobalConstants;
 import frc.robot.constants.IPs;
 import frc.utils.CMDHandler;
 import frc.utils.DriverStationUtils;
 import frc.utils.dashboard.LoggedTableBoolean;
 import org.littletonrobotics.junction.Logger;
-import java.lang.RuntimeException;
 
 import java.nio.file.Path;
 
 class BatteryLimiter extends Command {
 
 	private static final int NUMBER_OF_SAMPLES_TAKEN_IN_AVERAGE = 50;
-	private static final double PYTHON_CYCLE_TIME_SECONDS = 0.1;
-	private static final double MESSAGE_STARTUP_TIME = PYTHON_CYCLE_TIME_SECONDS * 2;
 
 	private final LoggedTableBoolean isBatteryLow;
 	private final LinearFilter voltageFilter;
-	private double showedMessageTime;
 
 	public BatteryLimiter() {
 		this.isBatteryLow = new LoggedTableBoolean("Battery", "is low", false);
 		this.voltageFilter = LinearFilter.movingAverage(NUMBER_OF_SAMPLES_TAKEN_IN_AVERAGE);
-		this.showedMessageTime = 0;
 
 		if (Robot.ROBOT_TYPE.isSimulation()) {
 			CMDHandler.runPythonClass(Path.of("BatteryMessage"), IPs.SIMULATION_IP);
@@ -37,12 +31,7 @@ class BatteryLimiter extends Command {
 	private void showBatteryMessage() {
 		if (!isBatteryLow.get()) {
 			isBatteryLow.set(true);
-			showedMessageTime = Timer.getFPGATimestamp();
 		}
-	}
-
-	private boolean passedMessageStartUpTime() {
-		return Timer.getFPGATimestamp() - showedMessageTime > MESSAGE_STARTUP_TIME;
 	}
 
 	private static void reportLowBattery() {

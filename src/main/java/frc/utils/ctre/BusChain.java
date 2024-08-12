f0cef45ea65a9b1a63db6974a0664268de2384f7
Dror Talmi <dror.gg.talmi@gmail.com>
2024-08-13 00:25:26 +0300

    better?

--- src/main/java/frc/utils/ctre/BusChain.java
+++ src/main/java/frc/utils/ctre/BusChain.java
@@ -36,36 +36,35 @@ public enum BusChain {
 	}
 
 	public void logStatus() {
-		String currentLogPath = logPath;
-		Logger.recordOutput(currentLogPath + "/Status", busStatus.Status.getName());
-		Logger.recordOutput(currentLogPath + "/Utilization", busStatus.BusUtilization);
-		Logger.recordOutput(currentLogPath + "/TimesDisconnected", busStatus.BusOffCount);
-		Logger.recordOutput(currentLogPath + "/Full", busStatus.TxFullCount);
-		Logger.recordOutput(currentLogPath + "/ReceiveError", busStatus.REC);
-		Logger.recordOutput(currentLogPath + "/TransmitError", busStatus.TEC);
+		Logger.recordOutput(logPath + "/Status", busStatus.Status.getName());
+		Logger.recordOutput(logPath + "/Utilization", busStatus.BusUtilization);
+		Logger.recordOutput(logPath + "/TimesDisconnected", busStatus.BusOffCount);
+		Logger.recordOutput(logPath + "/Full", busStatus.TxFullCount);
+		Logger.recordOutput(logPath + "/ReceiveError", busStatus.REC);
+		Logger.recordOutput(logPath + "/TransmitError", busStatus.TEC);
 	}
 
 	public void reportAlerts() {
-		String currentAlertLogPath = LogPaths.ALERT_LOG_PATH + LOG_PATH + getChainName();
+		String alertLogPath = LogPaths.ALERT_LOG_PATH + logPath;
 		double currentTime = Timer.getFPGATimestamp();
 
 		if (!busStatus.Status.isOK()) {
-			Logger.recordOutput(currentAlertLogPath + "/StatusErrorAt", currentTime);
+			Logger.recordOutput(alertLogPath + "/StatusErrorAt", currentTime);
 		}
 		if (busStatus.BusUtilization > MAX_CAN_UTILIZATION_PERCENT) {
-			Logger.recordOutput(currentAlertLogPath + "/FloodedAt", currentTime);
+			Logger.recordOutput(alertLogPath + "/FloodedAt", currentTime);
 		}
 		if (busStatus.BusOffCount > 0) {
-			Logger.recordOutput(currentAlertLogPath + "/DisconnectedAt", currentTime);
+			Logger.recordOutput(alertLogPath + "/DisconnectedAt", currentTime);
 		}
 		if (busStatus.TxFullCount > 0) {
-			Logger.recordOutput(currentAlertLogPath + "/FullAt", currentTime);
+			Logger.recordOutput(alertLogPath + "/FullAt", currentTime);
 		}
 		if (busStatus.REC > MAX_RECEIVE_ERRORS) {
-			Logger.recordOutput(currentAlertLogPath + "/ReceiveErrorAt", currentTime);
+			Logger.recordOutput(alertLogPath + "/ReceiveErrorAt", currentTime);
 		}
 		if (busStatus.TEC > MAX_TRANSMIT_ERRORS) {
-			Logger.recordOutput(currentAlertLogPath + "/TransmitErrorsAt", currentTime);
+			Logger.recordOutput(alertLogPath + "/TransmitErrorsAt", currentTime);
 		}
 	}
 

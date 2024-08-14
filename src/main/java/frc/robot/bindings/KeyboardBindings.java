1393c31a79ab7eaea4987b36385609d42a810ea5
greenblitz <greenblitz@gmail.com>
2024-08-14 16:46:36 +0300

    Yoni - removed prints

--- src/main/java/frc/robot/bindings/KeyboardBindings.java
+++ src/main/java/frc/robot/bindings/KeyboardBindings.java
@@ -1,6 +1,5 @@
 package frc.robot.bindings;
 
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import frc.utils.controllers.keyboard.KeyboardController;
 
 public class KeyboardBindings {
@@ -9,8 +8,6 @@ public class KeyboardBindings {
 
     public static void configureBindings() {
         KeyboardController usedKeyboard = KEYBOARD_CONTROLLER;
-        usedKeyboard.A.whileTrue(new PrintCommand("A is pressed"));
-        usedKeyboard.B.whileTrue(new PrintCommand("B is pressed"));
     }
 
 }

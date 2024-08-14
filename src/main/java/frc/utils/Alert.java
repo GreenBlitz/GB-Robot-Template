5f7eca0d7999efa5c2c645b1bd4d1294f6d14449
hillelv <hillel.vider@gmail.com>
2024-08-14 16:32:40 +0300

    danna - yoav and romys cr

--- src/main/java/frc/utils/Alert.java
+++ src/main/java/frc/utils/Alert.java
@@ -1,5 +1,6 @@
 package frc.utils;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.constants.LogPaths;
 import org.littletonrobotics.junction.Logger;
@@ -13,18 +14,19 @@ public class Alert {
 
     private final AlertType type;
     private final String name;
+	private final String logKey;
+	private final String logKeyAndTime;
 
-    public Alert(AlertType alertType, String alertName){
-        this.type = alertType;
-        this.name = alertName;
+    public Alert(AlertType type, String name) {
+        this.type = type;
+        this.name = name;
+		logKey = LogPaths.ALERT_LOG_PATH + type.toString() + "/" + name;
+		logKeyAndTime = logKey + " at time: " + Timer.getFPGATimestamp();
     }
 
-    public void logAlert(){
-        Logger.recordOutput(LogPaths.ALERT_LOG_PATH +type.toString()+"/"+name, Timer.getFPGATimestamp());
-    }
-
-    public void logAlert(GBSubsystem subsystem){
-        Logger.recordOutput(LogPaths.ALERT_LOG_PATH+subsystem.toString()+"/"+type.toString()+"/"+name, Timer.getFPGATimestamp());
+    public void logAlert() {
+        Logger.recordOutput(logKey, Timer.getFPGATimestamp());
+		DriverStation.reportWarning(logKeyAndTime, false);
     }
 
 }

06844264a90ed67a2723f53014891da716a50d2d
hillelv <hillel.vider@gmail.com>
2024-08-14 17:20:39 +0300

    danna - spotless

--- src/main/java/frc/utils/Alert.java
+++ src/main/java/frc/utils/Alert.java
@@ -7,26 +7,26 @@ import org.littletonrobotics.junction.Logger;
 
 public class Alert {
 
-    public enum AlertType {
-        ERROR,
-        WARNING;
-    }
+	public enum AlertType {
+		ERROR,
+		WARNING;
+	}
 
-    private final AlertType type;
-    private final String name;
+	private final AlertType type;
+	private final String name;
 	private final String logPath;
 	private final String logPathAndTime;
 
-    public Alert(AlertType type, String name) {
-        this.type = type;
-        this.name = name;
+	public Alert(AlertType type, String name) {
+		this.type = type;
+		this.name = name;
 		this.logPath = LogPaths.ALERT_LOG_PATH + type.toString() + "/" + name;
 		this.logPathAndTime = logPath + " at time: " + Timer.getFPGATimestamp();
-    }
+	}
 
-    public void logAlert() {
-        Logger.recordOutput(logPath, Timer.getFPGATimestamp());
+	public void logAlert() {
+		Logger.recordOutput(logPath, Timer.getFPGATimestamp());
 		DriverStation.reportWarning(logPathAndTime, false);
-    }
+	}
 
 }

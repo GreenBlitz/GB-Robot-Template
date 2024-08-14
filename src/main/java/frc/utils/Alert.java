1fa0f6f0df1e9178c37d8a48efb837b2d10024b1
hillelv <hillel.vider@gmail.com>
2024-08-14 16:51:38 +0300

    danna - yoni cr

--- src/main/java/frc/utils/Alert.java
+++ src/main/java/frc/utils/Alert.java
@@ -14,19 +14,19 @@ public class Alert {
 
     private final AlertType type;
     private final String name;
-	private final String logKey;
-	private final String logKeyAndTime;
+	private final String logPath;
+	private final String logPathAndTime;
 
     public Alert(AlertType type, String name) {
         this.type = type;
         this.name = name;
-		logKey = LogPaths.ALERT_LOG_PATH + type.toString() + "/" + name;
-		logKeyAndTime = logKey + " at time: " + Timer.getFPGATimestamp();
+		this.logPath = LogPaths.ALERT_LOG_PATH + type.toString() + "/" + name;
+		this.logPathAndTime = logPath + " at time: " + Timer.getFPGATimestamp();
     }
 
     public void logAlert() {
-        Logger.recordOutput(logKey, Timer.getFPGATimestamp());
-		DriverStation.reportWarning(logKeyAndTime, false);
+        Logger.recordOutput(logPath, Timer.getFPGATimestamp());
+		DriverStation.reportWarning(logPathAndTime, false);
     }
 
 }

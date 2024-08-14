bc4dbb1a31a7778216537dc0ad0173767b25476c
GreenBlitz <greenblitz4590@gmail.com>
2024-08-14 18:56:25 +0300

    Yoni - start

--- src/main/java/frc/robot/ComputerMain.java
+++ src/main/java/frc/robot/ComputerMain.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import frc.utils.CMDHandler;
+
 /**
  * Unless you know what you are doing, do not rename this file because it's being used elsewhere.
  */
@@ -11,6 +13,7 @@ public class ComputerMain {
 
 	private static void startComputerPrograms() {
 		// start programs on computer...
+		CMDHandler.runCMDCommand("echo sus");
 	}
 
 }

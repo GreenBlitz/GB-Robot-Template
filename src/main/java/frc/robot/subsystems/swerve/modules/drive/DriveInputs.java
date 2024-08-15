f6b540e72e264cf77cff5e0a6b630f8804b9bbbc
YHerm <0549405422.yh@gmail.com>
2024-08-15 16:47:50 +0300

    format

--- src/main/java/frc/robot/subsystems/swerve/modules/drive/DriveInputs.java
+++ src/main/java/frc/robot/subsystems/swerve/modules/drive/DriveInputs.java
@@ -1,8 +1,6 @@
 package frc.robot.subsystems.swerve.modules.drive;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.utils.calibration.sysid.SysIdCalibrator;
 import org.littletonrobotics.junction.AutoLog;
 
 @AutoLog

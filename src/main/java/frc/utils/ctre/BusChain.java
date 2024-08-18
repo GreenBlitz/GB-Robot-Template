190ebf83a419b422199a70ed3bdab99b4f3ca3de
greenblitz <greenblitz@gmail.com>
2024-08-18 17:57:06 +0300

    dror - finished romi's cr

--- src/main/java/frc/utils/ctre/BusChain.java
+++ src/main/java/frc/utils/ctre/BusChain.java
@@ -4,20 +4,21 @@ import com.ctre.phoenix6.CANBus;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.constants.LogPaths;
 import org.littletonrobotics.junction.Logger;
+import com.ctre.phoenix6.CANBus.CANBusStatus;
 
 public enum BusChain {
 
 	ROBORIO("rio"),
 	CANIVORE("CANivore");
 
-	private static final double MAX_CAN_UTILIZATION_PERCENT = 0.6;
+	private static final double MAX_CAN_UTILIZATION_DECIMAL_PERCENT = 0.6;
 	private static final double MAX_RECEIVE_ERRORS = 0;
 	private static final double MAX_TRANSMIT_ERRORS = 0;
 	private static final String LOG_PATH_PREFIX = "Bus/";
 
 	private final String chainName;
 	private final String logPath;
-	private CANBus.CANBusStatus busStatus;
+	private CANBusStatus busStatus;
 
 	BusChain(String chainName) {
 		this.chainName = chainName;
@@ -51,7 +52,7 @@ public enum BusChain {
 		if (!busStatus.Status.isOK()) {
 			Logger.recordOutput(alertLogPath + "StatusErrorAt", currentTime);
 		}
-		if (busStatus.BusUtilization > MAX_CAN_UTILIZATION_PERCENT) {
+		if (busStatus.BusUtilization > MAX_CAN_UTILIZATION_DECIMAL_PERCENT) {
 			Logger.recordOutput(alertLogPath + "FloodedAt", currentTime);
 		}
 		if (busStatus.BusOffCount > 0) {

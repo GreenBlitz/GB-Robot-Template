b0b833a5590d5c7ca59d6ebad2aae19b2272cad8
RJ <ranelj2008@gmail.com>
2024-08-21 19:12:33 +0300

    Ran - added the visualizer

---
+++ src/main/java/frc/utils/ObjectVisualizer/Translation3DRan.java
@@ -0,0 +1,9 @@
+package frc.utils.ObjectVisualizer;
+
+import edu.wpi.first.math.geometry.Translation3d;
+
+public class Translation3DRan extends Translation3d {
+    public Translation3DRan(double... location){
+        super(location[0],location[1],location[2]);
+    }
+}
---
+++ src/main/java/frc/utils/ObjectVisualizer/Visualizer.java
@@ -0,0 +1,29 @@
+package frc.utils.ObjectVisualizer;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N3;
+
+public class Visualizer {
+    Vector<N3> acceleration;
+    Vector<N3> velocity;
+    Vector<N3> location;
+
+    public Visualizer(Vector<N3> acceleration, Vector<N3> velocity){
+        this.acceleration = new Vector<N3>(acceleration);
+        this.velocity = new Vector<N3>(velocity);
+        this.location = VecBuilder.fill(0,0,0);
+    }
+    public Visualizer(Vector<N3> acceleration, Vector<N3> velocity, Vector<N3> location){
+        this.acceleration = new Vector<N3>(acceleration);
+        this.velocity = new Vector<N3>(velocity);
+        this.location = new Vector<N3>(location);
+    }
+    public Translation3d currentPosition(){
+        this.velocity.plus(acceleration.div(500));
+        this.location.plus(velocity.div(500));
+        return new Translation3DRan(location.getData());
+    }
+}

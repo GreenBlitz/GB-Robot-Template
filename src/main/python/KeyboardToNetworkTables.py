5b64b00a0bef9c4b151e216138de1ad17b5a7e1a
greenblitz <greenblitz@gmail.com>
2024-08-21 15:49:06 +0300

    Yoni & Shahar - fixed oopsi poopsi

--- src/main/python/KeyboardToNetworkTables.py
+++ src/main/python/KeyboardToNetworkTables.py
@@ -1,14 +1,14 @@
-from NetworkTableManager import NetworkTableInstance, NetworkTable, NetworkTableClient
-
 import sys
 import time
 from typing import Callable, TypeVar
 
 from pynput import keyboard
 
-SpecialKey = TypeVar('SpecialKey')
-ASCIIKey = TypeVar('ASCIIKey')
-UndefinedKey = TypeVar('UndefinedKey')
+from NetworkTableManager import NetworkTableInstance, NetworkTable, NetworkTableClient
+
+SpecialKey = TypeVar("SpecialKey")
+ASCIIKey = TypeVar("ASCIIKey")
+UndefinedKey = TypeVar("UndefinedKey")
 
 KEYBOARD_EVENT_CHECKING_COOLDOWN_SECONDS = 0.01
 KEYBOARD_KEYS_TABLE = "Keyboard/Keys"
@@ -21,19 +21,16 @@ def key_type_checker(key: object) -> TypeVar:
         return SpecialKey
     elif hasattr(key, "char"):
         return ASCIIKey
-
     return UndefinedKey
 
 
-def keys_handler(table: NetworkTableInstance, is_pressed: bool) -> Callable:
+def keys_event_functions_factory(table: NetworkTableInstance, is_pressed: bool) -> Callable:
     # key type is changing in runtime + depends on platform. Checked for Xorg keyboard layout.
     def update_table(key) -> None:
         if key_type_checker(key) is UndefinedKey:
             return
-
         elif key_type_checker(key) is SpecialKey:
             table.putBoolean(key.name, is_pressed)
-
         elif key_type_checker(key) is ASCIIKey:
             if key.char == "/":
                 table.putBoolean("slash", is_pressed)
@@ -44,17 +41,27 @@ def keys_handler(table: NetworkTableInstance, is_pressed: bool) -> Callable:
     return update_table
 
 
-def track_keyboard_until_client_disconnect(keys_table: NetworkTable, keyboard_client: NetworkTableClient):
-    listener = keyboard.Listener(
-        on_press=keys_handler(keys_table, True),
-        on_release=keys_handler(keys_table, False),
-    )
-    listener.start()
+def wait_until_client_disconnect(keyboard_client: NetworkTableClient):
     while True:
         if not keyboard_client.is_connected():
-            listener.stop()
             break
         time.sleep(KEYBOARD_EVENT_CHECKING_COOLDOWN_SECONDS)
+
+
+def listener_factory(keys_table: NetworkTable) -> keyboard.Listener:
+    return keyboard.Listener(
+        on_press=keys_event_functions_factory(keys_table, True),
+        on_release=keys_event_functions_factory(keys_table, False),
+    )
+
+
+def track_keyboard_until_client_disconnect(
+    keys_table: NetworkTable, keyboard_client: NetworkTableClient
+):
+    keyboard_listener = listener_factory(keys_table)
+    keyboard_listener.start()
+    wait_until_client_disconnect(keyboard_client)
+    keyboard_listener.stop()
 
 
 def run_keyboard_tracking_client():

908c2e8a58912a87bd96a0661f98229d03f44bcf
Dror Talmi <dror.gg.talmi@gmail.com>
2024-08-24 21:47:48 +0300

    spotless yahoo

--- src/main/java/frc/utils/breakState/breakStateManager.java
+++ src/main/java/frc/utils/breakState/breakStateManager.java
@@ -6,21 +6,22 @@ import java.util.ArrayList;
 
 public class breakStateManager {
 
-    private static ArrayList<GBSubsystem> subsystemsList = new ArrayList<>();
+	private static ArrayList<GBSubsystem> subsystemsList = new ArrayList<>();
 
-    public static void addSubsystem(GBSubsystem subsystem){
-        subsystemsList.add(subsystem);
-    }
+	public static void addSubsystem(GBSubsystem subsystem) {
+		subsystemsList.add(subsystem);
+	}
 
-    public static void breakState(){
-        for (GBSubsystem subsystem : subsystemsList) {
-            subsystem.setBreak();
-        }
-    }
+	public static void breakState() {
+		for (GBSubsystem subsystem : subsystemsList) {
+			subsystem.setBreak();
+		}
+	}
 
-    public static void coastState(){
-        for (GBSubsystem subsystem : subsystemsList) {
-            subsystem.setCoast();
-        }
-    }
+	public static void coastState() {
+		for (GBSubsystem subsystem : subsystemsList) {
+			subsystem.setCoast();
+		}
+	}
+
 }

a3d9a314a3a5e19bc8cfa4dcb9d94438b2e73808
GilStein1 <gils.server@gmail.com>
2024-08-25 18:34:23 +0300

    Gil - renamed class and even used add before commit

--- src/main/java/frc/utils/LoopingUtils.java
+++ src/main/java/frc/utils/LoopingUtils.java
@@ -2,7 +2,7 @@ package frc.utils;
 
 import java.util.function.Consumer;
 
-public class GBLooper {
+public class LoopingUtils {
 
 	public static void loop(Consumer<Integer> loop, int iterations) {
 		for (int index = 0; index < iterations; index++) {

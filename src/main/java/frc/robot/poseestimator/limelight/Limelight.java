07ffc4682f6b84d49a3ee557d987af10bff086ab
Shahar Perets <perets.shahar@gmail.com>
2024-08-25 16:03:55 +0300

    Shahar: copy psated from Crrrrresendo

---
+++ src/main/java/frc/robot/poseestimator/limelight/Limelight.java
@@ -0,0 +1,65 @@
+package edu.greenblitz.robotName.subsystems.limelight;
+
+import edu.greenblitz.robotName.VisionConstants;
+import edu.greenblitz.robotName.utils.AllianceUtilities;
+import edu.greenblitz.robotName.utils.GBSubsystem;
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Timer;
+
+import java.util.Optional;
+
+public class Limelight extends GBSubsystem {
+
+	private NetworkTableEntry robotPoseEntry, idEntry, tagPoseEntry;
+
+	private String name;
+
+	public Limelight(String limelightName) {
+		this.name = limelightName;
+		String robotPoseQuery =  "botpose_wpiblue";
+		robotPoseEntry = NetworkTableInstance.getDefault().getTable(name).getEntry(robotPoseQuery);
+		tagPoseEntry = NetworkTableInstance.getDefault().getTable(name).getEntry("targetpose_cameraspace");
+		idEntry = NetworkTableInstance.getDefault().getTable(name).getEntry("tid");
+	}
+
+	public Optional<Pair<Pose2d, Double>> getUpdatedPose2DEstimation() {
+		double[] poseArray = robotPoseEntry.getDoubleArray(new double[VisionConstants.LIMELIGHT_ENTRY_ARRAY_LENGTH]);
+		double processingLatency = poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.TOTAL_LATENCY)] / 1000;
+		double timestamp = Timer.getFPGATimestamp() - processingLatency;
+		int id = (int) idEntry.getInteger(-1);
+		double angleOffset = AllianceUtilities.isBlueAlliance() ? 0 : 180;
+		if (id == -1) {
+			return Optional.empty();
+		}
+		Pose2d robotPose = new Pose2d(
+				poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.X_AXIS)],
+				poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.Y_AXIS)],
+				Rotation2d.fromDegrees(poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.PITCH_ANGLE)] - angleOffset)
+		);
+		return Optional.of(new Pair<>(robotPose, timestamp));
+	}
+
+	public double getTagHeight() {
+		double[] poseArray = tagPoseEntry.getDoubleArray(new double[VisionConstants.LIMELIGHT_ENTRY_ARRAY_LENGTH]);
+		return poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.Y_AXIS)];
+	}
+
+	public boolean getTagConfidence() {
+		boolean tagHeightConfidence = getTagHeight() < VisionConstants.APRIL_TAG_HEIGHT_METERS + VisionConstants.APRIL_TAG_HEIGHT_TOLERANCE_METERS
+				|| getTagHeight() > VisionConstants.APRIL_TAG_HEIGHT_METERS - VisionConstants.APRIL_TAG_HEIGHT_TOLERANCE_METERS;
+		return tagHeightConfidence;
+	}
+
+	public double getDistanceFromTag() {
+		double[] poseArray = tagPoseEntry.getDoubleArray(new double[VisionConstants.LIMELIGHT_ENTRY_ARRAY_LENGTH]);
+		return poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.Z_AXIS)];
+	}
+
+	public boolean hasTarget() {
+		return getUpdatedPose2DEstimation().isPresent();
+	}
+}
---
+++ src/main/java/frc/robot/poseestimator/limelight/MultiLimelight.java
@@ -0,0 +1,77 @@
+package edu.greenblitz.robotName.subsystems.limelight;
+
+import edu.greenblitz.robotName.VisionConstants;
+import edu.greenblitz.robotName.utils.GBSubsystem;
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Pose2d;
+import org.littletonrobotics.junction.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class MultiLimelight extends GBSubsystem {
+
+	private static MultiLimelight instance;
+
+	private List<Limelight> limelights;
+
+	private MultiLimelight() {
+		limelights = new ArrayList<>();
+		for (String limelightName : VisionConstants.LIMELIGHT_NAMES) {
+			limelights.add(new Limelight(limelightName));
+		}
+	}
+
+	public static void init() {
+		instance = new MultiLimelight();
+	}
+
+	public static MultiLimelight getInstance() {
+		if (instance == null) {
+			init();
+		}
+		return instance;
+	}
+
+	public List<Optional<Pair<Pose2d, Double>>> getAll2DEstimates() {
+		ArrayList<Optional<Pair<Pose2d, Double>>> estimates = new ArrayList<>();
+		for (Limelight limelight : limelights) {
+			if (limelight.hasTarget()) {
+				if (limelight.getTagConfidence())
+					estimates.add(limelight.getUpdatedPose2DEstimation());
+			}
+		}
+		return estimates;
+	}
+
+	public void recordEstimatedPositions() {
+		int i = 0;
+		for (Optional<Pair<Pose2d, Double>> estimation : MultiLimelight.getInstance().getAll2DEstimates()) {
+			if (estimation.isPresent()) {
+				Logger.recordOutput("estimation " + i, estimation.get().getFirst());
+			}
+		}
+	}
+
+	public double getDynamicStdDevs(int limelightId) {
+		return limelights.get(limelightId).getDistanceFromTag() / VisionConstants.VISION_TO_STANDARD_DEVIATION;
+	}
+
+	public boolean hasTarget(int limelightId) {
+		return limelights.get(limelightId).hasTarget();
+	}
+
+	public Optional<Pair<Pose2d, Double>> getFirstAvailableTarget() {
+		for (Optional<Pair<Pose2d, Double>> output : getAll2DEstimates()) {
+			if (output.isPresent()) {
+				return output;
+			}
+		}
+		return Optional.empty();
+	}
+
+	public boolean isConnected() {
+		return limelights.get(0).hasTarget();
+	}
+}

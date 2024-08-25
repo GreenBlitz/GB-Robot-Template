8d674ef743190c6d7528acecc6881da4f8e033ee
YHerm <0549405422.yh@gmail.com>
2024-08-25 21:13:58 +0300

    foramt

--- src/main/java/frc/robot/hardware/CloseLoopControl.java
+++ src/main/java/frc/robot/hardware/CloseLoopControl.java
@@ -3,6 +3,4 @@ package frc.robot.hardware;
 import com.ctre.phoenix6.controls.ControlRequest;
 import edu.wpi.first.math.geometry.Rotation2d;
 
-
 public record CloseLoopControl(ControlRequest controlRequest, int pidSlot, Rotation2d targetSetPoint, ControlState controlState) {}
-

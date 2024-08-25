3f780d4372268d39b49e051e83c0c7fa18d94e5e
Shahar Perets <perets.shahar@gmail.com>
2024-08-25 16:46:01 +0300

    Shahar: spotless"

--- src/main/java/frc/robot/poseestimator/limelight/Limelight.java
+++ src/main/java/frc/robot/poseestimator/limelight/Limelight.java
@@ -12,59 +12,59 @@ import java.util.Optional;
 
 public class Limelight extends GBSubsystem {
 
-	private NetworkTableEntry robotPoseEntry, idEntry, tagPoseEntry;
+    private NetworkTableEntry robotPoseEntry, idEntry, tagPoseEntry;
 
-	private String name;
+    private String name;
 
-	public Limelight(String limelightName) {
+    public Limelight(String limelightName) {
         super("LimeLight " + limelightName + "\\");
-		this.name = limelightName;
-		String robotPoseQuery = "botpose_wpiblue";
-		robotPoseEntry = NetworkTableInstance.getDefault().getTable(name).getEntry(robotPoseQuery);
-		tagPoseEntry = NetworkTableInstance.getDefault().getTable(name).getEntry("targetpose_cameraspace");
-		idEntry = NetworkTableInstance.getDefault().getTable(name).getEntry("tid");
-	}
+        this.name = limelightName;
+        String robotPoseQuery = "botpose_wpiblue";
+        robotPoseEntry = NetworkTableInstance.getDefault().getTable(name).getEntry(robotPoseQuery);
+        tagPoseEntry = NetworkTableInstance.getDefault().getTable(name).getEntry("targetpose_cameraspace");
+        idEntry = NetworkTableInstance.getDefault().getTable(name).getEntry("tid");
+    }
 
     public Optional<Pair<Pose2d, Double>> getUpdatedPose2DEstimation() {
-		double[] poseArray = robotPoseEntry.getDoubleArray(new double[VisionConstants.LIMELIGHT_ENTRY_ARRAY_LENGTH]);
-		double processingLatencySeconds = poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.TOTAL_LATENCY)] / 1000;
-		double timestamp = Timer.getFPGATimestamp() - processingLatencySeconds;
-		int id = (int) idEntry.getInteger(-1);
+        double[] poseArray = robotPoseEntry.getDoubleArray(new double[VisionConstants.LIMELIGHT_ENTRY_ARRAY_LENGTH]);
+        double processingLatencySeconds = poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.TOTAL_LATENCY)] / 1000;
+        double timestamp = Timer.getFPGATimestamp() - processingLatencySeconds;
+        int id = (int) idEntry.getInteger(-1);
 
-		double angleOffset = AllianceUtilities.isBlueAlliance() ? 0 : 180;
+        Rotation2d angleOffset = AllianceUtilities.isBlueAlliance() ? Rotation2d.fromDegrees(0) : Rotation2d.fromDegrees(180);
 
-		if (id == -1) {
-			return Optional.empty();
-		}
+        if (id == -1) {
+            return Optional.empty();
+        }
 
-		Pose2d robotPose = new Pose2d(
-				poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.X_AXIS)],
-				poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.Y_AXIS)],
-				Rotation2d.fromDegrees(poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.PITCH_ANGLE)] - angleOffset)
-		);
+        Pose2d robotPose = new Pose2d(
+                poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.X_AXIS)],
+                poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.Y_AXIS)],
+                Rotation2d.fromDegrees(poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.PITCH_ANGLE)] - angleOffset.getDegrees())
+        );
 
-		return Optional.of(new Pair<>(robotPose, timestamp));
-	}
+        return Optional.of(new Pair<>(robotPose, timestamp));
+    }
 
-	public double getAprilTagHeight() {
-		double[] poseArray = tagPoseEntry.getDoubleArray(new double[VisionConstants.LIMELIGHT_ENTRY_ARRAY_LENGTH]);
-		return poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.Y_AXIS)];
-	}
+    public double getAprilTagHeight() {
+        double[] poseArray = tagPoseEntry.getDoubleArray(new double[VisionConstants.LIMELIGHT_ENTRY_ARRAY_LENGTH]);
+        return poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.Y_AXIS)];
+    }
 
-	public boolean getAprilTagConfidence() {
-		boolean tagHeightConfidence = getAprilTagHeight() < VisionConstants.APRIL_TAG_HEIGHT_METERS + VisionConstants.APRIL_TAG_HEIGHT_TOLERANCE_METERS
-				|| getAprilTagHeight() > VisionConstants.APRIL_TAG_HEIGHT_METERS - VisionConstants.APRIL_TAG_HEIGHT_TOLERANCE_METERS;
-		return tagHeightConfidence;
-	}
+    public boolean getAprilTagConfidence() {
+        boolean aprilTagHeightConfidence = getAprilTagHeight() < VisionConstants.APRIL_TAG_HEIGHT_METERS + VisionConstants.APRIL_TAG_HEIGHT_TOLERANCE_METERS
+                || getAprilTagHeight() > VisionConstants.APRIL_TAG_HEIGHT_METERS - VisionConstants.APRIL_TAG_HEIGHT_TOLERANCE_METERS;
+        return aprilTagHeightConfidence;
+    }
 
-	public double getDistanceFromTag() {
-		double[] poseArray = tagPoseEntry.getDoubleArray(new double[VisionConstants.LIMELIGHT_ENTRY_ARRAY_LENGTH]);
-		return poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.Z_AXIS)];
-	}
+    public double getDistanceFromAprilTag() {
+        double[] poseArray = tagPoseEntry.getDoubleArray(new double[VisionConstants.LIMELIGHT_ENTRY_ARRAY_LENGTH]);
+        return poseArray[VisionConstants.getValue(VisionConstants.LIMELIGHT_ARRAY_VALUES.Z_AXIS)];
+    }
 
-	public boolean hasTarget() {
-		return getUpdatedPose2DEstimation().isPresent();
-	}
+    public boolean hasTarget() {
+        return getUpdatedPose2DEstimation().isPresent();
+    }
 
     @Override
     protected void subsystemPeriodic() {
--- src/main/java/frc/robot/poseestimator/limelight/MultiLimelight.java
+++ src/main/java/frc/robot/poseestimator/limelight/MultiLimelight.java
@@ -12,69 +12,69 @@ import java.util.Optional;
 
 public class MultiLimelight extends GBSubsystem {
 
-	private static MultiLimelight instance;
+    private static MultiLimelight instance;
 
-	private List<Limelight> limelights;
+    private List<Limelight> limelights;
 
-	private MultiLimelight() {
+    private MultiLimelight() {
         super("MultiLimeLight/");
         limelights = new ArrayList<>();
-		for (String limelightName : VisionConstants.LIMELIGHT_NAMES) {
-			limelights.add(new Limelight(limelightName));
-		}
-	}
+        for (String limelightName : VisionConstants.LIMELIGHT_NAMES) {
+            limelights.add(new Limelight(limelightName));
+        }
+    }
 
-	public static void init() {
-		instance = new MultiLimelight();
-	}
+    public static void init() {
+        instance = new MultiLimelight();
+    }
 
-	public static MultiLimelight getInstance() {
-		if (instance == null) {
-			init();
-		}
-		return instance;
-	}
+    public static MultiLimelight getInstance() {
+        if (instance == null) {
+            init();
+        }
+        return instance;
+    }
 
-	public List<Optional<Pair<Pose2d, Double>>> getAll2DEstimates() {
-		ArrayList<Optional<Pair<Pose2d, Double>>> estimates = new ArrayList<>();
-		for (Limelight limelight : limelights) {
-			if (limelight.hasTarget()) {
-				if (limelight.getAprilTagConfidence())
-					estimates.add(limelight.getUpdatedPose2DEstimation());
-			}
-		}
-		return estimates;
-	}
+    public List<Optional<Pair<Pose2d, Double>>> getAll2DEstimates() {
+        ArrayList<Optional<Pair<Pose2d, Double>>> estimates = new ArrayList<>();
+        for (Limelight limelight : limelights) {
+            if (limelight.hasTarget()) {
+                if (limelight.getAprilTagConfidence())
+                    estimates.add(limelight.getUpdatedPose2DEstimation());
+            }
+        }
+        return estimates;
+    }
 
-	public void recordEstimatedPositions() {
-		int i = 0;
-		for (Optional<Pair<Pose2d, Double>> estimation : MultiLimelight.getInstance().getAll2DEstimates()) {
-			if (estimation.isPresent()) {
-				Logger.recordOutput(super.getLogPath() + "estimation " + i, estimation.get().getFirst());
-			}
-		}
-	}
+    public void recordEstimatedPositions() {
+        int i = 0;
+        for (Optional<Pair<Pose2d, Double>> estimation : MultiLimelight.getInstance().getAll2DEstimates()) {
+            if (estimation.isPresent()) {
+                Logger.recordOutput(super.getLogPath() + "estimation " + i, estimation.get().getFirst());
+            }
+        }
+    }
 
-	public double getDynamicStdDevs(int limelightId) {
-		return limelights.get(limelightId).getDistanceFromTag() / VisionConstants.VISION_TO_STANDARD_DEVIATION;
-	}
+    public double getDynamicStdDevs(int limelightId) {
+        return limelights.get(limelightId).getDistanceFromAprilTag() / VisionConstants.VISION_TO_STANDARD_DEVIATION;
+    }
 
-	public boolean hasTarget(int limelightId) {
-		return limelights.get(limelightId).hasTarget();
-	}
+    public boolean hasTarget(int limelightId) {
+        return limelights.get(limelightId).hasTarget();
+    }
 
-	public Optional<Pair<Pose2d, Double>> getFirstAvailableTarget() {
-		for (Optional<Pair<Pose2d, Double>> output : getAll2DEstimates()) {
-			if (output.isPresent()) {
-				return output;
-			}
-		}
-		return Optional.empty();
-	}
+    public Optional<Pair<Pose2d, Double>> getFirstAvailableTarget() {
+        for (Optional<Pair<Pose2d, Double>> output : getAll2DEstimates()) {
+            if (output.isPresent()) {
+                return output;
+            }
+        }
+        return Optional.empty();
+    }
 
-	public boolean isConnected() {
-		return limelights.get(0).hasTarget();
-	}
+    public boolean isConnected() {
+        return limelights.get(0).hasTarget();
+    }
 
     @Override
     protected void subsystemPeriodic() {

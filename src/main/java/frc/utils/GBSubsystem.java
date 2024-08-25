82a959ade96065a7ef3ef72ea2fecab214f550fc
Dror Talmi <dror.gg.talmi@gmail.com>
2024-08-25 15:44:22 +0300

    removed unused import

--- src/main/java/frc/utils/GBSubsystem.java
+++ src/main/java/frc/utils/GBSubsystem.java
@@ -3,7 +3,6 @@ package frc.utils;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
-import frc.utils.brakestate.BrakeStateManager;
 
 public abstract class GBSubsystem extends SubsystemBase {
 

89600db4c5e523d22b975caa3e00de68ae519551
Nitay4 <nitay@rotbart.co.il>
2024-08-25 18:54:40 +0300

    Nitay - added consumerBeamBreaker

---
+++ src/main/java/frc/utils/beamBreaker/consumerBeamBreaker/ConsumerBeamBreaker.java
@@ -0,0 +1,25 @@
+package frc.utils.beamBreaker.consumerBeamBreaker;
+
+import frc.utils.beamBreaker.BeamBreakerInputsAutoLogged;
+import frc.utils.beamBreaker.IBeamBreaker;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class ConsumerBeamBreaker implements IBeamBreaker {
+
+    Supplier<Boolean> isObstructedConsumer;
+
+    public ConsumerBeamBreaker(Supplier<Boolean> isObstructedConsumer){
+        this.isObstructedConsumer=isObstructedConsumer;
+    }
+
+    @Override
+    public void updateInputs(BeamBreakerInputsAutoLogged inputs) {
+        inputs.isObstructed=isObstructedConsumer.get();
+    }
+
+
+
+
+}

f21bf8a72c2b764b7500e11cf54a1065f278bba3
hillelv <hillel.vider@gmail.com>
2024-08-26 17:23:17 +0300

    hillel - ill be back from helping gil

--- src/main/java/frc/utils/auto/PathPlannerUtils.java
+++ src/main/java/frc/utils/auto/PathPlannerUtils.java
@@ -7,6 +7,7 @@ import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.pathfinding.LocalADStar;
 import com.pathplanner.lib.pathfinding.Pathfinding;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import edu.wpi.first.math.Pair;
@@ -28,12 +29,12 @@ public class PathPlannerUtils {
     private static List<Pair<Translation2d, Translation2d>> dynamicObstacles = List.of();
 
     public static void startPathfinder() {
-        setAdvantageKitCompatiblePathfinder();
+        setPathfinder();
         scheduleWarmup();
     }
 
-    private static void setAdvantageKitCompatiblePathfinder() {
-        Pathfinding.setPathfinder(new LocalADStarAK());
+    private static void setPathfinder() {
+        Pathfinding.setPathfinder(new LocalADStar());
     }
 
     private static void scheduleWarmup() {

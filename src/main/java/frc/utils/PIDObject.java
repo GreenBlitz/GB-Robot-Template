45e420602841d99ea74dc7f1b6a2bc2f2d9a78ab
Dror Talmi <dror.gg.talmi@gmail.com>
2024-08-29 21:41:13 +0300

    removed setters and most of constructors

--- src/main/java/frc/utils/PIDObject.java
+++ src/main/java/frc/utils/PIDObject.java
@@ -12,7 +12,7 @@ public class PIDObject {
 	private double maxPower;
 
 	public PIDObject() {
-		this(0);
+		this(0,0,0,0,0,0,0);
 	}
 
 	public PIDObject(double kp, double ki, double kd, double kff, double dFilter, double iZone, double maxPower) {
@@ -23,30 +23,6 @@ public class PIDObject {
 		this.dFilter = dFilter;
 		this.iZone = iZone;
 		this.maxPower = maxPower;
-	}
-
-	public PIDObject(double kp, double ki, double kd, double kff, double dFilter, double iZone) {
-		this(kp, ki, kd, kff, dFilter, iZone, 1);
-	}
-
-	public PIDObject(double kp, double ki, double kd, double kff, double iZone) {
-		this(kp, ki, kd, kff, 0, iZone);
-	}
-
-	public PIDObject(double kp, double ki, double kd, double kff) {
-		this(kp, ki, kd, kff, 0);
-	}
-
-	public PIDObject(double kp, double ki, double kd) {
-		this(kp, ki, kd, 0.0);
-	}
-
-	public PIDObject(double kp, double ki) {
-		this(kp, ki, 0.0);
-	}
-
-	public PIDObject(double kp) {
-		this(kp, 0.0);
 	}
 
 	public PIDObject(PIDObject other) {
@@ -63,85 +39,62 @@ public class PIDObject {
 		return kp;
 	}
 
-	public void setKp(double kp) {
-		this.kp = kp;
-	}
-
 	public double getKd() {
 		return kd;
-	}
-
-	public void setKd(double kd) {
-		this.kd = kd;
 	}
 
 	public double getKi() {
 		return ki;
 	}
 
-	public void setKi(double ki) {
-		this.ki = ki;
-	}
-
 	public double getKff() {
 		return ff;
-	}
-
-	public void setFF(double ff) {
-		this.ff = ff;
 	}
 
 	public double getIZone() {
 		return iZone;
 	}
 
-	public void setIZone(double iZone) {
-		this.iZone = iZone;
-	}
-
 	public double getMaxPower() {
 		return maxPower;
-	}
-
-	public void setMaxPower(double maxPower) {
-		this.maxPower = maxPower;
 	}
 
 	public double getDFilter() {
 		return dFilter;
 	}
 
-	public void setDFilter(double dFilter) {
-		this.dFilter = dFilter;
-	}
-
 	public PIDObject withKp(double kp) {
-		this.setKp(kp);
+		this.kp = kp;
 		return this;
 	}
 
 	public PIDObject withKi(double ki) {
-		this.setKi(ki);
+		this.ki = ki;
 		return this;
 	}
 
 	public PIDObject withKd(double kd) {
-		this.setKd(kd);
+		this.kd = kd;
 		return this;
 	}
 
 	public PIDObject withFF(double ff) {
-		this.setFF(ff);
+		this.ff = ff;
+		return this;
+	}
+
+	public PIDObject withDFilter(double dFilter){
+		this.dFilter = dFilter;
 		return this;
 	}
 
 	public PIDObject withIZone(double iZone) {
-		setIZone(iZone);
+		this.iZone = iZone;
 		return this;
 	}
 
 	public PIDObject withMaxPower(double maxPower) {
-		setMaxPower(maxPower);
+		this.maxPower = maxPower;
 		return this;
 	}
 

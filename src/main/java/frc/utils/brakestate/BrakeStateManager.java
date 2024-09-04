516d7ab12ef28352b3c438f79c91c8f95537b573
GreenBlitz <greenblitz4590@gmail.com>
2024-09-04 17:14:24 +0300

    MotorSubsystem, with the same function, the manager now gets two runnables, and has two lists that do basically the same as before..

--- src/main/java/frc/utils/brakestate/BrakeStateManager.java
+++ src/main/java/frc/utils/brakestate/BrakeStateManager.java
@@ -1,27 +1,28 @@
 package frc.utils.brakestate;
 
-import frc.utils.GBSubsystem;
 import frc.utils.MotorSubsystem;
 
 import java.util.ArrayList;
 
 public class BrakeStateManager {
 
-	public static final ArrayList<MotorSubsystem> subsystems = new ArrayList<>();
+	public static final ArrayList<Runnable> coastRunnables = new ArrayList<>();
+	public static final ArrayList<Runnable> brakeRunnables = new ArrayList<>();
 
-	public static void addSubsystem(MotorSubsystem subsystem) {
-		subsystems.add(subsystem);
+	public static void add(Runnable brake, Runnable coast) {
+		coastRunnables.add(coast);
+		brakeRunnables.add(brake);
 	}
 
 	public static void brake() {
-		for (MotorSubsystem subsystem : subsystems) {
-			subsystem.setBrake(true);
+		for (Runnable brakeRunnable : brakeRunnables) {
+			brakeRunnable.run();
 		}
 	}
 
 	public static void coast() {
-		for (MotorSubsystem subsystem : subsystems) {
-			subsystem.setBrake(false);
+		for (Runnable coastRunnable : coastRunnables) {
+			coastRunnable.run();
 		}
 	}
 

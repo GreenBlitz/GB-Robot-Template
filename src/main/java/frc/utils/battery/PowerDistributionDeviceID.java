a665ef945c875ae1daa298daca1fb3bcdb80104b
Dror Talmi <dror.gg.talmi@gmail.com>
2024-09-06 13:51:41 +0300

    spotless

--- src/main/java/frc/utils/battery/PowerDistributionDeviceID.java
+++ src/main/java/frc/utils/battery/PowerDistributionDeviceID.java
@@ -2,5 +2,4 @@ package frc.utils.battery;
 
 import edu.wpi.first.wpilibj.PowerDistribution;
 
-public record PowerDistributionDeviceID(int ID, PowerDistribution.ModuleType type) {
-}
+public record PowerDistributionDeviceID(int ID, PowerDistribution.ModuleType type) {}

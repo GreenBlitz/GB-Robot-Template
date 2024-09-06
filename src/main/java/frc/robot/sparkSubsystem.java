762a1af908c47acac1a32f7f201f4d40a5a57d15
Dror Talmi <dror.gg.talmi@gmail.com>
2024-09-06 19:11:05 +0300

    spotless

--- src/main/java/frc/robot/sparkSubsystem.java
+++ src/main/java/frc/robot/sparkSubsystem.java
@@ -4,23 +4,22 @@ import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import frc.utils.GBSubsystem;
-import frc.utils.brakestate.BrakeStateManager;
 
 public class sparkSubsystem extends GBSubsystem {
-    private final CANSparkMax motor;
 
-    public sparkSubsystem(String logPath, int id) {
-        super(logPath);
-        motor = new CANSparkMax(id, MotorType.kBrushless);
-    }
+	private final CANSparkMax motor;
 
-    public void setBrake(boolean brake) {
-        CANSparkBase.IdleMode x = brake ? CANSparkBase.IdleMode.kBrake : CANSparkBase.IdleMode.kCoast;
-        motor.setIdleMode(x);
-    }
+	public sparkSubsystem(String logPath, int id) {
+		super(logPath);
+		motor = new CANSparkMax(id, MotorType.kBrushless);
+	}
 
-    @Override
-    protected void subsystemPeriodic() {
+	public void setBrake(boolean brake) {
+		CANSparkBase.IdleMode x = brake ? CANSparkBase.IdleMode.kBrake : CANSparkBase.IdleMode.kCoast;
+		motor.setIdleMode(x);
+	}
 
-    }
+	@Override
+	protected void subsystemPeriodic() {}
+
 }
--- src/main/java/frc/robot/talonSubsystem.java
+++ src/main/java/frc/robot/talonSubsystem.java
@@ -1,29 +1,24 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.revrobotics.CANSparkBase;
 import frc.utils.GBSubsystem;
 import frc.utils.devicewrappers.TalonFXWrapper;
-import frc.utils.brakestate.BrakeStateManager;
 
 public class talonSubsystem extends GBSubsystem {
 
-    private final TalonFXWrapper motor;
+	private final TalonFXWrapper motor;
 
-    public talonSubsystem(String logPath, int id) {
-        super(logPath);
-        motor = new TalonFXWrapper(id);
+	public talonSubsystem(String logPath, int id) {
+		super(logPath);
+		motor = new TalonFXWrapper(id);
+	}
 
-    }
+	public void setBrake(boolean brake) {
+		NeutralModeValue x = brake ? NeutralModeValue.Brake : NeutralModeValue.Coast;
+		motor.setNeutralMode(x);
+	}
 
-    public void setBrake(boolean brake) {
-        NeutralModeValue x = brake ? NeutralModeValue.Brake : NeutralModeValue.Coast;
-        motor.setNeutralMode(x);
-    }
+	@Override
+	protected void subsystemPeriodic() {}
 
-    @Override
-    protected void subsystemPeriodic() {
-
-    }
 }

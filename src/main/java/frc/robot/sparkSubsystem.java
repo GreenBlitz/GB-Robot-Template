dd9a4e14ce3d6bc66c6846b1e524d248df92e3b7
Dror Talmi <dror.gg.talmi@gmail.com>
2024-09-06 18:52:49 +0300

    tester

---
+++ src/main/java/frc/robot/sparkSubsystem.java
@@ -0,0 +1,26 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import frc.utils.GBSubsystem;
+import frc.utils.brakestate.BrakeStateManager;
+
+public class sparkSubsystem extends GBSubsystem {
+    private final CANSparkMax motor;
+
+    public sparkSubsystem(String logPath, int id) {
+        super(logPath);
+        motor = new CANSparkMax(id, MotorType.kBrushless);
+    }
+
+    public void setBrake(boolean brake) {
+        CANSparkBase.IdleMode x = brake ? CANSparkBase.IdleMode.kBrake : CANSparkBase.IdleMode.kCoast;
+        motor.setIdleMode(x);
+    }
+
+    @Override
+    protected void subsystemPeriodic() {
+
+    }
+}

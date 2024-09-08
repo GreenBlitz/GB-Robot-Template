c1b21abc5eb551912f4910de8232c66a60fa7350
greenblitz <greenblitz@gmail.com>
2024-09-08 20:44:37 +0300

    format

--- src/main/java/frc/robot/hardware/signal/DoubleSignal.java
+++ src/main/java/frc/robot/hardware/signal/DoubleSignal.java
@@ -2,8 +2,8 @@ package frc.robot.hardware.signal;
 
 public abstract class DoubleSignal extends InputSignal<Double> {
 
-    protected DoubleSignal() {
-        super(0.0);
-    }
+	protected DoubleSignal() {
+		super(0.0);
+	}
 
 }

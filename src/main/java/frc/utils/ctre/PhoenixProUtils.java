f846ea46ae4159c1d31b2ab4f9ecd0fdcdf353f2
GilStein1 <gils.server@gmail.com>
2024-09-08 16:13:03 +0300

    spotless

--- src/main/java/frc/utils/ctre/PhoenixProUtils.java
+++ src/main/java/frc/utils/ctre/PhoenixProUtils.java
@@ -7,17 +7,17 @@ import java.util.function.Supplier;
 
 public class PhoenixProUtils {
 
-    public static <T> StatusSignal<T> getRefreshedSignal(boolean refresh, StatusSignal<T> signal) {
-        return refresh ? signal.refresh() : signal;
-    }
+	public static <T> StatusSignal<T> getRefreshedSignal(boolean refresh, StatusSignal<T> signal) {
+		return refresh ? signal.refresh() : signal;
+	}
 
-    public static StatusCode checkWithRetry(Supplier<StatusCode> statusCodeSupplier, int numberOfTries) {
-        for (int i = 0; i < numberOfTries - 1; i++) {
-            if (statusCodeSupplier.get().isOK()) {
-                return StatusCode.OK;
-            }
-        }
-        return statusCodeSupplier.get();
-    }
+	public static StatusCode checkWithRetry(Supplier<StatusCode> statusCodeSupplier, int numberOfTries) {
+		for (int i = 0; i < numberOfTries - 1; i++) {
+			if (statusCodeSupplier.get().isOK()) {
+				return StatusCode.OK;
+			}
+		}
+		return statusCodeSupplier.get();
+	}
 
 }

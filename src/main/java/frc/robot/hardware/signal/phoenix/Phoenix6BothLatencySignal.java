6d83d114b0fa12b435197d08e34c6fa92bbb428f
itamaroryan <itamar.oryan@gmail.com>
2024-09-11 20:25:04 +0300

    split files

---
+++ src/main/java/frc/robot/hardware/signal/phoenix/Phoenix6BothLatencySignal.java
@@ -0,0 +1,23 @@
+package frc.robot.hardware.signal.phoenix;
+
+
+import com.ctre.phoenix6.StatusSignal;
+import frc.utils.AngleUnit;
+
+public class Phoenix6BothLatencySignal extends Phoenix6LatencySignal {
+
+	protected Phoenix6BothLatencySignal(
+		String name,
+		StatusSignal<Double> signal,
+		StatusSignal<Double> slope,
+		AngleUnit angleUnit
+	) {
+		super(name, signal, slope, angleUnit);
+	}
+
+	public StatusSignal<Double> getSignalSlope() {
+		// For using refresh all with more signals...
+		return slopeSignal;
+	}
+
+}

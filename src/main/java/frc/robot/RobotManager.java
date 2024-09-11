9e18afa34a99a400087f9f9027fd4dc3136f0f70
Shahar Perets <perets.shahar@gmail.com>
2024-09-11 19:31:29 +0300

    Shahar: forgot to remove

--- src/main/java/frc/robot/RobotManager.java
+++ src/main/java/frc/robot/RobotManager.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.poseestimator.limelights.VisionObservationFiltered;
 import frc.robot.simulation.SimulationManager;
 import frc.utils.battery.BatteryUtils;
 import frc.utils.ctre.BusChain;
@@ -24,7 +23,6 @@ public class RobotManager extends LoggedRobot {
 	private Command autonomousCommand;
 
 	private Robot robot;
-	private VisionObservationFiltered test;
 
 	@Override
 	public void robotInit() {

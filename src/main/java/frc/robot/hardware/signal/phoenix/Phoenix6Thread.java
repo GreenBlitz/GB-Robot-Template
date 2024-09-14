7b140fb648c1e6f7d794df3cca8990ab0ec7ced6
YHerm <0549405422.yh@gmail.com>
2024-09-14 21:22:11 +0300

    daemon & lock

--- src/main/java/frc/robot/hardware/signal/phoenix/Phoenix6Thread.java
+++ src/main/java/frc/robot/hardware/signal/phoenix/Phoenix6Thread.java
@@ -1,16 +1,19 @@
 package frc.robot.hardware.signal.phoenix;
 
 import com.ctre.phoenix6.BaseStatusSignal;
-import edu.wpi.first.hal.HALUtil;
 import edu.wpi.first.wpilibj.Timer;
-import frc.utils.Conversions;
 import frc.utils.ctre.PhoenixProUtils;
+import frc.utils.cycletime.CycleTimeUtils;
+import org.littletonrobotics.junction.Logger;
 
 import java.util.ArrayList;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Phoenix6Thread extends Thread {
+
+	public static final ReentrantLock LOCK = new ReentrantLock();
 
 	private static Phoenix6Thread instance;
 
@@ -24,27 +27,33 @@ public class Phoenix6Thread extends Thread {
 	private static final int UPDATES_PER_ROBORIO_CYCLE = 20;
 	private static final int FREQUENCY = 250;
 	private static final int FREQUENCY_SET_RETRIES = 5;
-	private static final double WAIT_TIME_SECONDS = 0.01;
-	private static final double STARTING_DELAY_SECONDS = 0.01;
+	private static final double STARTING_DELAY_SECONDS = 5;
 
-	private final Queue<Double> timestamps = new ArrayBlockingQueue<>(UPDATES_PER_ROBORIO_CYCLE);
-	private final ArrayList<BaseStatusSignal> signals = new ArrayList<>();
-	private final ArrayList<Queue<Double>> queues = new ArrayList<>();
+	private final Queue<Double> timestamps;
+	private final ArrayList<BaseStatusSignal> signals;
+	private final ArrayList<Queue<Double>> queues;
 
 	private Phoenix6Thread() {
+		this.timestamps = new ArrayBlockingQueue<>(UPDATES_PER_ROBORIO_CYCLE);
+		this.signals = new ArrayList<>();
+		this.queues = new ArrayList<>();
+
 		setName(Phoenix6Thread.class.getSimpleName());
+		setDaemon(true);
 		start();
 	}
 
-	public Queue<Double> getTimestamps() {
+	public Queue<Double> getTimestampsQueue() {
 		return timestamps;
 	}
 
 	public Queue<Double> registerSignal(BaseStatusSignal signal) {
 		Queue<Double> signalQueue = new ArrayBlockingQueue<>(UPDATES_PER_ROBORIO_CYCLE);
+		LOCK.lock();
 		queues.add(signalQueue);
 		signals.add(signal);
 		PhoenixProUtils.checkWithRetry(() -> signal.setUpdateFrequency(FREQUENCY), FREQUENCY_SET_RETRIES);
+		LOCK.unlock();
 		return signalQueue;
 	}
 
@@ -52,21 +61,21 @@ public class Phoenix6Thread extends Thread {
 	public void run() {
 		Timer.delay(STARTING_DELAY_SECONDS);
 		while (true) {
+			LOCK.lock();
 			fetchSignals();
 			updateQueues();
+			LOCK.unlock();
 		}
 	}
 
-	void fetchSignals() {
-		BaseStatusSignal.waitForAll(WAIT_TIME_SECONDS, signals.toArray(new BaseStatusSignal[0]));
+	private void fetchSignals() {
+		BaseStatusSignal.waitForAll(CycleTimeUtils.DEFAULT_CYCLE_TIME_SECONDS, signals.toArray(new BaseStatusSignal[0]));
 	}
 
-	void updateQueues() {
-		BaseStatusSignal signal = signals.get(0);
-		queues.get(0).offer(signal.getValueAsDouble());
-		double timestamp = Conversions.microSecondsToSeconds(HALUtil.getFPGATime()) - signal.getTimestamp().getLatency();
-		timestamps.offer(timestamp);
-		for (int i = 1; i < signals.size(); i++) {
+	private void updateQueues() {
+		double latencyCompensatedTimestamp = (Logger.getRealTimestamp() / 1.0e6) - signals.get(0).getTimestamp().getLatency();
+		timestamps.offer(latencyCompensatedTimestamp);
+		for (int i = 0; i < signals.size(); i++) {
 			queues.get(i).offer(signals.get(i).getValueAsDouble());
 		}
 	}

28df7f2607bb4614369239bc5addaddb7e2617f5
hillelv <hillel.vider@gmail.com>
2024-09-16 16:00:34 +0300

    yoav: threadsignal

---
+++ src/main/java/frc/robot/hardware/signal/phoenix/Phoenix6ThreadSignal.java
@@ -0,0 +1,27 @@
+package frc.robot.hardware.signal.phoenix;
+
+import frc.robot.hardware.signal.AngleArraySignal;
+import frc.utils.AngleUnit;
+
+import java.util.Queue;
+
+public class Phoenix6ThreadSignal extends AngleArraySignal {
+
+    private final Queue<Double> signalQueue;
+
+    public Phoenix6ThreadSignal(Queue<Double> signalQueue, String name, AngleUnit angleUnit) {
+        super(name, angleUnit);
+        this.signalQueue = signalQueue;
+    }
+
+    @Override
+    protected double[] getNewValues() {
+        Phoenix6Thread.LOCK.lock();
+        try {
+            return signalQueue.stream().mapToDouble(Double::doubleValue).toArray();
+        } finally {
+            Phoenix6Thread.LOCK.unlock();
+        }
+    }
+
+}

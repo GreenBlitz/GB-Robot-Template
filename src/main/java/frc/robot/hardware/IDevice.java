9d2549cb67cc17a925808f4fabf72cc3ee1f6d0f
itamaroryan <itamar.oryan@gmail.com>
2024-09-17 22:29:08 +0300

    format

--- src/main/java/frc/robot/hardware/IDevice.java
+++ src/main/java/frc/robot/hardware/IDevice.java
@@ -3,9 +3,9 @@ package frc.robot.hardware;
 import frc.robot.hardware.signal.InputSignal;
 
 public interface IDevice {
-	
+
 	boolean isConnected();
-	
+
 	void updateSignals(InputSignal... signals);
-	
+
 }

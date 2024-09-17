f0ac99db8dce28513da92783fb3660b9dc1a7600
DANNA2828 <dan71967@gmail.com>
2024-09-17 12:58:12 +0300

    danna - added feedforward

--- src/main/java/frc/robot/hardware/request/cansparkmax/CANSparkMAXRequest.java
+++ src/main/java/frc/robot/hardware/request/cansparkmax/CANSparkMAXRequest.java
@@ -3,25 +3,28 @@ package frc.robot.hardware.request.cansparkmax;
 import com.revrobotics.CANSparkBase;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.hardware.request.IRequest;
+import java.util.function.BiFunction;
 
 public class CANSparkMAXRequest implements IRequest<Double> {
 
     private double setPoint;
     private final CANSparkBase.ControlType controlType;
     private int pidSlot;
+    private BiFunction<Double, Double, Double> feedforwardCalculator;
 
-    public CANSparkMAXRequest(double setPoint, CANSparkBase.ControlType controlType, int pidSlot) {
+    private CANSparkMAXRequest(double setPoint, CANSparkBase.ControlType controlType, int pidSlot, BiFunction<Double, Double, Double> feedforwardCalculator) {
         this.setPoint = setPoint;
         this.controlType = controlType;
         this.pidSlot = pidSlot;
+        this.feedforwardCalculator = feedforwardCalculator;
     }
 
-    public CANSparkMAXRequest(double positionSetPoint, int pidSlot) {
-        this(positionSetPoint, CANSparkBase.ControlType.kPosition, pidSlot);
+    public CANSparkMAXRequest(double positionSetPoint, int pidSlot, BiFunction<Double, Double, Double> feedforwardCalculator) {
+        this(positionSetPoint, CANSparkBase.ControlType.kPosition, pidSlot, feedforwardCalculator);
     }
 
-    public CANSparkMAXRequest(Rotation2d velocitySetPoint, int pidSlot) {
-        this(velocitySetPoint.getRotations(), CANSparkBase.ControlType.kVelocity, pidSlot);
+    public CANSparkMAXRequest(Rotation2d velocitySetPoint, int pidSlot, BiFunction<Double, Double, Double> feedforwardCalculator) {
+        this(velocitySetPoint.getRotations(), CANSparkBase.ControlType.kVelocity, pidSlot, feedforwardCalculator);
     }
 
     @Override
@@ -35,6 +38,11 @@ public class CANSparkMAXRequest implements IRequest<Double> {
         return this;
     }
 
+    public CANSparkMAXRequest withFeedforwardCalculator(BiFunction<Double, Double, Double> feedforwardCalculator) {
+        this.feedforwardCalculator = feedforwardCalculator;
+        return this;
+    }
+
     public double getSetPoint() {
         return setPoint;
     }
@@ -45,6 +53,10 @@ public class CANSparkMAXRequest implements IRequest<Double> {
 
     public int getPidSlot() {
         return pidSlot;
+    }
+
+    public BiFunction<Double, Double, Double> getFeedforwardCalculator() {
+        return feedforwardCalculator;
     }
 
 }

e2a7db3fb81df415d863e0de45178fd38bf927b2
itamaroryan <itamar.oryan@gmail.com>
2024-09-18 20:01:39 +0300

    gg

--- src/main/java/frc/robot/hardware/encoder/CANCoderEncoder.java
+++ src/main/java/frc/robot/hardware/encoder/CANCoderEncoder.java
@@ -6,37 +6,50 @@ import com.ctre.phoenix6.hardware.CANcoder;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.hardware.ConnectedInputAutoLogged;
 import frc.robot.hardware.IDevice;
-import frc.robot.hardware.signal.AngleSignal;
 import frc.robot.hardware.signal.InputSignal;
+import frc.robot.hardware.signal.phoenix.Phoenix6BothLatencySignal;
 import frc.robot.hardware.signal.phoenix.Phoenix6SignalBuilder;
-import frc.utils.AngleUnit;
+import org.littletonrobotics.junction.Logger;
 
-public class CANCoderEncoder extends CTRED im IAngleEncoder {
+import java.util.LinkedList;
+import java.util.Locale;
 
-	private final CANcoder encoder;
+public class CANCoderEncoder implements IAngleEncoder, IDevice {
 	
-	public CANCoderEncoder(CANcoder encoder, double frequency) {
+	private final CANcoder encoder;
+	private final ConnectedInputAutoLogged connectedInput;
+	private final String logPath;
+	
+	public CANCoderEncoder(CANcoder encoder, String logPath) {
 		this.encoder = encoder;
+		this.connectedInput = new ConnectedInputAutoLogged();
+		this.logPath = logPath;
 	}
-
+	
 	@Override
 	public void setPosition(Rotation2d position) {
 		encoder.setPosition(position.getRotations());
 	}
+	
 	@Override
 	public boolean isConnected() {
-		return BaseStatusSignal.isAllGood(encoder.getPosition());
+		connectedInput.connected = BaseStatusSignal.isAllGood(encoder.getPosition());
+		return connectedInput.connected;
 	}
 	
 	@Override
 	public void updateSignals(InputSignal... signals) {
-		StatusSignal<>
+		LinkedList<StatusSignal<Double>> statusSignals = new LinkedList<>();
 		for (InputSignal signal : signals) {
 			if (signal instanceof Phoenix6SignalBuilder.SignalGetter) {
-				BaseStatusSignal.refreshAll(((Phoenix6SignalBuilder.SignalGetter) signal).getSignal());
+				statusSignals.add(((Phoenix6SignalBuilder.SignalGetter) signal).getSignal());
+				if (signal instanceof Phoenix6BothLatencySignal) {
+					statusSignals.add(((Phoenix6BothLatencySignal) signal).getSignalSlope());
+				}
 			}
 		}
-		
+		connectedInput.connected = BaseStatusSignal.refreshAll(statusSignals.toArray(StatusSignal[]::new)).isOK();
+		Logger.processInputs(logPath, connectedInput);
 	}
-
+	
 }

d150f7e9d2e4d43e2eb2cde9ebdf1506966666f2
YHerm <0549405422.yh@gmail.com>
2024-09-19 00:23:35 +0300

    emptygryo

---
+++ src/main/java/frc/robot/hardware/gyro/EmptyGyro.java
@@ -0,0 +1,24 @@
+package frc.robot.hardware.gyro;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.hardware.IDevice;
+import frc.robot.hardware.signal.InputSignal;
+
+public class EmptyGyro implements IGyro, IDevice {
+
+    @Override
+    public boolean isConnected() {
+        return false;
+    }
+
+    @Override
+    public void updateSignals(InputSignal... signals) {
+
+    }
+
+    @Override
+    public void setYaw(Rotation2d yaw) {
+
+    }
+
+}

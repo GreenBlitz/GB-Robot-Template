1e2b5444d171709dd1977dda59d47ef180c838fb
DANNA2828 <dan71967@gmail.com>
2024-09-18 23:06:38 +0300

    danna - two classes

---
+++ src/main/java/frc/robot/hardware/request/cansparkmax/SparkAngleRequest.java
@@ -0,0 +1,63 @@
+package frc.robot.hardware.request.cansparkmax;
+
+import com.revrobotics.CANSparkBase;
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.hardware.request.IRequest;
+
+import java.util.function.BiFunction;
+
+public class SparkAngleRequest implements IRequest<Rotation2d> {
+
+	public enum SparkAngleRequestType {
+
+		POSITION(CANSparkBase.ControlType.kPosition),
+		VELOCITY(CANSparkBase.ControlType.kVelocity);
+
+		private final CANSparkBase.ControlType type;
+
+		SparkAngleRequestType(CANSparkBase.ControlType type) {
+			this.type = type;
+		}
+
+	}
+
+	private final SparkAngleRequestType controlType;
+	private final int pidSlot;
+	private final BiFunction<Rotation2d, Rotation2d, Double> feedforwardCalculator;
+	private Rotation2d setPoint;
+
+	public SparkAngleRequest(
+		Rotation2d setPoint,
+		SparkAngleRequestType controlType,
+		int pidSlot,
+		BiFunction<Rotation2d, Rotation2d, Double> feedforwardCalculator
+	) {
+		this.setPoint = setPoint;
+		this.controlType = controlType;
+		this.pidSlot = pidSlot;
+		this.feedforwardCalculator = feedforwardCalculator;
+	}
+
+	@Override
+	public SparkAngleRequest withSetPoint(Rotation2d setPoint) {
+		this.setPoint = setPoint;
+		return this;
+	}
+
+	public Rotation2d getSetPoint() {
+		return setPoint;
+	}
+
+	public CANSparkBase.ControlType getControlType() {
+		return controlType.type;
+	}
+
+	public int getPidSlot() {
+		return pidSlot;
+	}
+
+	public BiFunction<Rotation2d, Rotation2d, Double> getFeedforwardCalculator() {
+		return feedforwardCalculator;
+	}
+
+}
---
+++ src/main/java/frc/robot/hardware/request/cansparkmax/SparkDoubleRequest.java
@@ -0,0 +1,63 @@
+package frc.robot.hardware.request.cansparkmax;
+
+import com.revrobotics.CANSparkBase;
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.hardware.request.IRequest;
+
+import java.util.function.BiFunction;
+
+public class SparkDoubleRequest implements IRequest<Double> {
+
+	public enum SparkDoubleRequestType {
+
+		CURRENT(CANSparkBase.ControlType.kCurrent),
+		VOLTAGE(CANSparkBase.ControlType.kVoltage);
+
+		private final CANSparkBase.ControlType type;
+
+		SparkDoubleRequestType(CANSparkBase.ControlType type) {
+			this.type = type;
+		}
+
+	}
+
+	private final SparkDoubleRequestType controlType;
+	private final int pidSlot;
+	private final BiFunction<Rotation2d, Rotation2d, Double> feedforwardCalculator;
+	private double setPoint;
+
+	public SparkDoubleRequest(
+		double setPoint,
+		SparkDoubleRequestType controlType,
+		int pidSlot,
+		BiFunction<Rotation2d, Rotation2d, Double> feedforwardCalculator
+	) {
+		this.setPoint = setPoint;
+		this.controlType = controlType;
+		this.pidSlot = pidSlot;
+		this.feedforwardCalculator = feedforwardCalculator;
+	}
+
+	@Override
+	public SparkDoubleRequest withSetPoint(Double setPoint) {
+		this.setPoint = setPoint;
+		return this;
+	}
+
+	public double getSetPoint() {
+		return setPoint;
+	}
+
+	public CANSparkBase.ControlType getControlType() {
+		return controlType.type;
+	}
+
+	public int getPidSlot() {
+		return pidSlot;
+	}
+
+	public BiFunction<Rotation2d, Rotation2d, Double> getFeedforwardCalculator() {
+		return feedforwardCalculator;
+	}
+
+}

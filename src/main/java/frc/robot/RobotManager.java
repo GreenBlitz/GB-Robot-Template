e0500d0fe1e5d5d381d7b6a95dd8c5fcbc9a1abd
YHerm <0549405422.yh@gmail.com>
2024-09-19 00:36:00 +0300

    manager

--- src/main/java/frc/robot/RobotManager.java
+++ src/main/java/frc/robot/RobotManager.java
@@ -4,27 +4,17 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.StatusSignal;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.hardware.gyro.phoenix6.Pigeon2Gyro;
-import frc.robot.hardware.gyro.phoenix6.Pigeon2Wrapper;
-import frc.robot.hardware.signal.AngleSignal;
-import frc.robot.hardware.signal.InputSignal;
-import frc.robot.hardware.signal.phoenix.Phoenix6AngleSignal;
-import frc.robot.hardware.signal.phoenix.Phoenix6SignalBuilder;
 import frc.robot.simulation.SimulationManager;
-import frc.utils.AngleUnit;
-import frc.utils.alerts.AlertManager;
 import frc.utils.DriverStationUtils;
+import frc.utils.alerts.AlertManager;
 import frc.utils.battery.BatteryUtils;
+import frc.utils.brakestate.BrakeStateManager;
 import frc.utils.ctre.BusChain;
-import frc.utils.ctre.CTREDeviceID;
 import frc.utils.cycletime.CycleTimeUtils;
 import frc.utils.logger.LoggerFactory;
 import org.littletonrobotics.junction.LoggedRobot;
-import frc.utils.brakestate.BrakeStateManager;
-import org.littletonrobotics.junction.Logger;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as described in the
@@ -36,20 +26,13 @@ public class RobotManager extends LoggedRobot {
 	private Command autonomousCommand;
 
 	private Robot robot;
-	private Pigeon2Gyro gyro;
-    private Phoenix6AngleSignal yaw,pitch,roll;
 
 	@Override
 	public void robotInit() {
 		LoggerFactory.initializeLogger();
 		BatteryUtils.scheduleLimiter();
 
-        Pigeon2Wrapper wrapper = new Pigeon2Wrapper(new CTREDeviceID(0, BusChain.ROBORIO));
-		gyro = new Pigeon2Gyro("gyro",wrapper);
 		this.robot = new Robot();
-		yaw = Phoenix6SignalBuilder.generatePhoenix6Signal(wrapper.getYaw(), 50,AngleUnit.DEGREES);
-		pitch = Phoenix6SignalBuilder.generatePhoenix6Signal(wrapper.getPitch(), 50,AngleUnit.DEGREES);
-		roll = Phoenix6SignalBuilder.generatePhoenix6Signal(wrapper.getRoll(), 50,AngleUnit.DEGREES);
 	}
 
 	@Override
@@ -89,11 +72,6 @@ public class RobotManager extends LoggedRobot {
 		BatteryUtils.logStatus();
 		BusChain.logChainsStatuses();
 		AlertManager.reportAlerts();
-
-		gyro.updateSignals(yaw,pitch,roll);
-		Logger.recordOutput("yaw",yaw.getLatestValue());
-		Logger.recordOutput("pitch",pitch.getLatestValue());
-		Logger.recordOutput("roll",roll.getLatestValue());
 	}
 
 	@Override

a5c20ac1decc58f37b6d66ff3add1aafecff85e8
GreenBlitz <greenblitz4590@gmail.com>
2024-09-19 12:16:36 +0300

    noam - added sparkmaxangle signal

---
+++ src/main/java/frc/robot/hardware/signal/cansparkmax/SparkMaxAngleSignal.java
@@ -0,0 +1,25 @@
+package frc.robot.hardware.signal.cansparkmax;
+
+import com.ctre.phoenix.motorcontrol.IMotorController;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.hardware.signal.AngleSignal;
+import frc.utils.AngleUnit;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class SparkMaxAngleSignal extends AngleSignal {
+
+    Supplier<Double> angleSignalSupplier;
+
+    public SparkMaxAngleSignal (String name, Supplier<Double> angleSignalSupplier, AngleUnit angleUnit){
+        super(name, angleUnit);
+        this.angleSignalSupplier = angleSignalSupplier;
+    }
+
+    @Override
+    protected double getNewValue() {
+        return angleSignalSupplier.get();
+    }
+}

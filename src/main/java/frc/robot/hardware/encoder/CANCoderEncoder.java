348940c6edfed896abb5fbe34ec3096d4ce0e69f
GreenBlitz <greenblitz4590@gmail.com>
2024-09-19 14:07:24 +0300

    removed unused things that are already in phoenix6device

--- src/main/java/frc/robot/hardware/encoder/CANCoderEncoder.java
+++ src/main/java/frc/robot/hardware/encoder/CANCoderEncoder.java
@@ -2,7 +2,6 @@ package frc.robot.hardware.encoder;
 
 import com.ctre.phoenix6.hardware.CANcoder;
 import edu.wpi.first.math.geometry.Rotation2d;
-import frc.robot.hardware.ConnectedInputAutoLogged;
 import frc.robot.hardware.Phoenix6Device;
 import frc.robot.hardware.signal.InputSignal;
 
@@ -10,14 +9,10 @@ import frc.robot.hardware.signal.InputSignal;
 public class CANCoderEncoder extends Phoenix6Device implements IAngleEncoder {
 
 	private final CANcoder encoder;
-	private final ConnectedInputAutoLogged connectedInput;
-	private final String logPath;
 
 	public CANCoderEncoder(CANcoder encoder, String logPath) {
 		super(logPath);
 		this.encoder = encoder;
-		this.connectedInput = new ConnectedInputAutoLogged();
-		this.logPath = logPath;
 	}
 
 	@Override

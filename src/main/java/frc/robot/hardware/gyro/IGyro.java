395755b2f72bb41cb8b7bef763293b07489bbba6
greenblitz <greenblitz@gmail.com>
2024-09-19 09:33:24 +0300

    yeroshua

--- src/main/java/frc/robot/hardware/gyro/IGyro.java
+++ src/main/java/frc/robot/hardware/gyro/IGyro.java
@@ -3,7 +3,7 @@ package frc.robot.hardware.gyro;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.hardware.IDevice;
 
-public interface IGyro {
+public interface IGyro extends IDevice {
 
 	void setYaw(Rotation2d yaw);
 

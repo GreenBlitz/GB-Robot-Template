43761cf00f610abd60b8b0fc1285bd9cb0b25d94
YHerm <0549405422.yh@gmail.com>
2024-09-19 18:56:03 +0300

    format

--- src/main/java/frc/robot/hardware/EmptyDevice.java
+++ src/main/java/frc/robot/hardware/EmptyDevice.java
@@ -5,23 +5,21 @@ import org.littletonrobotics.junction.Logger;
 
 public abstract class EmptyDevice implements IDevice {
 
-    private final String logPath;
-    private final ConnectedInputAutoLogged connectedInput;
+	private final String logPath;
+	private final ConnectedInputAutoLogged connectedInput;
 
-    public EmptyDevice(String logPath) {
-        this.logPath = logPath;
-        this.connectedInput = new ConnectedInputAutoLogged();
-    }
+	public EmptyDevice(String logPath) {
+		this.logPath = logPath;
+		this.connectedInput = new ConnectedInputAutoLogged();
+	}
 
-    @Override
-    public boolean isConnected() {
-        Logger.processInputs(logPath, connectedInput);
-        return connectedInput.connected;
-    }
+	@Override
+	public boolean isConnected() {
+		Logger.processInputs(logPath, connectedInput);
+		return connectedInput.connected;
+	}
 
-    @Override
-    public void fetchSignals(InputSignal... signals) {
-
-    }
+	@Override
+	public void fetchSignals(InputSignal... signals) {}
 
 }

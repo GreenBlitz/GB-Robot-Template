c34e0a6babac43bde3c3693c533349032df5de17
hillelv <hillel.vider@gmail.com>
2024-09-19 16:14:08 +0300

    lol

--- src/main/java/frc/robot/hardware/encoder/EmptyEncoder.java
+++ src/main/java/frc/robot/hardware/encoder/EmptyEncoder.java
@@ -1,10 +1,9 @@
 package frc.robot.hardware.encoder;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import frc.robot.hardware.IDevice;
 import frc.robot.hardware.signal.InputSignal;
 
-public class EmptyEncoder implements IAngleEncoder, IDevice {
+public class EmptyEncoder implements IAngleEncoder {
 
 	@Override
 	public void setPosition(Rotation2d position) {}

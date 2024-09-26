04644b9b2cf4966132b1548654a306a043a0ee2a
greenblitz <greenblitz@gmail.com>
2024-09-26 11:55:12 +0300

    stuuf

--- src/main/java/frc/robot/constants/GlobalConstants.java
+++ src/main/java/frc/robot/constants/GlobalConstants.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 
 public class GlobalConstants {
 
-	public static final double DEFAULT_SIGNALS_FREQUENCY_HERTZ = 50;
+	public static final double DEFAULT_SIGNALS_FREQUENCY_HERTZ = 60;
 
 	public static final DriverStation.Alliance SIMULATION_ALLIANCE = DriverStation.Alliance.Blue;
 

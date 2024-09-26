3381a9fd290a9494254000896e588ab5f109fef0
greenblitz <greenblitz@gmail.com>
2024-09-26 14:07:10 +0300

    d

---
+++ src/main/java/frc/robot/subsystems/elbow/Elbow.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems.elbow;
+
+import frc.utils.GBSubsystem;
+
+public class Elbow extends GBSubsystem {
+}
---
+++ src/main/java/frc/robot/subsystems/elbow/ElbowCommands.java
@@ -0,0 +1,4 @@
+package frc.robot.subsystems.elbow;
+
+public class ElbowCommands {
+}

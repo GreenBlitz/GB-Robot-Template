90633eb848ccc0fa1a9d2a25b569d4d73ac6fc8f
greenblitz4590 <greenblitz4590@gmail.com>
2024-09-27 15:18:43 +0300

    states2.0

--- src/main/java/frc/robot/superstructure/RobotState.java
+++ src/main/java/frc/robot/superstructure/RobotState.java
@@ -4,6 +4,11 @@ public enum RobotState {
 
     IDLE, //flywheel, pivot, arm
     PRE_AMP, //swerve, arm
-    PRE_SPEAKER //flywheel, pivot
+    PRE_SPEAKER, //flywheel, pivot
+    TRANSFER_SHOOTER_ARM, //SEQUENCE: funnel, roller, arm, pivot
+    TRANSFER_ARM_SHOOTER, //SEQUENCE: funnel, roller, arm, pivot
+    INTAKE, //intake, pivot, funnel
+    SPEAKER, // Funnel
+    AMP // Roller
 
 }
--- src/main/java/frc/robot/superstructure/Supersturctrue.java
+++ src/main/java/frc/robot/superstructure/Supersturctrue.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
+import frc.robot.subsystems.flywheel.FlywheelState;
+import frc.robot.subsystems.flywheel.FlywheelStateHandler;
 import frc.robot.subsystems.pivot.PivotState;
 import frc.robot.subsystems.pivot.PivotStateHandler;
 
@@ -12,20 +14,31 @@ public class Supersturctrue {
     private final Robot robot;
 
     private final PivotStateHandler pivotStateHandler;
+    private final FlywheelStateHandler flywheelStateHandler;
 
     public Supersturctrue(Robot robot) {
         this.robot = robot;
         this.pivotStateHandler = new PivotStateHandler(robot.getPivot());
+        this.flywheelStateHandler = new FlywheelStateHandler(robot.getFlywheel());
     }
 
-    public void setState(RobotState state) {
+    public Command setState(RobotState state) {
+        return switch (state) {
+            case PRE_SPEAKER -> preSpeaker();
+            case IDLE ->
+        }
+    }
 
+    private Command idle() {
+        return new ParallelCommandGroup(
+
+        )
     }
 
     private Command preSpeaker() {
         return new ParallelCommandGroup(
-                pivotStateHandler.setState(PivotState.SHOOT),
-
+            pivotStateHandler.setState(PivotState.SHOOT),
+            flywheelStateHandler.setState(FlywheelState.SHOOT)
         );
     }
 

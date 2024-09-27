349de120ba4433913585e80d16f744340fa2b886
greenblitz <greenblitz@gmail.com>
2024-09-27 12:53:04 +0300

    Romy - cr

--- src/main/java/frc/robot/subsystems/elbow/ElbowConstants.java
+++ src/main/java/frc/robot/subsystems/elbow/ElbowConstants.java
@@ -4,7 +4,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public class ElbowConstants {
 
-	public static final String LOG_PATH = "Elbow/";
+	public static final String LOG_PATH = "Subsystems/Elbow/";
 
 	public static final Rotation2d FORWARD_LIMIT = Rotation2d.fromDegrees(90);
 	public static final Rotation2d BACKWARD_LIMIT = Rotation2d.fromDegrees(-78);

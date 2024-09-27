cb4e2b1d73c0f1b200fade5f477a3dc29f8ff867
greenblitz <greenblitz@gmail.com>
2024-09-27 11:57:14 +0300

    suo command

--- src/main/java/frc/robot/subsystems/intake/IntakeCommandsBuilder.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeCommandsBuilder.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems.intake;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 
+import java.util.function.DoubleSupplier;
+
 public class IntakeCommandsBuilder {
 
 	private final Intake intake;
@@ -14,6 +16,10 @@ public class IntakeCommandsBuilder {
 	public Command moveByPower(double power) {
 		return new FunctionalCommand(() -> intake.setPower(power), () -> {}, interrupted -> intake.stop(), () -> false, intake)
 			.withName("Move by power: " + power);
+	}
+	public Command moveByPower(DoubleSupplier power) {
+		return new FunctionalCommand(() -> {}, () -> intake.setPower(power.getAsDouble()), interrupted -> intake.stop(), () -> false, intake)
+				.withName("Move power supplier");
 	}
 
 }

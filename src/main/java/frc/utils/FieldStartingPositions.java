e2e19bb3870219afeaa24a36225fecb9273b3107
Shahar Perets <perets.shahar@gmail.com>
2024-09-29 17:34:16 +0300

    Shahar: comment added

--- src/main/java/frc/utils/FieldStartingPositions.java
+++ src/main/java/frc/utils/FieldStartingPositions.java
@@ -4,6 +4,8 @@ import edu.wpi.first.math.geometry.Pose2d;
 
 public enum FieldStartingPositions {
 
+    // ! should have data from the actual field
+    //TODO: add those
 	NORTH_BLUE_ALLIANCE(new Pose2d()),
 	SOUTH_BLUE_ALLIANCE(new Pose2d()),
 	MIDDLE_BLUE_ALLIANCE(new Pose2d()),

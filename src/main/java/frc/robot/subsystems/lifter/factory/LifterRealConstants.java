5aa6c75c911d36f8459fd9de2c4c8280f6bc9ae6
hillelv <hillel.vider@gmail.com>
2024-09-30 17:46:31 +0300

    added gear ratio

--- src/main/java/frc/robot/subsystems/lifter/factory/LifterRealConstants.java
+++ src/main/java/frc/robot/subsystems/lifter/factory/LifterRealConstants.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.lifter.factory;
 
 import com.ctre.phoenix6.SignalLogger;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.constants.GlobalConstants;
 import frc.robot.constants.IDs;
@@ -15,32 +17,41 @@ import static edu.wpi.first.units.Units.Volts;
 
 public class LifterRealConstants {
 
-	private static final double DRUM_RADIUS = 0.05;
-	private static final double EXTENDING_POWER = 0.9;
-	private static final double RETRACTING_POWER = 0.9;
+    private static final double DRUM_RADIUS = 0.05;
+    private static final double EXTENDING_POWER = 0.9;
+    private static final double RETRACTING_POWER = 0.9;
+    private static final TalonFXConfiguration CONFIGURATION = new TalonFXConfiguration();
 
-	private static SysIdRoutine.Config generateSysidConfig() {
-		return new SysIdRoutine.Config(
-			Volts.of(1).per(Seconds.of(1)),
-			Volts.of(7),
-			Seconds.of(10),
-			(state) -> SignalLogger.writeString("state", state.toString())
-		);
-	}
+    static {
+        FeedbackConfigs FEEDBACK_CONFIGS = new FeedbackConfigs();
+        FEEDBACK_CONFIGS.SensorToMechanismRatio = 9 * 60.0 / 14.0;
 
-	public static LifterStuff generateLifterStuff(String logPath) {
-		TalonFXWrapper talonFXWrapper = new TalonFXWrapper(IDs.TalonFXIDs.LIFTER);
+		CONFIGURATION.withFeedback(FEEDBACK_CONFIGS);
+    }
 
-		return new LifterStuff(
-			logPath,
-			new TalonFXMotor(logPath, talonFXWrapper, generateSysidConfig()),
-			DRUM_RADIUS,
-			Phoenix6SignalBuilder
-				.generatePhoenix6Signal(talonFXWrapper.getPosition(), GlobalConstants.DEFAULT_SIGNALS_FREQUENCY_HERTZ, AngleUnit.ROTATIONS),
-			EXTENDING_POWER,
-			RETRACTING_POWER
-		);
-	}
+    private static SysIdRoutine.Config generateSysidConfig() {
+        return new SysIdRoutine.Config(
+                Volts.of(1).per(Seconds.of(1)),
+                Volts.of(7),
+                Seconds.of(10),
+                (state) -> SignalLogger.writeString("state", state.toString())
+        );
+    }
+
+    public static LifterStuff generateLifterStuff(String logPath) {
+        TalonFXWrapper talonFXWrapper = new TalonFXWrapper(IDs.TalonFXIDs.LIFTER);
+		talonFXWrapper.applyConfiguration(CONFIGURATION);
+
+        return new LifterStuff(
+                logPath,
+                new TalonFXMotor(logPath, talonFXWrapper, generateSysidConfig()),
+                DRUM_RADIUS,
+                Phoenix6SignalBuilder
+                        .generatePhoenix6Signal(talonFXWrapper.getPosition(), GlobalConstants.DEFAULT_SIGNALS_FREQUENCY_HERTZ, AngleUnit.ROTATIONS),
+                EXTENDING_POWER,
+                RETRACTING_POWER
+        );
+    }
 
 
 }

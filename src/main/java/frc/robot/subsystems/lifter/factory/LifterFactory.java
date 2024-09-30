1b6dd7f6fc0609e94f7d9c3be82ba093afe043e5
hillelv <hillel.vider@gmail.com>
2024-09-30 17:20:00 +0300

    added factory

--- src/main/java/frc/robot/subsystems/lifter/factory/LifterFactory.java
+++ src/main/java/frc/robot/subsystems/lifter/factory/LifterFactory.java
@@ -1,4 +1,15 @@
 package frc.robot.subsystems.lifter.factory;
 
+import frc.robot.Robot;
+import frc.robot.subsystems.lifter.LifterStuff;
+
 public class LifterFactory {
+
+    public static LifterStuff create(String logPath){
+        return switch (Robot.ROBOT_TYPE){
+            case REAL -> LifterRealConstants.generateLifterStuff(logPath);
+            case SIMULATION -> null;
+        };
+    }
+
 }

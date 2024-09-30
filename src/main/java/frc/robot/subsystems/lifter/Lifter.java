3cbd7e72f387ecf4629280dade1177fedb23561f
hillelv <hillel.vider@gmail.com>
2024-09-30 16:23:07 +0300

    added subsystem

---
+++ src/main/java/frc/robot/subsystems/lifter/Lifter.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems.lifter;
+
+import edu.wpi.first.math.MathUtil;
+import frc.robot.hardware.motor.ControllableMotor;
+import frc.robot.hardware.request.IRequest;
+import frc.robot.hardware.signal.InputSignal;
+import frc.utils.GBSubsystem;
+
+public class Lifter extends GBSubsystem {
+
+    private final ControllableMotor motor;
+    private final LifterStuff lifterStuff;
+
+    public Lifter(LifterStuff lifterStuff){
+        super(lifterStuff.logPath());
+        this.motor = lifterStuff.motor();
+        this.lifterStuff = lifterStuff;
+    }
+
+    public void setPower(double power){
+        motor.setPower(power);
+    }
+
+    public void setBrake(boolean brake){
+        motor.setBrake(brake);
+    }
+
+    public void setTargetPosition(double position){
+        motor.applyDoubleRequest(lifterStuff.positionRequest().withSetPoint(position));
+    }
+
+    public boolean isAtTargetPosition(double expectedPosition, double positionTolerance){
+        return MathUtil.isNear(
+                expectedPosition,
+                lifterStuff.positionSignal().getLatestValue(),
+                positionTolerance
+        );
+    }
+
+    @Override
+    protected void subsystemPeriodic() {
+        motor.updateSignals(lifterStuff.positionSignal());
+        motor.updateSignals(lifterStuff.otherSignals());
+    }
+}
--- src/main/java/frc/robot/subsystems/lifter/LifterStuff.java
+++ src/main/java/frc/robot/subsystems/lifter/LifterStuff.java
@@ -1,10 +1,13 @@
 package frc.robot.subsystems.lifter;
 
 import frc.robot.hardware.motor.ControllableMotor;
+import frc.robot.hardware.request.IRequest;
 import frc.robot.hardware.signal.InputSignal;
 
 public record LifterStuff (
         String logPath,
         ControllableMotor motor,
-        InputSignal<Double> positionSignal
+        InputSignal<Double> positionSignal,
+        IRequest<Double> positionRequest,
+        InputSignal... otherSignals
 ){}

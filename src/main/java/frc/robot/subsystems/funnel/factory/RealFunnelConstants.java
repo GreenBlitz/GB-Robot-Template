35651c0a329d3a34015d0ecfc9c9e865ecd47ea2
Dror Talmi <dror.gg.talmi@gmail.com>
2024-10-02 01:01:22 +0300

    lol

--- src/main/java/frc/robot/subsystems/funnel/factory/RealFunnelConstants.java
+++ src/main/java/frc/robot/subsystems/funnel/factory/RealFunnelConstants.java
@@ -23,18 +23,15 @@ public class RealFunnelConstants {
 	private final static SparkLimitSwitch.Type REVERSE_LIMIT_SWITCH_TYPE = SparkLimitSwitch.Type.kNormallyOpen;
 	private final static SparkLimitSwitch.Type FORWARD_LIMIT_SWITCH_TYPE = SparkLimitSwitch.Type.kNormallyOpen;
 
-	private final static int CURRENT_LIMIT = 30;
-	private final static double GEAR_RATIO = 1;
-
 
 	public static FunnelStuff generateFunnelStuff(String logPath) {
 		SparkMaxWrapper sparkMAXWrapper = new SparkMaxWrapper(IDs.CANSparkMAXIDs.FUNNEL);
 
 		SysIdRoutine.Config config = new SysIdRoutine.Config();
 
-		sparkMAXWrapper.setSmartCurrentLimit(CURRENT_LIMIT);
-		sparkMAXWrapper.getEncoder().setPositionConversionFactor(GEAR_RATIO);
-		sparkMAXWrapper.getEncoder().setVelocityConversionFactor(GEAR_RATIO);
+		sparkMAXWrapper.setSmartCurrentLimit(30);
+		sparkMAXWrapper.getEncoder().setPositionConversionFactor(1);
+		sparkMAXWrapper.getEncoder().setVelocityConversionFactor(1);
 
 		BrushlessSparkMAXMotor motor = new BrushlessSparkMAXMotor(logPath, sparkMAXWrapper, config);
 

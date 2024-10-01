2843668362858aaf9e75f99d3ec741d28b49c8e2
YHerm <0549405422.yh@gmail.com>
2024-10-01 21:25:36 +0300

    no type

--- src/main/java/frc/robot/subsystems/swerve/module/Modules.java
+++ src/main/java/frc/robot/subsystems/swerve/module/Modules.java
@@ -15,9 +15,9 @@ public class Modules {
 	private final Module[] modules;
 	private final String logPath;
 
-	public Modules(SwerveType swerveType, Module[] modules) {
+	public Modules(String logPath, Module[] modules) {
 		this.modules = modules;
-		this.logPath = swerveType.getLogPath() + ModuleConstants.LOG_PATH_ADDITION;
+		this.logPath = logPath + ModuleConstants.LOG_PATH_ADDITION;
 	}
 
 	public Module getModule(ModuleUtils.ModulePosition modulePosition) {

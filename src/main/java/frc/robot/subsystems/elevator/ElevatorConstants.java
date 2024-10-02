db106805644e2a0c6f0cd1aa5dd5ee500907183f
Shahar Perets <perets.shahar@gmail.com>
2024-10-02 17:16:52 +0300

    Shahar: gear ratio added

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -7,7 +7,6 @@ public class ElevatorConstants {
 
 	public static final int ELEVATOR_PID_SLOT = 0;
 
-	// TODO: check this later
-	public final static double GEAR_RATIO = 0;
+	public final static double GEAR_RATIO = 48.0 / 14.0;
 
 }

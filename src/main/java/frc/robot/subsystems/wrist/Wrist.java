0f15f9c83f77e80834dc073bea8c25d259abfe20
itamaroryan <itamar.oryan@gmail.com>
2024-10-03 17:10:00 +0300

    added wrist subsystem

---
+++ src/main/java/frc/robot/subsystems/wrist/Wrist.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems.wrist;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.hardware.motor.ControllableMotor;
+import frc.robot.hardware.request.IRequest;
+import frc.robot.hardware.signal.InputSignal;
+import frc.utils.GBSubsystem;
+
+public class Wrist extends GBSubsystem {
+	
+	private final ControllableMotor motor;
+	private final IRequest<Rotation2d> positionRequest;
+	private final InputSignal<Rotation2d> positionSignal;
+	private final WristStuff wristStuff;
+	
+	public Wrist(WristStuff wristStuff) {
+		super(wristStuff.logPath());
+		this.motor = wristStuff.motor();
+		this.positionRequest = wristStuff.positionRequest();
+		this.positionSignal = wristStuff.positionSignal();
+		this.wristStuff = wristStuff;
+	}
+	
+	public void setTargetPosition (Rotation2d position){
+		motor.applyAngleRequest(positionRequest.withSetPoint(position));
+	}
+	
+	public void setPower(double power){
+		motor.setPower(power);
+	}
+	
+	public void setBrake(boolean brake){
+		motor.setBrake(brake);
+	}
+	
+	public void resetPosition(Rotation2d position){
+		motor.resetPosition(position);
+	}
+	
+	private void updateInputs(){
+		motor.updateSignals(wristStuff.positionSignal());
+		motor.updateSignals(wristStuff.otherSignals());
+	}
+	
+	@Override
+	protected void subsystemPeriodic() {
+		updateInputs();
+	}
+}

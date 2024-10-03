460e570932d9d6ddb8ec0ea68516778a6bd280ff
GreenBlitz <greenblitz4590@gmail.com>
2024-10-03 17:00:42 +0300

    danna - pivotState class

---
+++ src/main/java/frc/robot/subsystems/intake/pivot/PivotState.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems.intake.pivot;
+
+public enum PivotState {
+
+    DOWN(0),
+    UP(1);
+
+    private double degrees;
+
+    PivotState(double degrees) {
+        this.degrees = degrees;
+    }
+
+    public double getDegrees() {
+        return degrees;
+    }
+
+}

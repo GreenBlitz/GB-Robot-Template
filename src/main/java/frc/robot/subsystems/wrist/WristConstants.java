84c29f3cd6438d954cf7c4c0f3252558b94be369
hillelv <hillel.vider@gmail.com>
2024-10-03 18:56:34 +0300

    W

--- src/main/java/frc/robot/subsystems/wrist/WristConstants.java
+++ src/main/java/frc/robot/subsystems/wrist/WristConstants.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.wrist;
 
 public class WristConstants {
 
-	public static final String LOG_PATH = "Subsystems/wrist";
+	public static final String LOG_PATH = "Subsystems/Wrist";
 
 	public static final double GEAR_RATIO = 2;
 

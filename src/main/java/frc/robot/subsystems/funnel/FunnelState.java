2e5a916b26d4e8b83e582787c25cb8819b5ac010
Dror Talmi <dror.gg.talmi@gmail.com>
2024-10-04 18:21:02 +0300

    didstuff

--- src/main/java/frc/robot/subsystems/funnel/FunnelState.java
+++ src/main/java/frc/robot/subsystems/funnel/FunnelState.java
@@ -2,11 +2,12 @@ package frc.robot.subsystems.funnel;
 
 public enum FunnelState {
 
-	NOTE_TO_SHOOTER(0.1),
-	SPEAKER(0.2),
-	SHOOTER_TO_ELEVATOR(-0.1),
-	AMP(-0.2),
-	ELEVATOR_TO_SHOOTER(0.3),
+	SPEAKER(0.6),
+	AMP(-0.6),
+	OUTTAKE(0.4),
+	NOTE_TO_SHOOTER(0.5),
+	SHOOTER_TO_ELEVATOR(-0.5),
+	ELEVATOR_TO_SHOOTER(0.5),
 	STOP(0);
 
 	private final double power;

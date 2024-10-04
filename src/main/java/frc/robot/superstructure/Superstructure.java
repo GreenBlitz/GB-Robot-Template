63b1b23106cddf8e8505bae5e418bc288a681178
YHerm <0549405422.yh@gmail.com>
2024-10-04 13:31:30 +0300

    form

--- src/main/java/frc/robot/superstructure/Superstructure.java
+++ src/main/java/frc/robot/superstructure/Superstructure.java
@@ -6,33 +6,33 @@ import org.littletonrobotics.junction.Logger;
 
 public class Superstructure {
 
-    private final Robot robot;
-    private RobotState currentState;
+	private final Robot robot;
+	private RobotState currentState;
 
-    public Superstructure(Robot robot) {
-        this.robot = robot;
-    }
+	public Superstructure(Robot robot) {
+		this.robot = robot;
+	}
 
-    public RobotState getCurrentState() {
-        return currentState;
-    }
+	public RobotState getCurrentState() {
+		return currentState;
+	}
 
-    public void logStatus() {
-        Logger.recordOutput("CurrentState", currentState);
-    }
+	public void logStatus() {
+		Logger.recordOutput("CurrentState", currentState);
+	}
 
-    public Command setState(RobotState state) {
-        return switch (state) {
-            case IDLE -> null;
-            case INTAKE -> null;
-            case PRE_SPEAKER -> null;
-            case SPEAKER -> null;
-            case PRE_AMP -> null;
-            case AMP -> null;
-            case TRANSFER_SHOOTER_ELEVATOR -> null;
-            case TRANSFER_ELEVATOR_SHOOTER -> null;
-            case SHOOTER_OUTTAKE -> null;
-        };
-    }
+	public Command setState(RobotState state) {
+		return switch (state) {
+			case IDLE -> null;
+			case INTAKE -> null;
+			case PRE_SPEAKER -> null;
+			case SPEAKER -> null;
+			case PRE_AMP -> null;
+			case AMP -> null;
+			case TRANSFER_SHOOTER_ELEVATOR -> null;
+			case TRANSFER_ELEVATOR_SHOOTER -> null;
+			case SHOOTER_OUTTAKE -> null;
+		};
+	}
 
 }

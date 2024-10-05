4a5b88949f60add4c41e111d6711bef572c0f658
itamaroryan <itamar.oryan@gmail.com>
2024-10-05 12:21:42 +0300

    aa

--- src/main/java/frc/robot/JoysticksBindings.java
+++ src/main/java/frc/robot/JoysticksBindings.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import frc.robot.subsystems.wrist.WristState;
+import frc.robot.subsystems.wrist.WristStateHandler;
 import frc.utils.joysticks.JoystickPorts;
 import frc.utils.joysticks.SmartJoystick;
 
@@ -23,6 +25,13 @@ public class JoysticksBindings {
 
 	private static void mainJoystickButtons(Robot robot) {
 		SmartJoystick usedJoystick = MAIN_JOYSTICK;
+		
+		WristStateHandler a = new WristStateHandler(robot.getWrist());
+		usedJoystick.B.onTrue(a.setState(WristState.IN_ARM));
+		usedJoystick.X.onTrue(a.setState(WristState.TRAP));
+		usedJoystick.Y.onTrue(a.setState(WristState.PRE_TRAP));
+		
+		
 		// bindings...
 	}
 
--- src/main/java/frc/robot/subsystems/wrist/WristStateHandler.java
+++ src/main/java/frc/robot/subsystems/wrist/WristStateHandler.java
@@ -1,17 +1,17 @@
 package frc.robot.subsystems.wrist;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.superstructure.Tolerances;
 
 public class WristStateHandler {
-
+	
 	private final Wrist wrist;
-
+	
 	public WristStateHandler(Wrist wrist) {
 		this.wrist = wrist;
 	}
-
+	
 	public Command setState(WristState state) {
-		return wrist.getCommandsBuilder().moveToPosition(state.getPosition());
+		return wrist.getCommandsBuilder().moveToPosition(state.getPosition()).until(() -> wrist.isAtPosition(state.getPosition(), Tolerances.WRIST_POSITION_TOLERANCE));
 	}
-
 }

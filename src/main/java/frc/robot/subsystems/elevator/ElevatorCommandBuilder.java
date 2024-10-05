26697a44519d83d53fd09075f2d40612257c1ff9
Shahar Perets <perets.shahar@gmail.com>
2024-10-05 15:24:21 +0300

    Shahar: feedforward calibration

--- src/main/java/frc/robot/subsystems/elevator/ElevatorCommandBuilder.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorCommandBuilder.java
@@ -1,9 +1,14 @@
 package frc.robot.subsystems.elevator;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.elevator.factories.RealElevatorConstants;
+import frc.utils.utilcommands.LoggedDashboardCommand;
 
 import java.util.function.DoubleSupplier;
 
@@ -52,6 +57,14 @@ public class ElevatorCommandBuilder {
 
     public Command stop() {
         return new InstantCommand(elevator::stop);
+    }
+
+    public Command calibratedFeedForward() {
+        double[] oldValues = new double[] {RealElevatorConstants.FEEDFORWARD_CALCULATOR.ka, RealElevatorConstants.FEEDFORWARD_CALCULATOR.kg, RealElevatorConstants.FEEDFORWARD_CALCULATOR.kv, RealElevatorConstants.FEEDFORWARD_CALCULATOR.ks};
+        return new ParallelCommandGroup(
+                new LoggedDashboardCommand("ks", (Double newkS) -> RealElevatorConstants.FEEDFORWARD_CALCULATOR = new ElevatorFeedforward(oldValues[0], oldValues[1], oldValues[2], newkS)),
+                new LoggedDashboardCommand("kg", (Double newkG) -> RealElevatorConstants.FEEDFORWARD_CALCULATOR = new ElevatorFeedforward(oldValues[0], newkG, oldValues[2], oldValues[3]))
+        );
     }
     //@formatter:on
 

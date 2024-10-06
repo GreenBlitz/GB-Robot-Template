95378c5889a95906f08006c3f41292760d557a9a
Yotam7 <76944434+Yotam7@users.noreply.github.com>
2024-10-06 19:15:46 +0300

    Yotam- removed line for better formatting

--- src/main/java/frc/robot/constants/IDs.java
+++ src/main/java/frc/robot/constants/IDs.java
@@ -4,7 +4,6 @@ package frc.robot.constants;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import frc.utils.battery.PowerDistributionDeviceID;
 
-
 public class IDs {
 
 	public static final PowerDistributionDeviceID POWER_DISTRIBUTION_DEVICE_ID = new PowerDistributionDeviceID(

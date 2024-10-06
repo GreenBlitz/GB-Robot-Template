2e7c800e88cd8eb6cb0966578151bc311c070362
Maya Romm <maya.romm0401@gmail.com>
2024-10-06 15:31:09 +0300

    Maya- I hate yotam

--- src/main/java/frc/robot/subsystems/intake/IntakeRollerCommandsBuilder.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeRollerCommandsBuilder.java
@@ -15,25 +15,25 @@ public class IntakeRollerCommandsBuilder {
 	}
 
 	//@formatter:off
-    public Command setPower(double power) {
-        return new FunctionalCommand(
-                () -> {},
-                () -> intakeRoller.setPower(power),
-                interrupted -> intakeRoller.stop(),
-                () -> false,
-                intakeRoller
-        ).withName("set power: " + power);
-    }
+	public Command setPower(double power) {
+		return new FunctionalCommand(
+			() -> {},
+			() -> intakeRoller.setPower(power),
+			interrupted -> intakeRoller.stop(),
+			() -> false,
+			intakeRoller
+		).withName("set power: " + power);
+	}
 
-    public Command setPower(DoubleSupplier power) {
-        return new FunctionalCommand(
-                () -> {},
-                () -> intakeRoller.setPower(power.getAsDouble()),
-                interrupted -> intakeRoller.stop(),
-                () -> false,
-                intakeRoller
-        ).withName("set power by supplier");
-    }
+	public Command setPower(DoubleSupplier power) {
+		return new FunctionalCommand(
+			() -> {},
+			() -> intakeRoller.setPower(power.getAsDouble()),
+			interrupted -> intakeRoller.stop(),
+			() -> false,
+			intakeRoller
+		).withName("set power by supplier");
+	}
     //@formatter:on
 
 	public Command stop() {

0dc88b6ff0f96a0610bfef5c4aa27cb3527cdd59
Nitay4 <nitay@rotbart.co.il>
2024-10-06 03:43:13 -0400

    Nitay - working on cr

--- src/main/java/frc/robot/subsystems/flywheel/FlywheelState.java
+++ src/main/java/frc/robot/subsystems/flywheel/FlywheelState.java
@@ -5,7 +5,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public enum FlywheelState {
 
 	DEFAULT(Rotation2d.fromRotations(40)),
-	SHOOTER_OUTTAKE(Rotation2d.fromRotations(30)),
+	SHOOTER_OUTTAKE(Rotation2d.fromRotations(50)),
 	SHOOTING(Rotation2d.fromRotations(60));
 
 	private final Rotation2d velocity;

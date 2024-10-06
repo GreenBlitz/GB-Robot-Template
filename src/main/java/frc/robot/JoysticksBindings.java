883085fcf88109a3c56f8cbbc4920c027d0fb860
YHerm <0549405422.yh@gmail.com>
2024-10-06 17:35:44 +0300

    remove ninds

--- src/main/java/frc/robot/JoysticksBindings.java
+++ src/main/java/frc/robot/JoysticksBindings.java
@@ -26,17 +26,6 @@ public class JoysticksBindings {
 	private static void mainJoystickButtons(Robot robot) {
 		SmartJoystick usedJoystick = MAIN_JOYSTICK;
 		// bindings...
-		robot.getSwerve().setDefaultCommand(robot.getSwerve().getCommandsBuilder().driveBySavedState(() -> 0, () -> 0, () -> 0));
-
-		usedJoystick.A.onTrue(robot.getSuperstructure().setState(RobotState.IDLE));
-		usedJoystick.B.onTrue(robot.getSuperstructure().setState(RobotState.SHOOTER_INTAKE));
-		usedJoystick.X.onTrue(robot.getSuperstructure().setState(RobotState.ARM_INTAKE));
-		usedJoystick.Y.onTrue(robot.getSuperstructure().setState(RobotState.PRE_AMP));
-		usedJoystick.POV_UP.onTrue(robot.getSuperstructure().setState(RobotState.PRE_SPEAKER));
-		usedJoystick.POV_LEFT.onTrue(robot.getSuperstructure().setState(RobotState.TRANSFER_SHOOTER_TO_ARM));
-		usedJoystick.POV_RIGHT.onTrue(robot.getSuperstructure().setState(RobotState.TRANSFER_ARM_TO_SHOOTER));
-
-		usedJoystick.L1.whileTrue(robot.getRoller().getCommandsBuilder().setPower(() -> usedJoystick.getAxisValue(Axis.LEFT_Y)));
 	}
 
 	private static void secondJoystickButtons(Robot robot) {

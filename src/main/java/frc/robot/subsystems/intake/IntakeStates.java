96880703117c7479b3ec2eedf871dc3d88a2b11a
Maya Romm <maya.romm0401@gmail.com>
2024-10-08 15:03:29 +0300

    maya- spotless

--- src/main/java/frc/robot/subsystems/intake/IntakeStates.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeStates.java
@@ -1,18 +1,20 @@
 package frc.robot.subsystems.intake;
+
 public enum IntakeStates {
 
-    NOTE_TO_SHOOTER(0),
-    INTAKE(0),
-    STOP(0),
-    SHOOTER_TO_ELEVATOR(0);
+	NOTE_TO_SHOOTER(0),
+	INTAKE(0),
+	STOP(0),
+	SHOOTER_TO_ELEVATOR(0);
 
-    private final double power;
+	private final double power;
 
-    IntakeStates(double power) {
-        this.power = power;
-    }
+	IntakeStates(double power) {
+		this.power = power;
+	}
 
-    public double getPower() {
-        return power;
-    }
+	public double getPower() {
+		return power;
+	}
+
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeStatesHandler.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeStatesHandler.java
@@ -4,14 +4,14 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public class IntakeStatesHandler {
 
-        private final IntakeRoller intakeRoller;
+	private final IntakeRoller intakeRoller;
 
-        public IntakeStatesHandler(IntakeRoller intakeRoller) {
-            this.intakeRoller = intakeRoller;
-        }
+	public IntakeStatesHandler(IntakeRoller intakeRoller) {
+		this.intakeRoller = intakeRoller;
+	}
 
-        public Command setState(IntakeStates intakeStates) {
-            return intakeRoller.getCommandsBuilder().setPower(intakeStates.getPower());
-        }
+	public Command setState(IntakeStates intakeStates) {
+		return intakeRoller.getCommandsBuilder().setPower(intakeStates.getPower());
+	}
 
 }

5d89961daaf22f9c7fd2c80ce01c0677dd9cb578
Shahar Perets <perets.shahar@gmail.com>
2024-10-09 19:58:16 +0300

    Shahar: fixed log path

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.elevator;
 
 public class ElevatorConstants {
 
-	public static final String LOG_PATH = "Elevator/";
+	public static final String LOG_PATH = "Subsystems/Elevator/";
 
 	public final static double GEAR_RATIO = 48.0 / 14.0;
 

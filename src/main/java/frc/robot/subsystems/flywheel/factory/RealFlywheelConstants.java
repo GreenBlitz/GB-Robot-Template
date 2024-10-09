d073d0c6bc3dc714358161a6d4f61863e6d3ffc6
Nitay4 <nitay@rotbart.co.il>
2024-10-09 08:21:29 -0400

    Nitay - firmat

--- src/main/java/frc/robot/subsystems/flywheel/factory/RealFlywheelConstants.java
+++ src/main/java/frc/robot/subsystems/flywheel/factory/RealFlywheelConstants.java
@@ -30,7 +30,7 @@ public class RealFlywheelConstants {
 
 	private static final Function<Rotation2d, Double> feedForwardCalculator = velocity -> feedforward.calculate(velocity.getRotations());
 
-	private static final double GEAR_RATIO = 1 / 1;
+	private static final double GEAR_RATIO = 1;
 
 	private static SysIdRoutine.Config generateSysidConfig() {
 		return new SysIdRoutine.Config(

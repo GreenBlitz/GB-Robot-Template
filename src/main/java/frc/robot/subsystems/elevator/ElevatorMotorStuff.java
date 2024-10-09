481b37fe702e9a83defdbffc9a1b549745004958
Shahar Perets <perets.shahar@gmail.com>
2024-10-09 23:28:17 +0300

    Shahar: CR

--- src/main/java/frc/robot/subsystems/elevator/ElevatorMotorStuff.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorMotorStuff.java
@@ -4,4 +4,10 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.hardware.motor.ControllableMotor;
 import frc.robot.hardware.signal.InputSignal;
 
-public record ElevatorMotorStuff(ControllableMotor motor, InputSignal<Double> voltageSignal, InputSignal<Rotation2d> positionSignal) {}
+//@formatter:off
+public record ElevatorMotorStuff(
+	ControllableMotor motor,
+	InputSignal<Double> voltageSignal,
+	InputSignal<Rotation2d> positionSignal
+) {}
+//@formatter:on

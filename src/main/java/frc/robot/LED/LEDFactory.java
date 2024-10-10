680bd6393c6934023571c61131a3fc16b0e36c75
Noam Kam <noamkam@Noams-MacBook-Air.local>
2024-10-10 22:11:36 +0300

    noam - changed param in the factory

--- src/main/java/frc/robot/LED/LEDFactory.java
+++ src/main/java/frc/robot/LED/LEDFactory.java
@@ -1,13 +1,12 @@
 package frc.robot.LED;
 
-import frc.robot.RobotType;
+import frc.robot.Robot;
 
 public class LEDFactory {
 
-	public static ILED create(RobotType robotType) {
-		return switch (robotType) {
+	public static ILED create() {
+		return switch (Robot.ROBOT_TYPE) {
 			case REAL -> LEDConstants.LEDFactory.LED_IS_CANDLE ? new CANdle() : new LEDStrip();
-
 			case SIMULATION -> null;
 		};
 	}

431a59b65f4ca89a8a6f133ffb610ca374f7b695
YHerm <0549405422.yh@gmail.com>
2024-10-11 16:26:48 +0300

    swerve toleranmce

--- src/main/java/frc/robot/superstructure/Tolerances.java
+++ src/main/java/frc/robot/superstructure/Tolerances.java
@@ -4,8 +4,8 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public class Tolerances {
 
-	public static final Rotation2d SWERVE_HEADING = Rotation2d.fromDegrees(1.5);
-	public static final Rotation2d ROTATION_VELOCITY_DEADBAND = Rotation2d.fromRadians(0.05);
+	public static final Rotation2d SWERVE_HEADING = Rotation2d.fromDegrees(3);
+	public static final Rotation2d ROTATION_VELOCITY_DEADBAND = Rotation2d.fromRadians(0.2);
 	public static final double TRANSLATION_METERS = 0.05;
 	public static final double TRANSLATION_VELOCITY_DEADBAND = 0.05;
 

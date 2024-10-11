e41bc0cab76b3c3ab68b517285b847efb8c767b9
YHerm <0549405422.yh@gmail.com>
2024-10-11 16:26:04 +0300

    pivot forward limit

--- src/main/java/frc/robot/subsystems/pivot/PivotConstants.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotConstants.java
@@ -8,7 +8,7 @@ public class PivotConstants {
 
 	public static final double GEAR_RATIO = 15.0 * (72.0 / 14.0) * 2.0;
 
-	public static final Rotation2d FORWARD_ANGLE_LIMIT = Rotation2d.fromDegrees(60);
+	public static final Rotation2d FORWARD_ANGLE_LIMIT = Rotation2d.fromDegrees(67);
 	public static final Rotation2d BACKWARD_ANGLE_LIMIT = Rotation2d.fromDegrees(17);
 
 	public static final Rotation2d MINIMUM_ACHIEVABLE_ANGLE = Rotation2d.fromDegrees(16);

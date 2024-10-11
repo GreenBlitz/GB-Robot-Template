c83600a1b2e87e2c8727a576f7b07ad1c73cda6a
NoamRosenberg08 <noamrosenberg097@gmail.com>
2024-10-11 12:24:34 +0300

    crd

--- src/main/java/frc/robot/superstructure/climb/ClimbStateHandler.java
+++ src/main/java/frc/robot/superstructure/climb/ClimbStateHandler.java
@@ -1,7 +1,6 @@
 package frc.robot.superstructure.climb;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.lifter.LifterState;
 import frc.robot.subsystems.lifter.LifterStateHandler;
 import frc.robot.subsystems.solenoid.SolenoidState;
@@ -21,20 +20,14 @@ public class ClimbStateHandler {
 		return switch (state) {
 			case STOP -> lifterStateHandler.setState(LifterState.HOLD).alongWith(solenoidStateHandler.setState(SolenoidState.OFF));
 			case EXTEND ->
-				(lifterStateHandler.setState(LifterState.BACKWARD).raceWith(new WaitCommand(ClimbConstants.SOLENOID_RELEASE_TIME_SECONDS)))
+				(lifterStateHandler.setState(LifterState.BACKWARD).withTimeout(ClimbConstants.SOLENOID_RELEASE_TIME_SECONDS))
 					.andThen(
-						(solenoidStateHandler.setState(SolenoidState.RETRACT))
-							.raceWith(new WaitCommand(ClimbConstants.SOLENOID_RETRACTING_UNTIL_HOLDING_TIME))
+						(solenoidStateHandler.setState(SolenoidState.RETRACT)).withTimeout(ClimbConstants.SOLENOID_RETRACTING_UNTIL_HOLDING_TIME)
 					)
-					.andThen(
-						lifterStateHandler.setState(LifterState.EXTENDED)
-							.alongWith(
-								solenoidStateHandler.setState(SolenoidState.HOLD)
-									.raceWith(new WaitCommand(ClimbConstants.SOLENOID_TIMEOUT_SECONDS))
-									.andThen(solenoidStateHandler.setState(SolenoidState.OFF))
-							)
-					);
+					.andThen(lifterStateHandler.setState(LifterState.EXTENDED).raceWith(solenoidStateHandler.setState(SolenoidState.HOLD)))
+					.andThen(solenoidStateHandler.setState(SolenoidState.OFF));
 			case RETRACT -> lifterStateHandler.setState(LifterState.RETRACTED).alongWith(solenoidStateHandler.setState(SolenoidState.OFF));
 		};
 	}
+
 }

0068e0cdee32f10299a6ab9e478f4a346c66419e
YHerm <0549405422.yh@gmail.com>
2024-10-13 22:04:23 +0300

    crreeate

--- src/main/java/frc/robot/subsystems/wrist/factory/WristFactory.java
+++ src/main/java/frc/robot/subsystems/wrist/factory/WristFactory.java
@@ -5,7 +5,7 @@ import frc.robot.subsystems.wrist.WristStuff;
 
 public class WristFactory {
 
-	public static WristStuff generateWristStuff(String logPath) {
+	public static WristStuff create(String logPath) {
 		return switch (Robot.ROBOT_TYPE) {
 			case REAL -> RealWristConstants.generateWristStuff(logPath);
 			case SIMULATION -> null;

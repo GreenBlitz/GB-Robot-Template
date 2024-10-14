730466fbd1003c88aaa991ac2ee67741e412ed6c
itamaroryan <itamar.oryan@gmail.com>
2024-10-14 11:35:58 +0300

    crd

--- src/main/java/frc/robot/subsystems/lifter/LifterState.java
+++ src/main/java/frc/robot/subsystems/lifter/LifterState.java
@@ -10,8 +10,7 @@ public enum LifterState {
 
 	private final double targetPosition;
 	private final double power;
-
-
+	
 	LifterState(double targetPosition, double power) {
 		this.targetPosition = targetPosition;
 		this.power = power;

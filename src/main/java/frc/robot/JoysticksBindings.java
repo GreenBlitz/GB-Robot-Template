693b5058e4cf16b65e168c0b1612749341c305d9
YHerm <0549405422.yh@gmail.com>
2024-10-16 23:39:09 +0300

    format

--- src/main/java/frc/robot/JoysticksBindings.java
+++ src/main/java/frc/robot/JoysticksBindings.java
@@ -1,8 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.utils.joysticks.JoystickPorts;
 import frc.utils.joysticks.SmartJoystick;
 

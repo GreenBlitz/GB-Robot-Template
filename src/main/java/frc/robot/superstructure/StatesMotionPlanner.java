b52777f7229296b391cf367d36b6473f56a140e3
YHerm <0549405422.yh@gmail.com>
2024-10-16 12:31:29 +0300

    states m planner

--- src/main/java/frc/robot/superstructure/StatesMotionPlanner.java
+++ src/main/java/frc/robot/superstructure/StatesMotionPlanner.java
@@ -1,6 +1,7 @@
 package frc.robot.superstructure;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class StatesMotionPlanner {
 
@@ -13,9 +14,11 @@ public class StatesMotionPlanner {
 	public Command setState(RobotState state) {
 		return switch (state) {
 			case INTAKE, ARM_INTAKE, SPEAKER, TRANSFER_SHOOTER_TO_ARM, TRANSFER_ARM_TO_SHOOTER ->
-				superstructure.setState(state)
-					.until(superstructure::isEnableChangeStateAutomatically)
-					.andThen(superstructure.setState(RobotState.IDLE));
+				new SequentialCommandGroup(
+					superstructure.enableChangeStateAutomatically(false),
+					superstructure.setState(state).until(superstructure::isEnableChangeStateAutomatically),
+					superstructure.setState(RobotState.IDLE)
+				);
 			case INTAKE_OUTTAKE, AMP, ARM_OUTTAKE, PRE_AMP, PRE_SPEAKER, IDLE -> superstructure.setState(state);
 		};
 	}

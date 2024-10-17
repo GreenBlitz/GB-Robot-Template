ba54a152b76e2a0cfe499cf3186b5b05dba52029
Shahar Perets <perets.shahar@gmail.com>
2024-10-17 16:58:27 +0300

    Shahar: spotless

--- src/main/java/frc/robot/poseestimator/PoseEstimationMath.java
+++ src/main/java/frc/robot/poseestimator/PoseEstimationMath.java
@@ -6,7 +6,6 @@ import frc.robot.constants.Field;
 import frc.robot.poseestimator.observations.VisionObservation;
 import frc.robot.vision.limelights.LimelightRawData;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -94,16 +93,12 @@ public class PoseEstimationMath {
 		ArrayList<Double> XSet = new ArrayList<>();
 		ArrayList<Double> YSet = new ArrayList<>();
 		ArrayList<Double> AngleSet = new ArrayList<>();
-		for (Pose2d data : dataset)  {
+		for (Pose2d data : dataset) {
 			XSet.add(data.getX());
 			YSet.add(data.getY());
 			AngleSet.add(data.getRotation().getRadians());
 		}
-		return new double[] {
-			function.apply(XSet),
-			function.apply(YSet),
-			function.apply(AngleSet)
-		};
+		return new double[] {function.apply(XSet), function.apply(YSet), function.apply(AngleSet)};
 	}
 
 	public static double[] calculateStandardDeviationOfPose(LimelightRawData limelightRawData, Pose2d currentEstimatedPose) {
@@ -118,9 +113,7 @@ public class PoseEstimationMath {
 
 	public static Pose2d meanOfPose(ArrayList<Pose2d> dataset) {
 		double[] deconstructPose = applyFunctionOnPose(dataset, PoseEstimationMath::mean);
-		return new Pose2d(
-			deconstructPose[0], deconstructPose[1], Rotation2d.fromRadians(deconstructPose[2])
-		);
+		return new Pose2d(deconstructPose[0], deconstructPose[1], Rotation2d.fromRadians(deconstructPose[2]));
 	}
 
 	public static double mean(ArrayList<Double> dataset) {
--- src/main/java/frc/robot/poseestimator/VisionDenoiser.java
+++ src/main/java/frc/robot/poseestimator/VisionDenoiser.java
@@ -1,7 +1,6 @@
 package frc.robot.poseestimator;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.interpolation.Interpolatable;
 import edu.wpi.first.math.interpolation.TimeInterpolatableBuffer;
 import frc.robot.poseestimator.observations.VisionObservation;
 import frc.utils.time.TimeUtils;
@@ -41,11 +40,13 @@ public class VisionDenoiser {
 			return Optional.empty();
 		}
 		ArrayList<Pose2d> poses = getRobotPoseObservations();
-		return Optional.of(new VisionObservation(
-			PoseEstimationMath.meanOfPose(poses),
-			PoseEstimationMath.calculateStandardDeviationOfPose(poses),
-			overwrittenObservations.peekLast().timestamp()
-		));
+		return Optional.of(
+			new VisionObservation(
+				PoseEstimationMath.meanOfPose(poses),
+				PoseEstimationMath.calculateStandardDeviationOfPose(poses),
+				overwrittenObservations.peekLast().timestamp()
+			)
+		);
 	}
 
 	public Optional<VisionObservation> calculateAverage() {
@@ -57,44 +58,55 @@ public class VisionDenoiser {
 			return Optional.empty();
 		}
 		ArrayList<Pose2d> poses = getRobotPoseObservations();
-		return Optional.of(new VisionObservation(
-			PoseEstimationMath.weightedPoseMean(overwrittenObservations),
-			PoseEstimationMath.calculateStandardDeviationOfPose(poses),
-			overwrittenObservations.peekLast().timestamp()
-		));
+		return Optional.of(
+			new VisionObservation(
+				PoseEstimationMath.weightedPoseMean(overwrittenObservations),
+				PoseEstimationMath.calculateStandardDeviationOfPose(poses),
+				overwrittenObservations.peekLast().timestamp()
+			)
+		);
 	}
 
 	public Optional<VisionObservation> calculateWeightedAverage() {
 		return calculateWeightedAverage(observations);
 	}
 
-	private LinkedList<VisionObservation> fixAccordingToOdometry(Pose2d currentPose, Pose2d odometryPose, TimeInterpolatableBuffer<Pose2d> odometryPoseInterpolator) {
+	private LinkedList<VisionObservation>
+		fixAccordingToOdometry(Pose2d currentPose, Pose2d odometryPose, TimeInterpolatableBuffer<Pose2d> odometryPoseInterpolator) {
 		LinkedList<VisionObservation> output = new LinkedList<>();
 		for (VisionObservation observation : observations) {
 			double currentTimestamp = TimeUtils.getCurrentTimeSeconds();
 			Optional<Pose2d> odometryFix = odometryPoseInterpolator.getSample(currentTimestamp);
 			odometryFix.ifPresent(odometryPoseSample -> {
-				Pose2d currentEstimation = PoseEstimationMath
-					.combineVisionToOdometry(observation, odometryPoseSample, currentPose, odometryPose, PoseEstimatorConstants.DEFAULT_ODOMETRY_STANDARD_DEVIATIONS);
+				Pose2d currentEstimation = PoseEstimationMath.combineVisionToOdometry(
+					observation,
+					odometryPoseSample,
+					currentPose,
+					odometryPose,
+					PoseEstimatorConstants.DEFAULT_ODOMETRY_STANDARD_DEVIATIONS
+				);
 				Pose2d fixedPose = new Pose2d(currentEstimation.getTranslation(), odometryPoseSample.getRotation());
-				output.add(new VisionObservation(
-					fixedPose,
-					PoseEstimationMath.calculateStandardDeviationOfPose(new ArrayList<>(List.of(fixedPose, currentPose))),
-					currentTimestamp
-				));
+				output.add(
+					new VisionObservation(
+						fixedPose,
+						PoseEstimationMath.calculateStandardDeviationOfPose(new ArrayList<>(List.of(fixedPose, currentPose))),
+						currentTimestamp
+					)
+				);
 			});
 		}
 		return output;
 	}
 
-	public Optional<VisionObservation> calculateAverageFixedPose(Pose2d currentPose, Pose2d odometryPose, TimeInterpolatableBuffer<Pose2d> odometryPoseInterpolator) {
+	public Optional<VisionObservation>
+		calculateAverageFixedPose(Pose2d currentPose, Pose2d odometryPose, TimeInterpolatableBuffer<Pose2d> odometryPoseInterpolator) {
 		return calculateAverage(fixAccordingToOdometry(currentPose, odometryPose, odometryPoseInterpolator));
 	}
 
-	public Optional<VisionObservation> calculateWeightedAverageFixedPose(Pose2d currentPose, Pose2d odometryPose, TimeInterpolatableBuffer<Pose2d> odometryPoseInterpolator) {
+	public Optional<VisionObservation>
+		calculateWeightedAverageFixedPose(Pose2d currentPose, Pose2d odometryPose, TimeInterpolatableBuffer<Pose2d> odometryPoseInterpolator) {
 		return calculateWeightedAverage(fixAccordingToOdometry(currentPose, odometryPose, odometryPoseInterpolator));
 	}
-
 
 
 }

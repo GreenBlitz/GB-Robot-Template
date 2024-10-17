1cad47e40f0a56142788d42c6e1dfa017353c620
GreenBlitz <greenblitz4590@gmail.com>
2024-10-17 21:27:46 +0300

    stats

--- src/main/java/frc/robot/subsystems/funnel/FunnelState.java
+++ src/main/java/frc/robot/subsystems/funnel/FunnelState.java
@@ -2,12 +2,12 @@ package frc.robot.subsystems.funnel;
 
 public enum FunnelState {
 
-	SPEAKER(0.6),
+	SPEAKER(0.8),
 	AMP(-0.6),
-	INTAKE_OUTTAKE(-0.4),
-	SHOOTER_OUTTAKE(0.4),
+	INTAKE_OUTTAKE(-0.8),
+	SHOOTER_OUTTAKE(0.7),
 	NOTE_TO_SHOOTER(0.5),
-	SHOOTER_TO_ELEVATOR(-0.5),
+	SHOOTER_TO_ELEVATOR(-0.7),
 	STOP(0);
 
 	private final double power;

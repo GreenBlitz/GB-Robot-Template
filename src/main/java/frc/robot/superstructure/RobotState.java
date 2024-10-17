bef3e905fae5ef6fd1daabd577261470efa64e9c
YHerm <0549405422.yh@gmail.com>
2024-10-17 09:47:11 +0300

    robot states clean

--- src/main/java/frc/robot/superstructure/RobotState.java
+++ src/main/java/frc/robot/superstructure/RobotState.java
@@ -5,17 +5,16 @@ public enum RobotState {
 	IDLE,
 	INTAKE,
 	ARM_INTAKE,
-//	SHOOTER_OUTTAKE, // intake, pivot, funnel
-	SPEAKER, // Funnel
-	AMP, // Roller
-	PRE_CLIMB,
-	CLIMB,
-	TRAP,
 	PRE_SPEAKER,
+	SPEAKER,
 	PRE_AMP,
+	AMP,
 	TRANSFER_SHOOTER_TO_ARM,
 	TRANSFER_ARM_TO_SHOOTER,
 	INTAKE_OUTTAKE,
 	ARM_OUTTAKE,
+	PRE_CLIMB,
+	CLIMB,
+	TRAP,
 
 }

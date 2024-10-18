3adc83cea7dc476185e7f9dee81041d6779cae8e
YHerm <0549405422.yh@gmail.com>
2024-10-18 22:40:50 +0300

    order

--- src/main/java/frc/robot/superstructure/Timeouts.java
+++ src/main/java/frc/robot/superstructure/Timeouts.java
@@ -4,12 +4,12 @@ public class Timeouts {
 
 	public static final double INTAKE_FUNNEL_SECONDS = 0.4;
 	public static final double INTAKE_ROLLER_SECONDS = 0.5;
-	public static final double AMP_RELEASE_SECONDS = 3;
 	public static final double SHOOTING_SECONDS = 3;
+	public static final double AMP_RELEASE_SECONDS = 3;
 	public static final double WRIST_TO_POSITION_SECONDS = 0.7;
 	public static final double TRANSFER_SHOOTER_ARM_SECONDS = 0.6;
-	public static final double INTAKE_ARM_1_ROTATION_SECONDS = 0.15;
 	public static final double TRANSFER_ARM_SHOOTER_SECONDS = 0.5;
+	public static final double INTAKE_ARM_1_ROTATION_SECONDS = 0.15;
 	public static final double NOTE_IN_RUMBLE = 0.2;
 
 }

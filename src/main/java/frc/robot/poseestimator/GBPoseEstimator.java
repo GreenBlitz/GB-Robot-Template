1f8ecc0cdef54f0eb46a609b91480608a3cd9229
Shahar Perets <perets.shahar@gmail.com>
2024-10-18 12:09:37 +0300

    Shahar: gyro angle for MT2 is now correct (though it still doesn't work

--- src/main/java/frc/robot/poseestimator/GBPoseEstimator.java
+++ src/main/java/frc/robot/poseestimator/GBPoseEstimator.java
@@ -190,7 +190,7 @@ public class GBPoseEstimator extends GBSubsystem implements IPoseEstimator {
 		if (!hasHeadingOffsetBeenInitialized) {
 			calculateHeadingOffset(observation.gyroAngle());
 		}
-		updateGyroAnglesInLimeLight(observation.gyroAngle());
+		updateGyroAnglesInLimeLight(observation.gyroAngle().plus(headingOffset));
 		Twist2d twist = kinematics.toTwist2d(latestWheelPositions, observation.wheelsPositions());
 		twist = PoseEstimationMath.addGyroToTwist(twist, observation.gyroAngle().plus(headingOffset), latestGyroAngle.plus(headingOffset));
 		latestGyroAngle = observation.gyroAngle();

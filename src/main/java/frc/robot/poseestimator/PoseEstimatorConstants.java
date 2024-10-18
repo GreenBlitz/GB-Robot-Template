c2b7d2b63cd3fe38049b86e92911b0d77e69b490
Shahar Perets <perets.shahar@gmail.com>
2024-10-18 22:57:52 +0300

    Shahar: magic numbers

--- src/main/java/frc/robot/poseestimator/PoseEstimatorConstants.java
+++ src/main/java/frc/robot/poseestimator/PoseEstimatorConstants.java
@@ -5,6 +5,8 @@ public class PoseEstimatorConstants {
 
 	protected static final String LOG_PATH = "PoseEstimator/";
 
+	public static final int LINEAR_FILTER_SAMPLES_FOR_EACH_VISION_CALCULATION = 20;
+
 	protected static final int VISION_OBSERVATION_COUNT_FOR_AVERAGED_POSE_CALCULATION = 25;
 
 	protected static final double POSE_BUFFER_SIZE_SECONDS = 2.0;

f1bd21672919944b124743e608cb29baa517f126
YHerm <0549405422.yh@gmail.com>
2024-10-18 22:27:43 +0300

    format

--- src/main/java/frc/robot/superstructure/Timeouts.java
+++ src/main/java/frc/robot/superstructure/Timeouts.java
@@ -2,7 +2,7 @@ package frc.robot.superstructure;
 
 public class Timeouts {
 
-    public static final double AMP_SECONDS = 3;
-    public static final double TRANSFER_SHOOTER_ARM_SECONDS = 1.5;
+	public static final double AMP_SECONDS = 3;
+	public static final double TRANSFER_SHOOTER_ARM_SECONDS = 1.5;
 
 }

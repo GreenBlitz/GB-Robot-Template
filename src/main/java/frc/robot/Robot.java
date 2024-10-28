f38b19fa8560b4dbbca673461289c7d7f44c9b13
itamaroryan <itamar.oryan@gmail.com>
2024-10-28 18:13:36 +0200

    dror ahh git push:

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.subsystems.tryddddd;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very little robot logic should
@@ -19,7 +18,6 @@ public class Robot {
 
 	public Robot() {
 		configureBindings();
-		tryddddd tryddddd = new tryddddd("ahhh");
 	}
 
 	private void configureBindings() {

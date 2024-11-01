680902b31f9a9cbc9f4e6a03aca4b81eb44eef92
YHerm <0549405422.yh@gmail.com>
2024-11-01 14:58:46 +0200

    fix

--- src/main/java/frc/robot/hardware/request/phoenix6/Phoenix6RequestBuilder.java
+++ src/main/java/frc/robot/hardware/request/phoenix6/Phoenix6RequestBuilder.java
@@ -9,19 +9,27 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public class Phoenix6RequestBuilder {
 
 	public static Phoenix6Request<Rotation2d> build(PositionVoltage positionVoltage) {
-		return new Phoenix6Request<>(new Rotation2d(), positionVoltage, setPoint -> positionVoltage.withPosition(setPoint.getRotations()));
+		return new Phoenix6Request<>(
+			Rotation2d.fromRotations(positionVoltage.Position),
+			positionVoltage,
+			setPoint -> positionVoltage.withPosition(setPoint.getRotations())
+		);
 	}
 
 	public static Phoenix6Request<Rotation2d> build(VelocityVoltage velocityVoltage) {
-		return new Phoenix6Request<>(new Rotation2d(), velocityVoltage, setPoint -> velocityVoltage.withVelocity(setPoint.getRotations()));
+		return new Phoenix6Request<>(
+			Rotation2d.fromRotations(velocityVoltage.Velocity),
+			velocityVoltage,
+			setPoint -> velocityVoltage.withVelocity(setPoint.getRotations())
+		);
 	}
 
 	public static Phoenix6Request<Double> build(VoltageOut voltageOut) {
-		return new Phoenix6Request<>(0.0, voltageOut, voltageOut::withOutput);
+		return new Phoenix6Request<>(voltageOut.Output, voltageOut, voltageOut::withOutput);
 	}
 
 	public static Phoenix6Request<Double> build(TorqueCurrentFOC torqueCurrentFOC) {
-		return new Phoenix6Request<>(0.0, torqueCurrentFOC, torqueCurrentFOC::withOutput);
+		return new Phoenix6Request<>(torqueCurrentFOC.Output, torqueCurrentFOC, torqueCurrentFOC::withOutput);
 	}
 
 }

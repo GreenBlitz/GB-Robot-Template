1fbf52ffb66df8561e8a72fb09db7d4b3311b70c
YHerm <0549405422.yh@gmail.com>
2024-11-07 22:03:55 +0200

    mk4i

--- src/main/java/frc/robot/subsystems/swerve/factories/modules/SimulationModuleGenerator.java
+++ src/main/java/frc/robot/subsystems/swerve/factories/modules/SimulationModuleGenerator.java
@@ -14,10 +14,10 @@ public class SimulationModuleGenerator {
 	private static final DCMotor STEER_MOTOR = DCMotor.getFalcon500(1);
 	private static final double DRIVE_CURRENT_LIMIT_AMPS = 60;
 	private static final SwerveModuleSimulation.DRIVE_WHEEL_TYPE DRIVE_WHEEL_TYPE = SwerveModuleSimulation.DRIVE_WHEEL_TYPE.RUBBER;
-	private static final int GEAR_RATIO_LEVEL = 3; // L3
+	private static final int GEAR_RATIO_LEVEL = 3; // L3 gear ratio
 
 	public static Supplier<SwerveModuleSimulation> generate() {
-		return SwerveModuleSimulation.getMark4(DRIVE_MOTOR, STEER_MOTOR, DRIVE_CURRENT_LIMIT_AMPS, DRIVE_WHEEL_TYPE, GEAR_RATIO_LEVEL);
+		return SwerveModuleSimulation.getMark4i(DRIVE_MOTOR, STEER_MOTOR, DRIVE_CURRENT_LIMIT_AMPS, DRIVE_WHEEL_TYPE, GEAR_RATIO_LEVEL);
 	}
 
 	private static final PIDConstants STEER_PID_RADIANS = new PIDConstants(7);

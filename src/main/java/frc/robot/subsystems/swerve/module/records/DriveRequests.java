b12395bf391a4960c6edc7a269db885d24b02031
YHerm <0549405422.yh@gmail.com>
2024-11-11 21:48:57 +0200

    fix

--- src/main/java/frc/robot/subsystems/swerve/module/records/DriveRequests.java
+++ src/main/java/frc/robot/subsystems/swerve/module/records/DriveRequests.java
@@ -4,11 +4,4 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.hardware.interfaces.IRequest;
 import frc.robot.hardware.interfaces.InputSignal;
 
-public record DriveRequests(
-	IRequest<Rotation2d> velocityRequest,
-	IRequest<Double> voltageRequest,
-	InputSignal<Rotation2d> positionSignal,
-	InputSignal<Rotation2d> velocitySignal,
-	InputSignal<Double> currentSignal,
-	InputSignal<Double> voltageSignal
-) {}
+public record DriveRequests(IRequest<Rotation2d> velocityRequest, IRequest<Double> voltageRequest) {}

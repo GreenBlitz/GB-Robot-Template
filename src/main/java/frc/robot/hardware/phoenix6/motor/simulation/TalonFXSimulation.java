186150d0be3c075cfae00f0af4147065b4888685
hillelv <hillel.vider@gmail.com>
2024-11-13 19:37:36 +0200

    remove funnt func

--- src/main/java/frc/robot/hardware/phoenix6/motor/simulation/TalonFXSimulation.java
+++ src/main/java/frc/robot/hardware/phoenix6/motor/simulation/TalonFXSimulation.java
@@ -13,10 +13,6 @@ public class TalonFXSimulation {
 	private final TalonFXSimState motorSimState;
 	private final MechanismSimulation mechanismSimulation;
 
-	public TalonFXSimState getMotorSimState() {
-		return motorSimState;
-	}
-
 	public TalonFXSimulation(TalonFXWrapper talonFXWrapper, TalonFXConfiguration configuration, MechanismSimulation simulation) {
 		this.mechanismSimulation = simulation;
 		this.motorSimState = talonFXWrapper.getSimState();

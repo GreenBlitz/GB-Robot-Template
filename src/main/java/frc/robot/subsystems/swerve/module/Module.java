7c58c4e4debd19de28fea8cfd9bfa6618d2b8e61
YHerm <0549405422.yh@gmail.com>
2024-11-14 23:33:28 +0200

    update simulation

--- src/main/java/frc/robot/subsystems/swerve/module/Module.java
+++ src/main/java/frc/robot/subsystems/swerve/module/Module.java
@@ -98,6 +98,9 @@ public class Module {
 	}
 
 	public void updateInputs() {
+		steer.updateSimulation();
+		drive.updateSimulation();
+
 		encoder.updateInputs(encoderStuff.positionSignal());
 		steer.updateInputs(steerStuff.positionSignal(), steerStuff.velocitySignal(), steerStuff.currentSignal(), steerStuff.voltageSignal());
 		drive.updateInputs(driveStuff.positionSignal(), driveStuff.velocitySignal(), driveStuff.currentSignal(), driveStuff.voltageSignal());
@@ -149,7 +152,7 @@ public class Module {
 
 	public void pointSteer(Rotation2d steerTargetPosition, boolean optimize) {
 		SwerveModuleState moduleState = new SwerveModuleState(0, steerTargetPosition);
-		targetState.angle = optimize ? SwerveModuleState.optimize(moduleState, getSteerPosition()).angle : moduleState.angle;
+		targetState.angle = optimize ? targetState.optimize(moduleState, getSteerPosition()).angle : moduleState.angle;
 		setTargetSteerPosition(targetState.angle);
 	}
 

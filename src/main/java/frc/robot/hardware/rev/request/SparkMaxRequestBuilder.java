a9a00dcc42f72e75176f0c30be6e79b165dfe437
greenblitz <greenblitz@gmail.com>
2024-11-14 15:48:54 +0200

    funny

--- src/main/java/frc/robot/hardware/rev/request/SparkMaxRequestBuilder.java
+++ src/main/java/frc/robot/hardware/rev/request/SparkMaxRequestBuilder.java
@@ -2,6 +2,7 @@ package frc.robot.hardware.rev.request;
 
 import com.revrobotics.CANSparkBase;
 import edu.wpi.first.math.geometry.Rotation2d;
+import frc.utils.Conversions;
 
 import java.util.function.Function;
 
@@ -13,10 +14,16 @@ public class SparkMaxRequestBuilder {
 		int pidSlot,
 		Function<Rotation2d, Double> feedforwardCalculator
 	) {
+		if (controlType == CANSparkBase.ControlType.kVelocity) {
+			return new SparkMaxRequest<>(Rotation2d.fromRotations(Conversions.perSecondToPerMinute(setPoint.getRotations())), controlType, pidSlot, feedforwardCalculator, Rotation2d::getRotations);
+		}
 		return new SparkMaxRequest<>(setPoint, controlType, pidSlot, feedforwardCalculator, Rotation2d::getRotations);
 	}
 
 	public static SparkMaxRequest<Rotation2d> build(Rotation2d setPoint, CANSparkBase.ControlType controlType, int pidSlot) {
+		if (controlType == CANSparkBase.ControlType.kVelocity) {
+			return new SparkMaxRequest<>(Rotation2d.fromRotations(Conversions.perSecondToPerMinute(setPoint.getRotations())), controlType, pidSlot, Rotation2d::getRotations);
+		}
 		return new SparkMaxRequest<>(setPoint, controlType, pidSlot, Rotation2d::getRotations);
 	}
 

d85165af96f1f1cee3cf80986717e50ef18c2191
YHerm <0549405422.yh@gmail.com>
2024-11-18 18:23:49 +0200

    gyro factory

--- src/main/java/frc/robot/subsystems/swerve/factories/gyro/GyroFactory.java
+++ src/main/java/frc/robot/subsystems/swerve/factories/gyro/GyroFactory.java
@@ -1,21 +1,36 @@
 package frc.robot.subsystems.swerve.factories.gyro;
 
 import frc.robot.Robot;
+import frc.robot.hardware.interfaces.IGyro;
+import frc.robot.hardware.phoenix6.gyro.Pigeon2Gyro;
 import frc.robot.subsystems.swerve.GyroSignals;
 import frc.robot.subsystems.swerve.SwerveType;
 
 public class GyroFactory {
 
-	private static GyroSignals createSwerveGyroStuff(String logPath) {
+	private static IGyro createSwerveGyro(String logPath) {
 		return switch (Robot.ROBOT_TYPE) {
-			case REAL -> RealGyroConstants.generateGyroStuff(logPath);
-			case SIMULATION -> null;// TODO
+			case REAL -> RealGyroConstants.generateGyro(logPath);
+			case SIMULATION -> SimulationGyroConstants.generateGyro(logPath);
 		};
 	}
 
-	public static GyroSignals create(SwerveType swerveType) {
+	public static IGyro createGyro(SwerveType swerveType) {
 		return switch (swerveType) {
-			case SWERVE -> createSwerveGyroStuff(swerveType.getLogPath() + "Gyro/");
+			case SWERVE -> createSwerveGyro(swerveType.getLogPath() + "Gyro/");
+		};
+	}
+
+	private static GyroSignals createSwerveGyroSignals(IGyro gyro) {
+		return switch (Robot.ROBOT_TYPE) {
+			case REAL -> RealGyroConstants.generateSignals((Pigeon2Gyro) gyro);
+			case SIMULATION -> SimulationGyroConstants.generateSignals();
+		};
+	}
+
+	public static GyroSignals createSignals(SwerveType swerveType, IGyro gyro) {
+		return switch (swerveType) {
+			case SWERVE -> createSwerveGyroSignals(gyro);
 		};
 	}
 

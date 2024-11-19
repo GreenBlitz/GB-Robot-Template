39b69aa2af700238f935f4c6ea712e0b32fed9ef
hillelv <hillel.vider@gmail.com>
2024-11-19 14:22:53 +0200

    singlejointed

---
+++ src/main/java/frc/robot/hardware/mechanisms/SingleJointedArmSimulation.java
@@ -0,0 +1,35 @@
+package frc.robot.hardware.mechanisms;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import frc.utils.time.TimeUtils;
+
+public class SingleJointedArmSimulation extends MechanismSimulation{
+
+    private final SingleJointedArmSim singleJointedArmSim;
+
+    public SingleJointedArmSimulation(SingleJointedArmSim singleJointedArmSim, double gearRatio) {
+        super(gearRatio);
+        this.singleJointedArmSim = singleJointedArmSim;
+    }
+
+    @Override
+    public Rotation2d getSystemPosition() {
+        return Rotation2d.fromRadians(singleJointedArmSim.getAngleRads());
+    }
+
+    @Override
+    public Rotation2d getSystemVelocityAnglesPerSecond() {
+        return Rotation2d.fromRadians(singleJointedArmSim.getVelocityRadPerSec());
+    }
+
+    @Override
+    public void setInputVoltage(double voltage) {
+        singleJointedArmSim.setInputVoltage(voltage);
+    }
+
+    @Override
+    public void updateMotor() {
+        singleJointedArmSim.update(TimeUtils.getCurrentCycleTimeSeconds());
+    }
+}

58488614ba9b81631c11feb3156a8b2535d3441e
Maya Romm <maya.romm0401@gmail.com>
2024-11-21 20:26:43 +0200

    maya- add getCommandsBuilder,setPower, setBrake, subsystemPeriodic and MotorCommandBuilder

--- src/main/java/frc/robot/subsystems/MotorSubsystemMaya.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystemMaya.java
@@ -1,4 +1,33 @@
 package frc.robot.subsystems;
 
-public class MotorSubsystemMaya {
+import frc.robot.hardware.interfaces.ControllableMotor;
+
+public class MotorSubsystemMaya extends GBSubsystem {
+
+	public ControllableMotor motor;
+	public final MotorCommandBuilder motorCommandBuilder;
+
+	public MotorSubsystemMaya(ControllableMotor motor, String logPath, MotorCommandBuilder motorCommandBuilder) {
+		super(logPath);
+		this.motor = motor;
+		this.motorCommandBuilder = motorCommandBuilder;
+	}
+
+	public MotorCommandBuilder getCommandsBuilder() {
+		return motorCommandBuilder;
+	}
+
+	public void setPower(double power) {
+		motor.setPower(power);
+	}
+
+	public void setBrake(boolean brake) {
+		motor.setBrake(brake);
+	}
+
+	@Override
+	protected void subsystemPeriodic() {
+		motor.updateInputs();
+	}
+
 }

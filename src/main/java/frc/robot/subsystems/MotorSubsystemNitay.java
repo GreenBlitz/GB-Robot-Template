627b96c07d117c8aa9150dc9d8ae5c9d02e36fe8
Nitay4 <nitay@rotbart.co.il>
2024-11-21 16:23:21 +0200

    Nitay - added constractor and arguments

--- src/main/java/frc/robot/subsystems/MotorSubsystemNitay.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystemNitay.java
@@ -1,4 +1,25 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.hardware.interfaces.ControllableMotor;
+import frc.robot.hardware.interfaces.IRequest;
+import frc.robot.hardware.interfaces.InputSignal;
+
 public class MotorSubsystemNitay {
+
+	ControllableMotor motor;
+	String logPath;
+	IRequest<Rotation2d> positionRequest;
+	IRequest<Rotation2d> velocityRequest;
+	InputSignal<Rotation2d> positionSignal;
+	InputSignal<Rotation2d> velocitySignal;
+	Rotation2d targetPosition;
+	Rotation2d targetVelocity;
+
+	public MotorSubsystemNitay(ControllableMotor motor, String logPath) {
+		this.motor = motor;
+		this.logPath = logPath;
+	}
+
+
 }

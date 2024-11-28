5d3e92e17f52025b70b355a35f96cbbc52952b72
Maya Romm <maya.romm0401@gmail.com>
2024-11-28 15:22:25 +0200

    maya - add getPosition, setTargetPosition, isAtPosition, resetPosition, convertToMeters, isPast, stayInPlace, isBehind, withPositionControl, FunctionalCommand -  moveToAngle, moveToAngle by supplier

--- src/main/java/frc/robot/subsystems/MotorSubsystemPosition.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystemPosition.java
@@ -6,23 +6,19 @@ import frc.robot.hardware.interfaces.ControllableMotor;
 import frc.robot.hardware.interfaces.IRequest;
 import frc.robot.hardware.interfaces.InputSignal;
 import frc.utils.Conversions;
-import frc.utils.calibration.sysid.SysIdCalibrator;
 
-public class MotorSubsystemMaya extends GBSubsystem {
+public class MotorSubsystemPosition extends GBSubsystem {
 
 	ControllableMotor motor;
 	MotorCommandBuilder commandBuilder;
 	IRequest<Rotation2d> positionRequest;
 	InputSignal<Rotation2d> positionSignal;
-	IRequest<Double> voltageRequest;
-	SysIdCalibrator sysIdCalibrator;
 	Rotation2d targetPosition;
 
-	public MotorSubsystemMaya(ControllableMotor motor, String logPath) {
+	public MotorSubsystemPosition(ControllableMotor motor, String logPath) {
 		super(logPath);
 		this.motor = motor;
 		this.commandBuilder = new MotorCommandBuilder();
-		this.sysIdCalibrator = new SysIdCalibrator(motor.getSysidConfigInfo(), this, this::setVoltage);
 	}
 
 	public Rotation2d getPosition() {
@@ -30,10 +26,6 @@ public class MotorSubsystemMaya extends GBSubsystem {
 			throw new NullPointerException("the position request is null, try using: '.withPositionControl'");
 		}
 		return positionSignal.getLatestValue();
-	}
-
-	public void setBrake(boolean brake) {
-		motor.setBrake(brake);
 	}
 
 	public void setTargetPosition(Rotation2d targetPosition) {
@@ -47,27 +39,8 @@ public class MotorSubsystemMaya extends GBSubsystem {
 		return MathUtil.isNear(targetPosition.getRotations(), getPosition().getRotations(), tolerance.getRotations());
 	}
 
-	public MotorCommandBuilder getCommandsBuilder() {
-		return commandBuilder;
-	}
-
-	public void setPower(double power) {
-		motor.setPower(power);
-	}
-
 	public void resetPosition(Rotation2d resetPosition) {
 		motor.resetPosition(resetPosition);
-	}
-
-	public SysIdCalibrator getSysIdCalibrator() {
-		return sysIdCalibrator;
-	}
-
-	public void setVoltage(double voltage) {
-		if (voltageRequest == null) {
-			throw new NullPointerException("the voltage request is null, try using: '.withVoltageControl'");
-		}
-		voltageRequest.withSetPoint(voltage);
 	}
 
 	public double convertToMeters(Rotation2d motorPosition, double wheelDiameter) {
@@ -78,20 +51,19 @@ public class MotorSubsystemMaya extends GBSubsystem {
 		return getPosition().getRadians() > expectedPosition.getRadians();
 	}
 
+	protected void stayInPlace() {
+		setTargetPosition(getPosition());
+	}
+
 	public boolean isBehind(Rotation2d expectedPosition) {
 		return getPosition().getRadians() < expectedPosition.getRadians();
 	}
 
-	public MotorSubsystemMaya withPositionControl(IRequest<Rotation2d> positionRequest, InputSignal<Rotation2d> positionSignal) {
+	public MotorSubsystemPosition withPositionControl(IRequest<Rotation2d> positionRequest, InputSignal<Rotation2d> positionSignal) {
 		this.positionRequest = positionRequest;
 		this.positionSignal = positionSignal;
 		this.targetPosition = positionSignal.getLatestValue();
 		return this;
-	}
-
-	@Override
-	protected void subsystemPeriodic() {
-		motor.updateInputs();
 	}
 
 }

58e481eccb9283a1ad43df3934d3c803e10cb48a
Shahar Perets <perets.shahar@gmail.com>
2024-12-01 17:16:07 +0200

    Shahar: with observation count helper

--- src/main/java/frc/robot/poseestimator/OdometryValues.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.poseestimator;
-
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-
-public record OdometryValues(SwerveDriveKinematics kinematics, SwerveModulePosition[] wheelPositions, Rotation2d gyroAngle) {}
--- src/main/java/frc/robot/vision/VisionFiltererConfig.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.vision;
-
-import java.util.function.BiFunction;
-
-public record VisionFiltererConfig(
-	String logPath,
-	VisionFiltersTolerances VisionFiltersTolerances,
-	BiFunction<RawVisionData, VisionFiltersTolerances, Boolean> filters
-) {}

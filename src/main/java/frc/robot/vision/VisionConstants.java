4653f8fed1e7e993ed762e308303735a9a520935
Shahar Perets <perets.shahar@gmail.com>
2024-12-01 18:13:54 +0200

    Shahar: unwanted files removed

--- src/main/java/frc/robot/vision/VisionConstants.java
+++ src/main/java/frc/robot/vision/VisionConstants.java
@@ -16,18 +16,6 @@ public class VisionConstants {
 
 	public static final int NO_APRILTAG_ID = -1;
 
-	public static final Rotation2d ROLL_TOLERANCE = Rotation2d.fromDegrees(10);
-
-	public static final Rotation2d PITCH_TOLERANCE = Rotation2d.fromDegrees(10);
-
-	public static final double ROBOT_TO_GROUND_TOLERANCE_METERS = 0.2;
-
-	public static final VisionFiltersTolerances DEFAULT_VISION_FILTERS_TOLERANCES = new VisionFiltersTolerances(
-		ROLL_TOLERANCE,
-		PITCH_TOLERANCE,
-		ROBOT_TO_GROUND_TOLERANCE_METERS
-	);
-
 	public static final double VISION_ANGLE_STANDARD_DEVIATION = 0.6;
 
 }
--- src/main/java/frc/robot/vision/VisionFiltersTolerances.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.vision;
-
-import edu.wpi.first.math.geometry.Rotation2d;
-
-public record VisionFiltersTolerances(Rotation2d rollTolerance, Rotation2d pitchTolerance, double robotToGroundToleranceMeters) {}

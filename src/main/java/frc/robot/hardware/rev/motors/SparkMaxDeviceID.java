162bfa74b3687da2aa622c33b7bc6ef016b1f840
YHerm <0549405422.yh@gmail.com>
2024-12-03 21:17:49 +0200

    names

--- src/main/java/frc/robot/hardware/rev/motors/SparkMaxDeviceID.java
+++ src/main/java/frc/robot/hardware/rev/motors/SparkMaxDeviceID.java
@@ -2,7 +2,7 @@ package frc.robot.hardware.rev.motors;
 
 import com.revrobotics.CANSparkLowLevel;
 
-public record SparkMaxDeviceID(int ID, CANSparkLowLevel.MotorType type) {
+public record SparkMaxDeviceID(int id, CANSparkLowLevel.MotorType type) {
 
 	public SparkMaxDeviceID(int ID) {
 		this(ID, CANSparkLowLevel.MotorType.kBrushless);

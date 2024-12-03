94a5f44154033c8aec336cb41b0c145f3e572dfd
Shahar Perets <perets.shahar@gmail.com>
2024-12-03 17:39:35 +0200

    Shahar: now in simulation

---
+++ src/main/java/frc/robot/hardware/interfaces/IAccelerometer.java
@@ -0,0 +1,13 @@
+package frc.robot.hardware.interfaces;
+
+public interface IAccelerometer {
+
+	double getAccelerationMagnitude();
+
+	double getAccelerationX();
+
+	double getAccelerationY();
+
+	double getAccelerationZ();
+
+}
---
+++ src/main/java/frc/robot/hardware/phoenix6/accelerometer/simulatedAccelerometer.java
@@ -0,0 +1,63 @@
+package frc.robot.hardware.phoenix6.accelerometer;
+
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import frc.robot.hardware.interfaces.IAccelerometer;
+import frc.robot.hardware.phoenix6.gyro.Pigeon2Wrapper;
+import frc.robot.subsystems.GBSubsystem;
+import frc.utils.time.TimeUtils;
+import org.ejml.simple.SimpleMatrix;
+
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class simulatedAccelerometer extends GBSubsystem implements IAccelerometer {
+
+	private final Supplier<ChassisSpeeds> robotSimulatedVelocityRelativeToRobot;
+	private final Supplier<Double> noise;
+	private double accelerationX;
+	private double accelerationY;
+	private ChassisSpeeds previousVelocity;
+
+	public simulatedAccelerometer(Supplier<ChassisSpeeds> robotSimulatedVelocityRelativeToRobot, double noiseAmount, String logPath) {
+		super(logPath);
+		Random randomNumbersGenerator = new Random();
+
+		this.robotSimulatedVelocityRelativeToRobot = robotSimulatedVelocityRelativeToRobot;
+		this.noise = () -> randomNumbersGenerator.nextGaussian() * noiseAmount;
+	}
+
+
+	@Override
+	public double getAccelerationMagnitude() {
+		Vector<N3> accelerationVector = new Vector<>(new SimpleMatrix(new double[][] {{getAccelerationX(), getAccelerationY(), getAccelerationZ()}}));
+		return accelerationVector.norm();
+	}
+
+	@Override
+	public double getAccelerationX() {
+		return accelerationX + noise.get();
+	}
+
+	@Override
+	public double getAccelerationY() {
+		return accelerationY + noise.get();
+	}
+
+	@Override
+	public double getAccelerationZ() {
+		return 0 + noise.get();
+	}
+
+	@Override
+	protected void subsystemPeriodic() {
+		accelerationX = (previousVelocity.vxMetersPerSecond - robotSimulatedVelocityRelativeToRobot.get().vxMetersPerSecond) / TimeUtils.getCurrentCycleTimeSeconds();
+		accelerationY = (previousVelocity.vyMetersPerSecond - robotSimulatedVelocityRelativeToRobot.get().vyMetersPerSecond) / TimeUtils.getCurrentCycleTimeSeconds();
+		previousVelocity = robotSimulatedVelocityRelativeToRobot.get();
+	}
+}

450dd799a7dad38c1bb12c25a714c6c3c7e6ee66
GilStein1 <gils.server@gmail.com>
2024-12-04 19:43:16 +0200

    Gil - cr for Yoni

--- src/main/java/frc/robot/vision/GyroAngleValues.java
+++ src/main/java/frc/robot/vision/GyroAngleValues.java
@@ -1,7 +1,9 @@
 package frc.robot.vision;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+
 /**
  * A record that represents the gyro's angle which is passed into the limelight's network tables, to the robot_orientation_set entry. Its values
  * were found in limelightlib sourcecode. The limelight is using it to calculate the BotPose2 position.
  */
-public record GyroAngleValues(double yaw, double yawRate, double pitch, double pitchRate, double roll, double rollRate) {}
+public record GyroAngleValues(Rotation3d angles, double yawRate, double pitchRate, double rollRate) {}

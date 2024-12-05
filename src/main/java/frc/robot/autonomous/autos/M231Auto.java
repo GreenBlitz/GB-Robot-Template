63c33ffb9cde2e00bb707af3734a17ce200e4281
hillelv <hillel.vider@gmail.com>
2024-12-05 19:26:46 +0200

    hillel - none instead of parameter

--- src/main/java/frc/robot/autonomous/autos/M231Auto.java
+++ src/main/java/frc/robot/autonomous/autos/M231Auto.java
@@ -1,10 +1,10 @@
 package frc.robot.autonomous.autos;
 
 import com.pathplanner.lib.commands.PathPlannerAuto;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 public class M231Auto extends PathPlannerAuto {
-    public M231Auto(Command autoCommand) {
-        super(autoCommand);
+    public M231Auto() {
+        super(Commands.none());
     }
 }

35ca192161d9e60b0b4d4e97f3f4bbb064446cf2
Shahar Perets <perets.shahar@gmail.com>
2024-12-10 14:29:17 +0200

    Shahar: visionObservation renamed

--- src/main/java/frc/robot/poseestimator/IVisionEstimator.java
+++ src/main/java/frc/robot/poseestimator/IVisionEstimator.java
@@ -1,14 +1,14 @@
 package frc.robot.poseestimator;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import frc.robot.poseestimator.observations.VisionObservation;
+import frc.robot.poseestimator.observations.VisionRobotPoseObservation;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IVisionEstimator {
 
-	void updateVision(List<VisionObservation> visionObservation);
+	void updateVision(List<VisionRobotPoseObservation> visionRobotPoseObservation);
 
 	Optional<Pose2d> getVisionPose();
 

cae4d91cf23c346e3064a8ae425ce5eceb5c40a7
Shahar Perets <perets.shahar@gmail.com>
2024-12-12 11:22:56 +0200

    Shahar: CR

--- src/main/java/frc/robot/vision/rawdata/RawAprilTagVisionData.java
+++ src/main/java/frc/robot/vision/rawdata/RawAprilTagVisionData.java
@@ -1,16 +1,21 @@
 package frc.robot.vision.rawdata;
 
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose3d;
 
 public class RawAprilTagVisionData extends RawVisionData {
 
 	private final double aprilTagHeight;
 	private final double distanceFromAprilTag;
+	private final AprilTag aprilTag;
 
-	public RawAprilTagVisionData(Pose3d estimatedPose, double aprilTagHeight, double distanceFromAprilTag, double timestamp) {
+	public RawAprilTagVisionData(Pose3d estimatedPose, double aprilTagHeight, double distanceFromAprilTag, double timestamp, AprilTag aprilTag) {
 		super(estimatedPose, timestamp);
 		this.aprilTagHeight = aprilTagHeight;
 		this.distanceFromAprilTag = distanceFromAprilTag;
+		this.aprilTag = aprilTag;
 	}
 
 	public double getAprilTagHeight() {
@@ -20,5 +25,7 @@ public class RawAprilTagVisionData extends RawVisionData {
 	public double getDistanceFromAprilTag() {
 		return distanceFromAprilTag;
 	}
+
+	public AprilTag getTrackedAprilTag() { return aprilTag; }
 
 }
--- src/main/java/frc/robot/vision/rawdata/RawVisionData.java
+++ src/main/java/frc/robot/vision/rawdata/RawVisionData.java
@@ -1,9 +1,9 @@
 package frc.robot.vision.rawdata;
 
 import edu.wpi.first.math.geometry.Pose3d;
-import frc.robot.poseestimator.observations.VisionRobotPoseObservation;
+import frc.robot.poseestimator.observations.IRobotPoseVisionObservation;
 
-public class RawVisionData implements VisionRobotPoseObservation {
+public class RawVisionData implements IRobotPoseVisionObservation {
 
 	private final Pose3d estimatedPose;
 	private final double timestamp;
@@ -13,10 +13,12 @@ public class RawVisionData implements VisionRobotPoseObservation {
 		this.timestamp = timestamp;
 	}
 
+	@Override
 	public Pose3d getEstimatedPose() {
 		return estimatedPose;
 	}
 
+	@Override
 	public double getTimestamp() {
 		return timestamp;
 	}

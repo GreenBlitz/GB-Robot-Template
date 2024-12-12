4e4a5708d5861086b7bcc59bd6b405d5d8515c65
Shahar Perets <perets.shahar@gmail.com>
2024-12-12 16:18:34 +0200

    Shahar: yay an empty class

--- src/main/java/frc/robot/vision/rawdata/IRawVisionData.java
+++ src/main/java/frc/robot/vision/rawdata/IRawVisionData.java
@@ -3,10 +3,4 @@ package frc.robot.vision.rawdata;
 import edu.wpi.first.math.geometry.Pose3d;
 import frc.robot.poseestimator.observations.IRobotPoseVisionObservation;
 
-public interface IRawVisionData extends IRobotPoseVisionObservation {
-
-	public Pose3d getEstimatedPose();
-
-	public double getTimestamp();
-
-}
+public interface IRawVisionData extends IRobotPoseVisionObservation {}

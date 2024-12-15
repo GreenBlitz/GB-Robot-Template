f94f5be0e3965c604a0b76036c2c373bd9ae89a2
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-12-15 17:00:43 +0200

    fixed some names

--- src/main/java/frc/robot/poseestimator/helpers/DataAccumulator.java
+++ src/main/java/frc/robot/poseestimator/helpers/DataAccumulator.java
@@ -3,18 +3,19 @@ package frc.robot.poseestimator.helpers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
-public class ObservationAccumulator<T> {
+
+public class DataAccumulator<T> {
 
 	private final Supplier<List<T>> observationsSupplier;
 	private final int maxDataIntakes;
 	private List<T> accumulatedObservations;
 	private int dataIntakeCounter;
 
-	public ObservationAccumulator(Supplier<List<T>> observationSupplier, int maxCount) {
-		this .observationsSupplier = observationSupplier;
+	public DataAccumulator(Supplier<List<T>> observationSupplier, int maxCount) {
+		this.observationsSupplier = observationSupplier;
 		this.maxDataIntakes = maxCount;
-		this .accumulatedObservations = new ArrayList<>();
-		this .dataIntakeCounter = 0;
+		this.accumulatedObservations = new ArrayList<>();
+		this.dataIntakeCounter = 0;
 	}
 
 	public List<T> getAccumulatedList() {
@@ -23,12 +24,11 @@ public class ObservationAccumulator<T> {
 
 		if (dataIntakeCounter >= maxDataIntakes) {
 			dataIntakeCounter = 0;
-			List<T> stackedObservationsCopy  = accumulatedObservations;
+			List<T> accumulatedObservationsCopy = accumulatedObservations;
 			accumulatedObservations = new ArrayList<>();
-			return stackedObservationsCopy ;
+			return accumulatedObservationsCopy;
 		}
 		return new ArrayList<>();
-
 	}
 
 	public int getCount() {

b6c77cd47af4712c2aa0318e35e8d792acd7a2d1
hillelv <hillel.vider@gmail.com>
2024-12-18 17:03:34 +0200

    danna - merge

--- src/main/java/frc/robot/vision/filters/VisionFilters.java
+++ src/main/java/frc/robot/vision/filters/VisionFilters.java
@@ -1,25 +1,27 @@
 package frc.robot.vision.filters;
 
 import edu.wpi.first.math.geometry.*;
-import frc.robot.vision.rawdata.RawAprilTagVisionData;
-import frc.robot.vision.rawdata.RawVisionData;
+import frc.robot.vision.rawdata.AprilTagVisionData;
+import frc.robot.vision.rawdata.VisionData;
 
 public class VisionFilters {
 
-	protected static Filter<RawVisionData> isPitchInTolerance(Rotation2d pitchTolerance) {
+	protected static Filter<VisionData> isPitchInTolerance(Rotation2d pitchTolerance) {
 		return new Filter<>(rawVisionData -> Math.abs(rawVisionData.getEstimatedPose().getRotation().getY()) <= pitchTolerance.getRadians());
 	}
 
-	protected static Filter<RawVisionData> isRollInTolerance(Rotation2d rollTolerance) {
+	protected static Filter<VisionData> isRollInTolerance(Rotation2d rollTolerance) {
 		return new Filter<>(rawVisionData -> Math.abs(rawVisionData.getEstimatedPose().getRotation().getX()) <= rollTolerance.getRadians());
 	}
 
-	protected static Filter<RawVisionData> isRobotOnGround(double robotToGroundToleranceMeters) {
+	protected static Filter<VisionData> isRobotOnGround(double robotToGroundToleranceMeters) {
 		return new Filter<>(rawVisionData -> rawVisionData.getEstimatedPose().getZ() <= robotToGroundToleranceMeters);
 	}
 
-	protected static Filter<
-		RawAprilTagVisionData> isAprilTagHeightInTolerance(double aprilTagHeightToleranceMeters, double aprilTagRealHeightMeters) {
+	protected static Filter<AprilTagVisionData> isAprilTagHeightInTolerance(
+		double aprilTagHeightToleranceMeters,
+		double aprilTagRealHeightMeters
+	) {
 		return new Filter<>(
 			rawAprilTagVisionData -> Math.abs(rawAprilTagVisionData.getAprilTagHeight() - aprilTagRealHeightMeters)
 				<= aprilTagHeightToleranceMeters

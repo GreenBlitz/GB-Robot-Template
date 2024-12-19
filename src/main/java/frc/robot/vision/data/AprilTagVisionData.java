7a4e61e7224d375766f3e1548b5052e3e1f03366
GilStein1 <gils.server@gmail.com>
2024-12-19 14:08:31 +0200

    Gil - spotless

--- src/main/java/frc/robot/vision/data/AprilTagVisionData.java
+++ src/main/java/frc/robot/vision/data/AprilTagVisionData.java
@@ -8,7 +8,13 @@ public class AprilTagVisionData extends VisionData {
 	private final double distanceFromAprilTagMeters;
 	private final int aprilTagId;
 
-	public AprilTagVisionData(Pose3d estimatedPose, double timestamp, double aprilTagHeightMeters, double distanceFromAprilTagMeters, int aprilTagId) {
+	public AprilTagVisionData(
+		Pose3d estimatedPose,
+		double timestamp,
+		double aprilTagHeightMeters,
+		double distanceFromAprilTagMeters,
+		int aprilTagId
+	) {
 		super(estimatedPose, timestamp);
 		this.aprilTagHeightMeters = aprilTagHeightMeters;
 		this.distanceFromAprilTagMeters = distanceFromAprilTagMeters;

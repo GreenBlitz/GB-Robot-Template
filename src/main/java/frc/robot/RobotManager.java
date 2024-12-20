25b24a724289262e42034118c35002a44caa8373
YHerm <0549405422.yh@gmail.com>
2024-12-20 19:32:17 +0200

    space

--- src/main/java/frc/robot/RobotManager.java
+++ src/main/java/frc/robot/RobotManager.java
@@ -17,7 +17,6 @@ import org.littletonrobotics.junction.LoggedRobot;
 import frc.utils.brakestate.BrakeStateManager;
 import org.littletonrobotics.junction.Logger;
 
-
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after creating this project, you must also update the build.gradle file in

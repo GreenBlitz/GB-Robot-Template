b6de6b7b3d965bfa93f4607a25add659b2126295
itamaroryan <itamar.oryan@gmail.com>
2024-12-23 19:31:53 +0200

    done

--- src/main/java/frc/robot/subsystems/GBSubsystem.java
+++ src/main/java/frc/robot/subsystems/GBSubsystem.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
@@ -13,14 +12,14 @@ public abstract class GBSubsystem extends SubsystemBase {
 
 	public GBSubsystem(String logPath) {
 		this.logPath = logPath;
-		this.currentCommand = new InstantCommand().withName("No Command");
+		this.currentCommand = new InstantCommand().withName("None");
 	}
-	
+
 	@Override
 	public Command getCurrentCommand() {
 		return currentCommand;
 	}
-	
+
 	public String getLogPath() {
 		return logPath;
 	}
@@ -33,11 +32,8 @@ public abstract class GBSubsystem extends SubsystemBase {
 
 	protected void subsystemPeriodic() {}
 
-	protected Command withSetCurrentCommand(Command command){
-		return new SequentialCommandGroup(
-				new InstantCommand(() -> currentCommand = command),
-				command
-		);
+	public Command asSubsystemCommand(Command command) {
+		return command.beforeStarting(new InstantCommand(() -> currentCommand = command));
 	}
 
 }

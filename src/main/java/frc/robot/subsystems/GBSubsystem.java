729c24d381dd924a2d54ee4de1f2fa653e5b63dc
hillelv <hillel.vider@gmail.com>
2024-12-23 09:32:07 +0200

    spotless

--- src/main/java/frc/robot/subsystems/GBSubsystem.java
+++ src/main/java/frc/robot/subsystems/GBSubsystem.java
@@ -1,10 +1,8 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
-import java.util.function.Supplier;
 
 public abstract class GBSubsystem extends SubsystemBase {
 

10e3edc9f3ae6fd8b52b0cbc44ae76a3353d1edf
tomer-hershman <lili.hershman@gmail.com>
2024-12-23 19:55:18 +0200

    tomer- aproved DIP

--- src/main/java/frc/robot/poseestimator/WPILibOdometryEstimator.java
+++ src/main/java/frc/robot/poseestimator/WPILibOdometryEstimator.java
@@ -3,7 +3,6 @@ package frc.robot.poseestimator;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import frc.robot.poseestimator.observations.OdometryObservation;
 import org.littletonrobotics.junction.Logger;
@@ -13,15 +12,9 @@ public class WPILibOdometryEstimator implements IOdometryEstimator {
 	private final String logPath;
 	private final SwerveDrivePoseEstimator odometryEstimator;
 
-	public WPILibOdometryEstimator(
-		String logPath,
-		SwerveDriveKinematics kinematics,
-		Rotation2d initialGyroAngle,
-		SwerveModulePosition[] initialWheelPositions,
-		Pose2d initialPose
-	) {
+	public WPILibOdometryEstimator(String logPath, SwerveDrivePoseEstimator odometryEstimator) {
 		this.logPath = logPath;
-		this.odometryEstimator = new SwerveDrivePoseEstimator(kinematics, initialGyroAngle, initialWheelPositions, initialPose);
+		this.odometryEstimator = odometryEstimator;
 	}
 
 	@Override
@@ -47,7 +40,7 @@ public class WPILibOdometryEstimator implements IOdometryEstimator {
 	}
 
 	public void periodic() {
-		Logger.recordOutput(logPath + "estimatedOdometryPose", odometryEstimator.getEstimatedPosition());
+		Logger.recordOutput(logPath + "estimatedOdometryPose: ", odometryEstimator.getEstimatedPosition());
 	}
 
 }

6a7a78a33391da38a4717e1c4a6c570d6b5355c2
hillelv <hillel.vider@gmail.com>
2024-12-23 19:55:57 +0200

    danna - cr

--- src/main/java/frc/utils/ToleranceCalculations.java
+++ src/main/java/frc/utils/ToleranceCalculations.java
@@ -1,12 +1,13 @@
 package frc.utils;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.constants.MathConstants;
 
 public class ToleranceCalculations {
 
 	public static boolean isRotation2dInTolerance(Rotation2d tolerance, Rotation2d rotation, Rotation2d wantedRotation) {
 		return Math.abs(wantedRotation.getRadians() - rotation.getRadians()) <= tolerance.getRadians()
-			|| Math.abs(wantedRotation.getRadians() - rotation.getRadians()) >= Math.toRadians(360) - tolerance.getRadians();
+			|| Math.abs(wantedRotation.getRadians() - rotation.getRadians()) >= MathConstants.FULL_CIRCLE.getRadians() - tolerance.getRadians();
 	}
 
 	public static boolean isDoubleInRange(double tolerance, double value, double maximumValue, double minimumValue) {

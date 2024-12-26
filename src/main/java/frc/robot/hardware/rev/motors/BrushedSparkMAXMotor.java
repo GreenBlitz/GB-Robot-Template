791f75adcb034deec6e07c85c472ce189068ff12
Dror Talmi <dror.gg.talmi@gmail.com>
2024-12-26 19:28:51 +0200

    done

--- src/main/java/frc/robot/hardware/rev/motors/BrushedSparkMAXMotor.java
+++ src/main/java/frc/robot/hardware/rev/motors/BrushedSparkMAXMotor.java
@@ -1,12 +1,12 @@
 package frc.robot.hardware.rev.motors;
 
-import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.spark.SparkLowLevel;
 
 public class BrushedSparkMAXMotor extends SparkMaxMotor {
 
 	public BrushedSparkMAXMotor(String logPath, SparkMaxWrapper motor) {
 		super(logPath, motor);
-		if (motor.getMotorType() != CANSparkLowLevel.MotorType.kBrushed) {
+		if (motor.getMotorType() != SparkLowLevel.MotorType.kBrushless) {
 			throw new IllegalArgumentException("inserted BrushlessSparkMAXMotor to BrushedSparkMAXMotor!");
 		}
 	}

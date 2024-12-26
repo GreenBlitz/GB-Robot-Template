a96b9aaade796e2118e887eb6b5034b5431dd66c
Yotam Cohen <76944434+Yotam7@users.noreply.github.com>
2024-12-26 15:42:48 +0200

    refactor(vision): change `APRIL_TAG_FIELD_CONFIG_FILE` to `Path`, move it to DirectoryPaths, use `exception` variable name instead of `e`

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -6,6 +6,8 @@ import frc.utils.alerts.Alert;
 
 import java.io.IOException;
 
+import static frc.robot.constants.DirectoryPaths.APRIL_TAG_FIELD_CONFIG_FILE_PATH;
+
 public class VisionConstants {
 
 	public static final String FILTERED_DATA_LOGPATH_ADDITION = "FilteredData/";
@@ -15,13 +17,11 @@ public class VisionConstants {
 	public static final String SOURCE_LOGPATH_ADDITION = "VisionSource/";
 
 
-	private static final String APRIL_TAG_FIELD_CONFIG_FILE_PATH = "april_tag_field_config.json";
-
 	public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT = getAprilTagFieldLayout();
 
 	private static AprilTagFieldLayout getAprilTagFieldLayout() {
 		try {
-			return AprilTagFieldLayout.loadFromResource(APRIL_TAG_FIELD_CONFIG_FILE_PATH);
+			return AprilTagFieldLayout.loadFromResource(APRIL_TAG_FIELD_CONFIG_FILE_PATH.toString());
 		} catch (IOException exception) {
 			new Alert(
 				Alert.AlertType.WARNING,

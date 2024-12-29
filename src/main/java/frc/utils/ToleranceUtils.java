9ff2a19d1fad73712bb4c2316e191fb11f595864
hillelv <hillel.vider@gmail.com>
2024-12-29 15:18:03 +0200

    danna - cr

--- src/main/java/frc/utils/ToleranceUtils.java
+++ src/main/java/frc/utils/ToleranceUtils.java
@@ -17,12 +17,12 @@ public class ToleranceUtils {
 		);
 	}
 
-	public static boolean isInRange(double value, double maxValue, double minValue, double tolerance) {
-		return (minValue - tolerance) <= value && value <= (maxValue + tolerance);
+	public static boolean isInRange(double value, double min, double max, double tolerance) {
+		return (min - tolerance) <= value && value <= (max + tolerance);
 	}
 
-	public static boolean isInRange(double value, double maxValue, double minValue) {
-		return isInRange(value, maxValue, minValue, 0);
+	public static boolean isInRange(double value, double min, double max) {
+		return isInRange(value, min, max, 0);
 	}
 
 }

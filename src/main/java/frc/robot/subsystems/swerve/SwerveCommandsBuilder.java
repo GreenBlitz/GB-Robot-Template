cc3622281c1e4650a8a63f9a9479c1af29161ef1
hillelv <hillel.vider@gmail.com>
2024-12-29 18:32:07 +0200

    hillel - pid at end of follow path

--- src/main/java/frc/robot/subsystems/swerve/SwerveCommandsBuilder.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveCommandsBuilder.java
@@ -146,7 +146,7 @@ public class SwerveCommandsBuilder {
 
 	public Command followPathOrDriveToPathEnd(Supplier<Pose2d> currentPose, PathPlannerPath path, double closeToPathBeginningDeadbandMeters) {
 		return new ConditionalCommand(
-			PathPlannerUtils.followPath(path),
+			PathPlannerUtils.followPath(path).andThen(pidToPose(currentPose, PathPlannerUtils.getAllianceRelativePose(PathPlannerUtils.getLastPathPose(path)))),
 			driveToPose(currentPose, () -> PathPlannerUtils.getAllianceRelativePose(PathPlannerUtils.getLastPathPose(path))),
 			() -> PathPlannerUtils.isRobotCloseToPathBeginning(path, currentPose, closeToPathBeginningDeadbandMeters)
 		);

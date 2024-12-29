ebf53970aaded72211da50f3b3c3434cb969302d
Yotam Cohen <76944434+Yotam7@users.noreply.github.com>
2024-12-29 13:35:00 +0200

    git: resolve conflicts

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -19,16 +19,8 @@ public class VisionConstants {
 
 	private static AprilTagFieldLayout getAprilTagFieldLayout() {
 		try {
-<<<<<<< HEAD
-			return AprilTagFieldLayout.loadFromResource(APRIL_TAG_FIELD_CONFIG_FILE_PATH.toString());
-		} catch (IOException ioException) {
-||||||| parent of 029024848 (refactor(vision): improve variable naming, code reusing, and use constants without direct import)
-			return AprilTagFieldLayout.loadFromResource(APRIL_TAG_FIELD_CONFIG_FILE_PATH.toString());
-		} catch (IOException exception) {
-=======
 			return AprilTagFieldLayout.loadFromResource(DirectoryPaths.APRIL_TAG_FIELD_CONFIG_FILE_PATH.toString());
-		} catch (IOException exception) {
->>>>>>> 029024848 (refactor(vision): improve variable naming, code reusing, and use constants without direct import)
+		} catch (IOException ioException) {
 			new Alert(
 				Alert.AlertType.WARNING,
 				"Cannot read april tag field layout from " + DirectoryPaths.APRIL_TAG_FIELD_CONFIG_FILE_PATH + ", using default field layout"

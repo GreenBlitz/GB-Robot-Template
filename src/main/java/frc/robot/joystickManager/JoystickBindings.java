e04f664248d0d5e00ee37474e4db9f39814632e3
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-12-29 19:54:30 +0200

    Nitay - herman made it work

--- src/main/java/frc/robot/joystickManager/JoystickBindings.java
+++ src/main/java/frc/robot/joystickManager/JoystickBindings.java
@@ -1,10 +1,22 @@
 package frc.robot.joystickManager;
 
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.Robot;
 import frc.joysticks.SmartJoystick;
+import org.littletonrobotics.junction.Logger;
 
 public class JoystickBindings {
 
-	public static void configureBindings(SmartJoystick joystick, Robot robot) {}
+	public static void configureBindings(SmartJoystick joystick, Robot robot) {
+		applySwerveBindnigs(joystick, robot);
+		applySecondBindings(joystick, robot);
+	}
 
+	private static void applySwerveBindnigs(SmartJoystick joystick, Robot robot){
+		joystick.A.and(() -> joystick.getState() == JoystickState.SWERVE).whileTrue(new RunCommand(() -> System.out.println("swerve")));
+	}
+
+	private static void applySecondBindings(SmartJoystick joystick, Robot robot){
+		joystick.A.and(() -> joystick.getState() == JoystickState.SECOND).whileTrue(new RunCommand(() -> System.out.println("second")));
+	}
 }

fdc2c34f1ce4da0943d83ac9e39d40d1fe697817
Nitay4 <nitay@rotbart.co.il>
2024-12-30 13:39:45 +0200

    Nitay - format

--- src/main/java/frc/robot/joystickManager/JoystickBindings.java
+++ src/main/java/frc/robot/joystickManager/JoystickBindings.java
@@ -1,18 +1,17 @@
 package frc.robot.joystickManager;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Robot;
 import frc.joysticks.SmartJoystick;
 
-import java.util.BitSet;
 
 public class JoystickBindings {
 
 	public static void configureBindings(SmartJoystick joystick, Robot robot) {}
 
-	private static void Bind(SmartJoystick joystick, Trigger trigger, JoystickBindSet bindSetRequirement, Command command){
-		trigger.onTrue(command).and(() ->joystick.getBindSet() == bindSetRequirement);
+	private static void bind(SmartJoystick joystick, Trigger trigger, JoystickBindSet bindSetRequirement, Command command) {
+		trigger.onTrue(command).and(() -> joystick.getBindSet() == bindSetRequirement);
 	}
+
 }

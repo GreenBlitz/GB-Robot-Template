48fac217dafbe94a3b72eb5484e7566fe183e1a3
hillelv <hillel.vider@gmail.com>
2024-12-30 12:46:35 +0200

    hillel - format

--- src/main/java/frc/robot/autonomous/AutonomousConstants.java
+++ src/main/java/frc/robot/autonomous/AutonomousConstants.java
@@ -5,10 +5,10 @@ import com.pathplanner.lib.path.PathConstraints;
 
 public class AutonomousConstants {
 
-    public static final String LOG_PATH_PREFIX = "Autonomous/";
+	public static final String LOG_PATH_PREFIX = "Autonomous/";
 
-    public static final PathConstraints REAL_TIME_CONSTRAINTS = new PathConstraints(2.5, 2.5, 4, 4);
+	public static final PathConstraints REAL_TIME_CONSTRAINTS = new PathConstraints(2.5, 2.5, 4, 4);
 
-    public static final double CLOSE_TO_TARGET_POSITION_DEADBAND_METERS = 0.5;
+	public static final double CLOSE_TO_TARGET_POSITION_DEADBAND_METERS = 0.5;
 
 }

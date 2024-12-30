89eec712728a09625085300113d75819ff458738
Nitay4 <nitay@rotbart.co.il>
2024-12-30 13:37:55 +0200

    Niaty - Added a bind function

--- src/main/java/frc/robot/joystickManager/JoystickBindings.java
+++ src/main/java/frc/robot/joystickManager/JoystickBindings.java
@@ -1,11 +1,18 @@
 package frc.robot.joystickManager;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Robot;
 import frc.joysticks.SmartJoystick;
+
+import java.util.BitSet;
 
 public class JoystickBindings {
 
 	public static void configureBindings(SmartJoystick joystick, Robot robot) {}
 
+	private static void Bind(SmartJoystick joystick, Trigger trigger, JoystickBindSet bindSetRequirement, Command command){
+		trigger.onTrue(command).and(() ->joystick.getBindSet() == bindSetRequirement);
+	}
 }

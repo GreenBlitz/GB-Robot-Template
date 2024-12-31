c0939cc5bd1f499be6f72c7ff04e22da12ed054e
Shahar Perets <perets.shahar@gmail.com>
2024-12-31 18:41:57 +0200

    Shahar: we don't need that .with funcs

--- src/main/java/frc/robot/poseestimator/helpers/StandardDeviations2D.java
+++ src/main/java/frc/robot/poseestimator/helpers/StandardDeviations2D.java
@@ -11,18 +11,6 @@ public record StandardDeviations2D(double xStandardDeviations, double yStandardD
 		this(0, 0, 0);
 	}
 
-	public StandardDeviations2D withAngleStandardDeviations(double angleStandardDeviations) {
-		return new StandardDeviations2D(this.xStandardDeviations, this.yStandardDeviations, angleStandardDeviations);
-	}
-
-	public StandardDeviations2D withXStandardDeviations(double xStandardDeviationsMeters) {
-		return new StandardDeviations2D(xStandardDeviationsMeters, this.yStandardDeviations, this.angleStandardDeviations);
-	}
-
-	public StandardDeviations2D withYStandardDeviations(double yStandardDeviationsMeters) {
-		return new StandardDeviations2D(this.xStandardDeviations, yStandardDeviationsMeters, this.angleStandardDeviations);
-	}
-
 	public Matrix<N3, N1> getWPILibStandardDeviations() {
 		return VecBuilder.fill(xStandardDeviations, yStandardDeviations, angleStandardDeviations);
 	}

d859b47b7320e32fa157989112b6ee3a472f79ef
Shahar Perets <perets.shahar@gmail.com>
2025-01-05 19:08:08 +0200

    Shahar: angular acceleration

--- src/main/java/frc/robot/hardware/interfaces/IAccelerometer.java
+++ src/main/java/frc/robot/hardware/interfaces/IAccelerometer.java
@@ -10,4 +10,16 @@ public interface IAccelerometer {
 
 	double getAccelerationZ();
 
+	default double getAccelerationYaw() {
+		return Math.cos(getAccelerationX()) + Math.sin(getAccelerationY());
+	};
+
+	default double getAccelerationPitch() {
+		return Math.cos(getAccelerationZ()) + Math.sin(getAccelerationY());
+	};
+
+	default double getAccelerationRoll() {
+		return Math.cos(getAccelerationY()) + Math.sin(getAccelerationZ());
+	};
+
 }

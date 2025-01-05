59df14da17fbeadcdcc31a58c2a7467b03efb0ac
Shahar Perets <perets.shahar@gmail.com>
2025-01-05 18:50:37 +0200

    Shahar: another wrapper

--- src/main/java/frc/robot/hardware/phoenix6/accelerometer/PigeonAccelerometer.java
+++ src/main/java/frc/robot/hardware/phoenix6/accelerometer/PigeonAccelerometer.java
@@ -1,28 +1,45 @@
 package frc.robot.hardware.phoenix6.accelerometer;
 
+import com.ctre.phoenix6.hardware.ParentDevice;
 import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.hardware.interfaces.IAccelerometer;
 import frc.robot.hardware.interfaces.IGyro;
 import frc.robot.hardware.interfaces.InputSignal;
+import frc.robot.hardware.phoenix6.Phoenix6Device;
 import frc.robot.hardware.phoenix6.Pigeon2Wrapper;
 
-public class PigeonAccelerometer implements IGyro {
+public class PigeonAccelerometer extends Phoenix6Device implements IAccelerometer {
+
+	Pigeon2Wrapper accelerometer;
 
 	public PigeonAccelerometer(String logPath, Pigeon2Wrapper accelerometer) {
-
+		super(logPath);
+		this.accelerometer = accelerometer;
 	}
 
 	@Override
-	public void setYaw(Rotation2d yaw) {
-
+	public double getAccelerationMagnitude() {
+		return accelerometer.getAccelerationMagnitude();
 	}
 
 	@Override
-	public boolean isConnected() {
-		return false;
+	public double getAccelerationX() {
+		return accelerometer.getAccelerationX().getValue().magnitude();
 	}
 
 	@Override
-	public void updateInputs(InputSignal<?>... inputSignals) {
-
+	public double getAccelerationY() {
+		return accelerometer.getAccelerationY().getValue().magnitude();
 	}
+
+	@Override
+	public double getAccelerationZ() {
+		return accelerometer.getAccelerationZ().getValue().magnitude();
+	}
+
+	@Override
+	public ParentDevice getDevice() {
+		return accelerometer;
+	}
+
 }

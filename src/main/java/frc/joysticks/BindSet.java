90401fc0e90651abce1d43ed3a83ae1f6fbfe12a
Nitay4 <nitay@rotbart.co.il>
2025-01-09 11:31:12 +0200

    Nitay - cr

--- src/main/java/frc/joysticks/BindSet.java
+++ src/main/java/frc/joysticks/BindSet.java
@@ -5,10 +5,7 @@ import java.util.HashMap;
 public enum BindSet {
 
 	NO_JOYSTICK(0),
-	NO_BINDINGS(1),
-	SWERVE(2),
-	SECOND(3),
-	TESTING(4);
+	NO_BINDINGS(1);
 
 	private final int index;
 
@@ -21,14 +18,6 @@ public enum BindSet {
 	}
 
 	public static HashMap<Integer, BindSet> bindSets = new HashMap<Integer, BindSet>();
-
-	public static BindSet getBindSetByIndex(int index) {
-		return switch (index) {
-			case 0 -> NO_JOYSTICK;
-			case 1 -> NO_BINDINGS;
-			default -> NO_BINDINGS;
-		};
-	}
 
 	public static void addBindSetsToHashMap() {
 		for (BindSet bindSet : BindSet.values()) {

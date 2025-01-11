8da362f4e09dede9631b5c90b51634a648683583
drorta <125767200+drorta@users.noreply.github.com>
2025-01-11 13:54:59 +0200

    done

--- src/main/java/frc/constants/field/Field.java
+++ src/main/java/frc/constants/field/Field.java
@@ -1,7 +1,9 @@
 package frc.constants.field;
 
+import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.utils.DriverStationUtils;
+import frc.utils.math.MirrorMath;
 
 public class Field {
 
@@ -9,6 +11,23 @@ public class Field {
 
 	public static boolean isFieldConventionAlliance() {
 		return DriverStationUtils.getAlliance() == RELATIVE_FIELD_CONVENTION_ALLIANCE;
+	}
+
+	private static Pose2d getAllianceRelativePose2d(Pose2d pose2d, boolean mirrorY, boolean mirrorX, boolean invertAngle) {
+		return isFieldConventionAlliance() ? pose2d : MirrorMath.getMirrored(pose2d, mirrorY, mirrorX, invertAngle);
+	}
+
+	private static Translation2d getAllianceRelativeTranslation2d(Translation2d translation2d, boolean invertY) {
+		return isFieldConventionAlliance() ? translation2d : MirrorMath.getMirrored(translation2d, invertY);
+	}
+
+	private static Translation3d getAllianceRelativeTranslation3d(Translation3d translation3d, boolean invertZ) {
+		return isFieldConventionAlliance() ? translation3d : MirrorMath.getMirrored(translation3d, invertZ);
+	}
+
+	private static Pose3d getAllianceRelativePose3d(Pose3d pose3d, boolean mirrorAngle) {
+		Translation3d translation3d = getAllianceRelativeTranslation3d(pose3d.getTranslation(), false);
+		return mirrorAngle ? new Pose3d(translation3d, pose3d.getRotation().unaryMinus()) : new Pose3d(translation3d, pose3d.getRotation());
 	}
 
 	public static final double LENGTH_METERS = 16.54175;
--- src/main/java/frc/utils/math/MirrorMath.java
+++ src/main/java/frc/utils/math/MirrorMath.java
@@ -1,6 +1,6 @@
 package frc.utils.math;
 
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.*;
 import frc.constants.field.Field;
 import frc.constants.MathConstants;
 
@@ -16,6 +16,39 @@ public class MirrorMath {
 
 	public static double getMirroredY(double y) {
 		return Field.WIDTH_METERS - y;
+	}
+
+	public static Pose2d getMirrored(Pose2d pose2d, boolean mirrorY, boolean mirrorX, boolean invertAngle) {
+		if (mirrorY && mirrorX) {
+			return invertAngle
+				? new Pose2d(getMirroredX(pose2d.getX()), getMirroredY(pose2d.getY()), getMirroredAngle(pose2d.getRotation()))
+				: new Pose2d(getMirroredX(pose2d.getX()), getMirroredY(pose2d.getY()), pose2d.getRotation());
+		}
+		if (mirrorY) {
+			return invertAngle
+				? new Pose2d(pose2d.getX(), getMirroredY(pose2d.getY()), getMirroredAngle(pose2d.getRotation()))
+				: new Pose2d(pose2d.getX(), getMirroredY(pose2d.getY()), pose2d.getRotation());
+		}
+		if (mirrorX) {
+			return invertAngle
+				? new Pose2d(getMirroredX(pose2d.getX()), pose2d.getY(), getMirroredAngle(pose2d.getRotation()))
+				: new Pose2d(getMirroredX(pose2d.getX()), pose2d.getY(), pose2d.getRotation());
+		}
+		return invertAngle
+			? new Pose2d(pose2d.getX(), pose2d.getY(), getMirroredAngle(pose2d.getRotation()))
+			: new Pose2d(pose2d.getX(), pose2d.getY(), pose2d.getRotation());
+	}
+
+	public static Translation3d getMirrored(Translation3d translation3d, boolean invertZ) {
+		return invertZ
+			? new Translation3d(getMirroredX(translation3d.getX()), getMirroredY(translation3d.getY()), 0 - translation3d.getZ())
+			: new Translation3d(getMirroredX(translation3d.getX()), getMirroredY(translation3d.getY()), translation3d.getZ());
+	}
+
+	public static Translation2d getMirrored(Translation2d translation2d, boolean invertY) {
+		return invertY
+			? new Translation2d(getMirroredX(translation2d.getX()), getMirroredY(translation2d.getY()))
+			: new Translation2d(getMirroredX(translation2d.getX()), translation2d.getY());
 	}
 
 }

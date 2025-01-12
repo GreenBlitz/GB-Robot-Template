851987d631d8d17f93053aa43b1361067334dffd
Nitay4 <nitay@rotbart.co.il>
2025-01-12 16:41:17 +0200

    Nitay - not working

--- src/main/java/frc/JoystickBindings.java
+++ src/main/java/frc/JoystickBindings.java
@@ -1,5 +1,7 @@
 package frc;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.joysticks.BindSet;
 import frc.robot.Robot;
@@ -7,7 +9,11 @@ import frc.joysticks.SmartJoystick;
 
 public class JoystickBindings {
 
-	public static void configureBindings(SmartJoystick joystick, Robot robot) {}
+	public static void configureBindings(SmartJoystick joystick, Robot robot) {
+		applySwerveBindings(joystick);
+		applySecondBindings(joystick);
+		applyNoBindingsBindings(joystick);
+	}
 
 	private static Trigger bindSetTrigger(SmartJoystick joystick, Trigger bind, BindSet bindSetRequirement) {
 		return bind.and(bindSetTrigger(joystick, bindSetRequirement));
@@ -15,6 +21,23 @@ public class JoystickBindings {
 
 	private static Trigger bindSetTrigger(SmartJoystick joystick, BindSet bindSetRequirement) {
 		return new Trigger(() -> bindSetRequirement == joystick.getBindSet());
+	}
+
+	private static void applySwerveBindings(SmartJoystick joystick) {
+		bindSetTrigger(joystick, joystick.A, BindSet.SWERVE).onTrue(new InstantCommand(() -> joystick.setBindSet(BindSet.cycleBindSet(joystick.getBindSet(), 1))));
+		bindSetTrigger(joystick, joystick.B, BindSet.SWERVE).whileTrue(new RunCommand(() -> System.out.println("swerve")));
+	}
+
+	private static void applySecondBindings(SmartJoystick joystick) {
+		bindSetTrigger(joystick, joystick.A, BindSet.SECOND).onTrue(new InstantCommand(() -> joystick.setBindSet(BindSet.cycleBindSet(joystick.getBindSet(), 1))));
+		bindSetTrigger(joystick, joystick.B, BindSet.SECOND).whileTrue(new RunCommand(() -> System.out.println("second")));
+
+	}
+
+	private static void applyNoBindingsBindings(SmartJoystick joystick){
+		bindSetTrigger(joystick, joystick.A, BindSet.NO_BINDINGS).onTrue(new InstantCommand(() -> joystick.setBindSet(BindSet.cycleBindSet(joystick.getBindSet(), 1))));
+		bindSetTrigger(joystick, joystick.B, BindSet.NO_BINDINGS).whileTrue(new RunCommand(() -> System.out.println("empty")));
+
 	}
 
 
--- src/main/java/frc/joysticks/BindSet.java
+++ src/main/java/frc/joysticks/BindSet.java
@@ -3,7 +3,9 @@ package frc.joysticks;
 public enum BindSet {
 
 	NO_JOYSTICK(0),
-	NO_BINDINGS(1);
+	NO_BINDINGS(1),
+	SWERVE(2),
+	SECOND(3);
 
 	private final int index;
 
@@ -15,15 +17,21 @@ public enum BindSet {
 		return index;
 	}
 
-	public BindSet cycleBindSet(BindSet current, int jump) {
+	public static BindSet cycleBindSet(BindSet current, int jump) {
 		int index = current.getIndex();
 		BindSet[] bindSets = values();
-
+		System.out.println("Aaaaa"+current);
 		if (index + jump > bindSets.length - 1) {
+			System.out.println(1);
+			//System.out.println(bindSets[NO_BINDINGS.getIndex() + (index + jump - bindSets.length) % (bindSets.length - NO_BINDINGS.getIndex())].name());
 			return bindSets[NO_BINDINGS.getIndex() + (index + jump - bindSets.length) % (bindSets.length - NO_BINDINGS.getIndex())];
 		} else if (index + jump < NO_BINDINGS.getIndex()) {
+			System.out.println(2);
+			//System.out.println(bindSets[bindSets.length + ((index + jump - NO_BINDINGS.getIndex()) % (bindSets.length - NO_BINDINGS.getIndex()))].name());
 			return bindSets[bindSets.length + ((index + jump - NO_BINDINGS.getIndex()) % (bindSets.length - NO_BINDINGS.getIndex()))];
 		} else {
+			//System.out.println(bindSets[index + jump].name());
+			System.out.println(3);
 			return bindSets[index + jump];
 		}
 	}

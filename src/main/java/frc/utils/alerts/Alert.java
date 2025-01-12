98ac7a7de85c6469d4b916ef311cc00b7653758b
itamaroryan <itamar.oryan@gmail.com>
2025-01-12 20:37:38 +0200

    Romy - format

--- src/main/java/frc/utils/alerts/Alert.java
+++ src/main/java/frc/utils/alerts/Alert.java
@@ -6,14 +6,14 @@ import frc.utils.time.TimeUtils;
 import org.littletonrobotics.junction.Logger;
 
 public class Alert {
-	
+
 	public enum AlertType {
-		
+
 		ERROR,
 		WARNING;
-		
+
 	}
-	
+
 	private static final boolean LOG_TRACE = false;
 	private static final String ALERT_LOG_PATH = "Alerts/";
 	private static final double SECONDS_BETWEEN_REPORTS = 3;
@@ -21,20 +21,23 @@ public class Alert {
 	private final String logPath;
 	private double lastReportedTime;
 	private int timesHappenedBetweenReports;
-	
+
 	public Alert(AlertType type, String name) {
 		this.type = type;
 		this.logPath = ALERT_LOG_PATH + type.toString() + "/" + name;
 		this.lastReportedTime = -SECONDS_BETWEEN_REPORTS;
 		this.timesHappenedBetweenReports = 0;
 	}
-	
+
 	public void report() {
 		if (!DriverStationUtils.isMatch()) {
 			switch (type) {
 				case ERROR:
 					if (lastReportedTime <= TimeUtils.getCurrentTimeSeconds() - SECONDS_BETWEEN_REPORTS) {
-						DriverStation.reportError(logPath + " happened " + timesHappenedBetweenReports + 1 + " in the last " + SECONDS_BETWEEN_REPORTS + " seconds.", LOG_TRACE);
+						DriverStation.reportError(
+							logPath + " happened " + timesHappenedBetweenReports + 1 + " in the last " + SECONDS_BETWEEN_REPORTS + " seconds.",
+							LOG_TRACE
+						);
 						lastReportedTime = TimeUtils.getCurrentTimeSeconds();
 						timesHappenedBetweenReports = 0;
 					} else {
@@ -44,5 +47,5 @@ public class Alert {
 		}
 		Logger.recordOutput(logPath, TimeUtils.getCurrentTimeSeconds());
 	}
-	
+
 }

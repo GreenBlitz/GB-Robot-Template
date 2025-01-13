36e010b1194fcb0355d96ff54a64056bee5598ed
Yotam Cohen <76944434+Yotam7@users.noreply.github.com>
2025-01-13 20:12:33 +0200

    refactor(vision): add constructors to StandardDeviations2D by uncommenting preprepered code and doing slight adjustments so it compiles

--- src/main/java/frc/robot/poseestimator/helpers/StandardDeviations2D.java
+++ src/main/java/frc/robot/poseestimator/helpers/StandardDeviations2D.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import frc.robot.poseestimator.Pose2dComponentsValue;
 
 public record StandardDeviations2D(double xStandardDeviations, double yStandardDeviations, double angleStandardDeviations) {
 
@@ -15,23 +16,21 @@ public record StandardDeviations2D(double xStandardDeviations, double yStandardD
 		this(stdDev, stdDev, stdDev);
 	}
 
-// the following code is waiting for poseestimator-visionstuff-improved to get merged
+	public StandardDeviations2D(double[] stdDevs) {
+		this(
+			stdDevs[Pose2dComponentsValue.X_VALUE.getIndex()],
+			stdDevs[Pose2dComponentsValue.Y_VALUE.getIndex()],
+			stdDevs[Pose2dComponentsValue.ROTATION_VALUE.getIndex()]
+		);
+	}
 
-//	public StandardDeviations2D(double[] stdDevs) {
-//		this(
-//			stdDevs[Pose2dComponentsValue.X_VALUE.getIndex()],
-//			stdDevs[Pose2dComponentsValue.Y_VALUE.getIndex()],
-//			stdDevs[Pose2dComponentsValue.ROTATION_VALUE.getIndex()]
-//		);
-//	}
-
-//	public StandardDeviations2D (StandardDeviations2D standardDeviations2D) {
-//		return new StandardDeviations2D(
-//			standardDeviations2D.xAxisStandardDeviations,
-//			standardDeviations2D.yAxisStandardDeviations,
-//			standardDeviations2D.yawStandardDeviations
-//		);
-//	}
+	public StandardDeviations2D (StandardDeviations2D standardDeviations2D) {
+		this(
+			standardDeviations2D.xStandardDeviations,
+			standardDeviations2D.yStandardDeviations,
+			standardDeviations2D.angleStandardDeviations
+		);
+	}
 
 	public Matrix<N3, N1> asColumnVector() {
 		return VecBuilder.fill(xStandardDeviations, yStandardDeviations, angleStandardDeviations);

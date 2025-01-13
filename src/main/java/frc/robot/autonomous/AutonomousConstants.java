816460b14e923acf092821588fbc69ecc95789ea
YHerm <0549405422.yh@gmail.com>
2025-01-13 21:31:32 +0200

    space

--- src/main/java/frc/robot/autonomous/AutonomousConstants.java
+++ src/main/java/frc/robot/autonomous/AutonomousConstants.java
@@ -9,6 +9,7 @@ import edu.wpi.first.math.system.plant.DCMotor;
 public class AutonomousConstants {
 
 	public static final String LOG_PATH_PREFIX = "Autonomous/";
+
 	public static final PathConstraints REAL_TIME_CONSTRAINTS = new PathConstraints(2.5, 2.5, 4, 4);
 
 	public static final double CLOSE_TO_TARGET_POSITION_DEADBAND_METERS = 0.5;

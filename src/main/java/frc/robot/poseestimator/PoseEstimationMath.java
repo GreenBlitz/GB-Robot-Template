c4f9b2cf585fb2e867cc77ed280f9523596f8972
GilStein1 <gils.server@gmail.com>
2025-01-15 21:19:47 +0200

    Gil - deleted the stdDev method that I don't need

--- src/main/java/frc/robot/poseestimator/PoseEstimationMath.java
+++ src/main/java/frc/robot/poseestimator/PoseEstimationMath.java
@@ -1,32 +1,9 @@
 package frc.robot.poseestimator;
 
 import edu.wpi.first.math.geometry.*;
-import frc.robot.poseestimator.helpers.StandardDeviations2D;
-import frc.robot.vision.data.AprilTagVisionData;
 import frc.robot.vision.data.VisionData;
 
 public class PoseEstimationMath {
-
-	public static StandardDeviations2D calculateStandardDeviationOfPose(AprilTagVisionData rawVisionData, Pose2d currentEstimatedPose) {
-		double normalizedLimelightX = rawVisionData.getEstimatedPose().getX();
-		double normalizedLimelightY = rawVisionData.getEstimatedPose().getY();
-		double normalizedEstimatedX = currentEstimatedPose.getX();
-		double normalizedEstimatedY = currentEstimatedPose.getY();
-		return new StandardDeviations2D(
-			calculateStandardDeviation(normalizedLimelightX, normalizedEstimatedX),
-			calculateStandardDeviation(normalizedLimelightY, normalizedEstimatedY),
-			calculateStandardDeviation(
-				rawVisionData.getEstimatedPose().toPose2d().getRotation().getRadians(),
-				currentEstimatedPose.getRotation().getRadians()
-			)
-		);
-	}
-
-	@Deprecated
-	private static double calculateStandardDeviation(double estimatedValue, double currentValue) {
-		double mean = (estimatedValue + currentValue) / 2;
-		return Math.sqrt((Math.pow(estimatedValue - mean, 2) + Math.pow(currentValue - mean, 2)) / 2);
-	}
 
 	public static double deriveVisionData(VisionData starting, VisionData finish) {
 		double deltaTime = finish.getTimestamp() - starting.getTimestamp();

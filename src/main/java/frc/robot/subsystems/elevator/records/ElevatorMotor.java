45b170e798c3978d706bb87ed3f82611fb60390c
drorta <125767200+drorta@users.noreply.github.com>
2025-01-15 16:56:47 +0200

    what

--- src/main/java/frc/robot/subsystems/elevator/records/ElevatorMotor.java
+++ src/main/java/frc/robot/subsystems/elevator/records/ElevatorMotor.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.elevator.records;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.hardware.interfaces.ControllableMotor;
+import frc.robot.hardware.interfaces.IRequest;
+import frc.robot.hardware.interfaces.InputSignal;
 
-public record ElevatorMotor(ControllableMotor motor, ElevatorRequests requests, ElevatorSignals signals) {}
+public record ElevatorMotor(ControllableMotor motor, IRequest<Rotation2d> positionRequest, IRequest<Double> voltageRequest, InputSignal<Rotation2d> positionSignal, InputSignal<Double> voltageSignal, InputSignal<?>... otherSignals) {}

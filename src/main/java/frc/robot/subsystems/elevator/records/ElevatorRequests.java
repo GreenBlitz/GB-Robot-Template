da2ed686c0f46ec3b4a784beede15433ee3857d9
drorta <125767200+drorta@users.noreply.github.com>
2025-01-15 16:51:19 +0200

    rm

--- src/main/java/frc/robot/subsystems/elevator/records/ElevatorRequests.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.subsystems.elevator.records;
-
-import edu.wpi.first.math.geometry.Rotation2d;
-import frc.robot.hardware.interfaces.IRequest;
-
-public record ElevatorRequests(IRequest<Rotation2d> positionRequest, IRequest<Double> voltageRequest) {}

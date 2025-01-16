da9948a8b8adc10c1ee2bb65d493a15b08bbc17a
Yehonatan-Levi <yehonatan.levi0904@gmail.com>
2025-01-16 17:29:57 +0200

    Levi: moving code from gb template

---
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -0,0 +1,87 @@
+package frc.robot.subsystems.arm;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.hardware.interfaces.ControllableMotor;
+import frc.robot.hardware.interfaces.IRequest;
+import frc.robot.hardware.interfaces.InputSignal;
+import frc.robot.subsystems.GBSubsystem;
+import frc.utils.math.ToleranceMath;
+
+public class Arm extends GBSubsystem {
+
+    public static final Rotation2d STARTING_POSITION = Rotation2d.fromDegrees(0);
+
+    private final ControllableMotor motor;
+    private final IRequest<Rotation2d> positionRequest;
+    private final IRequest<Double> voltageRequest;
+    private final InputSignal<Rotation2d> positionSignal;
+    private final InputSignal<Double> voltageSignal;
+    private final ArmCommandsBuilder commandsBuilder;
+
+    public Arm(
+            String logPath,
+            ControllableMotor motor,
+            IRequest<Rotation2d> positionRequest,
+            IRequest<Double> voltageRequest,
+            InputSignal<Rotation2d> positionSignal,
+            InputSignal<Double> voltageSignal
+    ) {
+        super(logPath);
+        this.motor = motor;
+        this.positionRequest = positionRequest;
+        this.voltageRequest = voltageRequest;
+        this.positionSignal = positionSignal;
+        this.voltageSignal = voltageSignal;
+        this.commandsBuilder = new ArmCommandsBuilder(this);
+
+        motor.resetPosition(STARTING_POSITION);
+        updateInputs();
+    }
+
+    @Override
+    protected void subsystemPeriodic() {
+        updateInputs();
+    }
+
+    private void updateInputs() {
+        motor.updateSimulation();
+        motor.updateInputs(positionSignal, voltageSignal);
+    }
+
+    public ArmCommandsBuilder getCommandsBuilder() {
+        return commandsBuilder;
+    }
+
+    public Rotation2d getPosition() {
+        return positionSignal.getLatestValue();
+    }
+
+    protected void setTargetPosition(Rotation2d angle) {
+        motor.applyRequest(positionRequest.withSetPoint(angle));
+    }
+
+    public Double getVoltage() {
+        return voltageSignal.getLatestValue();
+    }
+
+    protected void setVoltage(double voltage) {
+        motor.applyRequest(voltageRequest.withSetPoint(voltage));
+    }
+
+    public void setBrake(boolean brake) {
+        motor.setBrake(brake);
+    }
+
+    protected void setPower(double power) {
+        motor.setPower(power);
+    }
+
+    protected void stayInPlace() {
+        setTargetPosition(positionSignal.getLatestValue());
+    }
+
+    public boolean isAtPosition(Rotation2d position, Rotation2d tolerance) {
+        return ToleranceMath.isNearWrapped(position, positionSignal.getLatestValue(), tolerance);
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/arm/ArmCommandsBuilder.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems.arm;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.utils.utilcommands.LoggedDashboardCommand;
+
+public class ArmCommandsBuilder {
+
+    private final Arm arm;
+
+    public ArmCommandsBuilder(Arm arm) {
+        this.arm = arm;
+    }
+
+    public Command moveToPosition(Rotation2d position) {
+        return arm.asSubsystemCommand(new InstantCommand(() -> arm.setTargetPosition(position)), "Move example arm to position: " + position);
+    }
+
+    public Command stayInPlace() {
+        return arm.asSubsystemCommand(new RunCommand(arm::stayInPlace), "Example arm stay in place");
+    }
+
+    public Command setVoltage(double voltage) {
+        return arm.asSubsystemCommand(new RunCommand(() -> arm.setVoltage(voltage)), "Set example arm voltage to: " + voltage);
+    }
+
+    public Command setPower(double power) {
+        return arm.asSubsystemCommand(new RunCommand(() -> arm.setPower(power)), "Set example arm power to: " + power);
+    }
+
+    public Command loggedDashboardSetVoltage() {
+        return arm.asSubsystemCommand(
+                new LoggedDashboardCommand("Set Example Arm Voltage", (voltage) -> arm.setVoltage(voltage)),
+                "Set example arm voltage through dashboard, set voltage to the given value"
+        );
+    }
+
+}

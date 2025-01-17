ce70c8105b2e97c99be9565f05bce12a7c7b40dc
drorta <125767200+drorta@users.noreply.github.com>
2025-01-18 01:00:13 +0200

    Update with other branches

--- src/main/java/frc/robot/subsystems/elevator/factory/ElevatorConstants.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.subsystems.elevator.factory;
-
-public class ElevatorConstants {
-
-    private static final double MASS_KG = 10;
-
-    public static final double MINIMUM_ACHIEVABLE_POSITION_METERS = 0;
-
-}

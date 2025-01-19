fe90aeadc831d6bd1607fac71e37b01ff1da75b7
Nitay4 <nitay@rotbart.co.il>
2025-01-19 17:00:37 +0200

    Nitay - added the states

---
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorState.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems.endEffector;
+
+public enum EndEffectorState {
+
+    INTAKE(0.2),
+    OUTTAKE(-0.2),
+    IDLE(0);
+
+    double power;
+
+    EndEffectorState(double power){
+        this.power = power;
+    }
+
+    public double getPower() {
+        return power;
+    }
+}

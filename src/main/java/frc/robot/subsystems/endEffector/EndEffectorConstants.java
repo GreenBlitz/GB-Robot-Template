ee0b13720491541fedd211ba0671e2f0e8eaafb4
Nitay4 <nitay@rotbart.co.il>
2025-01-21 20:20:22 +0200

    nitay - fix herman

--- src/main/java/frc/robot/subsystems/endEffector/EndEffectorConstants.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorConstants.java
@@ -4,6 +4,6 @@ import frc.robot.RobotConstants;
 
 public class EndEffectorConstants {
 
-	public static final String LOG_PATH = RobotConstants.SUBSYSTEM_LOGPATH_PREFIX + "/" + "EndEffector";
+	public static final String LOG_PATH = RobotConstants.SUBSYSTEM_LOGPATH_PREFIX + "/EndEffector";
 
 }

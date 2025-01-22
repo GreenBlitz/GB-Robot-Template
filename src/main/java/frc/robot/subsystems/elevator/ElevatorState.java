7772a58336032670faf2c2c70a8c842cf496265b
Nitay4 <nitay@rotbart.co.il>
2025-01-22 21:37:34 +0200

    nitay - added the states

--- src/main/java/frc/robot/subsystems/elevator/ElevatorState.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorState.java
@@ -2,22 +2,22 @@ package frc.robot.subsystems.elevator;
 
 public enum ElevatorState {
 
-    CLOSED(0),
-    FEEDER(0.5),
-    L1(0.1),
-    L2(0.2),
-    L3(0.3),
-    L4(0.4),
-    OUTTAKE(0.05);
+	CLOSED(0),
+	FEEDER(0.5),
+	L1(0.1),
+	L2(0.2),
+	L3(0.3),
+	L4(0.4),
+	OUTTAKE(0.05);
 
-    private final double heightMeters;
+	private final double heightMeters;
 
-    ElevatorState(double heightMeters) {
-        this.heightMeters = heightMeters;
-    }
+	ElevatorState(double heightMeters) {
+		this.heightMeters = heightMeters;
+	}
 
-    public double getHeightMeters() {
-        return heightMeters;
-    }
+	public double getHeightMeters() {
+		return heightMeters;
+	}
 
 }
--- src/main/java/frc/robot/subsystems/elevator/ElevatorStateHandler.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorStateHandler.java
@@ -4,14 +4,14 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public class ElevatorStateHandler {
 
-    private final Elevator elevator;
+	private final Elevator elevator;
 
-    public ElevatorStateHandler(Elevator elevator){
-        this.elevator = elevator;
-    }
+	public ElevatorStateHandler(Elevator elevator) {
+		this.elevator = elevator;
+	}
 
-    public Command setState(ElevatorState state){
-        return elevator.getCommandsBuilder().setTargetPositionMeters(state.getHeightMeters());
-    }
+	public Command setState(ElevatorState state) {
+		return elevator.getCommandsBuilder().setTargetPositionMeters(state.getHeightMeters());
+	}
 
 }

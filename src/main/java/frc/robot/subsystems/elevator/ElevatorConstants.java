20f961dd37882fcf52daa2d3826e5c9733f05ca4
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2025-01-22 12:49:36 +0200

    ids

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -2,17 +2,16 @@ package frc.robot.subsystems.elevator;
 
 public class ElevatorConstants {
 
-	public static final double DRUM_RADIUS_METERS = -1;
-	public static final double ELEVATOR_RADIUS_METERS = -1;
-	public static final double MASS_KG = -1;
-	public static final double MINIMUM_HEIGHT_METERS = -1;
-	public static final double MAXIMUM_HEIGHT_METERS = -1;
-	public static final double MAXIMUM_FIRST_STAGE_HEIGHT = -1;
+	public static final double DRUM_RADIUS_METERS = 0.05;
+	public static final double MASS_KG = 5;
+	public static final double MINIMUM_HEIGHT_METERS = 0;
+	public static final double MAXIMUM_HEIGHT_METERS = 1.4;
+	public static final double MAXIMUM_FIRST_STAGE_HEIGHT_METERS = 0.7;
 
-	public final static double GEAR_RATIO = -1;
+	public final static double GEAR_RATIO = 4;
 
-	public static final double REVERSE_SOFT_LIMIT_VALUE_METERS = -1;
+	public static final double REVERSE_SOFT_LIMIT_VALUE_METERS = 0;
 
-	public static final double FORWARD_SOFT_LIMIT_VALUE_METERS = -1;
+	public static final double FORWARD_SOFT_LIMIT_VALUE_METERS = 1.4;
 
 }

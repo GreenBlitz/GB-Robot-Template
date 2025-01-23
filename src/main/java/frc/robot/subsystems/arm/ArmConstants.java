53ce2e572b312d71e62900bcc47065d47761105c
drorta <125767200+drorta@users.noreply.github.com>
2025-01-23 15:43:39 +0200

    1

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -4,7 +4,7 @@ public class ArmConstants {
 
 	public static final int FORWARD_SOFTWARE_LIMIT = 50;
 	public static final int REVERSED_SOFTWARE_LIMIT = 17;
-	public static final double LENGTH_METERS = 0.4;
+	public static final double LENGTH_METERS = 1;
 	public static final double MASS_KG = 5;
 
 }

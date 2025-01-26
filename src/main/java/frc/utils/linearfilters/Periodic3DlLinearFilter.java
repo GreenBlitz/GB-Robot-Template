ad75de21f0b0717c7ef940a8f340a01b574cb03f
Shahar Perets <perets.shahar@gmail.com>
2025-01-26 15:58:58 +0200

    partial CR

--- src/main/java/frc/utils/linearfilters/Periodic3DlLinearFilter.java
+++ src/main/java/frc/utils/linearfilters/Periodic3DlLinearFilter.java
@@ -3,28 +3,28 @@ package frc.utils.linearfilters;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.math.numbers.N3;
+import frc.robot.poseestimator.Pose2dComponentsValue;
 import org.ejml.simple.SimpleMatrix;
 import org.littletonrobotics.junction.Logger;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
 public class Periodic3DlLinearFilter implements IPeriodicLinearFilter {
 
-	private final Supplier<ArrayList<Vector<N3>>> updateValues;
-	private final ArrayList<LinearFilter> linearFilters;
+	private final Supplier<? extends List<Vector<N3>>> updateValues;
+	private final List<LinearFilter> linearFilters;
 	private final String name;
 
 	public Periodic3DlLinearFilter(
-		Supplier<ArrayList<Vector<N3>>> updateValues,
+		Supplier<List<Vector<N3>>> updateValues,
 		LinearFilter xFilter,
 		LinearFilter yFilter,
 		LinearFilter zFilter,
 		String name
 	) {
 		this.updateValues = updateValues;
-		this.linearFilters = new ArrayList<>(List.of(xFilter, yFilter, zFilter));
+		this.linearFilters = List.of(xFilter, yFilter, zFilter);
 		this.name = name;
 	}
 
@@ -37,7 +37,7 @@ public class Periodic3DlLinearFilter implements IPeriodicLinearFilter {
 	public void log(String parentLogPath) {
 		String logPath = parentLogPath + name + "/";
 		for (int i = 0; i < updateValues.get().size(); i++) {
-			Logger.recordOutput(logPath + "input/" + i, updateValues.get().get(i));
+			Logger.recordOutput(logPath + "input" + i, updateValues.get().get(i));
 		}
 		Logger.recordOutput(logPath + "output", getAsColumnVector());
 	}
@@ -45,7 +45,7 @@ public class Periodic3DlLinearFilter implements IPeriodicLinearFilter {
 	@Override
 	public void update() {
 		for (Vector<N3> data : updateValues.get()) {
-			for (int i = 0; i < 3; i++) {
+			for (int i = 0; i < Pose2dComponentsValue.POSE2D_COMPONENTS_AMOUNT; i++) {
 				linearFilters.get(i).calculate(data.get(i));
 			}
 		}

305f0b88375f2efcc5601948fb2c881270cc21c8
YHerm <0549405422.yh@gmail.com>
2025-01-26 14:37:20 +0200

    format

--- src/main/java/frc/robot/subsystems/swerve/factories/modules/steer/SteerFactory.java
+++ src/main/java/frc/robot/subsystems/swerve/factories/modules/steer/SteerFactory.java
@@ -14,17 +14,13 @@ public class SteerFactory {
 		logPath += ModuleConstants.MODULES_LOG_PATH_ADDITION + "/" + modulePosition + "/Steer";
 		return switch (modulePosition) {
 			case FRONT_LEFT ->
-				Falcon500SteerBuilder
-					.buildSteer(logPath, IDs.TalonFXIDs.SWERVE_FRONT_LEFT_STEER, IDs.CANCodersIDs.SWERVE_FRONT_LEFT, true);
+				Falcon500SteerBuilder.buildSteer(logPath, IDs.TalonFXIDs.SWERVE_FRONT_LEFT_STEER, IDs.CANCodersIDs.SWERVE_FRONT_LEFT, true);
 			case FRONT_RIGHT ->
-				Falcon500SteerBuilder
-					.buildSteer(logPath, IDs.TalonFXIDs.SWERVE_FRONT_RIGHT_STEER, IDs.CANCodersIDs.SWERVE_FRONT_RIGHT, true);
+				Falcon500SteerBuilder.buildSteer(logPath, IDs.TalonFXIDs.SWERVE_FRONT_RIGHT_STEER, IDs.CANCodersIDs.SWERVE_FRONT_RIGHT, true);
 			case BACK_LEFT ->
-				Falcon500SteerBuilder
-					.buildSteer(logPath, IDs.TalonFXIDs.SWERVE_BACK_LEFT_STEER, IDs.CANCodersIDs.SWERVE_BACK_LEFT, false);
+				Falcon500SteerBuilder.buildSteer(logPath, IDs.TalonFXIDs.SWERVE_BACK_LEFT_STEER, IDs.CANCodersIDs.SWERVE_BACK_LEFT, false);
 			case BACK_RIGHT ->
-				Falcon500SteerBuilder
-					.buildSteer(logPath, IDs.TalonFXIDs.SWERVE_BACK_RIGHT_STEER, IDs.CANCodersIDs.SWERVE_BACK_RIGHT, true);
+				Falcon500SteerBuilder.buildSteer(logPath, IDs.TalonFXIDs.SWERVE_BACK_RIGHT_STEER, IDs.CANCodersIDs.SWERVE_BACK_RIGHT, true);
 		};
 	}
 

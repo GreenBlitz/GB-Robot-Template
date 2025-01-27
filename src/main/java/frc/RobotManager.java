5ac14bc8dcf5b9df63327399214cc5b53f103415
itamaroryan <itamar.oryan@gmail.com>
2025-01-27 17:27:09 +0200

    calibbb

--- src/main/java/frc/RobotManager.java
+++ src/main/java/frc/RobotManager.java
@@ -4,6 +4,7 @@
 
 package frc;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 import frc.utils.auto.PathPlannerUtils;
@@ -64,6 +65,7 @@ public class RobotManager extends LoggedRobot {
 		if (autonomousCommand != null) {
 			autonomousCommand.cancel();
 		}
+		robot.getArm().getCommandsBuilder().moveToPosition(Rotation2d.fromDegrees(60)).schedule();
 	}
 
 	@Override

1d9376624a8313e6fa02b93b38a2e12a5077f7aa
hillelv <hillel.vider@gmail.com>
2025-01-28 20:56:54 +0200

    hillel - rename back

--- src/main/java/frc/utils/buffers/RingBuffer/RingBufferIterator.java
+++ src/main/java/frc/utils/buffers/RingBuffer/RingBufferIterator.java
@@ -5,25 +5,25 @@ import java.util.NoSuchElementException;
 
 public class RingBufferIterator<T> implements Iterator<T> {
 
-	private final BufferedQueue<T> bufferedQueue;
+	private final RingBuffer<T> ringBuffer;
 	private final int endIndex;
 	private int currentIndex;
 	private boolean used;
 
-	protected RingBufferIterator(BufferedQueue<T> bufferedQueue) {
-		int insertions = bufferedQueue.getInsertions();
+	protected RingBufferIterator(RingBuffer<T> ringBuffer) {
+		int insertions = ringBuffer.getInsertions();
 
-		this.bufferedQueue = bufferedQueue;
-		this.endIndex = bufferedQueue.getCurrentIndex();
-		this.currentIndex = insertions >= bufferedQueue.size()
-			? bufferedQueue.getCurrentIndex()
-			: bufferedQueue.wrapIndex(bufferedQueue.getCurrentIndex() - insertions);
+		this.ringBuffer = ringBuffer;
+		this.endIndex = ringBuffer.getCurrentIndex();
+		this.currentIndex = insertions >= ringBuffer.size()
+			? ringBuffer.getCurrentIndex()
+			: ringBuffer.wrapIndex(ringBuffer.getCurrentIndex() - insertions);
 		this.used = false;
 	}
 
 	@Override
 	public boolean hasNext() {
-		return bufferedQueue.existsAtIndex(currentIndex) && !((this.used) && currentIndex == endIndex);
+		return ringBuffer.existsAtIndex(currentIndex) && !((this.used) && currentIndex == endIndex);
 	}
 
 	@Override
@@ -31,8 +31,8 @@ public class RingBufferIterator<T> implements Iterator<T> {
 		if (!hasNext()) {
 			throw new NoSuchElementException("Ring buffer iterator exhausted");
 		}
-		T value = bufferedQueue.getAtIndex(currentIndex).get(); // the get operation is safe because we check if the value exists in hasNext()
-		currentIndex = bufferedQueue.wrapIndex(currentIndex + 1);
+		T value = ringBuffer.getAtIndex(currentIndex).get(); // the get operation is safe because we check if the value exists in hasNext()
+		currentIndex = ringBuffer.wrapIndex(currentIndex + 1);
 		this.used = true;
 		return value;
 	}

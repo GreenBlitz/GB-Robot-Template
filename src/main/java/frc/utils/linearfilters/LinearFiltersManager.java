6ee6e1157481092ff5e0b3828a83607b47781752
Shahar Perets <perets.shahar@gmail.com>
2025-01-29 15:10:17 +0200

    enabling

--- src/main/java/frc/utils/linearfilters/LinearFiltersManager.java
+++ src/main/java/frc/utils/linearfilters/LinearFiltersManager.java
@@ -5,21 +5,20 @@ import org.littletonrobotics.junction.Logger;
 import java.util.ArrayList;
 
 /**
- * A class for automatic updating of linear filters. Its periodic shall run ASAP.
+ * A class for automatic updating of linear filters. Its periodic shall run ASAP. Has the functionality to be disabled in case of taking too much
+ * resources.
  */
 public class LinearFiltersManager {
 
-	private static boolean running = true;
+	private static boolean enabled = true;
 
 	private static final ArrayList<IPeriodicLinearFilter> periodicLinearFilters = new ArrayList<>();
 
 	public static void periodic(String logPath) {
-		Logger.recordOutput(logPath + "/running", running);
-		if (running) {
-			for (IPeriodicLinearFilter filter : periodicLinearFilters) {
-				filter.update();
-				filter.log(logPath);
-			}
+		Logger.recordOutput(logPath + "/enabled", enabled);
+		for (IPeriodicLinearFilter filter : periodicLinearFilters) {
+			filter.update();
+			filter.log(logPath);
 		}
 	}
 
@@ -31,15 +30,16 @@ public class LinearFiltersManager {
 		periodicLinearFilters.forEach(IPeriodicLinearFilter::hardReset);
 	}
 
-	/**
-	 * disables the periodic method. Can be used if takes too much resources.
-	 */
 	public static void disable() {
-		running = false;
+		enabled = false;
 	}
 
-	public static void enable() {
-		running = true;
+	public static void enabled() {
+		enabled = true;
+	}
+
+	public static boolean isEnabled() {
+		return enabled;
 	}
 
 }

0e98debf2e9de7e8ad5ef63e0ae8aee8df9242a8
YHerm <0549405422.yh@gmail.com>
2025-01-29 21:43:34 +0200

    cleaned

--- src/main/java/frc/robot/scoringhelpers/ScoringHelpers.java
+++ src/main/java/frc/robot/scoringhelpers/ScoringHelpers.java
@@ -12,14 +12,8 @@ public class ScoringHelpers {
 		Translation2d branchTranslation = Field.getCoralPlacement(branch);
 		Rotation2d targetRobotAngle = Field.getMiddleOfReefSide(branch.getReefSide()).getRotation();
 
-		double xDistanceFromBranch = distanceFromBranchMeters * Math.cos(targetRobotAngle.getRadians());
-		double yDistanceFromBranch = distanceFromBranchMeters * Math.sin(targetRobotAngle.getRadians());
-
-		Translation2d scoringTranslation = new Translation2d(
-			branchTranslation.getX() - xDistanceFromBranch,
-			branchTranslation.getY() - yDistanceFromBranch
-		);
-		return new Pose2d(scoringTranslation, targetRobotAngle);
+		Translation2d differenceTranslation = new Translation2d(distanceFromBranchMeters, targetRobotAngle);
+		return new Pose2d(branchTranslation.minus(differenceTranslation), targetRobotAngle);
 	}
 
 }

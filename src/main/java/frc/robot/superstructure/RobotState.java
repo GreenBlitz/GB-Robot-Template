2d916ac452c2c429164baa08fe7f3095ef78ad75
drorta <125767200+drorta@users.noreply.github.com>
2025-01-31 01:03:43 +0200

    line

--- src/main/java/frc/robot/superstructure/RobotState.java
+++ src/main/java/frc/robot/superstructure/RobotState.java
@@ -2,17 +2,17 @@ package frc.robot.superstructure;
 
 public enum RobotState {
 
-    IDLE,
-    FEEDER_INTAKE,
-    L1,
-    L2,
-    L3,
-    L4,
-    PRE_L1,
-    PRE_L2,
-    PRE_L3,
-    PRE_L4,
-    OUTTAKE,
-    ALIGN_REEF
+	IDLE,
+	FEEDER_INTAKE,
+	L1,
+	L2,
+	L3,
+	L4,
+	PRE_L1,
+	PRE_L2,
+	PRE_L3,
+	PRE_L4,
+	OUTTAKE,
+	ALIGN_REEF
 
 }

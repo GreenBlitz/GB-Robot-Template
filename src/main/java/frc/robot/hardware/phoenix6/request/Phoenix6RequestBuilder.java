cca4a141d46eaa945ecacfb5f38aa5157f9a65e8
Nitay4 <nitay@rotbart.co.il>
2025-01-31 13:54:50 +0200

    nitay - added FF to MM

--- src/main/java/frc/robot/hardware/phoenix6/request/Phoenix6RequestBuilder.java
+++ src/main/java/frc/robot/hardware/phoenix6/request/Phoenix6RequestBuilder.java
@@ -11,8 +11,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public class Phoenix6RequestBuilder {
 
-	public static Phoenix6FeedForwardRequest build(PositionVoltage positionVoltage, double defaultArbitraryFeedForward, boolean enableFOC) {
-		positionVoltage.withFeedForward(defaultArbitraryFeedForward);
+	public static Phoenix6FeedForwardRequest build(PositionVoltage positionVoltage, boolean enableFOC) {
 		return new Phoenix6FeedForwardRequest(
 			Rotation2d.fromRotations(positionVoltage.Position),
 			positionVoltage.withEnableFOC(enableFOC),
@@ -21,8 +20,7 @@ public class Phoenix6RequestBuilder {
 		);
 	}
 
-	public static Phoenix6FeedForwardRequest build(VelocityVoltage velocityVoltage, double defaultArbitraryFeedForward, boolean enableFOC) {
-		velocityVoltage.withFeedForward(defaultArbitraryFeedForward);
+	public static Phoenix6FeedForwardRequest build(VelocityVoltage velocityVoltage, boolean enableFOC) {
 		return new Phoenix6FeedForwardRequest(
 			Rotation2d.fromRotations(velocityVoltage.Velocity),
 			velocityVoltage.withEnableFOC(enableFOC),
@@ -31,24 +29,26 @@ public class Phoenix6RequestBuilder {
 		);
 	}
 
-	public static Phoenix6Request<Double> build(VoltageOut voltageOut, boolean enableFOC) {
-		return new Phoenix6Request<>(voltageOut.Output, voltageOut.withEnableFOC(enableFOC), voltageOut::withOutput);
-	}
-
-	public static Phoenix6Request<Rotation2d> build(MotionMagicVoltage motionMagicVoltage, boolean enableFOC) {
-		return new Phoenix6Request<>(
+	public static Phoenix6FeedForwardRequest build(MotionMagicVoltage motionMagicVoltage, boolean enableFOC) {
+		return new Phoenix6FeedForwardRequest(
 			Rotation2d.fromRotations(motionMagicVoltage.Position),
 			motionMagicVoltage.withEnableFOC(enableFOC),
-			setPoint -> motionMagicVoltage.withPosition(setPoint.getRotations())
+			setPoint -> motionMagicVoltage.withPosition(setPoint.getRotations()),
+			motionMagicVoltage::withFeedForward
 		);
 	}
 
-	public static Phoenix6Request<Rotation2d> build(DynamicMotionMagicVoltage dynamicMotionMagicVoltage, boolean enableFOC) {
-		return new Phoenix6Request<>(
+	public static Phoenix6FeedForwardRequest build(DynamicMotionMagicVoltage dynamicMotionMagicVoltage, boolean enableFOC) {
+		return new Phoenix6FeedForwardRequest(
 			Rotation2d.fromRotations(dynamicMotionMagicVoltage.Position),
 			dynamicMotionMagicVoltage.withEnableFOC(enableFOC),
-			setPoint -> dynamicMotionMagicVoltage.withPosition(setPoint.getRotations())
+			setPoint -> dynamicMotionMagicVoltage.withPosition(setPoint.getRotations()),
+			dynamicMotionMagicVoltage::withFeedForward
 		);
+	}
+
+	public static Phoenix6Request<Double> build(VoltageOut voltageOut, boolean enableFOC) {
+		return new Phoenix6Request<>(voltageOut.Output, voltageOut.withEnableFOC(enableFOC), voltageOut::withOutput);
 	}
 
 	public static Phoenix6Request<Double> build(TorqueCurrentFOC torqueCurrentFOC) {

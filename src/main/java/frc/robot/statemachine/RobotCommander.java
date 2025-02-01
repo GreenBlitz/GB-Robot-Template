60cb0a5fa1c2ffa80e5006fefcc5588501f60820
drorta <125767200+drorta@users.noreply.github.com>
2025-02-01 20:20:26 +0200

    format

--- src/main/java/frc/robot/statemachine/RobotCommander.java
+++ src/main/java/frc/robot/statemachine/RobotCommander.java
@@ -2,7 +2,6 @@ package frc.robot.statemachine;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.*;

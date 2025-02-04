76a25783f3c954537f60ac9daba4da3de5d0955f
hillelv <hillel.vider@gmail.com>
2025-02-04 16:21:22 +0200

    Ran - cleaned robot manager again

--- src/main/java/frc/RobotManager.java
+++ src/main/java/frc/RobotManager.java
@@ -6,9 +6,6 @@ package frc;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
-import frc.robot.hardware.rev.motors.BrushlessSparkMAXMotor;
-import frc.robot.hardware.signal.supplied.SuppliedAngleSignal;
-import frc.robot.hardware.signal.supplied.SuppliedDoubleSignal;
 import frc.utils.DriverStationUtils;
 import frc.utils.alerts.AlertManager;
 import frc.utils.auto.PathPlannerUtils;
@@ -28,9 +25,6 @@ public class RobotManager extends LoggedRobot {
 	private final Robot robot;
 	private Command autonomousCommand;
 	private int roborioCycles;
-	private BrushlessSparkMAXMotor motor;
-	private SuppliedAngleSignal velocitySignal;
-	private SuppliedDoubleSignal voltageSignal;
 
 	public RobotManager() {
 		LoggerFactory.initializeLogger();

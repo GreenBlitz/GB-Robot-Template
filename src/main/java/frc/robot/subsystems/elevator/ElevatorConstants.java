0a16790b371401be0d82bbd88a6ee29c1a36e336
drorta <125767200+drorta@users.noreply.github.com>
2025-02-05 18:17:40 +0200

    wait

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -1,15 +1,17 @@
 package frc.robot.subsystems.elevator;
 
+import frc.constants.MathConstants;
+
 public class ElevatorConstants {
 
-	public static final double DRUM_DIAMETER_METERS = 0.05;
+	public static final double DRUM_DIAMETER_METERS = 0.04138 / Math.PI;
 	public static final double DRUM_RADIUS_METERS = DRUM_DIAMETER_METERS / 2;
 	public static final double MASS_KG = 5;
 	public static final double MINIMUM_HEIGHT_METERS = 0;
 	public static final double MAXIMUM_HEIGHT_METERS = 1.4;
 	public static final double FIRST_STAGE_MAXIMUM_HEIGHT_METERS = 0.7;
 
-	public final static double GEAR_RATIO = 4;
+	public final static double GEAR_RATIO = 63.0 / 17.0;
 
 	public static final double REVERSE_SOFT_LIMIT_VALUE_METERS = 0.01;
 

73988aaad066d42d74327259aeb09e20fa8bd41b
Yehonatan-Levi <yehonatan.levi0904@gmail.com>
2025-02-06 16:31:02 +0200

    Levi: rmoved space

--- src/main/java/frc/Main.java
+++ src/main/java/frc/Main.java
@@ -6,7 +6,6 @@ package frc;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
-
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what you are doing, do not modify this file
  * except to change the parameter class to the startRobot call.

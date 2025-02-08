0ecefb9e9d88dd91c2102c531e652a47fb579b16
GilStein1 <gils.server@gmail.com>
2025-02-08 18:26:57 +0200

    Gil - added a minimum stdDev value

--- src/main/java/frc/robot/poseestimator/WPILibPoseEstimator/WPILibPoseEstimatorConstants.java
+++ src/main/java/frc/robot/poseestimator/WPILibPoseEstimator/WPILibPoseEstimatorConstants.java
@@ -31,7 +31,9 @@ public class WPILibPoseEstimatorConstants {
 
 	public static final double VISION_STANDARD_DEVIATION_FACTOR = 0.001;
 
+	public static final double MINIMUM_STANDARD_DEVIATION = 0.0001;
+
 	public static final Function<AprilTagVisionData, StandardDeviations2D> VISION_STANDARD_DEVIATIONS_TRANSFORM = aprilTagVisionData ->
-		new StandardDeviations2D(Math.pow(aprilTagVisionData.getDistanceFromAprilTagMeters(), 2) * VISION_STANDARD_DEVIATION_FACTOR);
+		new StandardDeviations2D(Math.max(Math.pow(aprilTagVisionData.getDistanceFromAprilTagMeters(), 2) * VISION_STANDARD_DEVIATION_FACTOR, MINIMUM_STANDARD_DEVIATION));
 
 }
--- src/main/java/frc/utils/math/DataMath.java
+++ src/main/java/frc/utils/math/DataMath.java
@@ -4,16 +4,16 @@ import java.util.function.Function;
 
 public class DataMath {
 
-	public static <T extends Iterable<E>, E> double calculateStandardDeviations(T values, Function<E, Double> getValue) {
+	public static <T extends Iterable<E>, E> double calculateStandardDeviations(T iterable, Function<E, Double> getValue) {
 		int length = 0;
 		double total = 0;
-		for (E value : values) {
+		for (E value : iterable) {
 			length++;
 			total += getValue.apply(value);
 		}
 		double mean = total / length;
 		double deviationsTotal = 0;
-		for (E value : values) {
+		for (E value : iterable) {
 			deviationsTotal += Math.pow(getValue.apply(value) - mean, 2);
 		}
 		return Math.sqrt(deviationsTotal / length);

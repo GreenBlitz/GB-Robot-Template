decaf62e27ccc41af19ba3e2149b79824c96facb
itamaroryan <itamar.oryan@gmail.com>
2025-02-09 20:21:45 +0200

    Romy - food

---
+++ src/main/java/frc/robot/subsystems/solenoid/Solenoid.java
@@ -0,0 +1,42 @@
+package frc.robot.subsystems.solenoid;
+
+import frc.robot.hardware.interfaces.IMotor;
+import frc.robot.hardware.interfaces.InputSignal;
+import frc.robot.subsystems.GBSubsystem;
+
+public class Solenoid extends GBSubsystem {
+	
+	private final IMotor motor;
+	private final InputSignal<Double> voltageSignal;
+	private final SolenoidCommandsBuilder commandsBuilder;
+	
+	public Solenoid(SolenoidComponents solenoidComponents) {
+		super(solenoidComponents.logPath());
+		this.motor = solenoidComponents.solenoid();
+		this.voltageSignal = solenoidComponents.voltageSignal();
+		this.commandsBuilder = new SolenoidCommandsBuilder(this);
+		
+		updateInputs();
+	}
+	
+	public SolenoidCommandsBuilder getCommandsBuilder() {
+		return commandsBuilder;
+	}
+	
+	public void updateInputs() {
+		motor.updateInputs(voltageSignal);
+	}
+	
+	@Override
+	protected void subsystemPeriodic() {
+		updateInputs();
+	}
+	
+	protected void stop() {
+		motor.stop();
+	}
+	
+	protected void setPower(double power) {
+		motor.setPower(power);
+	}
+}

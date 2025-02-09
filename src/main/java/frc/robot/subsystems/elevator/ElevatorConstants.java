22c0f81825c5a0443a97f63081783fbdd7a2cdaa
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2025-02-09 19:15:27 +0200

    merged arm

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -7,7 +7,7 @@ public class ElevatorConstants {
 	public static final double DRUM_RADIUS_METERS = DRUM_DIAMETER_METERS / 2;
 	public static final double MASS_KG = 9.5154;
 	public static final double MINIMUM_HEIGHT_METERS = 0;
-	public static final double MAXIMUM_HEIGHT_METERS =1.2;
+	public static final double MAXIMUM_HEIGHT_METERS = 1.2;
 	public static final double FIRST_STAGE_MAXIMUM_HEIGHT_METERS = 0.7;
 
 	public final static double GEAR_RATIO = (63.0 / 17.0);

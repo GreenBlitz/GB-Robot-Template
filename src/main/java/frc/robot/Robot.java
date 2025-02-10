e7e903f18e0059d885b1dcaa35097c491effe9c9
YHerm <0549405422.yh@gmail.com>
2025-02-10 23:27:14 +0200

    un commnet

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import frc.robot.hardware.interfaces.IGyro;
 import frc.robot.hardware.phoenix6.BusChain;
 import frc.robot.poseestimator.WPILibPoseEstimator.WPILibPoseEstimatorConstants;
 import frc.robot.poseestimator.WPILibPoseEstimator.WPILibPoseEstimatorWrapper;
+import frc.robot.statemachine.RobotCommander;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.factory.ArmFactory;
 import frc.robot.subsystems.elevator.Elevator;
@@ -42,7 +43,7 @@ public class Robot {
 	private final EndEffector endEffector;
 
 	private final SimulationManager simulationManager;
-//	private final RobotCommander robotCommander;
+	private final RobotCommander robotCommander;
 
 	public Robot() {
 		BatteryUtil.scheduleLimiter();
@@ -74,13 +75,13 @@ public class Robot {
 		this.endEffector = EndEffectorFactory.create(RobotConstants.SUBSYSTEM_LOGPATH_PREFIX + "/EndEffector");
 
 		this.simulationManager = new SimulationManager("SimulationManager", this);
-//		this.robotCommander = new RobotCommander("StateMachine/RobotCommander", this);
+		this.robotCommander = new RobotCommander("StateMachine/RobotCommander", this);
 	}
 
 	public void periodic() {
 		swerve.update();
 		poseEstimator.updateOdometry(swerve.getAllOdometryObservations());
-//		arm.setReversedSoftLimit(robotCommander.getSuperstructure().getArmReversedSoftLimitByElevator());
+		arm.setReversedSoftLimit(robotCommander.getSuperstructure().getArmReversedSoftLimitByElevator());
 		BatteryUtil.logStatus();
 		BusChain.logChainsStatuses();
 		simulationManager.logPoses();
@@ -111,8 +112,8 @@ public class Robot {
 		return endEffector;
 	}
 
-//	public RobotCommander getRobotCommander() {
-//		return robotCommander;
-//	}
+	public RobotCommander getRobotCommander() {
+		return robotCommander;
+	}
 
 }

e35817481bfe2145510c73ebb7041a0c5f6c49ae
Yehonatan-Levi <yehonatan.levi0904@gmail.com>
2025-02-13 10:40:30 +0200

    Levi: updated to us2 and ls2

--- src/main/java/frc/robot/autonomous/AutonomousConstants.java
+++ src/main/java/frc/robot/autonomous/AutonomousConstants.java
@@ -52,9 +52,9 @@ public class AutonomousConstants {
 
 		public static final Pair<String, Pose2d> AUTO_LINE7 = Pair.of("AL7", new Pose2d(7.58, 0.81, Rotation2d.fromDegrees(137)));
 
-		public static final Pair<String, Pose2d> UPPER_CORAL_STATION = Pair.of("US", new Pose2d(1.63, 7.34, Rotation2d.fromDegrees(-54)));
+		public static final Pair<String, Pose2d> UPPER_CORAL_STATION2 = Pair.of("US2", new Pose2d(1.63, 7.34, Rotation2d.fromDegrees(-54)));
 
-		public static final Pair<String, Pose2d> LOWER_CORAL_STATION = Pair.of("LS", new Pose2d(1.61, 0.7, Rotation2d.fromDegrees(54)));
+		public static final Pair<String, Pose2d> LOWER_CORAL_STATION2 = Pair.of("LS2", new Pose2d(1.61, 0.7, Rotation2d.fromDegrees(54)));
 
 		public static final Pair<String, Pose2d> A = Pair.of("A", new Pose2d(3.16, 4.19, Rotation2d.fromDegrees(0)));
 

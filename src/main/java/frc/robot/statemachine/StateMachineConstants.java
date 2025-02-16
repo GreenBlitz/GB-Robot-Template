0f0a70b38f377cf5dd19e53827f0dd892a049ab8
drorta <125767200+drorta@users.noreply.github.com>
2025-02-16 21:53:55 +0200

    better

--- src/main/java/frc/robot/statemachine/StateMachineConstants.java
+++ src/main/java/frc/robot/statemachine/StateMachineConstants.java
@@ -6,7 +6,7 @@ public class StateMachineConstants {
 
 	public static final double ROBOT_SCORING_DISTANCE_FROM_REEF_METERS = 0.5;
 	public static final double OPEN_SUPERSTRUCTURE_DISTANCE_FROM_REEF_METERS = 1;
-	public static final double CLOSE_SUPERSTRUCTURE_DISTANCE_FROM_BRANCH_METERS = 0.3;
+	public static final double CLOSE_SUPERSTRUCTURE_DISTANCE_FROM_BRANCH_METERS = ROBOT_SCORING_DISTANCE_FROM_REEF_METERS + 0.3;
 
 	public static final Translation2d CLOSE_SUPERSTRUCTURE_LENGTH_AND_WIDTH = new Translation2d(0.3, 0.5);
 

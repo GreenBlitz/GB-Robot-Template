89414ef2cdc9b3dbda53a6c1a31f3d65758e6848
hillelv <hillel.vider@gmail.com>
2025-02-16 15:33:44 +0200

    hillel - helper uses starts with and not equals

--- src/main/java/frc/utils/auto/PathHelper.java
+++ src/main/java/frc/utils/auto/PathHelper.java
@@ -19,7 +19,7 @@ public class PathHelper {
 	public static List<AutoPath> getAllIntakingPaths() {
 		ArrayList<AutoPath> pathsToCoralStations = new ArrayList<>();
 		for (AutoPath autoPath : AutoPath.values()) {
-			if (autoPath.getEndPoint().getFirst().equals("US") || autoPath.getEndPoint().getFirst().equals("LS")) {
+			if (autoPath.getEndPoint().getFirst().startsWith("US") || autoPath.getEndPoint().getFirst().startsWith("LS")) {
 				pathsToCoralStations.add(autoPath);
 			}
 		}
@@ -29,7 +29,7 @@ public class PathHelper {
 	public static List<AutoPath> getAllScoringPathsFromCoralStations() {
 		ArrayList<AutoPath> pathsFromCoralStations = new ArrayList<>();
 		for (AutoPath autoPath : AutoPath.values()) {
-			if (autoPath.getStartingPoint().getFirst().equals("US") || autoPath.getStartingPoint().getFirst().equals("LS")) {
+			if (autoPath.getStartingPoint().getFirst().startsWith("US") || autoPath.getStartingPoint().getFirst().startsWith("LS")) {
 				pathsFromCoralStations.add(autoPath);
 			}
 		}

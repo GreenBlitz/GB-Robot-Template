775c92bdbffe3f08abfcac44f9bcd8a97db6d91b
Yehonatan-Levi <yehonatan.levi0904@gmail.com>
2025-02-16 10:11:28 +0200

    Levi: spotless

--- src/main/java/frc/robot/scoringhelpers/ButtonDriverHelper.java
+++ src/main/java/frc/robot/scoringhelpers/ButtonDriverHelper.java
@@ -12,12 +12,18 @@ import org.littletonrobotics.junction.Logger;
 public class ButtonDriverHelper {
 
 	private static final double METERS_FROM_REEF_FOR_SIDE_HIGHLIGHTING = 0.45;
-	private static final Pose2d L1_DISPLAY_PLACEMENT = Field.getAllianceRelative(new Pose2d(3.22, 6.85, Rotation2d.fromDegrees(0)), true, true, AngleTransform.INVERT);
-	private static final Pose2d L2_DISPLAY_PLACEMENT = Field.getAllianceRelative(new Pose2d(4.55, 6.85, Rotation2d.fromDegrees(0)), true, true, AngleTransform.INVERT);
-	private static final Pose2d L3_DISPLAY_PLACEMENT = Field.getAllianceRelative(new Pose2d(5.88, 6.85, Rotation2d.fromDegrees(0)), true, true, AngleTransform.INVERT);
-	private static final Pose2d L4_DISPLAY_PLACEMENT = Field.getAllianceRelative(new Pose2d(7.22, 6.85, Rotation2d.fromDegrees(0)), true, true, AngleTransform.INVERT);
-	private static final Pose2d LEFT_TOGGLE_PLACEMENT = Field.getAllianceRelative(new Pose2d(7.22, 4.55, Rotation2d.fromDegrees(0)), true, true, AngleTransform.INVERT);
-	private static final Pose2d RIGHT_TOGGLE_PLACEMENT = Field.getAllianceRelative(new Pose2d(7.22, 3.55, Rotation2d.fromDegrees(0)), true, true, AngleTransform.INVERT);
+	private static final Pose2d L1_DISPLAY_PLACEMENT = Field
+		.getAllianceRelative(new Pose2d(3.22, 6.85, Rotation2d.fromDegrees(0)), true, true, AngleTransform.INVERT);
+	private static final Pose2d L2_DISPLAY_PLACEMENT = Field
+		.getAllianceRelative(new Pose2d(4.55, 6.85, Rotation2d.fromDegrees(0)), true, true, AngleTransform.INVERT);
+	private static final Pose2d L3_DISPLAY_PLACEMENT = Field
+		.getAllianceRelative(new Pose2d(5.88, 6.85, Rotation2d.fromDegrees(0)), true, true, AngleTransform.INVERT);
+	private static final Pose2d L4_DISPLAY_PLACEMENT = Field
+		.getAllianceRelative(new Pose2d(7.22, 6.85, Rotation2d.fromDegrees(0)), true, true, AngleTransform.INVERT);
+	private static final Pose2d LEFT_TOGGLE_PLACEMENT = Field
+		.getAllianceRelative(new Pose2d(7.22, 4.55, Rotation2d.fromDegrees(0)), true, true, AngleTransform.INVERT);
+	private static final Pose2d RIGHT_TOGGLE_PLACEMENT = Field
+		.getAllianceRelative(new Pose2d(7.22, 3.55, Rotation2d.fromDegrees(0)), true, true, AngleTransform.INVERT);
 	private static final Pose2d HIDDEN_PLACEMENT = new Pose2d(-10, -10, Rotation2d.fromDegrees(90));
 	private static final int CHOSEN_SIDE = 0;
 	private static final int CHOSEN_SCORE_LEVEL = 0;

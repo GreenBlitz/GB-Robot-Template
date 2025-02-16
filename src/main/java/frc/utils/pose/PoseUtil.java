e9877295ffd8d81cec741bdd28f8293513fafee2
drorta <125767200+drorta@users.noreply.github.com>
2025-02-16 19:36:54 +0200

    broooooo

--- src/main/java/frc/utils/pose/PoseUtil.java
+++ src/main/java/frc/utils/pose/PoseUtil.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.utils.TimedValue;
 import frc.robot.poseestimator.Pose2dComponentsValue;

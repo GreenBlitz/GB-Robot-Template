d4e8c4bf257823d62c1164a83396f9f296120c0b
GilStein1 <gils.server@gmail.com>
2025-02-17 14:13:10 +0200

    Gil - spotlessapply

--- src/main/java/frc/robot/vision/multivisionsources/MultiAprilTagVisionSources.java
+++ src/main/java/frc/robot/vision/multivisionsources/MultiAprilTagVisionSources.java
@@ -1,6 +1,5 @@
 package frc.robot.vision.multivisionsources;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;

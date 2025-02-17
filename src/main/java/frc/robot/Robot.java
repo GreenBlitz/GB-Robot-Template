7055c321a101f23f2f2358373f86a41fc6436c5f
hillelv <hillel.vider@gmail.com>
2025-02-17 19:44:27 +0200

    hillel - edited paths in gui

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -161,7 +161,7 @@ public class Robot {
 			new InstantCommand(() -> ScoringHelpers.targetScoreLevel = ScoreLevel.L4).andThen(robotCommander.getSuperstructure().preScore())
 		);
 		new EventTrigger("INTAKE").onTrue(robotCommander.getSuperstructure().intake());
-		new EventTrigger("ARM_PRE_SCORE").onTrue(robotCommander.getSuperstructure().armPreScore());
+		new EventTrigger("ARM_PRE_SCORE").onTrue(new InstantCommand(() -> ScoringHelpers.targetScoreLevel = ScoreLevel.L4).andThen(robotCommander.getSuperstructure().armPreScore()));
 
 		this.startingPointAndWhereToScoreFirstObjectChooser = new AutonomousChooser(
 			"StartingPointAndScoreFirst",

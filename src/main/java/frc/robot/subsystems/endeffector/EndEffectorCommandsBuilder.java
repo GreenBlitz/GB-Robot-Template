f5ac87fbf57037dd8eca3821203d9db35d5eb4af
itamaroryan <itamar.oryan@gmail.com>
2025-02-18 12:40:58 +0200

    format

--- src/main/java/frc/robot/subsystems/endeffector/EndEffectorCommandsBuilder.java
+++ src/main/java/frc/robot/subsystems/endeffector/EndEffectorCommandsBuilder.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.endeffector;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 
 import java.util.function.DoubleSupplier;

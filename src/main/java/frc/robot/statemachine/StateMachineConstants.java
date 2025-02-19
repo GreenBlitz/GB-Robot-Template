8e428b165029c09605bca7541211a7b9c33f2b0b
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2025-02-19 17:27:42 +0200

    timoutes, invert

--- src/main/java/frc/robot/statemachine/StateMachineConstants.java
+++ src/main/java/frc/robot/statemachine/StateMachineConstants.java
@@ -6,13 +6,13 @@ import edu.wpi.first.math.geometry.Translation2d;
 public class StateMachineConstants {
 
 	public static final double ROBOT_SCORING_DISTANCE_FROM_REEF_METERS = 0.56;
-	public static final double OPEN_SUPERSTRUCTURE_DISTANCE_FROM_REEF_METERS = 0.8;
-	public static final double DISTANCE_TO_BRANCH_FOR_STARTING_PATH = 0.7;
+	public static final double OPEN_SUPERSTRUCTURE_DISTANCE_FROM_REEF_METERS = 0.9;
+	public static final double DISTANCE_TO_BRANCH_FOR_STARTING_PATH = 0.85;
 
 	public static final Translation2d CLOSE_SUPERSTRUCTURE_LENGTH_AND_WIDTH = new Translation2d(0.79, 1.03);
 
-	public static final double SCORE_OUTTAKE_TIME_AFTER_BEAM_BREAK_SECONDS = 0.5;
-	public static final double INTAKE_TIME_AFTER_BEAM_BREAK_SECONDS = 0.5;
+	public static final double SCORE_OUTTAKE_TIME_AFTER_BEAM_BREAK_SECONDS = 0;
+	public static final double INTAKE_TIME_AFTER_BEAM_BREAK_SECONDS = 0;
 
 	public static final Rotation2d ARM_POSITION_TO_CLOSE_ELEVATOR_L4 = Rotation2d.fromDegrees(37);
 

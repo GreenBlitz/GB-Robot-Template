2c56766229000747cd53342ce124f53b55546458
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2025-02-24 23:44:28 +0200

    fixes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.config.ModuleConfig;
 import com.pathplanner.lib.config.RobotConfig;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -177,7 +178,7 @@ public class Robot {
 	}
 
 	public Command getAutonomousCommand() {
-		return new InstantCommand();
+		return new PathPlannerAuto("Rotate 2m");
 	}
 
 	public IPoseEstimator getPoseEstimator() {

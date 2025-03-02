b88fd8a0811d18c917e57648cb4fa54a4f7fbf1b
greenblitz <greenblitz@gmail.com>
2025-03-02 14:16:52 +0200

    Yoni - name change

--- src/main/java/frc/utils/utilcommands/LogCommand.java
+++ src/main/java/frc/utils/utilcommands/LogCommand.java
@@ -38,36 +38,36 @@ public class LogCommand extends InstantCommand {
 		super(() -> Logger.recordOutput(key, value));
 	}
 
-	interface IntegerSupplier extends Supplier<Integer> {
-	}
 
-	interface StringSupplier extends Supplier<String> {
-	}
-
-	interface TSupplier<T> extends Supplier<T> {
-	}
-
-	public LogCommand(String key, StringSupplier value) {
-		super(() -> Logger.recordOutput(key, value.get()));
+	public LogCommand(String key, DoubleSupplier value) {
+		super(() -> Logger.recordOutput(key, value.getAsDouble()));
 	}
 
 	public LogCommand(String key, BooleanSupplier value) {
 		super(() -> Logger.recordOutput(key, value.getAsBoolean()));
 	}
 
+	private interface StringSupplier extends Supplier<String> {
+	}
+	public LogCommand(String key, StringSupplier value) {
+		super(() -> Logger.recordOutput(key, value.get()));
+	}
+
+	private interface IntegerSupplier extends Supplier<Integer> {
+	}
 	public LogCommand(String key, IntegerSupplier value) {
 		super(() -> Logger.recordOutput(key, value.get()));
 	}
 
-	public LogCommand(String key, DoubleSupplier value) {
-		super(() -> Logger.recordOutput(key, value.getAsDouble()));
+	private interface StructSupplier<T extends StructSerializable> extends Supplier<T> {
 	}
-
-	public <T extends StructSerializable> LogCommand(String key, TSupplier<T> value) {
+	public <T extends StructSerializable> LogCommand(String key, StructSupplier<T> value) {
 		super(() -> Logger.recordOutput(key, value.get()));
 	}
 
-	public <T extends WPISerializable> LogCommand(String key, Supplier<T> value) {
+	private interface WPISerializableSupplier<T extends WPISerializable> extends Supplier<T> {
+	}
+	public <T extends WPISerializable> LogCommand(String key, WPISerializableSupplier<T> value) {
 		super(() -> Logger.recordOutput(key, value.get()));
 	}
 

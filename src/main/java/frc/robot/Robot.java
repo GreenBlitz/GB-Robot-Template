d3bfb63c47dee6fd4a466959cabb2749d8a54656
hillelv <hillel.vider@gmail.com>
2025-03-02 20:57:23 +0200

    hillel - format

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -245,17 +245,17 @@ public class Robot {
 
 	private PathPlannerAutoWrapper getMultiChoosersAuto() {
 		return PathPlannerAutoWrapper.chainAutos(
-				firstObjectScoringLocationChooser.getChosenValue(),
-				PathPlannerAutoWrapper
-						.chainAutos(
-								secondObjectIntakingLocationChooser.getChosenValue(),
-								secondObjectScoringLocationChooser.getChosenValue(),
-								thirdObjectIntakingLocationChooser.getChosenValue(),
-								thirdObjectScoringLocationChooser.getChosenValue(),
-								fourthObjectIntakingLocationChooser.getChosenValue(),
-								fourthObjectScoringLocationChooser.getChosenValue()
-						)
-						.asProxyAuto()
+			firstObjectScoringLocationChooser.getChosenValue(),
+			PathPlannerAutoWrapper
+				.chainAutos(
+					secondObjectIntakingLocationChooser.getChosenValue(),
+					secondObjectScoringLocationChooser.getChosenValue(),
+					thirdObjectIntakingLocationChooser.getChosenValue(),
+					thirdObjectScoringLocationChooser.getChosenValue(),
+					fourthObjectIntakingLocationChooser.getChosenValue(),
+					fourthObjectScoringLocationChooser.getChosenValue()
+				)
+				.asProxyAuto()
 		);
 	}
 

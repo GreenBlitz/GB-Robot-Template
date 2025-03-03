7b3eb348d1b2947d7240e0cd5c887efcf8f24209
Yehonatan-Levi <yehonatan.levi0904@gmail.com>
2025-03-03 15:27:08 +0200

    Levi: fixed hillel's comments

--- src/main/java/frc/robot/autonomous/AutosBuilder.java
+++ src/main/java/frc/robot/autonomous/AutosBuilder.java
@@ -2,10 +2,11 @@ package frc.robot.autonomous;
 
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj2.command.*;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.constants.field.enums.Branch;
 import frc.robot.Robot;
 import frc.robot.subsystems.swerve.ChassisPowers;

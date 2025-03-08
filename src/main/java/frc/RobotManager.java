e49d925fa98805b16ce91525e3fffa4390dd860d
itamaroryan <itamar.oryan@gmail.com>
2025-03-08 12:33:10 +0200

    cleanded

--- src/main/java/frc/RobotManager.java
+++ src/main/java/frc/RobotManager.java
@@ -6,7 +6,9 @@ package frc;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Robot;
+import frc.robot.led.LEDConstants;
 import frc.robot.led.LEDState;
 import frc.utils.auto.PathPlannerAutoWrapper;
 import frc.utils.auto.PathPlannerUtil;
@@ -38,8 +40,16 @@ public class RobotManager extends LoggedRobot {
 
 		createAutoReadyForConstructionChooser();
 		JoysticksBindings.configureBindings(robot);
+		
+		initializeLEDTriggers();
 	}
-
+	
+	private void initializeLEDTriggers(){
+		Trigger noteIn = new Trigger(() -> robot.getRobotCommander().getSuperstructure().isCoralIn());
+		noteIn.onTrue(robot.getRobotCommander().getLedStateHandler().setState(LEDState.HAS_CORAL).withTimeout(LEDConstants.CORAL_IN_BLINK_TIME_SECONDS)
+				.onlyIf(robot.getRobotCommander().getSuperstructure()::isCoralIn));
+	}
+	
 	@Override
 	public void disabledInit() {
 		if (!DriverStationUtil.isMatch()) {

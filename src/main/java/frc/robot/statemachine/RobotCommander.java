3f5d15597210c04cd51e40cbcf460e0c63760a0f
greenblitz <greenblitz@gmail.com>
2025-03-08 17:23:38 +0200

    reverted autoscore for auto

--- src/main/java/frc/robot/statemachine/RobotCommander.java
+++ src/main/java/frc/robot/statemachine/RobotCommander.java
@@ -293,7 +293,6 @@ public class RobotCommander extends GBSubsystem {
 	public Command autoScoreForAutonomous() {
 		Supplier<Command> fullySuperstructureScore = () -> new SequentialCommandGroup(
 			superstructure.elevatorOpening(),
-			superstructure.closeClimb(),
 			superstructure.armPreScore().until(this::isReadyToOpenSuperstructure),
 			superstructure.preScore().until(superstructure::isPreScoreReady),
 			superstructure.scoreWithoutRelease().until(this::isReadyToScore),

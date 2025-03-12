ab37fea5e95e3eb1d3370799abc5b2a209999219
GilStein1 <gils.server@gmail.com>
2025-03-12 17:23:16 +0200

    Gil - calibrated the camera

--- src/main/java/frc/robot/vision/VisionConstants.java
+++ src/main/java/frc/robot/vision/VisionConstants.java
@@ -68,13 +68,13 @@ public class VisionConstants {
 		.and(VisionFilters.isOnGround(ROBOT_DISTANCE_TO_GROUND_TOLERANCE_METERS));
 
 	public static final Pose3d LIMELIGHT_LEFT_CAMERA_ROBOT_POSE = new Pose3d(
-		new Translation3d(0.19989, -0.11998, 0.50927),
-		AngleUnit.DEGREES.toRotation3d(-8.9, -24.37, -22.92)
+		new Translation3d(0.21, -0.115, 0.466),
+		AngleUnit.DEGREES.toRotation3d(-8.65, -24.75, -21.62)
 	);
 
 	public static final Pose3d LIMELIGHT_RIGHT_CAMERA_ROBOT_POSE = new Pose3d(
-		new Translation3d(0.185, 0.13, 0.505),
-		AngleUnit.DEGREES.toRotation3d(8.81, -25.55, 19.96)
+		new Translation3d(0.175, 0.125, 0.45),
+		AngleUnit.DEGREES.toRotation3d(9.9, -23.15, 25.6)
 	);
 
 	public static final Pose3d LIMELIGHT_FEEDER_CAMERA_ROBOT_POSE = new Pose3d(

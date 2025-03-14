d30bcf2f8c852ec2c70fe6059964cf44c8bd9006
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2025-03-14 20:19:45 +0200

    code

--- src/main/java/frc/robot/subsystems/swerve/factories/modules/drive/KrakenX60DriveBuilder.java
+++ src/main/java/frc/robot/subsystems/swerve/factories/modules/drive/KrakenX60DriveBuilder.java
@@ -33,7 +33,7 @@ public class KrakenX60DriveBuilder {
 
 	public static final double SLIP_CURRENT = 60;
 	public static final double GEAR_RATIO = 7.13;
-	public static final boolean IS_CURRENT_CONTROL = false;
+	public static final boolean IS_CURRENT_CONTROL = true;
 	private static final double MOMENT_OF_INERTIA_METERS_SQUARED = 0.001;
 
 	private static SysIdRoutine.Config buildSysidConfig(String logPath) {

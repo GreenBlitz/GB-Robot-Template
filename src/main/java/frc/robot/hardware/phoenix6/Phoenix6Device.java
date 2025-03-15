c6e7f21b9bae2ce7f80eb37b9ea0413830e8308a
YHerm <0549405422.yh@gmail.com>
2025-03-15 09:47:05 +0200

    is connected imrpove

--- src/main/java/frc/robot/hardware/phoenix6/Phoenix6Device.java
+++ src/main/java/frc/robot/hardware/phoenix6/Phoenix6Device.java
@@ -33,7 +33,7 @@ public abstract class Phoenix6Device implements IDevice {
 	}
 
 	public boolean isConnected() {
-		return getDevice().isConnected();
+		return connectedInput.connected;
 	}
 
 
@@ -87,9 +87,9 @@ public abstract class Phoenix6Device implements IDevice {
 		}
 		InputSignal<?>[] validSignals = getValidSignals(inputSignals);
 //		refreshSignals(validSignals);
-//		connectedInput.connected = isConnected();
-//		Logger.processInputs(logPath, connectedInput);
 		logSignals(validSignals);
+		connectedInput.connected = getDevice().isConnected();
+		Logger.processInputs(logPath, connectedInput);
 	}
 
 	public abstract ParentDevice getDevice();

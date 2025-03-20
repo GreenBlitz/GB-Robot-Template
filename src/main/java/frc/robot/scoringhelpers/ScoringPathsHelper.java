0a5499d563a8a6f26de42f580a11118129bf7f51
YHerm <0549405422.yh@gmail.com>
2025-03-21 00:30:39 +0200

    clean

--- src/main/java/frc/robot/scoringhelpers/ScoringPathsHelper.java
+++ src/main/java/frc/robot/scoringhelpers/ScoringPathsHelper.java
@@ -42,7 +42,7 @@ public class ScoringPathsHelper {
 				StateMachineConstants.MAX_ACCELERATION_WHILE_ELEVATOR_L4_ROTATION2D_PER_SECOND_SQUARED.getRadians()
 			),
 			new IdealStartingState(
-				1,
+				StateMachineConstants.MAX_VELOCITY_WHILE_ELEVATOR_L4_METERS_PER_SECOND,
 				Field.getReefSideMiddle(branch.getReefSide(), false).getRotation()
 			),
 			new GoalEndState(0, Field.getReefSideMiddle(branch.getReefSide(), false).getRotation())

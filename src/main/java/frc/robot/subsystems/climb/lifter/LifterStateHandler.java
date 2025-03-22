a3e07ddcbf842db91bca02b4e2d599592a57bcf5
YHerm <0549405422.yh@gmail.com>
2025-03-22 02:05:25 +0200

    yoav: rm space

--- src/main/java/frc/robot/subsystems/climb/lifter/LifterStateHandler.java
+++ src/main/java/frc/robot/subsystems/climb/lifter/LifterStateHandler.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.climb.lifter;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 
-
 public class LifterStateHandler {
 
 	private final Lifter lifter;

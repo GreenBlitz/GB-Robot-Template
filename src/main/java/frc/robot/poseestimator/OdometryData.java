d88ce426dbb6a31fdfc0a09b29d6871cd4a7f86f
Shahar Perets <perets.shahar@gmail.com>
2025-04-09 21:58:43 +0300

    odometry data optimization

--- src/main/java/frc/robot/poseestimator/OdometryData.java
+++ src/main/java/frc/robot/poseestimator/OdometryData.java
@@ -5,4 +5,42 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 
 import java.util.Optional;
 
-public record OdometryData(SwerveModulePosition[] wheelPositions, Optional<Rotation2d> gyroAngle, double timestamp) {}
+public class OdometryData {
+
+	private SwerveModulePosition[] wheelPositions = new SwerveModulePosition[4];
+	private Optional<Rotation2d> gyroAngle = Optional.empty();
+	private double timestamp = 0;
+
+	public OdometryData() {}
+
+	public OdometryData(SwerveModulePosition[] swerveModulePositions, Optional<Rotation2d> gyroAngle, double timestamp) {
+		this.wheelPositions = swerveModulePositions;
+		this.gyroAngle = gyroAngle;
+		this.timestamp = timestamp;
+	}
+
+	public SwerveModulePosition[] getWheelPositions() {
+		return wheelPositions;
+	}
+
+	public double getTimestamp() {
+		return timestamp;
+	}
+
+	public Optional<Rotation2d> getGyroAngle() {
+		return gyroAngle;
+	}
+
+	public void setWheelPositions(SwerveModulePosition[] wheelPositions) {
+		this.wheelPositions = wheelPositions;
+	}
+
+	public void setGyroAngle(Optional<Rotation2d> gyroAngle) {
+		this.gyroAngle = gyroAngle;
+	}
+
+	public void setTimestamp(double timestamp) {
+		this.timestamp = timestamp;
+	}
+
+}

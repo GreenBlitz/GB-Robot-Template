c59d28af107532f2a19ed9c2e14384775b53185d
YHerm <0549405422.yh@gmail.com>
2025-04-12 17:22:38 +0300

    added classes in ids

--- src/main/java/frc/robot/IDs.java
+++ src/main/java/frc/robot/IDs.java
@@ -7,4 +7,12 @@ public class IDs {
 
 	public static final PowerDistributionDeviceID POWER_DISTRIBUTION = new PowerDistributionDeviceID(1, PowerDistribution.ModuleType.kRev);
 
+	public static class TalonFXIDs {}
+
+	public static class CANCoderIDs {}
+
+	public static class SparkMAXIDs {}
+
+	public static class DigitalInputsIDs {}
+
 }

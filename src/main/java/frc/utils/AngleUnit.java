1c17cba114cdf0ce67ed75ea618d71dcdf3d6b77
Shahar Perets <perets.shahar@gmail.com>
2025-04-25 20:38:56 +0300

    last CR fix

--- src/main/java/frc/utils/AngleUnit.java
+++ src/main/java/frc/utils/AngleUnit.java
@@ -6,30 +6,20 @@ import java.util.function.DoubleFunction;
 
 public enum AngleUnit {
 
-	ROTATIONS(
-		Rotation2d::fromRotations,
-		(roll, pitch, yaw) -> new Rotation3d(
-			Rotation2d.fromRotations(roll).getRadians(),
-			Rotation2d.fromRotations(pitch).getRadians(),
-			Rotation2d.fromRotations(yaw).getRadians()
-		)
-	),
-	RADIANS(Rotation2d::fromRadians, Rotation3d::new),
-	DEGREES(
-		Rotation2d::fromDegrees,
-		(roll, pitch, yaw) -> new Rotation3d(
-			Rotation2d.fromDegrees(roll).getRadians(),
-			Rotation2d.fromDegrees(pitch).getRadians(),
-			Rotation2d.fromDegrees(yaw).getRadians()
-		)
-	);
+	ROTATIONS(Rotation2d::fromRotations),
+	RADIANS(Rotation2d::fromRadians),
+	DEGREES(Rotation2d::fromDegrees);
 
 	private final DoubleFunction<Rotation2d> toRotation2d;
 	private final TriFunction<Double, Double, Double, Rotation3d> toRotation3d;
 
-	AngleUnit(DoubleFunction<Rotation2d> toRotation2d, TriFunction<Double, Double, Double, Rotation3d> toRotation3d) {
+	AngleUnit(DoubleFunction<Rotation2d> toRotation2d) {
 		this.toRotation2d = toRotation2d;
-		this.toRotation3d = toRotation3d;
+		this.toRotation3d = (
+			roll,
+			pitch,
+			yaw
+		) -> new Rotation3d(toRotation2d.apply(roll).getRadians(), toRotation2d.apply(pitch).getRadians(), toRotation2d.apply(yaw).getRadians());
 	}
 
 	public Rotation2d toRotation2d(double value) {

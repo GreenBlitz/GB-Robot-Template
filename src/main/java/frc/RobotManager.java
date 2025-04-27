1604e65147f4d0c49fa14191f9e277d9c311d3d8
YHerm <140002259+YHerm@users.noreply.github.com>
2025-04-27 16:25:02 +0300

    setted robot main as a real time thread with priority of 10 (#675)
    Co-authored-by: greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>

--- src/main/java/frc/RobotManager.java
+++ src/main/java/frc/RobotManager.java
@@ -4,6 +4,7 @@
 
 package frc;
 
+import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 import frc.utils.auto.PathPlannerUtil;
@@ -27,6 +28,8 @@ public class RobotManager extends LoggedRobot {
 	private int roborioCycles;
 
 	public RobotManager() {
+		Threads.setCurrentThreadPriority(true, 10);
+
 		LoggerFactory.initializeLogger();
 		PathPlannerUtil.startPathfinder();
 

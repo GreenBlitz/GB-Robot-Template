4dbc43afe202d5109ca0051e7988b091a696f3d8
itamaroryan <itamar.oryan@gmail.com>
2025-05-05 18:38:28 +0300

    fixes stuff

--- src/main/java/frc/robot/Detection.java
+++ src/main/java/frc/robot/Detection.java
@@ -10,21 +10,25 @@ import org.littletonrobotics.junction.Logger;
 public class Detection {
 	
 	public static NetworkTableEntry isValid = NetworkTableInstance.getDefault().getTable("limelight-left").getEntry("tv");
-	public static NetworkTableEntry xAngle = NetworkTableInstance.getDefault().getTable("limelight-left").getEntry("tx");
-	public static NetworkTableEntry yAngle = NetworkTableInstance.getDefault().getTable("limelight-left").getEntry("ty");
+	public static NetworkTableEntry tx = NetworkTableInstance.getDefault().getTable("limelight-left").getEntry("tx");
+	public static NetworkTableEntry ty = NetworkTableInstance.getDefault().getTable("limelight-left").getEntry("ty");
 	public static NetworkTableEntry name = NetworkTableInstance.getDefault().getTable("limelight-left").getEntry("tdclass");
 	
 	public static double HEIGHT = 0.508;
 	public static Rotation3d rotation3d = AngleUnit.DEGREES.toRotation3d(-8.06180374425555, -27.07784559039065, -22.52372569716833);
 	
 	public void log() {
+		Rotation2d yaw = Rotation2d.fromDegrees(tx.getDouble(0));
+		Rotation2d pitch = Rotation2d.fromDegrees(ty.getDouble(0));
+		
 		Logger.recordOutput("isvalid", isValid.getDouble(0));
-		Logger.recordOutput("xAngle", xAngle.getDouble(0));
-		Logger.recordOutput("yAngle", yAngle.getDouble(0));
-		Logger.recordOutput("name", name.getString("yes"));
-//		Logger.recordOutput("distancex", (HEIGHT) / Math.tan(Rotation2d.fromDegrees(xAngle.getDouble(0) + -8.06180374425555).getRadians()));
-		Logger.recordOutput("distancey", (HEIGHT) / Math.tan(Rotation2d.fromDegrees(yAngle.getDouble(0) + -27.07784559039065).getRadians()));
+		Logger.recordOutput("yaw", yaw);
+		Logger.recordOutput("pitch", pitch);
+		Logger.recordOutput("name", name.getString(""));
+		double distanceY = (HEIGHT) / Math.tan(pitch.minus(Rotation2d.fromDegrees(-27.07784559039065)).getRadians());
+		Logger.recordOutput("distancey", distanceY);
+		Logger.recordOutput("distancey", distanceY * Math.tan(yaw.minus(Rotation2d.fromDegrees(-22.52372569716833)).getRadians()));
 	}
-
-
+	
+	
 }

f92b4416880f604479f08fc20e4e6ac002866c71
Nitay4 <nitay@rotbart.co.il>
2025-05-25 00:11:07 +0300

    nitay - cr

--- src/main/java/frc/robot/subsystems/swerve/module/Module.java
+++ src/main/java/frc/robot/subsystems/swerve/module/Module.java
@@ -4,7 +4,6 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import frc.robot.hardware.interfaces.ControllableMotor;
-import frc.robot.hardware.interfaces.IAngleEncoder;
 import frc.robot.subsystems.GBSubsystem;
 import frc.utils.Conversions;
 import frc.utils.battery.BatteryUtil;

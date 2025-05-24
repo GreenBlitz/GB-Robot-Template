02452221f72e6ea1c0708cbfdcbdf6a0badd8273
Nitay4 <nitay@rotbart.co.il>
2025-05-24 12:25:14 +0300

    nitay - deleted

--- src/main/java/frc/robot/subsystems/swerve/module/records/EncoderSignals.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.subsystems.swerve.module.records;
-
-import edu.wpi.first.math.geometry.Rotation2d;
-import frc.robot.hardware.interfaces.InputSignal;
-
-public record EncoderSignals(InputSignal<Rotation2d> position) {}

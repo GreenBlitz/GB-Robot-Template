a156f19e399bbba3ae485f3b3c80879f0c6498eb
hillelv <hillel.vider@gmail.com>
2025-05-28 17:14:57 +0300

    shit math w shit names

--- src/main/java/frc/utils/math/ObjectDetectionMath.java
+++ src/main/java/frc/utils/math/ObjectDetectionMath.java
@@ -6,16 +6,27 @@ import edu.wpi.first.math.geometry.Translation2d;
 
 public class ObjectDetectionMath {
 
-    public static Translation2d pixelsToPitchAndYaw(Translation2d pose) {
-        double yaw = pose.getX();
-        double pitch = pose.getY();
-        return new Translation2d(yaw, pitch);
+	public static Translation2d pixelsToPitchAndYaw(Translation2d pose) {
+		double yaw = pose.getX();
+		double pitch = pose.getY();
+		return new Translation2d(yaw, pitch);
+	}
+
+	public static Translation2d undoRoll(Translation2d pitchAndYaw, Pose3d cameraPose) {
+		double roll = cameraPose.getRotation().getX();
+		pitchAndYaw.rotateAround(new Translation2d(), Rotation2d.fromRadians(-roll));
+		return pitchAndYaw;
+	}
+
+	public static double sideTrigCalculation(Rotation2d pitchFromCamMiddle, Pose3d cameraPose) {
+		Rotation2d pitch = Rotation2d.fromRadians(Math.abs(cameraPose.getRotation().getY() + pitchFromCamMiddle.getRadians()));
+	    double cameraHeightMeters = cameraPose.getZ();
+        return (Math.tan(pitch.getRadians()) * cameraHeightMeters);
     }
 
-    public static Translation2d undoRoll(Translation2d pitchAndYaw, Pose3d cameraPose) {
-        double roll = cameraPose.getRotation().getX();
-        pitchAndYaw.rotateAround(new Translation2d(), Rotation2d.fromRadians(roll));
-        return pitchAndYaw;
+    public static double airviewTrigCalculation(Rotation2d yawFromCamMiddle, Pose3d cameraPose, double distanceMeters) {
+        Rotation2d yaw = Rotation2d.fromRadians(Math.abs(cameraPose.getRotation().getZ() + yawFromCamMiddle.getRadians()));
+        return (Math.tan(yaw.getRadians()) * distanceMeters);
     }
 
 }

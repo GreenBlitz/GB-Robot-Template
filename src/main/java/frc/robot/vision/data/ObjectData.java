c4ee85a4740bb188ea76e2a14d41d8f72a1c284e
hillelv <hillel.vider@gmail.com>
2025-05-28 17:25:55 +0300

    danna - object data

---
+++ src/main/java/frc/robot/vision/data/ObjectData.java
@@ -0,0 +1,18 @@
+package frc.robot.vision.data;
+
+import edu.wpi.first.math.geometry.Pose3d;
+
+public class ObjectData extends VisionData{
+
+    private final String objectType;
+
+    public ObjectData(String sourceName, Pose3d estimatedPose, String objectType, double timestamp) {
+        super(sourceName, estimatedPose, timestamp);
+        this.objectType = objectType;
+    }
+
+    public final String getObjectType() {
+        return objectType;
+    }
+
+}

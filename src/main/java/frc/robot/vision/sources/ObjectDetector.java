2d9b7dcc05e3979415666ba5190e22c80c026609
hillelv <hillel.vider@gmail.com>
2025-05-28 19:21:25 +0300

    danna - object detector

---
+++ src/main/java/frc/robot/vision/sources/ObjectDetector.java
@@ -0,0 +1,45 @@
+package frc.robot.vision.sources;
+
+import frc.robot.vision.data.ObjectData;
+import frc.utils.Filter;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.function.Function;
+
+public interface ObjectDetector{
+
+    String getName();
+
+    void update();
+
+    ArrayList<Optional<ObjectData>> getAllObjectData();
+
+    private Optional<ObjectData> getFilteredObjectData(Optional<ObjectData> objectData) {
+        if (objectData.isPresent() && getFilter().apply(objectData.get())) {
+            return objectData;
+        }
+        return Optional.empty();
+    }
+
+    default ArrayList<Optional<ObjectData>> getAllFilteredObjectData() {
+        ArrayList<Optional<ObjectData>> allObjectData = getAllObjectData();
+        allObjectData.replaceAll(this::getFilteredObjectData);
+        return allObjectData;
+    }
+
+    Optional<ObjectData> getClosestFilteredObjectData();
+
+    void setFilter(Filter<? super ObjectData> newFilter);
+
+    Filter<? super ObjectData> getFilter();
+
+    default void clearFilter() {
+        setFilter(Filter.nonFilteringFilter());
+    }
+
+    default void applyFunctionOnFilter(Function<Filter<? super ObjectData>, Filter<? super ObjectData>> filterChangingFunction) {
+        setFilter(filterChangingFunction.apply(getFilter()));
+    }
+
+}

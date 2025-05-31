8f52de7ed3c7148aa28890ca388b75e41b78a661
Nitay4 <nitay@rotbart.co.il>
2025-05-31 16:04:10 +0300

    nitay - format

--- src/main/java/frc/robot/subsystems/algaeIntake/AlgaeIntake.java
+++ src/main/java/frc/robot/subsystems/algaeIntake/AlgaeIntake.java
@@ -4,7 +4,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.hardware.interfaces.ControllableMotor;
 import frc.robot.hardware.interfaces.IRequest;
 import frc.robot.hardware.interfaces.InputSignal;
-import frc.robot.hardware.signal.AngleSignal;
 import frc.robot.subsystems.GBSubsystem;
 
 public class AlgaeIntake extends GBSubsystem {
@@ -17,11 +16,11 @@ public class AlgaeIntake extends GBSubsystem {
 	private final IRequest<Double> rollersVoltageRequest;
 
 	private final InputSignal<Rotation2d> pivotPositionSignal;
-    private final InputSignal<Double> pivotVoltageSignal;
-    private final InputSignal<Rotation2d> rollersVelocitySignal;
-    private final InputSignal<Double> rollersVoltageSignal;
+	private final InputSignal<Double> pivotVoltageSignal;
+	private final InputSignal<Rotation2d> rollersVelocitySignal;
+	private final InputSignal<Double> rollersVoltageSignal;
 
-    private final AlgaeIntakeCommandsBuilder commandsBuilder;
+	private final AlgaeIntakeCommandsBuilder commandsBuilder;
 
 	public AlgaeIntake(
 		String logPath,
@@ -30,10 +29,10 @@ public class AlgaeIntake extends GBSubsystem {
 		IRequest<Rotation2d> pivotPositionRequest,
 		IRequest<Rotation2d> rollersVelocityRequest,
 		IRequest<Double> rollersVoltageRequest,
-        InputSignal<Rotation2d> pivotPositionSignal,
-        InputSignal<Double> pivotVoltageSignal,
-        InputSignal<Rotation2d> rollersVelocitySignal,
-        InputSignal<Double> rollersVoltageSignal
+		InputSignal<Rotation2d> pivotPositionSignal,
+		InputSignal<Double> pivotVoltageSignal,
+		InputSignal<Rotation2d> rollersVelocitySignal,
+		InputSignal<Double> rollersVoltageSignal
 	) {
 		super(logPath);
 
@@ -44,63 +43,64 @@ public class AlgaeIntake extends GBSubsystem {
 		this.rollersVelocityRequest = rollersVelocityRequest;
 		this.rollersVoltageRequest = rollersVoltageRequest;
 
-        this.pivotPositionSignal = pivotPositionSignal;
-        this.pivotVoltageSignal = pivotVoltageSignal;
-        this.rollersVelocitySignal = rollersVelocitySignal;
-        this.rollersVoltageSignal = rollersVoltageSignal;
+		this.pivotPositionSignal = pivotPositionSignal;
+		this.pivotVoltageSignal = pivotVoltageSignal;
+		this.rollersVelocitySignal = rollersVelocitySignal;
+		this.rollersVoltageSignal = rollersVoltageSignal;
 
-        periodic();
+		periodic();
 
-        this.commandsBuilder = new AlgaeIntakeCommandsBuilder(this);
+		this.commandsBuilder = new AlgaeIntakeCommandsBuilder(this);
 	}
 
-    public AlgaeIntakeCommandsBuilder getCommandsBuilder() {
-        return commandsBuilder;
-    }
+	public AlgaeIntakeCommandsBuilder getCommandsBuilder() {
+		return commandsBuilder;
+	}
 
-    public Rotation2d getPivotPosition(){
-        return pivotPositionSignal.getLatestValue();
-    }
+	public Rotation2d getPivotPosition() {
+		return pivotPositionSignal.getLatestValue();
+	}
 
-    public double getPivotVoltage(){
-        return pivotVoltageSignal.getLatestValue();
-    }
+	public double getPivotVoltage() {
+		return pivotVoltageSignal.getLatestValue();
+	}
 
-    public Rotation2d getRollersVelocityRotation2dPerSecond(){
-        return rollersVelocitySignal.getLatestValue();
-    }
+	public Rotation2d getRollersVelocityRotation2dPerSecond() {
+		return rollersVelocitySignal.getLatestValue();
+	}
 
-    public double getRollersVoltage(){
-        return rollersVoltageSignal.getLatestValue();
-    }
+	public double getRollersVoltage() {
+		return rollersVoltageSignal.getLatestValue();
+	}
 
-    @Override
-    protected void subsystemPeriodic() {
-        updateInputs();
-    }
+	@Override
+	protected void subsystemPeriodic() {
+		updateInputs();
+	}
 
-    private void updateInputs(){
-        pivot.updateInputs(pivotPositionSignal, pivotVoltageSignal);
-        rollers.updateInputs(rollersVelocitySignal, rollersVoltageSignal);
+	private void updateInputs() {
+		pivot.updateInputs(pivotPositionSignal, pivotVoltageSignal);
+		rollers.updateInputs(rollersVelocitySignal, rollersVoltageSignal);
 
-        pivot.updateSimulation();
-        rollers.updateSimulation();
-    }
+		pivot.updateSimulation();
+		rollers.updateSimulation();
+	}
 
-    public void setBrake(boolean brake){
-        pivot.setBrake(brake);
-        rollers.setBrake(brake);
-    }
+	public void setBrake(boolean brake) {
+		pivot.setBrake(brake);
+		rollers.setBrake(brake);
+	}
 
-    protected void setTargetPivotPosition(Rotation2d targetPosition) {
-        pivot.applyRequest(pivotPositionRequest.withSetPoint(targetPosition));
-    }
+	protected void setTargetPivotPosition(Rotation2d targetPosition) {
+		pivot.applyRequest(pivotPositionRequest.withSetPoint(targetPosition));
+	}
 
-    protected void setTargetRollersVelocity(Rotation2d targetVelocityMPS){
-        rollers.applyRequest(rollersVelocityRequest.withSetPoint(targetVelocityMPS));
-    }
+	protected void setTargetRollersVelocity(Rotation2d targetVelocityMPS) {
+		rollers.applyRequest(rollersVelocityRequest.withSetPoint(targetVelocityMPS));
+	}
 
-    protected void setRollersVoltage(double voltage){
-        rollers.applyRequest(rollersVoltageRequest.withSetPoint(voltage));
-    }
+	protected void setRollersVoltage(double voltage) {
+		rollers.applyRequest(rollersVoltageRequest.withSetPoint(voltage));
+	}
+
 }
--- src/main/java/frc/robot/subsystems/algaeIntake/AlgaeIntakeCommandsBuilder.java
+++ src/main/java/frc/robot/subsystems/algaeIntake/AlgaeIntakeCommandsBuilder.java
@@ -2,9 +2,10 @@ package frc.robot.subsystems.algaeIntake;
 
 public class AlgaeIntakeCommandsBuilder {
 
-    private final AlgaeIntake algaeIntake;
+	private final AlgaeIntake algaeIntake;
 
-    public AlgaeIntakeCommandsBuilder(AlgaeIntake algaeIntake){
-        this.algaeIntake = algaeIntake;
-    }
+	public AlgaeIntakeCommandsBuilder(AlgaeIntake algaeIntake) {
+		this.algaeIntake = algaeIntake;
+	}
+
 }

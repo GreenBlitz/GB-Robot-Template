81005376f1b35b11fadec1d01be17008fc76509a
Nitay4 <nitay@rotbart.co.il>
2025-06-04 14:19:08 +0300

    nitay - remove test

--- src/main/java/frc/RobotManager.java
+++ src/main/java/frc/RobotManager.java
@@ -5,24 +5,16 @@
 package frc;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Threads;
-import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Robot;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.autonomous.AutonomousConstants;
-import frc.robot.hardware.mechanisms.wpilib.SingleJointedArmSimulation;
-import frc.robot.hardware.phoenix6.Phoenix6DeviceID;
-import frc.robot.hardware.phoenix6.motors.TalonFXMotor;
 import frc.robot.led.LEDConstants;
 import frc.robot.led.LEDState;
-import frc.robot.subsystems.algaeIntake.pivot.PivotConstants;
 import frc.robot.subsystems.climb.lifter.LifterConstants;
 import frc.utils.alerts.AlertManager;
 import frc.utils.auto.PathPlannerUtil;
@@ -43,45 +35,12 @@ public class RobotManager extends LoggedRobot {
 	private final Robot robot;
 	private Command auto;
 	private int roborioCycles;
-	TalonFXMotor test = new TalonFXMotor(
-		"teest" + "/Motor",
-		new Phoenix6DeviceID(17),
-		new SysIdRoutine.Config(),
-		new SingleJointedArmSimulation(
-			new SingleJointedArmSim(
-				LinearSystemId.createDCMotorSystem(
-					DCMotor.getKrakenX60Foc(1),
-					SingleJointedArmSim.estimateMOI(PivotConstants.LENGTH_METERS, PivotConstants.MASS_KG),
-					1.0
-				),
-				DCMotor.getKrakenX60Foc(1),
-				1.0,
-				PivotConstants.LENGTH_METERS,
-				Rotation2d.fromDegrees(5).getRadians(),
-				Rotation2d.fromDegrees(200).getRadians(),
-				false,
-				PivotConstants.STARTING_POSITION.getRadians()
-			),
-			1.0
-		)
-	);
-//
-//
-//	IRequest<Rotation2d> positionRequest = Phoenix6RequestBuilder
-//			.build(new PositionVoltage(PivotConstants.STARTING_POSITION.getRotations()), 0, true);
-//
-//	InputSignal<Rotation2d> positionSignal = Phoenix6SignalBuilder
-//			.build(test.getDevice().getPosition(), RobotConstants.DEFAULT_SIGNALS_FREQUENCY_HERTZ, AngleUnit.ROTATIONS, BusChain.ROBORIO);
-//	InputSignal<Double> voltageSignal = Phoenix6SignalBuilder
-//			.build(test.getDevice().getMotorVoltage(), RobotConstants.DEFAULT_SIGNALS_FREQUENCY_HERTZ, BusChain.ROBORIO);
-
 
 	public RobotManager() {
 		LoggerFactory.initializeLogger();
 		DriverStation.silenceJoystickConnectionWarning(true);
 		PathPlannerUtil.startPathfinder();
 		PathPlannerUtil.setupPathPlannerLogging();
-//		test.applyConfiguration(new TalonFXConfiguration().withSlot0(new Slot0Configs().withKP(1)));
 
 		this.roborioCycles = 0;
 		this.robot = new Robot();
@@ -155,7 +114,6 @@ public class RobotManager extends LoggedRobot {
 	@Override
 	public void teleopInit() {
 		robot.getRobotCommander().initializeDefaultCommand();
-//		robot.getPivot().setPower(0.5);
 	}
 
 	@Override

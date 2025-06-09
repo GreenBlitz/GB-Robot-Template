44f64689075db707d59a1af7e1bfb820695f61a2
greenblitz4590 <greenblitz4590@gmail.com>
2025-06-09 19:11:11 +0300

    rm

--- src/main/java/frc/robot/subsystems/swerve/SwerveCommandsBuilder.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveCommandsBuilder.java
@@ -226,11 +226,4 @@ public class SwerveCommandsBuilder {
 		);
 	}
 
-	public Command moveToPoseByPID(Supplier<Pose2d> currentPose, Pose2d targetPose, SwerveState swerveState) {
-		return swerve.asSubsystemCommand(
-			new InitExecuteCommand(swerve::resetPIDControllers, () -> swerve.moveToPoseByPID(currentPose.get(), targetPose, swerveState)),
-			"PID to pose: " + targetPose
-		);
-	}
-
 }

25d3536f1687c2c0a6079784ee4923d616580aa9
Nitay4 <nitay@rotbart.co.il>
2025-06-09 23:15:19 +0300

    nitay - shits and gigles

--- src/main/java/frc/robot/subsystems/algaeIntake/rollers/Factory/RollersFactory.java
+++ src/main/java/frc/robot/subsystems/algaeIntake/rollers/Factory/RollersFactory.java
@@ -1,11 +1,16 @@
 package frc.robot.subsystems.algaeIntake.rollers.Factory;
 
 import frc.robot.subsystems.algaeIntake.rollers.Rollers;
+import frc.robot.subsystems.algaeIntake.rollers.RollersConstants;
 
 public class RollersFactory {
 
 	public static Rollers create(String logPath) {
-		return SparkMaxRollersBuilder.generate(logPath);
+		if (RollersConstants.isMotorSparkMax) {
+			return SparkMaxRollersBuilder.generate(logPath);
+		} else {
+			return TalonFXRollersBuilder.generate(logPath);
+		}
 	}
 
 }

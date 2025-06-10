c62e633a6dcbf00202b5ad4be2079f2d93dbff76
DANNA2828 <dan71967@gmail.com>
2025-06-10 18:45:41 +0300

    danna - cleaner math

--- src/main/java/frc/utils/math/ObjectDetectionMath.java
+++ src/main/java/frc/utils/math/ObjectDetectionMath.java
@@ -2,7 +2,6 @@ package frc.utils.math;
 
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.*;
-import org.littletonrobotics.junction.Logger;
 
 public class ObjectDetectionMath {
 
@@ -11,13 +10,16 @@ public class ObjectDetectionMath {
 		double rollRadians = cameraPose.getRotation()
 			.rotateBy(new Rotation3d(0, -cameraPose.getRotation().getY(), -cameraPose.getRotation().getZ()))
 			.getX();
-		Logger.recordOutput("newRoll", Rotation2d.fromRadians(cameraPose.getRotation().getX()).minus(Rotation2d.fromRadians(rollRadians)));
 		yawAndPitch = yawAndPitch.rotateBy(Rotation2d.fromRadians(rollRadians).unaryMinus());
 		return new Pair<>(Rotation2d.fromRadians(yawAndPitch.getX()), Rotation2d.fromRadians(yawAndPitch.getY()));
 	}
 
 	public static double getCameraRelativeXAxisDistance(Rotation2d cameraRelativePitch, Pose3d cameraPose, double centerOfObjectHeightMeters) {
-		Rotation2d pitch = cameraRelativePitch.plus(Rotation2d.fromRadians(cameraPose.getRotation().getY()));
+		double cameraPitch = cameraPose.getRotation()
+			.rotateBy(new Rotation3d(-cameraPose.getRotation().getX(), 0, -cameraPose.getRotation().getZ()))
+			.getY();
+		Rotation2d pitch = cameraRelativePitch.plus(Rotation2d.fromRadians(cameraPitch));
+
 		double heightMeters = centerOfObjectHeightMeters - cameraPose.getZ();
 		return heightMeters / Math.tan(pitch.getRadians());
 	}
@@ -27,10 +29,8 @@ public class ObjectDetectionMath {
 	}
 
 	public static Translation2d cameraRelativeToRobotRelative(Translation2d translation, Pose3d cameraPose) {
-		translation = new Translation2d(translation.getX(), translation.getY());
 		translation = translation.rotateBy(Rotation2d.fromRadians(cameraPose.getRotation().getZ()).unaryMinus());
-		return new Translation2d(translation.getX() - cameraPose.getX(), translation.getY() - cameraPose.getY());
-//		return translation;
+		return new Translation2d(translation.getX() + cameraPose.getX(), translation.getY() + cameraPose.getY());
 	}
 
 	public static Translation2d cameraRollRelativeYawAndPitchToRobotRelativePose(
@@ -40,20 +40,17 @@ public class ObjectDetectionMath {
 		double centerOfObjectHeightMeters
 	) {
 		Pair<Rotation2d, Rotation2d> correctedYawAndPitch = correctForCameraRoll(cameraRelativeYaw, cameraRelativePitch, cameraPose);
-		Logger.recordOutput("newTX", correctedYawAndPitch.getFirst());
-		Logger.recordOutput("newTY", correctedYawAndPitch.getSecond());
-		Quaternion cameraRotation = Quaternion.fromRotationVector(cameraPose.getRotation().toVector());
-		cameraRotation.times(Quaternion.fromRotationVector(new Rotation3d(-cameraPose.getRotation().getX(), 0, 0).toVector()));
-//		Rotation3d newRot = new Rotation3d(cameraRotation.toRotationVector());
-		Rotation3d newRot = cameraPose.getRotation().rotateBy(new Rotation3d(-cameraPose.getRotation().getX(), 0, 0));
-		Logger.recordOutput("newYaw", Rotation2d.fromRadians(cameraPose.getRotation().getZ()).minus(Rotation2d.fromRadians(newRot.getZ())));
-		Logger.recordOutput("newPitch", Rotation2d.fromRadians(cameraPose.getRotation().getY()).minus(Rotation2d.fromRadians(newRot.getY())));
-		cameraPose = new Pose3d(cameraPose.getTranslation(), newRot);
+
+//		Quaternion cameraRotation = Quaternion.fromRotationVector(cameraPose.getRotation().toVector());
+//		cameraRotation = cameraRotation.times(Quaternion.fromRotationVector(new Rotation3d(-cameraPose.getRotation().getX(), 0, 0).toVector()));
+//
+//		Rotation3d newRot = cameraPose.getRotation().rotateBy(new Rotation3d(-cameraPose.getRotation().getX(), 0, 0));
+//		cameraPose = new Pose3d(cameraPose.getTranslation(), newRot);
+
 		double xDistance = getCameraRelativeXAxisDistance(correctedYawAndPitch.getSecond(), cameraPose, centerOfObjectHeightMeters);
 		double yDistance = getCameraRelativeYAxisDistance(correctedYawAndPitch.getFirst(), xDistance);
 		Translation2d cameraRelativeTranslation = new Translation2d(xDistance, yDistance);
-		return cameraRelativeTranslation;
-//		return cameraRelativeToRobotRelative(cameraRelativeTranslation, cameraPose);
+		return cameraRelativeToRobotRelative(cameraRelativeTranslation, cameraPose);
 	}
 
 }

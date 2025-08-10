40eed74f9df948e9b3eff52968494bbbd73ccb2c
YHerm <0549405422.yh@gmail.com>
2025-08-10 18:32:20 +0300

    watn

--- src/main/java/frc/robot/hardware/rev/motors/SparkMaxMotor.java
+++ src/main/java/frc/robot/hardware/rev/motors/SparkMaxMotor.java
@@ -35,7 +35,7 @@ public abstract class SparkMaxMotor implements IMotor {
 		this.warnings = motor.getWarnings();
 		this.faults = motor.getFaults();
 
-//		createAlerts();
+		createAlerts();
 	}
 
 	public SparkMaxMotor(String logPath, SparkMaxWrapper motor) {
@@ -233,8 +233,8 @@ public abstract class SparkMaxMotor implements IMotor {
 			}
 		}
 
-//		warnings = motor.getWarnings();
-//		faults = motor.getFaults();
+		warnings = motor.getWarnings();
+		faults = motor.getFaults();
 	}
 
 	@Override

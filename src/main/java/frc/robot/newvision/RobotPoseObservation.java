7e272531f3059d30b60ec17cc40d238d7ce6a5be
hillelv <140002343+hillelv@users.noreply.github.com>
2025-08-20 21:06:46 +0300

    Vision remake -> master (#693)
    * hillel - this is fireee
    
    * hillel - types and namings
    
    * hillel - stddevs
    
    * hillel - stddevs but not from limelight
    
    * hillel - copy the current stddev func
    
    * hillel - rads instead of deg
    
    * hillel - ugly but idc
    
    * hillel - name
    
    * hillel - fix no observation errors
    
    * hillel - is valid
    
    * hillel - different stddevs
    
    * hillel - is valid
    
    * hillel - timestamp
    
    * hillel - stdDifferent
    
    * hillel - import
    
    * hillel - lotta changes
    
    * hillel - std calculation final final 2.0 for real this time
    
    * hillel - 1,3 to 3,1
    
    * hillel - forgor
    
    * hillel - format
    
    * hillel - separate updates
    
    * hillel - filters but good
    
    * hillel - forgor static
    
    * hillel - added used filters
    
    * hillel - stuff from test
    
    * hillel - small fixes
    
    * hillel - abbreviations
    
    * hillel - format
    
    * hillel - forgor
    
    * hillel - naming
    
    * hillel - record :_(
    
    * hillel - gil's comments
    
    * hillel - is mt
    
    * hillel - std record
    
    * hillel - format

---
+++ src/main/java/frc/robot/newvision/RobotPoseObservation.java
@@ -0,0 +1,11 @@
+package frc.robot.newvision;
+
+import edu.wpi.first.math.geometry.Pose2d;
+
+public record RobotPoseObservation(double timestampSeconds, Pose2d robotPose, StdDevs stdDevs) {
+
+	public RobotPoseObservation() {
+		this(0, new Pose2d(), new StdDevs(0.0, 0.0, 0.0));
+	}
+
+}

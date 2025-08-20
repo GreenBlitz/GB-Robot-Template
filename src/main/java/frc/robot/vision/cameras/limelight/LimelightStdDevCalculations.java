0d901c5459aef0449efe260aaf5485119e774ce9
hillelv <hillel.vider@gmail.com>
2025-08-20 22:51:53 +0300

    hillel - format

--- src/main/java/frc/robot/vision/cameras/limelight/LimelightStdDevCalculations.java
+++ src/main/java/frc/robot/vision/cameras/limelight/LimelightStdDevCalculations.java
@@ -8,11 +8,19 @@ public class LimelightStdDevCalculations {
 
 	public static final StandardDeviations2D DEFAULT_STD_DEVS = new StandardDeviations2D(0.0003, 0.0003, 0.003);
 
-	public static Supplier<StandardDeviations2D> getMT1StdDevsCalculation(Limelight limelight, StandardDeviations2D minStdDevs, StandardDeviations2D stdDevFactors) {
+	public static Supplier<StandardDeviations2D> getMT1StdDevsCalculation(
+		Limelight limelight,
+		StandardDeviations2D minStdDevs,
+		StandardDeviations2D stdDevFactors
+	) {
 		return () -> averageTagDistanceParabola(limelight.getMT1RawData().avgTagDist, minStdDevs, stdDevFactors);
 	}
 
-	public static Supplier<StandardDeviations2D> getMT2StdDevsCalculation(Limelight limelight, StandardDeviations2D minStdDevs, StandardDeviations2D stdDevFactors) {
+	public static Supplier<StandardDeviations2D> getMT2StdDevsCalculation(
+		Limelight limelight,
+		StandardDeviations2D minStdDevs,
+		StandardDeviations2D stdDevFactors
+	) {
 		return () -> averageTagDistanceParabola(limelight.getMT2RawData().avgTagDist, minStdDevs, stdDevFactors);
 	}
 
@@ -25,7 +33,10 @@ public class LimelightStdDevCalculations {
 		return new StandardDeviations2D(
 			Math.max(minStandardDeviations.xStandardDeviations(), standardDeviationFactors.xStandardDeviations() * averageTagDistanceSquared),
 			Math.max(minStandardDeviations.yStandardDeviations(), standardDeviationFactors.yStandardDeviations() * averageTagDistanceSquared),
-			Math.max(minStandardDeviations.angleStandardDeviations(), standardDeviationFactors.angleStandardDeviations() * averageTagDistanceSquared)
+			Math.max(
+				minStandardDeviations.angleStandardDeviations(),
+				standardDeviationFactors.angleStandardDeviations() * averageTagDistanceSquared
+			)
 		);
 	}
 

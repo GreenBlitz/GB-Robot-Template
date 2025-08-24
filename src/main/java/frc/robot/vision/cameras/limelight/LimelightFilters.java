6deb35f55638c00af732bf2b76ac6d5467f524e5
hillelv <140002343+hillelv@users.noreply.github.com>
2025-08-24 18:40:32 +0300

    Vision packages (#697)
    * hillel - package and pe
    
    * hillel - heading estimator
    
    * hillel - optional
    
    * hillel - format
    
    * hillel - del filter
    
    * hillel - changed to old std devs
    
    * hillel - format
    
    * hillel - noptionals
    
    * hillel - more noptionals
    
    * hillel - i want varargs now :)
    
    * hillel - eh why not annotate \_('-')_/
    
    * hillel - no change heading estimator

--- src/main/java/frc/robot/vision/cameras/limelight/LimelightFilters.java
+++ src/main/java/frc/robot/vision/cameras/limelight/LimelightFilters.java
@@ -1,4 +1,4 @@
-package frc.robot.newvision.cameras.limelight;
+package frc.robot.vision.cameras.limelight;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;

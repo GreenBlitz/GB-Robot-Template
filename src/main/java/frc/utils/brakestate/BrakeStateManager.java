6116008a642fc9188fb53670bcbdaeb222a53f9a
YHerm <0549405422.yh@gmail.com>
2025-08-25 23:21:12 +0300

    brake state manager fix

--- src/main/java/frc/utils/brakestate/BrakeStateManager.java
+++ src/main/java/frc/utils/brakestate/BrakeStateManager.java
@@ -7,6 +7,7 @@ public class BrakeStateManager {
 
 	private static final ArrayList<Runnable> brakeRunnables = new ArrayList<>();
 	private static final ArrayList<Runnable> coastRunnables = new ArrayList<>();
+	private static boolean isBrake = false;
 
 	public static void add(Runnable brake, Runnable coast) {
 		brakeRunnables.add(brake);
@@ -14,12 +15,20 @@ public class BrakeStateManager {
 	}
 
 	public static void brake() {
+		if (isBrake) {
+			return;
+		}
+		isBrake = true;
 		for (Runnable brake : brakeRunnables) {
 			brake.run();
 		}
 	}
 
 	public static void coast() {
+		if (!isBrake) {
+			return;
+		}
+		isBrake = false;
 		for (Runnable coast : coastRunnables) {
 			coast.run();
 		}

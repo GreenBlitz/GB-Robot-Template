cebdef5c103dfa3ff5c3046474c5b8d69c575ab0
itamaroryan <itamar.oryan@gmail.com>
2025-08-27 20:03:06 +0300

    fix

--- src/main/java/frc/robot/IDs.java
+++ src/main/java/frc/robot/IDs.java
@@ -1,11 +1,6 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.PowerDistribution;
-import frc.utils.battery.PowerDistributionDeviceID;
-
 public class IDs {
-
-	public static final PowerDistributionDeviceID POWER_DISTRIBUTION = new PowerDistributionDeviceID(1, PowerDistribution.ModuleType.kRev);
 
 	public static class TalonFXIDs {
 	}

3681092a8e3dde85d661ab3e17a4e90575f68067
Shahar Perets <perets.shahar@gmail.com>
2025-09-03 04:39:33 +0300

    another constrauctor

--- src/main/java/frc/utils/visuallogging/odometryvisualization/OdometryVisualization.java
+++ src/main/java/frc/utils/visuallogging/odometryvisualization/OdometryVisualization.java
@@ -1,25 +1,25 @@
 package frc.utils.visuallogging.odometryvisualization;
 
-import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 
 import java.util.function.Supplier;
 
 public class OdometryVisualization {
 
 	public final String logPath;
-	public final Supplier<Transform2d> deltaA;
-	public final Supplier<Transform2d> deltaB;
-	public final Supplier<Transform2d> deltaC;
-	public final Supplier<Transform2d> deltaD;
+	public final Supplier<Translation2d> deltaA;
+	public final Supplier<Translation2d> deltaB;
+	public final Supplier<Translation2d> deltaC;
+	public final Supplier<Translation2d> deltaD;
 	public final GridPlacements gridPlacements;
 
 	public OdometryVisualization(
 		String logPath,
-		Supplier<Transform2d> deltaA,
-		Supplier<Transform2d> deltaB,
-		Supplier<Transform2d> deltaC,
-		Supplier<Transform2d> deltaD,
+		Supplier<Translation2d> deltaA,
+		Supplier<Translation2d> deltaB,
+		Supplier<Translation2d> deltaC,
+		Supplier<Translation2d> deltaD,
 		GridPlacements gridPlacements
 	) {
 		this.logPath = logPath;
@@ -30,8 +30,24 @@ public class OdometryVisualization {
 		this.gridPlacements = gridPlacements;
 	}
 
+	public OdometryVisualization(String logPath, Supplier<SwerveModulePosition[]> modulePositions, GridPlacements gridPlacements) {
+		this(
+			logPath,
+			() -> modulePositionToTranslation(modulePositions.get()[0]),
+			() -> modulePositionToTranslation(modulePositions.get()[1]),
+			() -> modulePositionToTranslation(modulePositions.get()[2]),
+			() -> modulePositionToTranslation(modulePositions.get()[3]),
+			gridPlacements
+		);
+	}
+
+	private static Translation2d modulePositionToTranslation(SwerveModulePosition modulePosition) {
+		// polar to cartezian
+		return new Translation2d(modulePosition.distanceMeters, modulePosition.angle); // TODO: check units
+	}
+
 	private Translation2d getAverageChange() {
-		return deltaA.get().plus(deltaB.get()).plus(deltaC.get()).plus(deltaD.get()).div(4d).getTranslation();
+		return deltaA.get().plus(deltaB.get()).plus(deltaC.get()).plus(deltaD.get()).div(4d);
 	}
 
 	private Translation2d getAbsoluteCenter() {
@@ -43,15 +59,7 @@ public class OdometryVisualization {
 	}
 
 	private GridPlacements getPlacementsAfterChange() {
-		return this.gridPlacements
-			.plus(
-				new GridPlacements(
-					deltaA.get().getTranslation(),
-					deltaB.get().getTranslation(),
-					deltaC.get().getTranslation(),
-					deltaD.get().getTranslation()
-				)
-			)
+		return this.gridPlacements.plus(new GridPlacements(deltaA.get(), deltaB.get(), deltaC.get(), deltaD.get()))
 			.plus(new GridPlacements(getAbsoluteCenter().unaryMinus()));
 	}
 

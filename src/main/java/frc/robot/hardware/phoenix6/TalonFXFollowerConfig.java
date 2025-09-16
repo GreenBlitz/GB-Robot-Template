d80159211b5b0995e7afc95b024e373aac5b3a06
greenblitz4590 <greenblitz4590@gmail.com>
2025-09-16 19:16:22 +0300

    aa

--- src/main/java/frc/robot/hardware/phoenix6/TalonFXFollowerConfig.java
+++ src/main/java/frc/robot/hardware/phoenix6/TalonFXFollowerConfig.java
@@ -4,9 +4,12 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import frc.robot.hardware.mechanisms.MechanismSimulation;
 
 public class TalonFXFollowerConfig {
-    public Phoenix6DeviceID[] followerIDs = new Phoenix6DeviceID[0];
-    public MechanismSimulation[] mechanismSimulations = new MechanismSimulation[0];
-    public BusChain[] followerBuses = new BusChain[0];
-    public TalonFXConfiguration followerConfig = new TalonFXConfiguration();
-    public boolean[] followerOpposeMain = new boolean[0];
+
+
+
+    public Phoenix6DeviceID[] followerIDs;
+    public MechanismSimulation[] mechanismSimulations;
+    public BusChain[] followerBuses;
+    public TalonFXConfiguration followerConfig;
+    public boolean[] followerOpposeMain;
 }

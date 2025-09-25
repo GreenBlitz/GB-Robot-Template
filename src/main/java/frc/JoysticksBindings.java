972487b3a8ebc63da8473d7ae4fea7ae491fe7be
greenblitz4590 <greenblitz4590@gmail.com>
2025-09-25 13:11:51 +0300

    working yoav

--- src/main/java/frc/JoysticksBindings.java
+++ src/main/java/frc/JoysticksBindings.java
@@ -209,15 +209,18 @@ public class JoysticksBindings {
 	private static void mainJoystickButtons(Robot robot) {
 		SmartJoystick usedJoystick = MAIN_JOYSTICK;
 		// bindings...
-		usedJoystick.getAxisAsButton(Axis.RIGHT_TRIGGER).onTrue(closeReefActionChooser(robot));
+//		usedJoystick.getAxisAsButton(Axis.RIGHT_TRIGGER).onTrue(closeReefActionChooser(robot));
 
 		usedJoystick.L3.onTrue(new InstantCommand(() -> robot.getSwerve().rotateAxis = robot.getAxis(true))).onFalse(new InstantCommand(() -> robot.getSwerve().rotateAxis = RotateAxis.MIDDLE_OF_CHASSIS));
 		usedJoystick.R3.onTrue(new InstantCommand(() -> robot.getSwerve().rotateAxis = robot.getAxis(false))).onFalse(new InstantCommand(() -> robot.getSwerve().rotateAxis = RotateAxis.MIDDLE_OF_CHASSIS));
 
+		usedJoystick.getAxisAsButton(Axis.LEFT_TRIGGER).onTrue(new InstantCommand(() -> robot.getSwerve().rotateAxis = robot.getAxis(false))).onFalse(new InstantCommand(() -> robot.getSwerve().rotateAxis = RotateAxis.MIDDLE_OF_CHASSIS));
+		usedJoystick.getAxisAsButton(Axis.RIGHT_TRIGGER).onTrue(new InstantCommand(() -> robot.getSwerve().rotateAxis = robot.getAxis(true))).onFalse(new InstantCommand(() -> robot.getSwerve().rotateAxis = RotateAxis.MIDDLE_OF_CHASSIS));
+
 //		usedJoystick.L1.onTrue(robot.getRobotCommander().setState(RobotState.INTAKE_WITH_AIM_ASSIST));
 		usedJoystick.L1.onTrue(robot.getRobotCommander().setState(RobotState.ALGAE_INTAKE));
 
-		usedJoystick.getAxisAsButton(Axis.LEFT_TRIGGER).onTrue(intakeActionChooser(robot));
+//		usedJoystick.getAxisAsButton(Axis.LEFT_TRIGGER).onTrue(intakeActionChooser(robot));
 
 		usedJoystick.R1.onTrue(netActionChooser(robot));
 

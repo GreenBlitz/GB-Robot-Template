afbaff0b16524e8618419ef84c542aeaed95db4b
YHerm <0549405422.yh@gmail.com>
2025-10-02 15:16:15 +0300

    format

--- src/main/java/frc/robot/hardware/phoenix6/motors/TalonFXFollowerConfig.java
+++ src/main/java/frc/robot/hardware/phoenix6/motors/TalonFXFollowerConfig.java
@@ -5,7 +5,7 @@ import frc.robot.hardware.phoenix6.Phoenix6DeviceID;
 
 public class TalonFXFollowerConfig {
 
-	public record TalonFXFollowerID(String name, Phoenix6DeviceID id, boolean opposeMain){}
+	public record TalonFXFollowerID(String name, Phoenix6DeviceID id, boolean opposeMain) {}
 
 	public TalonFXFollowerID[] followerIDs = new TalonFXFollowerID[0];
 	public TalonFXConfiguration motorConfig = new TalonFXConfiguration();

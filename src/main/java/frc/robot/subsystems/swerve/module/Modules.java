1e0f000569659d4cb167089b8f7ae931ee08a3d9
Shahar Perets <perets.shahar@gmail.com>
2025-10-06 04:29:52 -0700

    bugfixes

--- src/main/java/frc/robot/subsystems/swerve/module/Modules.java
+++ src/main/java/frc/robot/subsystems/swerve/module/Modules.java
@@ -128,7 +128,7 @@ public class Modules {
 		for (int i = 0; i < modules.length; i++) {
 			swerveModulePositions[i] = modules[i].getNonThread();
 		}
-		return new SwerveModulePosition[] {swerveModulePositions[0]};
+		return swerveModulePositions;
 	}
 
 

558a4fc6ab67dc43c8336afc16d5a46ed2961617
rys4590 <ronnie.yadid@gmail.com>
2025-10-29 18:26:35 +0200

    in constructor you get the type

--- src/main/java/frc/robot/vision/cameras/limelight/LimelightPipeline.java
+++ src/main/java/frc/robot/vision/cameras/limelight/LimelightPipeline.java
@@ -5,8 +5,8 @@ import frc.robot.vision.DetectedObjectType;
 
 public enum LimelightPipeline {
 
-	APRIL_TAG(0, false, true),
-	OBJECT_DETECTION(1, true, false);
+	APRIL_TAG(0, false, true,new DetectedObjectType[0]),
+	OBJECT_DETECTION(1, true, false,new DetectedObjectType[0]);
 
 	private final int pipelineIndex;
 	private final boolean isDetectingObjects;
@@ -14,7 +14,8 @@ public enum LimelightPipeline {
 	private final int numOfObjects = 0;
 	private DetectedObjectType[] detectedObjectTypes = new DetectedObjectType[numOfObjects];
 
-	LimelightPipeline(int pipelineIndex, boolean isDetectingObjects, boolean isUsingMT) {
+	LimelightPipeline(int pipelineIndex, boolean isDetectingObjects, boolean isUsingMT, DetectedObjectType[] detectedObjectTypes) {
+		this.detectedObjectTypes = detectedObjectTypes;
 		this.pipelineIndex = pipelineIndex;
 		this.isDetectingObjects = isDetectingObjects;
 		this.isUsingMT = isUsingMT;

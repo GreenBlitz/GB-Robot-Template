415b48c997131042f55cab19781082988d6f5ba1
itamaroryan <itamar.oryan@gmail.com>
2025-10-30 19:34:12 +0200

    changess

--- src/main/java/frc/robot/vision/cameras/limelight/LimelightType.java
+++ src/main/java/frc/robot/vision/cameras/limelight/LimelightType.java
@@ -5,7 +5,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 public enum LimelightType {
 
 	LIMELIGHT3(new Translation2d(62.5, 48.9)), //horizontal-vertical in degrees
-	LIMELIGHT3GB(new Translation2d(0, 0)),
+	LIMELIGHT3GB(new Translation2d(72.25, 52.65)), //temp value
 	LIMELIGHT4(new Translation2d(82, 56.2));
 
 	private final Translation2d fieldOfView;

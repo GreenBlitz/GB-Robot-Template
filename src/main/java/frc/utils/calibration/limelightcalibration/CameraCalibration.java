195407abe2a37de7236f95ba18657db2bb8a9bc7
DanielleCarmi09 <daniellecarmi@gmail.com>
2025-11-02 20:24:19 +0200

    cameraCalibrationAfterCR

--- src/main/java/frc/utils/calibration/limelightcalibration/CameraCalibration.java
+++ src/main/java/frc/utils/calibration/limelightcalibration/CameraCalibration.java
@@ -12,38 +12,36 @@ import java.util.function.Supplier;
 
 public class CameraCalibration extends Command {
 
-	private final Limelight limelight;
-	private final Pose3d tagToRobot;
-	private List<Pose3d> pose;
+    private final Limelight limelight;
+    private final Pose3d tagToRobot;
+    private List<Pose3d> pose;
+    private Pose3d sum;
 
-	public CameraCalibration(Limelight limelight) {
-		this.limelight = limelight;
-		this.tagToRobot = limelight.getRobotRelativeCameraPose();
-		Logger.recordOutput("cameraCalibration", LimelightCalculations.getCameraToRobot(LimelightHelpers.getTargetPose3d_CameraSpace(limelight.getName()), tagToRobot));
-	}
+    public CameraCalibration(Limelight limelight, Pose3d tagToRobot) {
+        this.limelight = limelight;
+        this.tagToRobot = tagToRobot;
+        this.sum = new Pose3d();
+    }
 
-	public void addToPoseList() {
-		pose.add(LimelightCalculations.getCameraToRobot(LimelightHelpers.getTargetPose3d_CameraSpace(limelight.getName()), tagToRobot));
-	}
+    public void addToPoseList() {
+        pose.add(LimelightCalculations.getCameraToRobot(LimelightHelpers.getTargetPose3d_CameraSpace(limelight.getName()), tagToRobot));
+    }
 
-	public Pose3d getAvgPose() {
-		Pose3d sum = new Pose3d();
-		for (Pose3d currentPose : pose) {
-			sum.getTranslation().plus(currentPose.getTranslation());
-			sum.getRotation().plus(currentPose.getRotation());
-		}
-		return sum.div(pose.size());
-	}
+    public Pose3d getAvgPose() {
+        sum.getTranslation().times(pose.size() - 1).plus(pose.getLast().getTranslation());
+        sum.getRotation().times(pose.size() - 1).plus(pose.getLast().getRotation());
+        return sum.div(pose.size());
+    }
 
-	@Override
-	public void execute() {
-		addToPoseList();
-		Logger.recordOutput("/cameraCalibration/cameraToRobotPose", getAvgPose());
-	}
+    @Override
+    public void execute() {
+        addToPoseList();
+        Logger.recordOutput("/cameraCalibration/cameraToRobotPose", getAvgPose());
+    }
 
-	@Override
-	public boolean isFinished() {
-		return pose.size() > 100;
-	}
+    @Override
+    public boolean isFinished() {
+        return pose.size() > 100;
+    }
 
 }

507e3cea9bd99aaaa35804aac9daba7d792f7426
rys4590 <ronnie.yadid@gmail.com>
2025-11-02 20:33:04 +0200

    spotless

--- src/main/java/frc/robot/vision/cameras/limelight/Limelight.java
+++ src/main/java/frc/robot/vision/cameras/limelight/Limelight.java
@@ -79,10 +79,7 @@ public class Limelight implements ObjectDetector, IndependentRobotPoseSupplier, 
 				Logger.recordOutput(logPath + "/megaTag2PoseObservation", mt2PoseObservation);
 			}
 		} else if (pipeline.isDetectingObjects()) {
-			Logger.recordOutput(
-				logPath + "/detectedObjectObservations",
-				detectedObjectObservations.toArray(new DetectedObjectObservation[0])
-			);
+			Logger.recordOutput(logPath + "/detectedObjectObservations", detectedObjectObservations.toArray(new DetectedObjectObservation[0]));
 		}
 	}
 

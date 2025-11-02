52439fc439027e2a166a11986972ddb80f227e62
DanielleCarmi09 <daniellecarmi@gmail.com>
2025-11-02 18:17:09 +0200

    calcukation

--- src/main/java/frc/utils/calibration/limelightcalibration/CameraCalibration.java
+++ src/main/java/frc/utils/calibration/limelightcalibration/CameraCalibration.java
@@ -11,54 +11,67 @@ import java.util.function.Supplier;
 
 public class CameraCalibration extends Command {
 
-    private Supplier<Transform3d> tagToCamera;
-    private final Pose3d tagToRobot;
-    private List<Pose3d> pose;
+	private final Supplier<Transform3d> tagToCamera;
+	private final Pose3d tagToRobot;
+	private List<Pose3d> pose;
 
-    public CameraCalibration(Supplier<Transform3d> tagToCamera, Pose3d tagToRobot) {
-        this.tagToCamera = tagToCamera;
-        this.tagToRobot = tagToRobot;
-        Logger.recordOutput("cameraCalibration", LimelightCalculations.getCameraToRobot(tagToCamera, tagToRobot));
-    }
+	public CameraCalibration(Supplier<Transform3d> tagToCamera, Pose3d tagToRobot) {
+		this.tagToCamera = tagToCamera;
+		this.tagToRobot = tagToRobot;
+		Logger.recordOutput("cameraCalibration", LimelightCalculations.getCameraToRobot(tagToCamera, tagToRobot));
+	}
 
-    public void addToPoseList() {
-        pose.add(LimelightCalculations.getCameraToRobot(tagToCamera, tagToRobot));
-    }
+	public void addToPoseList() {
+		pose.add(LimelightCalculations.getCameraToRobot(tagToCamera, tagToRobot));
+	}
 
-    public Pose3d getAvgPose() {
-        double sumX = 0;
-        double sumY = 0;
-        double sumZ = 0;
-        double cosXRotation = 0;
-        double sinXRotation = 0;
-        double cosYRotation = 0;
-        double sinYRotation = 0;
-        double cosZRotation = 0;
-        double sinZRotation = 0;
-        for (Pose3d currentPose : pose) {
-            sumX += currentPose.getX();
-            sumY += currentPose.getY();
-            sumZ += currentPose.getZ();
-            cosXRotation += Math.cos(currentPose.getRotation().getX());
-            sinXRotation += Math.sin(currentPose.getRotation().getX());
-            cosYRotation += Math.cos(currentPose.getRotation().getY());
-            sinYRotation += Math.sin(currentPose.getRotation().getY());
-            cosZRotation += Math.cos(currentPose.getRotation().getZ());
-            sinZRotation += Math.sin(currentPose.getRotation().getZ());
-        }
-        return new Pose3d(sumX / pose.size(), sumY / pose.size(), sumZ / pose.size(), new Rotation3d(Math.atan2(sinXRotation, cosXRotation), Math.atan2(sinYRotation, cosYRotation), Math.atan2(sinZRotation, cosZRotation)
-        ));
-    }
+	public Pose3d getAvgPose() {
+//		double sumX = 0;
+//		double sumY = 0;
+//		double sumZ = 0;
+//		double cosXRotation = 0;
+//		double sinXRotation = 0;
+//		double cosYRotation = 0;
+//		double sinYRotation = 0;
+//		double cosZRotation = 0;
+//		double sinZRotation = 0;
+		Pose3d sum = new Pose3d();
+		for (Pose3d currentPose : pose) {
+//			sumX += currentPose.getX();
+//			sumY += currentPose.getY();
+//			sumZ += currentPose.getZ();
+//			cosXRotation += Math.cos(currentPose.getRotation().getX());
+//			sinXRotation += Math.sin(currentPose.getRotation().getX());
+//			cosYRotation += Math.cos(currentPose.getRotation().getY());
+//			sinYRotation += Math.sin(currentPose.getRotation().getY());
+//			cosZRotation += Math.cos(currentPose.getRotation().getZ());
+//			sinZRotation += Math.sin(currentPose.getRotation().getZ());
+//			sum.getRotation().plus(currentPose.getRotation());
+			sum = new Pose3d(sum.getTranslation().plus(currentPose.getTranslation()),
+LimelightCalculations.getSum(sum.getRotation(), currentPose.getRotation()));
+		}
+//		return new Pose3d(
+//			sumX / pose.size(),
+//			sumY / pose.size(),
+//			sumZ / pose.size(),
+//			new Rotation3d(
+//				Math.atan2(sinXRotation, cosXRotation),
+//				Math.atan2(sinYRotation, cosYRotation),
+//				Math.atan2(sinZRotation, cosZRotation)
+//			)
+//		);
+			return sum.div(pose.size());
+	}
 
-    @Override
-    public void execute(){
-        addToPoseList();
-        Logger.recordOutput("/cameraCalibration/cameraToRobot", getAvgPose());
-    }
+	@Override
+	public void execute() {
+		addToPoseList();
+		Logger.recordOutput("/cameraCalibration/cameraToRobot", getAvgPose());
+	}
 
-    @Override
-    public boolean isFinished(){
-        return pose.size()>100;
-    }
+	@Override
+	public boolean isFinished() {
+		return pose.size() > 100;
+	}
 
 }
--- src/main/java/frc/utils/calibration/limelightcalibration/LimelightCalculations.java
+++ src/main/java/frc/utils/calibration/limelightcalibration/LimelightCalculations.java
@@ -1,13 +1,23 @@
 package frc.utils.calibration.limelightcalibration;
 
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 
 import java.util.function.Supplier;
 
 public class LimelightCalculations {
 
-    public static Pose3d getCameraToRobot(Supplier<Transform3d> tagToCamera, Pose3d tagToRobot){
-        return tagToRobot.transformBy(tagToCamera.get().inverse());
-    }
+	public static Pose3d getCameraToRobot(Supplier<Transform3d> tagToCamera, Pose3d tagToRobot) {
+		return tagToRobot.transformBy(tagToCamera.get().inverse());
+	}
+
+	public static Rotation3d getSum(Rotation3d sum, Rotation3d other){
+		Rotation2d sumX = new Rotation2d(sum.getX());
+		Rotation2d sumY = new Rotation2d(sum.getY());
+		Rotation2d sumZ = new Rotation2d(sum.getZ());
+		return new Rotation3d(sumX.plus(new Rotation2d(other.getX())).getRadians(), sumY.plus(new Rotation2d(other.getY())).getRadians(), sumZ.plus(new Rotation2d(other.getZ())).getRadians());
+	}
+
 }

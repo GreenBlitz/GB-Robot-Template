38533a98e8f46fe721730be37b5ec099b0792948
rys4590 <ronnie.yadid@gmail.com>
2025-11-02 18:12:12 +0200

    svsdj

--- src/main/java/frc/robot/vision/cameras/limelight/Limelight.java
+++ src/main/java/frc/robot/vision/cameras/limelight/Limelight.java
@@ -89,6 +89,7 @@ public class Limelight implements ObjectDetector, IndependentRobotPoseSupplier, 
 	public void updateObjectDetection() {
 		if (pipeline.isDetectingObjects()) {
 			target2dValues = LimelightTarget2dValues.fromArray(LimelightHelpers.getT2DArray(name));
+			detectedObjectObservations.clear();
 			if (target2dValues.isValid()) {
 				LimelightHelpers.RawDetection[] rawDetections = LimelightHelpers.getRawDetections(name);
 				for (LimelightHelpers.RawDetection rawDetection : rawDetections) {

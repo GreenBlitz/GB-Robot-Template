a5c3d7caf09a2d13288bf113f6fafc400ae419b9
Nitay4 <nitay@rotbart.co.il>
2025-11-02 21:15:24 +0200

    danna - added calibrations

--- src/main/java/frc/robot/statemachine/PositionTargets.java
+++ src/main/java/frc/robot/statemachine/PositionTargets.java
@@ -1,19 +1,13 @@
 package frc.robot.statemachine;
 
 import frc.robot.Robot;
-import frc.robot.poseestimator.IPoseEstimator;
-import frc.robot.subsystems.swerve.Swerve;
 
 public class PositionTargets {
 
-	private final IPoseEstimator poseEstimator;
-	private final Swerve swerve;
+	private final Robot robot;
 
 	public PositionTargets(Robot robot) {
-//        this.poseEstimator = robot.getPoseEstimator();
-//        this.swerve = robot.getSwerve();
-		this.poseEstimator = null;
-		this.swerve = null;
+		this.robot = robot;
 	}
 
 }

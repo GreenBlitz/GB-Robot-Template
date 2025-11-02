db0876024eb1b7099a15f024528d3b017962fea6
rys4590 <ronnie.yadid@gmail.com>
2025-11-02 20:34:08 +0200

    ugh

--- src/main/java/frc/robot/vision/cameras/limelight/LimelightPipeline.java
+++ src/main/java/frc/robot/vision/cameras/limelight/LimelightPipeline.java
@@ -15,14 +15,14 @@ public enum LimelightPipeline {
 	private final boolean isUsingMT;
 	private final DetectedObjectType[] detectedObjectTypes;
 
-	LimelightPipeline(int pipelineIndex, boolean isUsingMT) {
-		this(pipelineIndex, isUsingMT, new DetectedObjectType[0]);
-	}
-
 	LimelightPipeline(int pipelineIndex, boolean isUsingMT, DetectedObjectType[] detectedObjectTypes) {
 		this.pipelineIndex = pipelineIndex;
 		this.isUsingMT = isUsingMT;
 		this.detectedObjectTypes = detectedObjectTypes;
+	}
+
+	LimelightPipeline(int pipelineIndex, boolean isUsingMT) {
+		this(pipelineIndex, isUsingMT, new DetectedObjectType[0]);
 	}
 
 	public int getPipelineIndex() {

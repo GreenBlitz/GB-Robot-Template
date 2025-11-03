43c85537c34ac193c29440b9d47cbccccf07c89b
rys4590 <ronnie.yadid@gmail.com>
2025-11-03 10:46:48 +0200

    ;

--- src/main/java/frc/robot/vision/DetectedObjectType.java
+++ src/main/java/frc/robot/vision/DetectedObjectType.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 public enum DetectedObjectType {
 
-	ALGAE("algae", 0.206375),
+	ALGAE("algae", 0.206375);
 
 	private final String name;
 	private final double centerHeightFromFloorMeters;

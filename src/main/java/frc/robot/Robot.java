bda250d02e215ac2236e0b13e3ed4016ab515d80
spiderh0ok <rafaelkeselman@gmail.com>
2025-11-05 20:43:23 +0200

    constants

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,20 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.RobotManager;
 import frc.robot.hardware.phoenix6.BusChain;
+import frc.robot.hardware.phoenix6.motors.TalonFXFollowerConfig;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.ArmBuilder;
+import frc.robot.subsystems.arm.ArmConstants;
+import frc.robot.subsystems.arm.DynamicMotionMagicArm;
 import frc.utils.auto.PathPlannerAutoWrapper;
 import frc.utils.battery.BatteryUtil;
+
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Volts;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very little robot logic should
@@ -18,7 +27,8 @@ import frc.utils.battery.BatteryUtil;
 public class Robot {
 
 	public static final RobotType ROBOT_TYPE = RobotType.determineRobotType();
-
+    private final Arm pivot = ;
+    private final DynamicMotionMagicArm arm = ArmBuilder.build("Arm/", new TalonFXFollowerConfig(),20,Volts.of(7),null,Volts.of(1).per(Second), ArmConstants.MASS_KG, Rotation2d.fromRotations(3),Rotation2d.fromRotations(3),);
 	public Robot() {
 		BatteryUtil.scheduleLimiter();
 	}
--- src/main/java/frc/robot/subsystems/arm/ArmBuilder.java
+++ src/main/java/frc/robot/subsystems/arm/ArmBuilder.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DynamicMotionMagicVoltage;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -16,6 +17,7 @@ import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Robot;
 import frc.robot.hardware.interfaces.IDynamicMotionMagicRequest;
 import frc.robot.hardware.mechanisms.wpilib.SimpleMotorSimulation;
 import frc.robot.hardware.phoenix6.BusChain;
@@ -34,7 +36,7 @@ import frc.utils.calibration.sysid.SysIdCalibrator;
 
 public class ArmBuilder {
 
-	public DynamicMotionMagicArm create(
+	public static DynamicMotionMagicArm build(
 		String logPath,
 		TalonFXFollowerConfig talonFXFollowerConfig,
 		int armId,
@@ -42,20 +44,23 @@ public class ArmBuilder {
 		Time timeout,
 		Velocity<VoltageUnit> rampUp,
 		double JKgMetersSquared,
-		double gearing,
 		Rotation2d maxAcceleration,
 		Rotation2d maxVelocity,
 		double feedForward,
 		double rotorRatio,
 		double mechanismRatio,
 		double kP,
+		double kS,
 		double kI,
 		double kD,
 		double kG,
-		double kV,
-		double kA
+		boolean isCousin,
+        Rotation2d elevatorOpenReversedSoftwareLimit,
+        Rotation2d forwardSoftwareLimit,
+        Rotation2d cruiseVelocityAnglesPerSeconds,
+        Rotation2d accelerationAnglesPerSecondSquared
 	) {
-        TalonFXMotor arm = arm(armId, logPath, talonFXFollowerConfig,JKgMetersSquared,gearing);
+        TalonFXMotor arm = arm(armId, logPath, talonFXFollowerConfig,JKgMetersSquared,rotorRatio*mechanismRatio);
 
 		Phoenix6AngleSignal velocity = Phoenix6SignalBuilder
 			.build(arm.getDevice().getVelocity(), ArmConstants.defaultFrequency, AngleUnit.ROTATIONS, BusChain.ROBORIO);
@@ -72,11 +77,11 @@ public class ArmBuilder {
 		Phoenix6Request<Double> voltageRequest = Phoenix6RequestBuilder.build(new VoltageOut(0), true);
 
 		IDynamicMotionMagicRequest positionRequest = Phoenix6RequestBuilder
-			.build(new DynamicMotionMagicVoltage(position.getLatestValue().getRotations(), 0, 0, 0), 0, true);
+			.build(new DynamicMotionMagicVoltage(0, 0, 0, 0), 0, true);
 		positionRequest.withMaxAccelerationRotation2dPerSecondSquared(maxAcceleration);
 		positionRequest.withMaxVelocityRotation2dPerSecond(maxVelocity);
 		positionRequest.withArbitraryFeedForward(feedForward);
-		TalonFXConfiguration configuration = configuration(rotorRatio, mechanismRatio, kP, kG, kI, kD);
+		TalonFXConfiguration configuration = configuration(rotorRatio, mechanismRatio, kP,kS, kG, kI, kD,isCousin,elevatorOpenReversedSoftwareLimit, forwardSoftwareLimit,cruiseVelocityAnglesPerSeconds,accelerationAnglesPerSecondSquared);
 		arm.applyConfiguration(configuration);
 		SysIdCalibrator.SysIdConfigInfo sysIdConfigInfo = new SysIdCalibrator.SysIdConfigInfo(
 			new SysIdRoutine.Config(rampUp, stepUpVoltage, timeout, state -> SignalLogger.writeString("state", state.toString())),
@@ -98,14 +103,13 @@ public class ArmBuilder {
 	}
 
 
-	public Arm create(
+	public static Arm build(
 		String logPath,
 		Velocity<VoltageUnit> rampUp,
 		Voltage stepUpVoltage,
 		Time timeout,
 		TalonFXFollowerConfig talonFXFollowerConfig,
 		int armId,
-		double gearing,
 		double JKgMetersSquared,
 		double feedforward,
 		double rotorRatio,
@@ -113,9 +117,14 @@ public class ArmBuilder {
 		double kP,
 		double kI,
 		double kD,
-		double kG
+		double kS,
+		double kG,
+        boolean isCousin,
+        Rotation2d elevatorOpenReversedSoftwareLimit,
+        Rotation2d forwardSoftwareLimit
+
 	) {
-        TalonFXMotor arm = arm(armId, logPath, talonFXFollowerConfig,JKgMetersSquared,gearing);
+        TalonFXMotor arm = arm(armId, logPath, talonFXFollowerConfig,JKgMetersSquared,rotorRatio*mechanismRatio);
 
 		Phoenix6AngleSignal velocity = Phoenix6SignalBuilder
 			.build(arm.getDevice().getVelocity(), ArmConstants.defaultFrequency, AngleUnit.ROTATIONS, BusChain.ROBORIO);
@@ -133,7 +142,8 @@ public class ArmBuilder {
 
 		Phoenix6FeedForwardRequest positionRequest = Phoenix6RequestBuilder
 			.build(new MotionMagicVoltage(position.getLatestValue().getRotations()), feedforward, true);
-		arm.applyConfiguration(configuration(rotorRatio, mechanismRatio, kP, kG, kI, kD));
+
+		arm.applyConfiguration(configuration(rotorRatio, mechanismRatio, kP,kS, kG, kI, kD,isCousin,elevatorOpenReversedSoftwareLimit, forwardSoftwareLimit));
 		SysIdCalibrator.SysIdConfigInfo sysIdConfigInfo = new SysIdCalibrator.SysIdConfigInfo(
 			new SysIdRoutine.Config(rampUp, stepUpVoltage, timeout, state -> SignalLogger.writeString("state", state.toString())),
 			true
@@ -142,18 +152,64 @@ public class ArmBuilder {
 		return new Arm(logPath, arm, velocity, position, voltage, current, voltageRequest, positionRequest, sysIdConfigInfo);
 	}
 
-	private static TalonFXConfiguration configuration(double rotorRatio, double mechanismRatio, double kP, double kG, double kI, double kD) {
+	private static TalonFXConfiguration configuration(double rotorRatio, double mechanismRatio, double kP, double kS,double kG, double kI, double kD,boolean isArmCosine, Rotation2d elevatorOpenReversedSoftwareLimit, Rotation2d forwardSoftwareLimit,Rotation2d cruiseVelocityAnglesPerSeconds,Rotation2d accelerationAnglesPerSecondSquared) {
 		TalonFXConfiguration talonFXConfiguration = new TalonFXConfiguration();
-		talonFXConfiguration.Feedback.RotorToSensorRatio = rotorRatio;
-		talonFXConfiguration.Feedback.SensorToMechanismRatio = mechanismRatio;
 		talonFXConfiguration.Slot0.kP = kP;
 		talonFXConfiguration.Slot0.kI = kI;
 		talonFXConfiguration.Slot0.kD = kD;
 		talonFXConfiguration.Slot0.kG = kG;
+        talonFXConfiguration.Slot0.kS = kS;
+
+        if (isArmCosine)
+            talonFXConfiguration.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+        else
+            talonFXConfiguration.Slot0.GravityType = GravityTypeValue.Elevator_Static;
+
 		talonFXConfiguration.MotorOutput.withInverted(InvertedValue.Clockwise_Positive);
-		talonFXConfiguration.CurrentLimits.withStatorCurrentLimitEnable(true);
+        talonFXConfiguration.MotorOutput.withNeutralMode(NeutralModeValue.Coast);
+
+        talonFXConfiguration.CurrentLimits.withStatorCurrentLimitEnable(true);
 		talonFXConfiguration.CurrentLimits.withStatorCurrentLimit(ArmConstants.CURRENT_LIMIT);
-		talonFXConfiguration.MotorOutput.withNeutralMode(NeutralModeValue.Coast);
+
+        talonFXConfiguration.Feedback.SensorToMechanismRatio = mechanismRatio;
+        talonFXConfiguration.Feedback.RotorToSensorRatio = rotorRatio;
+
+        talonFXConfiguration.SoftwareLimitSwitch.ForwardSoftLimitThreshold = forwardSoftwareLimit.getRotations();
+        talonFXConfiguration.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+        talonFXConfiguration.SoftwareLimitSwitch.ReverseSoftLimitThreshold = elevatorOpenReversedSoftwareLimit.getRotations();
+        talonFXConfiguration.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+
+        talonFXConfiguration.MotionMagic.MotionMagicAcceleration = accelerationAnglesPerSecondSquared.getRotations();
+        talonFXConfiguration.MotionMagic.MotionMagicCruiseVelocity = cruiseVelocityAnglesPerSeconds.getRotations();
+		return talonFXConfiguration;
+	}
+    private static TalonFXConfiguration configuration(double rotorRatio, double mechanismRatio, double kP, double kS,double kG, double kI, double kD,boolean isArmCosine, Rotation2d elevatorOpenReversedSoftwareLimit, Rotation2d forwardSoftwareLimit) {
+		TalonFXConfiguration talonFXConfiguration = new TalonFXConfiguration();
+		talonFXConfiguration.Slot0.kP = kP;
+		talonFXConfiguration.Slot0.kI = kI;
+		talonFXConfiguration.Slot0.kD = kD;
+		talonFXConfiguration.Slot0.kG = kG;
+        talonFXConfiguration.Slot0.kS = kS;
+
+        if (isArmCosine)
+            talonFXConfiguration.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+        else
+            talonFXConfiguration.Slot0.GravityType = GravityTypeValue.Elevator_Static;
+
+		talonFXConfiguration.MotorOutput.withInverted(InvertedValue.Clockwise_Positive);
+        talonFXConfiguration.MotorOutput.withNeutralMode(NeutralModeValue.Coast);
+
+        talonFXConfiguration.CurrentLimits.withStatorCurrentLimitEnable(true);
+		talonFXConfiguration.CurrentLimits.withStatorCurrentLimit(ArmConstants.CURRENT_LIMIT);
+
+        talonFXConfiguration.Feedback.SensorToMechanismRatio = mechanismRatio;
+        talonFXConfiguration.Feedback.RotorToSensorRatio = rotorRatio;
+
+        talonFXConfiguration.SoftwareLimitSwitch.ForwardSoftLimitThreshold = forwardSoftwareLimit.getRotations();
+        talonFXConfiguration.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+        talonFXConfiguration.SoftwareLimitSwitch.ReverseSoftLimitThreshold = elevatorOpenReversedSoftwareLimit.getRotations();
+        talonFXConfiguration.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+
 		return talonFXConfiguration;
 	}
 

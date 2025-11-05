f25d1471d5d51aeeef389d1519db9fa1bbbddf6f
greenblitz4590 <greenblitz4590@gmail.com>
2025-11-05 20:27:01 +0200

    103(8)

--- src/main/java/frc/robot/subsystems/Roller.java
+++ src/main/java/frc/robot/subsystems/Roller.java
@@ -1,8 +1,35 @@
 package frc.robot.subsystems;
 
-public class Roller extends GBSubsystem{
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.hardware.interfaces.ControllableMotor;
+import frc.robot.hardware.interfaces.IRequest;
+import frc.robot.hardware.interfaces.InputSignal;
 
-    public Roller(String logPath) {
+public class Roller extends GBSubsystem{
+    private final ControllableMotor roller;
+
+    private final IRequest<Double> setVoltageRequest;
+    private final InputSignal<Rotation2d> LatencySignal;
+    private final InputSignal<Rotation2d> valocetySignal;
+    private final InputSignal<Double> voltageSignal;
+    private final InputSignal<Rotation2d> currntSignal;
+
+    public Roller(String logPath, IRequest setPower, ControllableMotor roller, IRequest<Double> setVoltageRequest, InputSignal<Rotation2d> latencySignal, InputSignal<Rotation2d> valocetySignal, InputSignal<Double> voltageSignal, InputSignal<Rotation2d> currntSignal) {
         super(logPath);
+
+        this.roller = roller;
+        this.setVoltageRequest = setVoltageRequest;
+
+        LatencySignal = latencySignal;
+        this.valocetySignal = valocetySignal;
+        this.voltageSignal = voltageSignal;
+        this.currntSignal = currntSignal;
+    }
+    public void setVoltage(Double voltage){
+        roller.applyRequest(setVoltageRequest.withSetPoint(voltage));
+    }
+
+    public void stop(){
+        roller.stop();
     }
 }

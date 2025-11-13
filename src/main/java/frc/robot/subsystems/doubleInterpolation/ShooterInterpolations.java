1f7976abcd9b146e59fa56f157082dff759f9749
Yahli <dolev.yahli@gmail.com>
2025-11-13 13:47:44 +0200

    completed hermans comments

--- src/main/java/frc/robot/subsystems/doubleInterpolation/ShooterInterpolations.java
+++ src/main/java/frc/robot/subsystems/doubleInterpolation/ShooterInterpolations.java
@@ -7,40 +7,19 @@ import frc.utils.InterpolationMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
-public class TemporaryInterpolation {
+public class ShooterInterpolations {
 
 	public static final InterpolationMap<Double, Rotation2d> HOOD_INTERPOLATION_MAP = new InterpolationMap<Double, Rotation2d>(
 		InverseInterpolator.forDouble(),
 		InterpolationMap.interpolatorForRotation2d(),
 		Map.of(
-			1.0,
-			Rotation2d.fromDegrees(75),
-			2.0,
-			Rotation2d.fromDegrees(68),
-			3.0,
-			Rotation2d.fromDegrees(57),
-			4.0,
-			Rotation2d.fromDegrees(52),
-			5.0,
-			Rotation2d.fromDegrees(49)
 		)
 	);
 
 	public static final InterpolationMap<Double, Rotation2d> FLYWHEEL_INTERPOLATION_MAP = new InterpolationMap<Double, Rotation2d>(
 		InverseInterpolator.forDouble(),
 		InterpolationMap.interpolatorForRotation2d(),
-		Map.of(
-			1.0,
-			Rotation2d.fromRotations(5),
-			2.0,
-			Rotation2d.fromRotations(7),
-			3.0,
-			Rotation2d.fromRotations(10),
-			4.0,
-			Rotation2d.fromRotations(13),
-			5.0,
-			Rotation2d.fromRotations(16)
-		)
+		Map.of()
 	);
 
 	public static Rotation2d hoodInterpolation(Supplier<Double> distanceFromTower) {

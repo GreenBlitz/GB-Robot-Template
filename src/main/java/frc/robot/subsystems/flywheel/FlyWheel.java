c3236949d8c1aaad04a991b74472375e3deb790b
maya <maya.romm0401@gmail.com>
2025-11-16 17:58:42 +0200

    maya- working on CR

--- src/main/java/frc/robot/subsystems/flywheel/FlyWheel.java
+++ src/main/java/frc/robot/subsystems/flywheel/FlyWheel.java
@@ -93,10 +93,10 @@ public class FlyWheel extends GBSubsystem {
 	}
 
 	public void applyCalibrationsBindings(SmartJoystick joystick) {
-		joystick.X.onTrue(new InstantCommand(() -> getCommandBuilder().setIsSubsystemRunningIndependently(true)));
-		joystick.Y.onTrue(new InstantCommand(() -> getCommandBuilder().setIsSubsystemRunningIndependently(false)));
-		joystick.A.onTrue(getCommandBuilder().setTargetVelocity(Rotation2d.fromRotations(1)));
-		joystick.B.onTrue(getCommandBuilder().setTargetVelocity(Rotation2d.fromRotations(2)));
+		joystick.L1.onTrue(new InstantCommand(() -> getCommandBuilder().setIsSubsystemRunningIndependently(true)));
+		joystick.L3.onTrue(new InstantCommand(() -> getCommandBuilder().setIsSubsystemRunningIndependently(false)));
+		joystick.POV_LEFT.onTrue(getCommandBuilder().setTargetVelocity(Rotation2d.fromRotations(1)));
+		joystick.POV_UP.onTrue(getCommandBuilder().setTargetVelocity(Rotation2d.fromRotations(2)));
 		joystick.POV_DOWN.onTrue(getCommandBuilder().stop());
 		sysIdCalibrator.setAllButtonsForCalibration(joystick);
 	}

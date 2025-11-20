75a7524f321cec4e4685999a462343bcdd8dd5c9
tomer-hershman <lili.hershman@gmail.com>
2025-11-20 19:01:32 +0200

    tomer - pre merge

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,15 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.configs.FeedbackConfigs;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.RobotManager;
+import frc.constants.fourBar.FourBarConstants;
 import frc.robot.hardware.phoenix6.BusChain;
+import frc.robot.hardware.phoenix6.motors.TalonFXFollowerConfig;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.TalonFXArmBuilder;
 import frc.robot.subsystems.flywheel.FlyWheel;
 import frc.robot.subsystems.flywheel.KrakenX60FlyWheelBuilder;
 import frc.utils.auto.PathPlannerAutoWrapper;
@@ -21,10 +27,12 @@ public class Robot {
 
 	public static final RobotType ROBOT_TYPE = RobotType.determineRobotType(false);;
 	private final FlyWheel flyWheel;
+	private final Arm fourBar;
 
 	public Robot() {
 		BatteryUtil.scheduleLimiter();
 		this.flyWheel = KrakenX60FlyWheelBuilder.build("Subsystems/FlyWheel", IDs.TalonFXIDs.FLYWHEEL);
+		this.fourBar = createFourBar();
 	}
 
 	public void periodic() {
@@ -37,6 +45,13 @@ public class Robot {
 
 	public FlyWheel getFlyWheel() {
 		return flyWheel;
+	}
+
+	public Arm createFourBar(){
+		return TalonFXArmBuilder.buildDynamicMotionMagicArm(FourBarConstants.LOG_PATH,IDs.TalonFXIDs.FOUR_BAR,false,new TalonFXFollowerConfig(),new SysIdRoutine.Config(),FourBarConstants.FEEDBACK_CONFIGS,FourBarConstants.REAL_SLOTS_CONFIGS,FourBarConstants.SIMULATION_SLOTS_CONFIGS,FourBarConstants.CURRENT_LIMIT,RobotConstants.DEFAULT_SIGNALS_FREQUENCY_HERTZ,FourBarConstants.MOMENT_OF_INERTIA,FourBarConstants.ARM_LENGTH_METERS,0,FourBarConstants.FORWARD_SOFTWARE_LIMITS,FourBarConstants.BACKWARD_SOFTWARE_LIMITS,FourBarConstants.MAX_ACCELERATION_ROTATION2D_METERS_PER_SECONDS_SQUARE,FourBarConstants.MAX_VELOCITY_ROTATION2D_METERS_PER_SECONDS);
+	}
+	public Arm getFourBar(){
+		return fourBar;
 	}
 
 	public PathPlannerAutoWrapper getAutonomousCommand() {

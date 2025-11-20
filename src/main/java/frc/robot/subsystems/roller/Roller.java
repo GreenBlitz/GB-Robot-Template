e46d5d2cf0bc3cdfc0ede6c252cdf4f1cb6ec3f2
tomer-hershman <lili.hershman@gmail.com>
2025-11-20 16:51:52 +0200

    Generic roller subsistem (#752)
    * tomer- java Aint working
    
    * tomer- java working(thanks to hilel)
    
    * tomer- Roller+RollerCommands+RollerSparkMaxBuilder
    
    * tomer- TalonFXConfiguration
    
    * tomer- pre CR
    
    * tome- sS
    
    * tome- post dror cr
    
    * tomet - some extra
    
    * tomet - some extra
    
    * tomet - last varsion befor CR
    
    * tomet - post nitay CR
    
    * tomet - nitay CR
    
    * tomet - nitay CR
    
    * tomet - nitay CR
    
    * tomer - Yherm CR
    
    * tomer - sA
    
    * tomer - nitay CR
    
    * tomer - did some importent shit
    
    * tomer - to pull from master
    
    * tomer - sA
    
    * tomer - to pull from master
    
    * tomer - cr
    
    * tomer - removed position request
    
    * tomer - 1CR
    
    * tomer - dror CR
    
    * tomer - dror CR
    
    * tomer - code works
    
    * tomer - sA
    
    * tomer - removesed line
    
    * tomer - chanaged lambda
    
    * tomer - chanaged lambda
    
    * tomer - YHerm Cr
    
    * tomer - sA
    
    * tomer - YHerm Cr
    
    * tomer - YHerm Cr
    
    * tomer - gotta go to sleep
    
    * tomer - need to ask YHerm what he means in the last comment.
    
    * tomer - YHerm CR
    
    * tomer - sA
    
    * tomer - fuck you nitay

---
+++ src/main/java/frc/robot/subsystems/roller/Roller.java
@@ -0,0 +1,101 @@
+package frc.robot.subsystems.roller;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.hardware.interfaces.ControllableMotor;
+import frc.robot.hardware.interfaces.IRequest;
+import frc.robot.hardware.interfaces.InputSignal;
+import frc.robot.subsystems.GBSubsystem;
+
+public class Roller extends GBSubsystem {
+
+	protected final ControllableMotor roller;
+
+	private final InputSignal<Double> voltageSignal;
+	private final InputSignal<Double> currentSignal;
+	private final InputSignal<Rotation2d> positionSignal;
+	private final IRequest<Double> voltageRequest;
+	private final RollerCommandsBuilder commandsBuilder;
+	private Rotation2d targetPosition;
+
+	public Roller(
+		String logPath,
+		ControllableMotor roller,
+		InputSignal<Double> voltageSignal,
+		InputSignal<Double> currentSignal,
+		InputSignal<Rotation2d> positionSignal,
+		IRequest<Double> voltageRequest
+	) {
+		super(logPath);
+		this.roller = roller;
+		this.voltageSignal = voltageSignal;
+		this.currentSignal = currentSignal;
+		this.positionSignal = positionSignal;
+		this.voltageRequest = voltageRequest;
+		this.commandsBuilder = new RollerCommandsBuilder(this);
+		this.roller.resetPosition(Rotation2d.fromRotations(0));
+		this.targetPosition = Rotation2d.fromRotations(0);
+	}
+
+	public RollerCommandsBuilder getCommandsBuilder() {
+		return commandsBuilder;
+	}
+
+	public void updateTargetPosition(Rotation2d targetPosition) {
+		this.targetPosition = targetPosition;
+	}
+
+	public void setVoltage(double voltage) {
+		roller.applyRequest(voltageRequest.withSetPoint(voltage));
+	}
+
+	public void setPower(double power) {
+		roller.setPower(power);
+	}
+
+	public void stop() {
+		roller.stop();
+	}
+
+	public void setBrake(boolean brake) {
+		roller.setBrake(brake);
+	}
+
+	public Double getVoltage() {
+		return voltageSignal.getLatestValue();
+	}
+
+	public Double getCurrent() {
+		return currentSignal.getLatestValue();
+	}
+
+	public Rotation2d getPosition() {
+		return positionSignal.getLatestValue();
+	}
+
+	public boolean isAtPosition(Rotation2d position, Rotation2d tolerance) {
+		return positionSignal.isNear(position, tolerance);
+	}
+
+	public boolean isBehindPosition(Rotation2d position) {
+		return positionSignal.isLess(position);
+	}
+
+	public boolean isPastPosition(Rotation2d position) {
+		return positionSignal.isGreater(position);
+	}
+
+	public boolean isPastTargetPosition() {
+		return isPastPosition(targetPosition);
+	}
+
+	public boolean isBehindTargetPosition() {
+		return isBehindPosition(targetPosition);
+	}
+
+	@Override
+	public void subsystemPeriodic() {
+		roller.updateSimulation();
+		roller.updateInputs(voltageSignal, currentSignal, positionSignal);
+	}
+
+}

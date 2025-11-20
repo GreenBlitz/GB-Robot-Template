954f8963cf0e29e97c6dee45aba23b1c8ceba4dc
spiderh0ok <rafaelkeselman@gmail.com>
2025-11-20 21:50:42 +0200

    fix

--- src/main/java/frc/robot/subsystems/constants/omni/OmniConstant.java
+++ src/main/java/frc/robot/subsystems/constants/omni/OmniConstant.java
@@ -8,8 +8,8 @@ public class OmniConstant {
 	public static final String FUNNEL_INPUT_NAME = "FunnelSensor";
 	public static final double GEAR_RATIO = 25.0 / 6.0;
 	public static final int CURRENT_LIMIT = 20;
-	public static final double MOMENT_OF_INERTIA = 0.01;
-	public static final double DEBOUNCE_TIME = 0.1;
+	public static final double MOMENT_OF_INERTIA = 0.001;
+	public static final double DEBOUNCE_TIME = 0.01;
 	public static final boolean IS_FORWARD_LIMIT_SWITCH = false;
 	public static final boolean IS_FORWARD_LIMIT_SWITCH_INVERTED = false;
 	public static final boolean IS_INVERTED = true;

fcbadadfd9266ff9972e029c982ce21543bbe6fb
Yahli <dolev.yahli@gmail.com>
2025-11-20 19:13:51 +0200

    merged from master and sim passed

--- src/main/java/frc/robot/IDs.java
+++ src/main/java/frc/robot/IDs.java
@@ -10,9 +10,9 @@ public class IDs {
 		public final static Phoenix6DeviceID FLYWHEEL = new Phoenix6DeviceID(10, BusChain.ROBORIO);
 		public final static Phoenix6DeviceID FLYWHEEL_FOLLOWER = new Phoenix6DeviceID(11, BusChain.ROBORIO);
 
-		public static final Phoenix6DeviceID TURRET = new Phoenix6DeviceID(30, BusChain.ROBORIO);
-
 		public final static Phoenix6DeviceID HOOD = new Phoenix6DeviceID(20, BusChain.ROBORIO);
+
+		public static final Phoenix6DeviceID TURRET = new Phoenix6DeviceID(30, BusChain.ROBORIO);
 
 	}
 

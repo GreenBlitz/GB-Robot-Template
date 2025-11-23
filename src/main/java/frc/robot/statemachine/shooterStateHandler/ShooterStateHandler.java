d7293b8a8bc167f25fa08a97861fcf5ccbdd4678
Yahli <dolev.yahli@gmail.com>
2025-11-23 18:15:32 +0200

    Shooter state finished

--- src/main/java/frc/robot/statemachine/shooterStateHandler/ShooterStateHandler.java
+++ src/main/java/frc/robot/statemachine/shooterStateHandler/ShooterStateHandler.java
@@ -1,4 +1,79 @@
 package frc.robot.statemachine.shooterStateHandler;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.interpolation.InverseInterpolator;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.flywheel.FlyWheel;
+import frc.utils.InterpolationMap;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class ShooterStateHandler {
+
+    private final Arm turret;
+    private final Arm hood;
+    private final FlyWheel flyWheel;
+    private ShooterState currentState;
+    private final Supplier<Double> distanceFromTower;
+
+    public ShooterStateHandler(Arm turret, Arm hood, FlyWheel flyWheel, Supplier<Double> distanceFromTower) {
+        this.turret = turret;
+        this.hood = hood;
+        this.flyWheel = flyWheel;
+        this.distanceFromTower = distanceFromTower;
+    }
+
+    public ShooterState getCurrentState() {
+        return currentState;
+    }
+
+    public static final InterpolationMap<Double, Rotation2d> HOOD_INTERPOLATION_MAP = new InterpolationMap<Double, Rotation2d>(
+            InverseInterpolator.forDouble(),
+            InterpolationMap.interpolatorForRotation2d(),
+            Map.of()
+    );
+
+    public static final InterpolationMap<Double, Rotation2d> FLYWHEEL_INTERPOLATION_MAP = new InterpolationMap<Double, Rotation2d>(
+            InverseInterpolator.forDouble(),
+            InterpolationMap.interpolatorForRotation2d(),
+            Map.of()
+    );
+
+    public static Rotation2d hoodInterpolation(Supplier<Double> distanceFromTower) {
+        return HOOD_INTERPOLATION_MAP.get(distanceFromTower.get());
+    }
+
+    public static Rotation2d flywheelInterpolation(Supplier<Double> distanceFromTower) {
+        return FLYWHEEL_INTERPOLATION_MAP.get(distanceFromTower.get());
+    }
+
+    public Command setState(ShooterState shooterState) {
+        return new ParallelCommandGroup(
+                new InstantCommand(() -> currentState = shooterState),
+                switch (shooterState){
+                    case STAY_IN_PLACE -> stayInPlace();
+                    case IDLE -> idle();
+                }
+        );
+    }
+
+    private Command stayInPlace(){
+        return new ParallelCommandGroup(
+                turret.getCommandsBuilder().stayInPlace(),
+                hood.getCommandsBuilder().stayInPlace(),
+                flyWheel.getCommandBuilder().stop()
+        );
+    }
+
+    private Command idle() {
+        return new ParallelCommandGroup(
+                turret.getCommandsBuilder().stayInPlace(),
+                hood.getCommandsBuilder().setTargetPosition(hoodInterpolation(distanceFromTower)),
+                flyWheel.getCommandBuilder().setTargetVelocity(flywheelInterpolation(distanceFromTower))
+        );
+    }
 }

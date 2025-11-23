ceb51d3ad16308a5929dc167b09df180019354ba
hillelv <hillel.vider@gmail.com>
2025-11-23 19:21:21 +0200

    hillel - merge

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -81,6 +81,7 @@ public class Robot {
 				new Translation3d(0.215, -0.11, 0.508),
 				new Rotation3d(Units.Degrees.of(-8.06180374425555), Units.Degrees.of(-27.07784559039065), Units.Degrees.of(-22.52372569716833))
 			),
+			LimelightVersion.LIMELIGHT_4,
 			LimelightPipeline.APRIL_TAG
 		);
 		limelightFour.setMT1PoseFilter(
@@ -124,6 +125,7 @@ public class Robot {
 				new Translation3d(0.2022, 0.13, 0.508),
 				new Rotation3d(Units.Degrees.of(10.612258493096334), Units.Degrees.of(-27.18966371065684), Units.Degrees.of(20.10328620400214))
 			),
+			LimelightVersion.LIMELIGHT_3G,
 			LimelightPipeline.APRIL_TAG
 		);
 		limelightThreeGB.setMT1PoseFilter(
@@ -167,7 +169,8 @@ public class Robot {
 				new Translation3d(-0.08, 0.23, 0.865),
 				new Rotation3d(Units.Degrees.of(0), Units.Degrees.of(-27), Units.Degrees.of(-176.67))
 			),
-			LimelightPipeline.OBJECT_DETECTION
+			LimelightVersion.LIMELIGHT_3,
+			LimelightPipeline.COLOR_DETECTION
 		);
 
 		swerve.setHeadingSupplier(
@@ -216,6 +219,7 @@ public class Robot {
 		headingEstimator.log();
 
 		limelightObjectDetector.updateObjectDetection();
+		limelightObjectDetector.updateColorDetection();
 
 		BatteryUtil.logStatus();
 		BusChain.logChainsStatuses();

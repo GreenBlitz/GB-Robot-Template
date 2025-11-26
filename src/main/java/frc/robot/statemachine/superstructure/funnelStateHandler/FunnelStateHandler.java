4bafb4377362a081dbb4c039167664ebcd5745d0
itamaroryan <itamar.oryan@gmail.com>
2025-11-26 13:26:28 +0200

    logRafa

--- src/main/java/frc/robot/statemachine/superstructure/funnelStateHandler/FunnelStateHandler.java
+++ src/main/java/frc/robot/statemachine/superstructure/funnelStateHandler/FunnelStateHandler.java
@@ -2,18 +2,22 @@ package frc.robot.statemachine.superstructure.funnelStateHandler;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
 import frc.robot.hardware.digitalinput.DigitalInputInputsAutoLogged;
 import frc.robot.subsystems.roller.Roller;
+import org.littletonrobotics.junction.Logger;
 
 public class FunnelStateHandler {
 
 	private final Roller omni;
 	private final Roller belly;
+	private final String logPath;
 	private DigitalInputInputsAutoLogged sensor;
 
-	public FunnelStateHandler(Roller omni, Roller belly, DigitalInputInputsAutoLogged sensor) {
+	public FunnelStateHandler(Roller omni, Roller belly, String logPath, DigitalInputInputsAutoLogged sensor) {
 		this.omni = omni;
 		this.belly = belly;
+		this.logPath = logPath + "/State";
 	}
 
 	public Command setState(FunnelState state) {
@@ -30,6 +34,7 @@ public class FunnelStateHandler {
     }
 
 	private Command drive() {
+		Logger.recordOutput(logPath,"DRIVE");
 		return new ParallelCommandGroup(
 			omni.getCommandsBuilder().stop(),
 			belly.getCommandsBuilder().rollRotationsAtVoltageForwards(1, FunnelConstants.DRIVE_BELLY_VOLTAGE).until(this::isBallAtSensor)
@@ -37,6 +42,7 @@ public class FunnelStateHandler {
 	}
 
 	private Command shoot() {
+		Logger.recordOutput(logPath,"SHOOT");
 		return new ParallelCommandGroup(
 			omni.getCommandsBuilder().setVoltage(FunnelConstants.SHOOT_OMNI_VOLTAGE),
 			belly.getCommandsBuilder().setVoltage(FunnelConstants.SHOOT_BELLY_VOLTAGE)
@@ -44,6 +50,7 @@ public class FunnelStateHandler {
 	}
 
 	private Command intake() {
+		Logger.recordOutput(logPath,"INTAKE");
 		return new ParallelCommandGroup(
 			omni.getCommandsBuilder().stop(),
 			belly.getCommandsBuilder().setPower(FunnelConstants.INTAKE_BELLY_VOLTAGE)
@@ -51,6 +58,7 @@ public class FunnelStateHandler {
 	}
 
 	private Command stayInPlace() {
+		Logger.recordOutput(logPath,"STAY_IN_PLACE");
 		return new ParallelCommandGroup(omni.getCommandsBuilder().stop(), belly.getCommandsBuilder().stop());
 	}
 

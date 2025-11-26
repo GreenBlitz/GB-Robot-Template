0937db29a83300eb72d0eb13f65cf19d642d9021
spiderh0ok <rafaelkeselman@gmail.com>
2025-11-26 20:49:24 +0200

    fixFromTester

--- src/main/java/frc/robot/statemachine/superstructure/funnelStateHandler/FunnelStateHandler.java
+++ src/main/java/frc/robot/statemachine/superstructure/funnelStateHandler/FunnelStateHandler.java
@@ -1,81 +1,89 @@
 package frc.robot.statemachine.superstructure.funnelStateHandler;
 
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.hardware.digitalinput.DigitalInputInputs;
 import frc.robot.hardware.digitalinput.DigitalInputInputsAutoLogged;
 import frc.robot.hardware.digitalinput.IDigitalInput;
 import frc.robot.subsystems.roller.Roller;
+import frc.utils.time.TimeUtil;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
 
 public class FunnelStateHandler {
 
-	private final Roller omni;
-	private final Roller belly;
-	private final String logPath;
-	private final IDigitalInput sensor;
-    private final DigitalInputInputsAutoLogged inputInputsAutoLogged;
-	private final LoggedNetworkNumber bellyCalibrationPower = new LoggedNetworkNumber("BellyPower", 0);
-	private final LoggedNetworkNumber omniCalibrationPower = new LoggedNetworkNumber("OmniPower", 0);
+    private final Roller omni;
+    private final Roller belly;
+    private final String logPath;
+    private final IDigitalInput sensor;
+    private final DigitalInputInputsAutoLogged sensorInformation = new DigitalInputInputsAutoLogged();
+    private final LoggedNetworkNumber bellyCalibrationPower = new LoggedNetworkNumber("BellyPower", 0);
+    private final LoggedNetworkNumber omniCalibrationPower = new LoggedNetworkNumber("OmniPower", 0);
 
-	public FunnelStateHandler(Roller omni, Roller belly, String logPath, IDigitalInput sensor) {
-		this.omni = omni;
-		this.belly = belly;
-		this.sensor = sensor;
-		this.logPath = logPath + "/FunnelState";
-        this.inputInputsAutoLogged = new DigitalInputInputsAutoLogged();
-        this.sensor.updateInputs(inputInputsAutoLogged);
-	}
+    public FunnelStateHandler(Roller omni, Roller belly, String logPath, IDigitalInput sensor) {
+        this.omni = omni;
+        this.belly = belly;
+        this.sensor = sensor;
+        this.logPath = logPath + "/FunnelStateHandler";
+        sensor.updateInputs(sensorInformation);
+    }
 
-	public Command setState(FunnelState state) {
-		return switch (state) {
-			case DRIVE -> drive();
-			case SHOOT -> shoot();
-			case INTAKE -> intake();
-			case STOP -> stop();
-			case CALIBRATION -> calibration();
-		};
-	}
+    public Command setState(FunnelState state) {
+        return switch (state) {
+            case DRIVE -> drive();
+            case SHOOT -> shoot();
+            case INTAKE -> intake();
+            case STOP -> stop();
+            case CALIBRATION -> calibration();
+        };
+    }
 
-	public boolean isBallAtSensor() {
-		return true;
-	}
+    public boolean isBallAtSensor() {
+        return sensorInformation.debouncedValue;
+    }
 
-	private Command drive() {
-		Logger.recordOutput(logPath, "DRIVE");
-		return new ParallelCommandGroup(
-			omni.getCommandsBuilder().stop(),
-			belly.getCommandsBuilder().rollRotationsAtVoltageForwards(1, FunnelState.DRIVE.getBellyVoltage()).until(this::isBallAtSensor)
-		);
-	}
+    private Command drive() {
+        return new ParallelCommandGroup(
+                new InstantCommand(() -> Logger.recordOutput(logPath, "DRIVE")),
+                omni.getCommandsBuilder().stop(),
+                belly.getCommandsBuilder().rollRotationsAtVoltageForwards(1, FunnelState.DRIVE.getBellyVoltage()).until(this::isBallAtSensor)
+        );
+    }
 
-	private Command shoot() {
-		Logger.recordOutput(logPath, "SHOOT");
-		return new ParallelCommandGroup(
-			omni.getCommandsBuilder().setVoltage(FunnelState.SHOOT.getOmniVoltage()),
-			belly.getCommandsBuilder().setVoltage(FunnelState.SHOOT.getBellyVoltage())
-		);
-	}
+    private Command shoot() {
+        return new ParallelCommandGroup(
+                new InstantCommand(() -> Logger.recordOutput(logPath, "SHOOT")),
+                omni.getCommandsBuilder().setVoltage(FunnelState.SHOOT.getOmniVoltage()),
+                belly.getCommandsBuilder().setVoltage(FunnelState.SHOOT.getBellyVoltage())
+        );
+    }
 
-	private Command intake() {
-		Logger.recordOutput(logPath, "INTAKE");
-		return new ParallelCommandGroup(
-			omni.getCommandsBuilder().stop(),
-			belly.getCommandsBuilder().setVoltage(FunnelState.INTAKE.getBellyVoltage())
-		);
-	}
+    private Command intake() {
+        return new ParallelCommandGroup(
+                new InstantCommand(() -> Logger.recordOutput(logPath, "INTAKE")),
+                omni.getCommandsBuilder().stop(),
+                belly.getCommandsBuilder().setVoltage(FunnelState.INTAKE.getBellyVoltage())
+        );
+    }
 
-	private Command stop() {
-		Logger.recordOutput(logPath, "STOP");
-		return new ParallelCommandGroup(omni.getCommandsBuilder().stop(), belly.getCommandsBuilder().stop());
-	}
+    private Command stop() {
+        return new ParallelCommandGroup(
+                new InstantCommand(() -> Logger.recordOutput(logPath, "DRIVE")),
+                omni.getCommandsBuilder().stop(), belly.getCommandsBuilder().stop());
+    }
 
-	private Command calibration() {
-		Logger.recordOutput(logPath, "CALIBRATION");
-		return new ParallelCommandGroup(
-			omni.getCommandsBuilder().setPower(omniCalibrationPower.get()),
-			belly.getCommandsBuilder().setPower(bellyCalibrationPower.get())
-		);
-	}
+    private Command calibration() {
+        return new ParallelCommandGroup(
+                new InstantCommand(() -> Logger.recordOutput(logPath, "CALIBRATION")),
+                omni.getCommandsBuilder().setPower(omniCalibrationPower.get()),
+                belly.getCommandsBuilder().setPower(bellyCalibrationPower.get())
+        );
+    }
+
+    public void periodic(){
+        sensor.updateInputs(sensorInformation);
+    }
 
 }

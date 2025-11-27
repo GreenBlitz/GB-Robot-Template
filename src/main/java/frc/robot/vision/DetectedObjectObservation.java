e3856521d20745e6138000b1877a540db131a639
daniel <danielshapir1@gmail.com>
2025-11-27 19:14:58 +0200

    progress

--- src/main/java/frc/robot/vision/DetectedObjectObservation.java
+++ src/main/java/frc/robot/vision/DetectedObjectObservation.java
@@ -1,11 +1,18 @@
 package frc.robot.vision;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 
-public record DetectedObjectObservation(DetectedObjectType type, Translation2d robotRelativeObjectTranslation, double timestampSeconds) {
+public record DetectedObjectObservation(DetectedObjectType type, Translation3d robotRelativeObjectTranslation,
+                                        double timestampSeconds) {
 
-	public DetectedObjectObservation() {
-		this(null, Translation2d.kZero, 0);
-	}
+    public DetectedObjectObservation() {
+        this(null, Translation3d.kZero, 0);
+    }
 
+    public DetectedObjectObservation(DetectedObjectType type, Translation2d Translation2dObject, double timestampSeconds) {
+       Translation3d tempTranslation3d = new Translation3d(Translation2dObject.getX(),Translation2dObject.getY(),)
+
+        this(type,tempTranslation3d , timestampSeconds);
+    }
 }

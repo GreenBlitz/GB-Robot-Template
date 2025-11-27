de59176e8a84582938cde4187161c93fe37c5adb
spiderh0ok <rafaelkeselman@gmail.com>
2025-11-27 18:01:31 +0200

    code

--- src/main/java/frc/JoysticksBindings.java
+++ src/main/java/frc/JoysticksBindings.java
@@ -51,6 +51,7 @@ public class JoysticksBindings {
 
 	private static void mainJoystickButtons(Robot robot) {
 		SmartJoystick usedJoystick = MAIN_JOYSTICK;
+        applyHoodCalibrationBindings(robot.getHood(),usedJoystick,1);
 		// bindings...
 	}
 

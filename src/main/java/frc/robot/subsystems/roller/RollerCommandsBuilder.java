f25e89f3daecbda22306df98bc386259e1975e42
spiderh0ok <rafaelkeselman@gmail.com>
2025-11-27 15:27:31 +0200

    stopIsInstantNow

--- src/main/java/frc/robot/subsystems/roller/RollerCommandsBuilder.java
+++ src/main/java/frc/robot/subsystems/roller/RollerCommandsBuilder.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.roller;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.DeferredCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.GBCommandsBuilder;
 import frc.utils.utilcommands.InitExecuteCommand;
@@ -29,7 +30,7 @@ public class RollerCommandsBuilder extends GBCommandsBuilder {
 	}
 
 	public Command stop() {
-		return roller.asSubsystemCommand(new RunCommand(roller::stop), "Stop roller");
+		return roller.asSubsystemCommand(new InstantCommand(roller::stop), "Stop roller");
 	}
 
 	public Command setPower(Supplier<Double> supplier) {

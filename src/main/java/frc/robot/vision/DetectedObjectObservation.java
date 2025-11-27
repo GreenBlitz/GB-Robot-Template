986fa7ef8168b8971a3b8d1845350e9876f34620
daniel <danielshapir1@gmail.com>
2025-11-27 18:49:08 +0200

    fr

--- src/main/java/frc/robot/vision/DetectedObjectObservation.java
+++ src/main/java/frc/robot/vision/DetectedObjectObservation.java
@@ -3,7 +3,7 @@ package frc.robot.vision;
 import edu.wpi.first.math.geometry.Translation2d;
 
 public record DetectedObjectObservation(DetectedObjectType type, Translation2d robotRelativeObjectTranslation, double timestampSeconds) {
--
+
 	public DetectedObjectObservation() {
 		this(null, Translation2d.kZero, 0);
 	}

a09dd9d9e0d4ab1bdc37bfbd2819c3d027803c07
daniel <danielshapir1@gmail.com>
2025-12-03 20:43:06 +0200

    commit

--- src/main/java/frc/utils/calibration/Camera/CalibrateCamera.java
+++ src/main/java/frc/utils/calibration/Camera/CalibrateCamera.java
@@ -13,74 +13,78 @@ import frc.utils.math.FieldMath;
 import org.littletonrobotics.junction.Logger;
 
 public class CalibrateCamera extends Command {
-  private String PathPrefix;
-  private String cameraName;
-  private int tagID;
-  private double xRobotDistanceFromTag;
-  private double middleOfTagHeight;
-  private AprilTagFields field;
-  private Pose3d tagPoseFieldRelative;
-  private Pose3d cameraPoseFieldRelative;
-  private Pose2d robotPoseFieldRelative ;
-  private Rotation3d endRot;
-  private Translation3d endTranslation;
-   public CalibrateCamera(AprilTagFields field , String PathPrefix ,String cameraName,int tagID , double xRobotDistanceFromTag ,double middleOfTagHeight  ){
-    this.cameraName = cameraName;
-    this.middleOfTagHeight = middleOfTagHeight;
-    this.field = field;
-    this.PathPrefix = PathPrefix;
-    this.tagID = tagID;
-    this.xRobotDistanceFromTag = xRobotDistanceFromTag;
-    logCameraPose(PathPrefix ,  cameraName ,  tagID,  xRobotDistanceFromTag,  middleOfTagHeight);
-   }
+    private String PathPrefix;
+    private String cameraName;
+    private int tagID;
+    private double xRobotDistanceFromTag;
+    private double middleOfTagHeight;
+    private AprilTagFields field;
+    private Pose3d tagPoseFieldRelative;
+    private Pose3d cameraPoseFieldRelative;
+    private Pose2d robotPoseFieldRelative;
+    private Rotation3d endRot;
+    private Translation3d endTranslation;
+
+    public CalibrateCamera(AprilTagFields field, String PathPrefix, String cameraName, int tagID, double xRobotDistanceFromTag, double middleOfTagHeight) {
+        this.cameraName = cameraName;
+        this.middleOfTagHeight = middleOfTagHeight;
+        this.field = field;
+        this.PathPrefix = PathPrefix;
+        this.tagID = tagID;
+        this.xRobotDistanceFromTag = xRobotDistanceFromTag;
+        logCameraPose(PathPrefix, cameraName, tagID, xRobotDistanceFromTag, middleOfTagHeight);
+    }
 
 
-    private void logFunction(){
-       Logger.recordOutput("CameraCalibration/" + PathPrefix + "/tag/TagPoseFieldRelative", tagPoseFieldRelative);
+    private void logFunction() {
+        Logger.recordOutput("CameraCalibration/" + PathPrefix + "/tag/TagPoseFieldRelative", tagPoseFieldRelative);
 
-       Logger.recordOutput("CameraCalibration/" + PathPrefix + "/cam/cameraPoseFieldRelative", cameraPoseFieldRelative);
+        Logger.recordOutput("CameraCalibration/" + PathPrefix + "/cam/cameraPoseFieldRelative", cameraPoseFieldRelative);
 
-       Logger.recordOutput("CameraCalibration/" + PathPrefix + "/robot/robotFieldRelative", robotPoseFieldRelative);
+        Logger.recordOutput("CameraCalibration/" + PathPrefix + "/robot/robotFieldRelative", robotPoseFieldRelative);
 
-       Logger.recordOutput("CameraCalibration/" + PathPrefix + "/solution/endTranslation", endTranslation);
-   }
-    public void initialize(){
+        Logger.recordOutput("CameraCalibration/" + PathPrefix + "/solution/endTranslation", endTranslation);
+    }
+
+    public void initialize() {
 
     }
 
-    public void execute(){
+    public void execute() {
+        logCameraPose(PathPrefix, cameraName, tagID, xRobotDistanceFromTag, middleOfTagHeight);
+        logFunction();
+    }
+
+    public void end() {
 
     }
 
-    public void end(){
+    public boolean isFinished() {
+        return true; // TEMP
+    }
 
-    }
-    public boolean isFinished(){
-    return true; // TEMP
-    }
     private void logCameraPose(String PathPrefix, String cameraName, int tagID, double xRobotDistanceFromTag, double middleOfTagHeight) {
         // TODO - make a command, do an average, make field param, add option to tags that are not perfectly aligned
 
         LimelightHelpers.setCameraPose_RobotSpace(cameraName, 0, 0, 0, 0, 0, 0);
 
-         this.tagPoseFieldRelative = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded).getTagPose(tagID).get();
+        this.tagPoseFieldRelative = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded).getTagPose(tagID).get();
         this.cameraPoseFieldRelative = LimelightHelpers.getBotPose3d_wpiBlue(cameraName);
 
-         this.robotPoseFieldRelative = new Pose2d(
+        this.robotPoseFieldRelative = new Pose2d(
                 tagPoseFieldRelative.getX() - xRobotDistanceFromTag,
                 tagPoseFieldRelative.getY(),
                 FieldMath.transformAngle(tagPoseFieldRelative.getRotation().toRotation2d(), AngleTransform.INVERT)
         );
 
         // limelight is funny so we invert pitch
-         this.endRot = new Rotation3d(
+        this.endRot = new Rotation3d(
                 cameraPoseFieldRelative.getRotation().getX(),
                 -cameraPoseFieldRelative.getRotation().getY(),
                 cameraPoseFieldRelative.getRotation().getZ() - robotPoseFieldRelative.getRotation().getRadians()
         );
-
         // limelight is funny so we invert y-axis
-         this.endTranslation = new Translation3d(
+        this.endTranslation = new Translation3d(
                 cameraPoseFieldRelative.getX() - robotPoseFieldRelative.getX(),
                 -(cameraPoseFieldRelative.getY() - robotPoseFieldRelative.getY()),
                 cameraPoseFieldRelative.getZ() - tagPoseFieldRelative.getZ() + middleOfTagHeight
@@ -88,8 +92,6 @@ public class CalibrateCamera extends Command {
 
 
     }
-
-
 
 
 }

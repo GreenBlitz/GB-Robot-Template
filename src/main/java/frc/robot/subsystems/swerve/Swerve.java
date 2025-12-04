f8d0caffdd9078f32f5a6dccc577de6c9dd68b9f
DanielleCarmi09 <daniellecarmi@gmail.com>
2025-12-04 18:34:20 +0200

    collision

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -327,11 +327,13 @@ public class Swerve extends GBSubsystem {
 	}
 
 	public boolean isCollisionDetected() {
+		Logger.recordOutput(getLogPath() + "isCollisionDetected", imuSignals.getAccelerationEarthGravitationalAcceleration().toTranslation2d().getNorm() > 2);
 		return imuSignals.getAccelerationEarthGravitationalAcceleration().toTranslation2d().getNorm() > 2;
 	}
 
-	public Optional<Translation2d> collisionDirection(){
+	public Optional<Translation2d> getCurrentCollision(){
 		if(isCollisionDetected()){
+			Logger.recordOutput(getLogPath() + "collision", imuSignals.getAccelerationEarthGravitationalAcceleration().toTranslation2d());
 			return Optional.of(imuSignals.getAccelerationEarthGravitationalAcceleration().toTranslation2d());
 		}
 		return Optional.empty();

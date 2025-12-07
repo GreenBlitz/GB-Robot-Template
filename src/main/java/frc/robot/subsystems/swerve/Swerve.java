d606c0d69f235414f3be75d5b13fd3d50f902ccb
rys4590 <ronnie.yadid@gmail.com>
2025-12-07 17:24:57 +0200

    spotless

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -358,7 +358,10 @@ public class Swerve extends GBSubsystem {
 
 		for (int i = 1; i < currentModuleTranslationalStates.length; i++) {
 			Logger.recordOutput("robotTranslational Velocity", robotTranslationalVelocity);
-			Logger.recordOutput("currentModuleTrabskatuinalState" + ModuleUtil.ModulePosition.values()[i].toString(), currentModuleRotationalStates);
+			Logger.recordOutput(
+				"currentModuleTrabskatuinalState" + ModuleUtil.ModulePosition.values()[i].toString(),
+				currentModuleRotationalStates
+			);
 			areWheelsSkidding[i] = MathUtil.isNear(robotTranslationalVelocity.getX(), currentModuleTranslationalStates[i].getX(), 0.1)
 				|| MathUtil.isNear(robotTranslationalVelocity.getY(), currentModuleTranslationalStates[i].getY(), 0.1);
 		}

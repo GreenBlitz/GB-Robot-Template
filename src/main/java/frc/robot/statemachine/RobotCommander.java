49cc71090783002a355961dd5f27c076b0786b7f
tomer-hershman <lili.hershman@gmail.com>
2025-12-10 19:32:49 +0200

    tomer- nity CR

--- src/main/java/frc/robot/statemachine/RobotCommander.java
+++ src/main/java/frc/robot/statemachine/RobotCommander.java
@@ -39,13 +39,13 @@ public class RobotCommander extends GBSubsystem {
 						Set.of(
 							this,
 							swerve,
-                            robot.getIntakeRoller(),
-                            robot.getTurret(),
-                            robot.getFourBar(),
-                            robot.getBelly(),
-                            robot.getHood(),
-                            robot.getOmni(),
-                            robot.getFlyWheel()
+							robot.getIntakeRoller(),
+							robot.getTurret(),
+							robot.getFourBar(),
+							robot.getBelly(),
+							robot.getHood(),
+							robot.getOmni(),
+							robot.getFlyWheel()
 						)
 					).schedule()
 				),

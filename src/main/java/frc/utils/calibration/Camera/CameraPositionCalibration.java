312e96b95827655ba02769f5baea2073f0894195
daniel <danielshapir1@gmail.com>
2025-12-21 19:29:15 +0200

    plsssssssssssss

--- src/main/java/frc/utils/calibration/Camera/CameraPositionCalibration.java
+++ src/main/java/frc/utils/calibration/Camera/CameraPositionCalibration.java
@@ -2,7 +2,11 @@ package frc.utils.calibration.Camera;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.*;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.utils.limelight.LimelightHelpers;
 import frc.utils.math.AngleTransform;

07a54c3ebdaf3a98cd860fbc69eb29220464a286
DANNA2828 <149606002+DANNA2828@users.noreply.github.com>
2026-01-01 21:10:35 +0200

    isConnected -> master (#787)
    * danna -
    
    * danna -
    
    * danna -

--- src/main/java/frc/robot/vision/cameras/limelight/inputs/LimelightInputsSet.java
+++ src/main/java/frc/robot/vision/cameras/limelight/inputs/LimelightInputsSet.java
@@ -1,14 +1,23 @@
 package frc.robot.vision.cameras.limelight.inputs;
 
+import frc.robot.hardware.ConnectedInputAutoLogged;
+
 public record LimelightInputsSet(
 	MTInputsAutoLogged mt1Inputs,
 	MTInputsAutoLogged mt2Inputs,
 	NeuralDetectionInputsAutoLogged neuralDetectionInputs,
-	ColorDetectionInputsAutoLogged colorDetectionInputs
+	ColorDetectionInputsAutoLogged colorDetectionInputs,
+	ConnectedInputAutoLogged connectedInput
 ) {
 
 	public LimelightInputsSet() {
-		this(new MTInputsAutoLogged(), new MTInputsAutoLogged(), new NeuralDetectionInputsAutoLogged(), new ColorDetectionInputsAutoLogged());
+		this(
+			new MTInputsAutoLogged(),
+			new MTInputsAutoLogged(),
+			new NeuralDetectionInputsAutoLogged(),
+			new ColorDetectionInputsAutoLogged(),
+			new ConnectedInputAutoLogged()
+		);
 	}
 
 }

6e1e8bebfbf3b755f672f14ceeaf1a3c49601bc4
itamaroryan <itamar.oryan@gmail.com>
2026-01-12 22:08:31 +0200

    danna - new new calib

--- src/main/java/frc/robot/subsystems/swerve/factories/imu/Pigeon2IMUBuilder.java
+++ src/main/java/frc/robot/subsystems/swerve/factories/imu/Pigeon2IMUBuilder.java
@@ -23,9 +23,9 @@ class Pigeon2IMUBuilder {
 
 	private static Pigeon2Configuration buildIMUConfig() {
 		Pigeon2Configuration imuConfig = new Pigeon2Configuration();
-		imuConfig.MountPose.MountPoseYaw = 90.0335693359375;
-		imuConfig.MountPose.MountPosePitch = 0.15487580001354218;
-		imuConfig.MountPose.MountPoseRoll = -1.6686772108078003;
+		imuConfig.MountPose.MountPoseYaw = 90.20378112792969;
+		imuConfig.MountPose.MountPosePitch = 0.9536983370780945;
+		imuConfig.MountPose.MountPoseRoll = -1.5514165163040161;
 		return imuConfig;
 	}
 

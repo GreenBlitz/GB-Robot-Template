21d5d0a2a41ff4e19a7468da4577ced49fb7890c
Spiderh0ok <rafaelkeselman@gmail.com>
2026-07-22 21:02:16 +0300

    code added

--- src/main/java/frc/robot/subsystems/arm/ArmCommandBuilder.java
+++ src/main/java/frc/robot/subsystems/arm/ArmCommandBuilder.java
@@ -4,8 +4,10 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.GBCommandsBuilder;
+import frc.utils.utilcommands.ExecuteEndCommand;
 import frc.utils.utilcommands.InitExecuteCommand;
 
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
@@ -44,6 +46,15 @@ public class ArmCommandBuilder extends GBCommandsBuilder {
 
 	public Command setVoltage(DoubleSupplier voltage) {
 		return arm.asSubsystemCommand(new RunCommand(() -> arm.setVoltage(voltage.getAsDouble())), "Set voltage by supplier");
+	}
+
+	public Command setVoltageWithoutLimit(double voltage, BooleanSupplier isFinished) {
+		return arm
+			.asSubsystemCommand(
+				new ExecuteEndCommand(() -> arm.setVoltageWithoutLimit(voltage), () -> arm.stayInPlace()),
+				"Set voltage to: " + voltage + " without limits"
+			)
+			.until(isFinished);
 	}
 
 }

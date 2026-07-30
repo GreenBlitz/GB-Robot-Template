3af02a2dfe4e508f52756ffd62c8ecec3bbb1f2a
Spiderh0ok <rafaelkeselman@gmail.com>
2026-07-30 16:32:11 +0300

    code

--- src/main/java/frc/utils/auto/PathPlannerAutoWrapper.java
+++ src/main/java/frc/utils/auto/PathPlannerAutoWrapper.java
@@ -1,14 +1,19 @@
 package frc.utils.auto;
 
 import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.constants.field.Field;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class PathPlannerAutoWrapper extends PathPlannerAuto {
+
+	private PathPlannerPath[] paths;
 
 	public PathPlannerAutoWrapper() {
 		super(Commands.none());
@@ -18,9 +23,26 @@ public class PathPlannerAutoWrapper extends PathPlannerAuto {
 		super(autoName);
 	}
 
-	public PathPlannerAutoWrapper(Command autoCommand, Pose2d startingPose, String autoName) {
+	public PathPlannerAutoWrapper(
+		Command autoCommand,
+		Pose2d startingPose,
+		String autoName,
+		PathPlannerPath path,
+		PathPlannerPath... additionalPaths
+	) {
 		super(autoCommand, startingPose);
 		setName(autoName);
+
+		this.paths = new PathPlannerPath[additionalPaths.length + 1];
+		paths[0] = path;
+		System.arraycopy(additionalPaths, 0, paths, 1, additionalPaths.length);
+	}
+
+	private PathPlannerAutoWrapper(Command autoCommand, Pose2d startingPose, String autoName, PathPlannerPath[] paths) {
+		super(autoCommand, startingPose);
+		setName(autoName);
+
+		this.paths = paths;
 	}
 
 	public PathPlannerAutoWrapper withAutoName(String name) {
@@ -32,12 +54,27 @@ public class PathPlannerAutoWrapper extends PathPlannerAuto {
 		return new PathPlannerAutoWrapper(
 			this.beforeStarting(() -> resetPose.accept(Field.getAllianceRelative(getStartingPose()))),
 			this.getStartingPose(),
-			this.getName()
+			this.getName(),
+			this.paths
 		);
 	}
 
 	public PathPlannerAutoWrapper asProxyAuto() {
-		return new PathPlannerAutoWrapper(this.asProxy(), this.getStartingPose(), this.getName());
+		return new PathPlannerAutoWrapper(this.asProxy(), this.getStartingPose(), this.getName(), this.paths);
 	}
+
+	public List<Pose2d> getPath(boolean flip) {
+		List<Pose2d> finalList = new ArrayList<>();
+		if (paths != null) {
+			for (PathPlannerPath path : paths) {
+				if (flip) {
+					path = path.flipPath();
+				}
+				finalList.addAll(path.getPathPoses());
+			}
+		}
+		return finalList;
+	}
+
 
 }

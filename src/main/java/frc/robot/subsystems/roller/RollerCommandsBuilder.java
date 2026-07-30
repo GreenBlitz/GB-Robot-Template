ae8935da72a5739066db789d21a947cbc625ee07
tomer <tomer.hershman@gmail.com>
2026-07-30 19:03:13 +0300

    tomer - i think i did it?

--- src/main/java/frc/robot/subsystems/roller/RollerCommandsBuilder.java
+++ src/main/java/frc/robot/subsystems/roller/RollerCommandsBuilder.java
@@ -4,18 +4,16 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.DeferredCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import frc.robot.subsystems.GBCommandsBuilder;
 import frc.utils.utilcommands.InitExecuteCommand;
 
 import java.util.Set;
 import java.util.function.Supplier;
 
-public class RollerCommandsBuilder extends GBCommandsBuilder {
+public class RollerCommandsBuilder {
 
 	private final Roller roller;
 
 	protected RollerCommandsBuilder(Roller roller) {
-		super();
 		this.roller = roller;
 		roller.setDefaultCommand(stop());
 	}

8450cd6327cc760c98f00d04eb78c57a4b85693a
YHerm <0549405422.yh@gmail.com>
2026-07-30 13:18:48 +0300

    filters fix

--- src/main/java/frc/robot/vision/cameras/limelight/LimelightFilters.java
+++ src/main/java/frc/robot/vision/cameras/limelight/LimelightFilters.java
@@ -25,14 +25,13 @@ public class LimelightFilters {
 		Translation2d robotInFieldTolerance,
 		Rotation2d yawAtAngleTolerance
 	) {
-		double timestamp = limelight.getMT1RawData().timestampSeconds();
 		return MegaTagFilters.isRobotInField(() -> limelight.getMT1RawData().pose().getTranslation(), robotInFieldTolerance)
 			.and(
-				MegaTagFilters.doesYawExistAtTimestamp(timestamp, wantedYawAtTimestamp)
-					.and(
+				MegaTagFilters.doesYawExistAtTimestamp(limelight.getMT1RawData().timestampSeconds(), wantedYawAtTimestamp)
+					.implies(
 						MegaTagFilters.isYawAtExpectedAngle(
 							() -> limelight.getMT1RawData().pose().getRotation(),
-							() -> wantedYawAtTimestamp.apply(timestamp).get(),
+							() -> wantedYawAtTimestamp.apply(limelight.getMT1RawData().timestampSeconds()).get(),
 							isYawCalibrated,
 							yawAtAngleTolerance
 						)
@@ -47,14 +46,13 @@ public class LimelightFilters {
 		Translation2d robotInFieldTolerance,
 		Rotation2d yawAtAngleTolerance
 	) {
-		double timestamp = limelight.getMT2RawData().timestampSeconds();
 		return MegaTagFilters.isRobotInField(() -> limelight.getMT2RawData().pose().getTranslation(), robotInFieldTolerance)
 			.and(
-				MegaTagFilters.doesYawExistAtTimestamp(timestamp, wantedYawAtTimestamp)
-					.and(
+				MegaTagFilters.doesYawExistAtTimestamp(limelight.getMT2RawData().timestampSeconds(), wantedYawAtTimestamp)
+					.implies(
 						MegaTagFilters.isYawAtExpectedAngle(
 							() -> limelight.getMT2RawData().pose().getRotation(),
-							() -> wantedYawAtTimestamp.apply(timestamp).get(),
+							() -> wantedYawAtTimestamp.apply(limelight.getMT2RawData().timestampSeconds()).get(),
 							isYawCalibrated,
 							yawAtAngleTolerance
 						)

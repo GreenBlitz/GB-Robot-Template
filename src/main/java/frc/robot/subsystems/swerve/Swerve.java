b037d9252cde585da4dc172447d9f58987e77d3f
maya <maya.romm0401@gmail.com>
2026-07-30 19:51:04 +0300

    yay

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -105,9 +105,12 @@ public class Swerve extends GBSubsystem {
 		return imuSignals.getOrientation();
 	}
 
+	public Translation3d getIMUAccelerationG(){
+		return imuSignals.getAccelerationEarthGravitationalAcceleration();
+	}
+
 	public Translation3d getAccelerationFromIMUMetersPerSecondSquared() {
-		return imuSignals.getAccelerationEarthGravitationalAcceleration()
-			.times(RobotConstants.GRAVITATIONAL_ACCELERATION_METERS_PER_SECOND_SQUARED_ISRAEL);
+		return getIMUAccelerationG().times(RobotConstants.GRAVITATIONAL_ACCELERATION_METERS_PER_SECOND_SQUARED_ISRAEL);
 	}
 
 

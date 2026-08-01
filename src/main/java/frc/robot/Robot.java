d2a48da5324487162b9f3ea0329ad4fccd2fbdc5
Spiderh0ok <rafaelkeselman@gmail.com>
2026-08-02 02:49:45 +0300

    camera fix

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,6 +24,7 @@ import frc.robot.vision.cameras.limelight.LimelightPipeline;
 import frc.utils.auto.PathPlannerAutoWrapper;
 import frc.utils.battery.BatteryUtil;
 import frc.robot.hardware.interfaces.IIMU;
+import frc.utils.limelight.LimelightHelpersAdditions;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very little robot logic should
@@ -83,6 +84,10 @@ public class Robot {
 
 		swerve.update();
 		poseEstimator.updateOdometry(swerve.getAllOdometryData());
+
+        limelight.updateMT1();
+        limelight.getIndependentRobotPose().ifPresent(poseEstimator::updateVision);
+
 		poseEstimator.log();
 
 		BatteryUtil.logStatus();
--- src/main/java/frc/robot/vision/cameras/limelight/Limelight.java
+++ src/main/java/frc/robot/vision/cameras/limelight/Limelight.java
@@ -30,7 +30,7 @@ public class Limelight implements ObjectDetector, IndependentRobotPoseSupplier, 
 	private final ArrayList<DetectedObjectObservation> neuralDetections;
 	private final ArrayList<DetectedObjectObservation> colorDetections;
 
-	private final LimelightInputsSet inputs;
+	public final LimelightInputsSet inputs;
 
 	private RobotPoseObservation mt1PoseObservation;
 	private RobotPoseObservation mt2PoseObservation;

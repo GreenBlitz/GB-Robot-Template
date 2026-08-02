b882d51097e3c256b8ca828fe483fcb01e78d765
tomer <tomer.hershman@gmail.com>
2026-08-02 15:37:32 +0300

    tomer- did it

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -53,10 +53,15 @@ public class Robot {
 		swerve.setHeadingSupplier(() -> poseEstimator.getEstimatedPose().getRotation());
 	}
 
+    public void updateSubsystems(){
+        swerve.update();
+
+    }
+
 	public void periodic() {
 		BusChain.refreshAll();
 
-		swerve.update();
+        updateSubsystems();
 		poseEstimator.updateOdometry(swerve.getAllOdometryData());
 		poseEstimator.log();
 

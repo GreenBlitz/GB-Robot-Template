71581ef0e847661e18268f4139ead801d9e22a8e
Spiderh0ok <rafaelkeselman@gmail.com>
2026-08-04 17:12:52 +0300

    joysticks

--- src/main/java/frc/joysticks/SmartJoystick.java
+++ src/main/java/frc/joysticks/SmartJoystick.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
+import frc.robot.Robot;
 import frc.utils.alerts.Alert;
 import frc.utils.alerts.AlertManager;
 import frc.utils.alerts.PeriodicAlert;
@@ -61,7 +62,9 @@ public class SmartJoystick {
 		this.POV_DOWN = new POVButton(this.joystick, ButtonID.POV_DOWN.getId());
 		this.POV_LEFT = new POVButton(this.joystick, ButtonID.POV_LEFT.getId());
 
-		AlertManager.addAlert(new PeriodicAlert(Alert.AlertType.ERROR, logPath + "/DisconnectedAt", () -> !isConnected()));
+        if (Robot.ROBOT_TYPE.isReal() && alertOnDisconnect) {
+            AlertManager.addAlert(new PeriodicAlert(Alert.AlertType.ERROR, logPath + "/DisconnectedAt", () -> !isConnected(), true));
+        }
 	}
 
 	public String getLogPath() {

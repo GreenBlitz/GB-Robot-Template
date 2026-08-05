8269c1620444984766df5300e7f8dea0a2766c6a
spiderhook <rafaelkeselman@gmail.com>
2026-08-05 19:48:59 +0300

    set voltage without limit -> master (#799)

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -1,11 +1,13 @@
 package frc.robot.subsystems.arm;
 
+import com.ctre.phoenix6.controls.VoltageOut;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.joysticks.Axis;
 import frc.joysticks.SmartJoystick;
 import frc.robot.Robot;
 import frc.robot.hardware.interfaces.*;
+import frc.robot.hardware.phoenix6.request.Phoenix6Request;
 import frc.robot.subsystems.GBSubsystem;
 import frc.utils.battery.BatteryUtil;
 import frc.utils.calibration.sysid.SysIdCalibrator;
@@ -138,6 +140,17 @@ public class Arm extends GBSubsystem {
 
 		// Calibrate feed forward using sys id:
 		sysIdCalibrator.setAllButtonsForCalibration(joystick);
+	}
+
+	public void setVoltageWithoutLimit(double voltage) {
+		if (voltageRequest instanceof Phoenix6Request<Double> phoenix6VoltageRequest) {
+			if (phoenix6VoltageRequest.getControlRequest() instanceof VoltageOut voltageOutRequest) {
+				voltageOutRequest.IgnoreSoftwareLimits = true;
+				voltageRequest.withSetPoint(voltage);
+				motor.applyRequest(voltageRequest);
+				voltageOutRequest.IgnoreSoftwareLimits = false;
+			}
+		}
 	}
 
 }

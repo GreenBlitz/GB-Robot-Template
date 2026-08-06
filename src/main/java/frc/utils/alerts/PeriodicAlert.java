a6f61f6c81857f25abcabd061c390b75abc90623
Spiderh0ok <rafaelkeselman@gmail.com>
2026-08-07 02:30:21 +0300

    returning the change

--- src/main/java/frc/utils/alerts/PeriodicAlert.java
+++ src/main/java/frc/utils/alerts/PeriodicAlert.java
@@ -11,7 +11,6 @@ public class PeriodicAlert extends Alert {
 		this.reportCondition = reportCondition;
 	}
 
-
 	public PeriodicAlert(AlertType type, String name, BooleanSupplier reportCondition) {
 		super(type, name);
 		this.reportCondition = reportCondition;

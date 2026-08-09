f57994ad583a64ea5bb2df30288622eb45c3c1eb
dolev <dolev.yahli@gmail.com>
2026-08-09 23:22:59 +0300

    aaa

--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -371,7 +371,6 @@ public class Swerve extends GBSubsystem {
 		// Use phoenix tuner x to extract the position, velocity, motorVoltage, state signals into wpilog.
 		// Then enter the wpilog into wpilib sysid app and make sure you enter all info in the correct places.
 		// (see wpilib sysid in google)
-
 		joystick.Y.whileTrue(getCommandsBuilder().driveCalibration(true, SysIdRoutine.Direction.kForward));
 		joystick.A.whileTrue(getCommandsBuilder().driveCalibration(true, SysIdRoutine.Direction.kReverse));
 		joystick.X.whileTrue(getCommandsBuilder().driveCalibration(false, SysIdRoutine.Direction.kForward));
